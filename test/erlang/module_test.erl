bd16b5b23b2b4cfdd1968afd151fd17bcc2f8466
José Valim <jose.valim@gmail.com>
2011-01-14 23:22:04 +0100

    Small changes on tests.

--- test/erlang/module_test.erl
+++ test/erlang/module_test.erl
@@ -2,82 +2,48 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% Module functions
-%% TODO Assert 1 + module Foo or A = module Foo does not work
 
 module_body_is_executable_test() -> 
   F = fun() ->
     ?assertError({unbound_var, a}, elixir:eval("module Foo; a; end")),
     elixir:eval("module Bar; 1 + 2; end")
   end,
-  run_and_purge(F, ['Bar']).
+  test_helper:run_and_remove(F, ['Bar']).
+
+module_compiler_precedence_test() -> 
+  ?assertError({badmatch, _}, elixir:eval("1 + module Foo; end")).
 
 module_are_converted_into_erlang_modules_test() ->
   F = fun() ->
     elixir:eval("module Bar; 1 + 2; end"),
     {file, "nofile"} = code:is_loaded('Bar')
   end,
-  run_and_purge(F, ['Bar']).
+  test_helper:run_and_remove(F, ['Bar']).
 
 module_preceeded_by_other_expressions_test() ->
   F = fun() ->
     elixir:eval("1 + 2\nmodule Bar; 1 + 2; end"),
     {file, "nofile"} = code:is_loaded('Bar')
   end,
-  run_and_purge(F, ['Bar']).
+  test_helper:run_and_remove(F, ['Bar']).
 
 module_with_methods_test() ->
   F = fun() ->
     elixir:eval("module Bar; def foo(); 1 + 2; end; end"),
     ?assertEqual(3, 'Bar':foo(self))
   end,
-  run_and_purge(F, ['Bar']).
+  test_helper:run_and_remove(F, ['Bar']).
 
 nested_modules_with_methods_test() ->
   F = fun() ->
     elixir:eval("module Bar; module Baz; def foo(); 1 + 2; end; end; end"),
     ?assertEqual(3, 'Bar::Baz':foo(self))
   end,
-  run_and_purge(F, ['Bar', 'Bar::Baz']).
+  test_helper:run_and_remove(F, ['Bar', 'Bar::Baz']).
 
 nested_module_name_with_methods_test() ->
   F = fun() ->
     elixir:eval("module Bar::Baz; def foo(); 1 + 2; end; end"),
     ?assertEqual(3, 'Bar::Baz':foo(self))
   end,
-  run_and_purge(F, ['Bar::Baz']).
-
-%% Prototype handling
-%% TODO This is going to be removed as soon as we have the object model in place
-
-prototype_with_method_invocation_test() ->
-  F = fun() ->
-    elixir:eval("prototype Integer; def some_value(); 23; end; end"),
-    ?assertEqual({23,[]}, elixir:eval("1.some_value"))
-  end,
-  run_and_purge(F, ['@Integer']).
-
-prototype_with_self_result_test() ->
-  F = fun() ->
-    elixir:eval("prototype Integer; def some_value(x); self + x; end; end"),
-    ?assertEqual({25,[]}, elixir:eval("23.some_value(2)"))
-  end,
-  run_and_purge(F, ['@Integer']).
-
-% Execute a piece of code and purge given modules right after
-run_and_purge(Fun, Modules) ->
-  try
-    Fun()
-  after
-    [remove_module(Module) || Module <- Modules]
-  end.
-
-remove_module(Module) ->
-  code:purge(Module),
-  ets:delete(ex_constants, Module).
-
-% Helper to load files
-read_fixture(Filename) ->
-  Dirname = filename:dirname(?FILE),
-  Fullpath = filename:join([Dirname, "fixtures", Filename]),
-  {ok, Bin} = file:read_file(Fullpath),
-  binary_to_list(Bin).
+  test_helper:run_and_remove(F, ['Bar::Baz']).
--- test/erlang/test_helper.erl
+++ test/erlang/test_helper.erl
@@ -1,5 +1,5 @@
 -module(test_helper).
--export([test/0]).
+-export([test/0, run_and_remove/2, read_fixture/1]).
 
 test() ->
   elixir:boot(),
@@ -9,3 +9,22 @@ test() ->
     module_test,
     variables_test
   ]}).
+
+% Execute a piece of code and purge given modules right after
+run_and_remove(Fun, Modules) ->
+  try
+    Fun()
+  after
+    [remove_module(Module) || Module <- Modules]
+  end.
+
+remove_module(Module) ->
+  code:purge(Module),
+  ets:delete(ex_constants, Module).
+
+% Helper to load files
+read_fixture(Filename) ->
+  Dirname = filename:dirname(?FILE),
+  Fullpath = filename:join([Dirname, "fixtures", Filename]),
+  {ok, Bin} = file:read_file(Fullpath),
+  binary_to_list(Bin).
