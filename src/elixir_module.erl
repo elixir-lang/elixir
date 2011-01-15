f1b330909cfca186624c6e246813a2ba5150cdae
José Valim <jose.valim@gmail.com>
2011-01-15 20:15:49 +0100

    More cleanup.

--- src/elixir_module.erl
+++ src/elixir_module.erl
@@ -1,5 +1,5 @@
 -module(elixir_module).
--export([build/1, scope_for/2, transform/3, compile/2, wrap_method_definition/3, store_wrapped_method/2]).
+-export([scope_for/2, transform/3, compile/3, wrap_method_definition/3, store_wrapped_method/2]).
 -include("elixir.hrl").
 
 % Returns the new module name based on the previous scope.
@@ -7,48 +7,37 @@ scope_for([], Name) -> Name;
 scope_for(Scope, Name) -> ?ELIXIR_ATOM_CONCAT([Scope, '::', Name]).
 
 % Generates module transform. It wraps the module definition into
-% a function that is invoked with self as argument. We need to wrap
-% them into anonymous functions so nested module definitions have
-% the variable self shadowed.
+% a function that will be invoked by compile passing self as argument.
+% We need to wrap them into anonymous functions so nested module
+% definitions have the variable self shadowed.
 transform(Line, Name, Body) ->
-  % This is the hook called at the end of the function
-  % that will actually compiled the module.
-  Hook = ?ELIXIR_WRAP_CALL(Line, elixir_module, compile,
-    [{ var, Line, self }, {integer, Line, Line}]
-  ),
-
-  % The argument for the module invocation will be the
-  % result of elixir_module:build
-  Args = ?ELIXIR_WRAP_CALL(Line, elixir_module, build, [{ atom, Line, Name }]),
-
-  % Wraps the module definition into an anonymous function and
-  % return an abstract form that will invoke it.
-  Clause = { clause, Line, [{var, Line, self}], [], lists:append(Body, [Hook]) },
+  Clause = { clause, Line, [{var, Line, self}], [], Body },
   Fun = { 'fun', Line, { clauses, [Clause] } },
-  { call, Line, Fun, [Args] }.
+  Args = [{integer, Line, Line}, {atom, Line, Name}, Fun],
+  ?ELIXIR_WRAP_CALL(Line, elixir_module, compile, Args).
 
-% Build an object for this module that will be passed as self
-% to the module function invocation.
-build(Name) ->
-  Options = [bag, named_table, private],
-  ets:new(?ELIXIR_ATOM_CONCAT([tex_, Name]), Options),
-  #elixir_object{name=Name, parent='Module'}.
+% Receive the module function to be invoked, invoke it passing
+% self and then compile the added methods into an Erlang module
+% and loaded it in the VM.
+compile(Line, Name, Fun) ->
+  Table = ?ELIXIR_ATOM_CONCAT([tex_, Name]),
+  ets:new(Table, [bag, named_table, private]),
 
-% Compile the given module by executing its body and compiling
-% the result into binary and loading it into Erlang VM.
-compile(Object, Line) ->
-  Name = Object#elixir_object.name,
-  TempTable = ?ELIXIR_ATOM_CONCAT([tex_, Name]),
-  load_module(build_module_form(Line, Name, TempTable)),
-  ets:delete(TempTable).
+  try
+    Object = #elixir_object{name=Name, parent='Module'},
+    Result = Fun(Object),
+    load_module(build_module_form(Line, Name, Table)),
+    Result
+  after
+    ets:delete(Table)
+  end.
 
-% Store a method in the compiled table. Returns an Erlang Abstract Form
-% that, when executed, calls elixir_module:add_to_module which will
-% retrieve the function from CompiledTable and move it to the AddedTable.
+% Wraps the method into a call that will call store_wrapped_method
+% once the method definition is read. The method is compiled into a
+% meta tree to ensure we will receive the full method.
 %
-% All functions in the AddedTable are then copied to the module. We need
-% to do it in two steps to ensure functions inside if branches won't
-% propagate, for example:
+% We need to wrap methods instead of eagerly defining them to ensure
+% functions inside if branches won't propagate, for example:
 %
 %   module Foo
 %     if false
