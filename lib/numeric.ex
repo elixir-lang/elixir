8dc56448105e54f9bd81eaaa9e8ae2fd136ec1ac
José Valim <jose.valim@gmail.com>
2011-01-17 08:46:21 +0100

    Rename to elixir_numeric_methods.

--- lib/numeric.ex
+++ lib/numeric.ex
@@ -1,25 +1,25 @@
 module Numeric
   def +(other)
-    erl.elixir_methods.add(self, other)
+    erl.elixir_numeric_methods.add(self, other)
   end
 
   def -(other)
-    erl.elixir_methods.subtract(self, other)
+    erl.elixir_numeric_methods.subtract(self, other)
   end
 
   def *(other)
-    erl.elixir_methods.multiply(self, other)
+    erl.elixir_numeric_methods.multiply(self, other)
   end
 
   def /(other)
-    erl.elixir_methods.divide(self, other)
+    erl.elixir_numeric_methods.divide(self, other)
   end
 
   def @+
-    erl.elixir_methods.unary_plus(self)
+    erl.elixir_numeric_methods.unary_plus(self)
   end
 
   def @-
-    erl.elixir_methods.unary_minus(self)
+    erl.elixir_numeric_methods.unary_minus(self)
   end
 end
--- src/elixir_numeric_methods.erl
+++ src/elixir_numeric_methods.erl
@@ -1,5 +1,5 @@
 % Implement methods that are VM operations and are not exposed as erlang methods.
--module(elixir_methods).
+-module(elixir_numeric_methods).
 -export([add/2, subtract/2, multiply/2, divide/2, unary_plus/1, unary_minus/1]).
 -include("elixir.hrl").
 
