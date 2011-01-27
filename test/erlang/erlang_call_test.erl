803e731b54bbf6df802fb0a9b50cd8a634481b92
José Valim <jose.valim@gmail.com>
2011-01-27 08:54:00 +0100

    Added more erlang call assertions.

--- test/erlang/erlang_call_test.erl
+++ test/erlang/erlang_call_test.erl
@@ -3,4 +3,6 @@
 
 erlang_call_test() ->
   {1, []} = elixir:eval("Erlang.abs(-1)"),
-  {1, []} = elixir:eval("Erlang.erlang.abs(-1)").
+  {1, []} = elixir:eval("Erlang.erlang.abs(-1)"),
+  {_, []} = elixir:eval("Erlang.dict.new"),
+  {_, []} = elixir:eval("Erlang.dict.new()").
