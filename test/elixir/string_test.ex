25a18fb8891c5230630baa0786668a97494ba153
BigCat <miaout17@gmail.com>
2011-02-20 03:17:40 +0800

    adding string tests

---
+++ test/elixir/string_test.ex
@@ -0,0 +1,29 @@
+object StringTest
+  proto ExUnit::Case
+
+  def concatenate_test
+    "elixir" = "eli" + "xir"
+  end
+
+  def length_test
+    0 = "".length
+    6 = "elixir".length
+    4 = "josé".length
+    3 = "講中文".length
+    5 = "こんにちは".length
+  end
+
+  def to_list_test
+    [115, 116, 114, 105, 110, 103] = "string".to_list
+    [115, 116, 114, 105, 110, 103] = "string".to_char_list
+  end
+
+  def to_bin_test
+    <<115, 116, 114, 105, 110, 103>> = "string".to_bin
+  end
+
+  def to_s_test
+    "Hello" = "Hello".to_s
+  end
+
+end
--- test/elixir/test_helper.ex
+++ test/elixir/test_helper.ex
@@ -2,5 +2,6 @@ Code.require "ex_unit"
 Code.require "list_test"
 Code.require "tuple_test"
 Code.require "ordered_dict_test"
+Code.require "string_test"
 
-ExUnit.run([ListTest, TupleTest, OrderedDictTest])
+ExUnit.run([ListTest, TupleTest, OrderedDictTest, StringTest])
