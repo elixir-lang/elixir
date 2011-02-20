3d0f7132751decb451fc6d493b40a9b8e9b46ab5
BigCat <miaout17@gmail.com>
2011-02-20 15:08:21 +0800

    add some regexp match tests

--- test/elixir/regexp_test.ex
+++ test/elixir/regexp_test.ex
@@ -5,4 +5,17 @@ object RegexpTest
     true = ~r(foo)i == Regexp.new("foo", "i")
     true = ~r(foo)iu == Regexp.new("foo", "iu")
   end
+
+  def match_test
+    true = ~r(foo).match?("foo")
+    false = ~r(foo).match?("bar")
+
+    true = ~r(foo).match?("afooa")
+
+    false = ~r(^foo).match?("afooa")
+    true = ~r(^foo).match?("fooa")
+
+    false = ~r(foo$).match?("afooa")
+    true = ~r(foo$).match?("afoo")
+  end
 end
