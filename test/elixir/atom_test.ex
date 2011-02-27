553edb9810a574e53ddaa552efd6089f78aa0030
José Valim <jose.valim@gmail.com>
2011-02-27 09:57:46 +0100

    Atom#to_s tests.

--- test/elixir/atom_test.ex
+++ test/elixir/atom_test.ex
@@ -9,4 +9,13 @@ object AtomTest
     "'\"f!o\"" = '"f!o".inspect
     "'\"foo bar\"" = '"foo bar".inspect
   end
+
+  def to_s_test
+    "foo" = 'foo.to_s
+    "@foo" = '@foo.to_s
+    "foo?" = 'foo?.to_s
+    "foo!" = 'foo!.to_s
+    "f!o" = '"f!o".to_s
+    "foo bar" = '"foo bar".to_s
+  end
 end
