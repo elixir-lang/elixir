1b6113bd6973b3930881965e15248dcc34c4622e
BigCat <miaout17@gmail.com>
2011-03-01 01:49:16 +0800

    add IO.gets methods

--- lib/io.ex
+++ lib/io.ex
@@ -6,4 +6,12 @@ module IO
   def puts(message)
     Erlang.io.format(<<message.to_bin|binary, $\n>>)
   end
+
+  def gets
+    gets("")
+  end
+
+  def gets(prompt)
+    String.new Erlang.io.get_line(prompt.to_char_list)
+  end
 end
