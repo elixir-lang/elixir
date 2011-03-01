29ebda2592688e1acfa71ccc52d83a37a286e809
BigCat <miaout17@gmail.com>
2011-03-01 13:56:58 +0800

    `filename:absname` accepts binary string parameter since R14B01 R14B or prior version fails with binary string. In order to keep backward compatibility, use to_list instead.

--- lib/file.ex
+++ lib/file.ex
@@ -1,7 +1,7 @@
 object File
   module Mixin
     def expand_path(string)
-      fullpath = Erlang.filename.absname(string.to_bin)
+      fullpath = Erlang.filename.absname(string.to_list)
       ~r{((/\.)|(/[^/]*[^\\]/\.\.))(/|\z)}.replace_all(fullpath, "\\4")
     end
   end
