e4f7d27b6675b7396bddcea38b7c2b80e4652ec3
BigCat <miaout17@gmail.com>
2011-03-05 18:48:35 +0800

    multiline input in iex

--- lib/iex.ex
+++ lib/iex.ex
@@ -1,21 +1,31 @@
 % A simple REPL (Read-Eval-Print Loop) for Elixir
 object IEX
   def constructor
-    {'prompt: "IEX> ", 'binding: []}
+    {'binding: [], 'codecache: ""}
   end
 
   def loop
-    code = IO.gets @prompt
+    prompt = case @codecache
+    match ""
+      "IEX> "
+    else
+      "...> "
+    end
+
+    code = @codecache + IO.gets(prompt)
     try
       {result, new_binding} = Erlang.elixir.eval(code.to_list, @binding)
       IO.puts result.inspect
-      self.set_ivar('binding, new_binding).loop
+      % I think we need some syntax sugar of set_ivar :)
+      self.set_ivar('binding, new_binding).set_ivar('codecache, "").loop
+    catch 'error: {'badsyntax, _, _, _, '"$end"}
+      self.set_ivar('codecache, code).loop
     catch 'error: {'badsyntax, _line, _filename, error, token}
       IO.puts("Syntax Error: #{String.new error} #{token.inspect}")
-      loop
+      self.set_ivar('codecache, "").loop
     catch 'error: e
       IO.puts "Error: #{e.inspect}"
-      loop
+      self.set_ivar('codecache, "").loop
     end
   end
 end
