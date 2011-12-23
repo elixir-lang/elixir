934e7a0e18d8c604179ade4dc66b3e346a565c44
José Valim <jose.valim@gmail.com>
2011-12-23 16:52:39 +0100

    Improve coverage for functions.

--- test/erlang/function_test.erl
+++ test/erlang/function_test.erl
@@ -21,54 +21,11 @@ function_with_kv_args_test() ->
   {Fun, _} = elixir:eval("fn(a, [other: b, another: c], do: a + b + c)"),
   6 = Fun(1,[{another,3},{other,2}]).
 
-% function_assignment_multiline_test() ->
-%   {_, [{a, Res2}]} = elixir:eval("a = do \nx = 1\nx + 2\n end"),
-%   3 = Res2(),
-%   {_, [{a, Res4}]} = elixir:eval("a = do (y) \nx = 1\nx + y\n end"),
-%   3 = Res4(2).
-% 
-% function_assignment_with_assignment_test() ->
-%   {_, [{a, Res1}]} = elixir:eval("a = -> b = 3"),
-%   3 = Res1().
-% 
-% function_assignment_with_empty_args_test() ->
-%   {_, [{a, Res1}]} = elixir:eval("a = -> () 1 + 2"),
-%   3 = Res1(),
-%   {_, [{a, Res3}]} = elixir:eval("a = -> (\n) (1 + 2)"),
-%   3 = Res3().
-% 
-% function_assignment_with_args_test() ->
-%   {_, [{a, Res1}]} = elixir:eval("a = -> (x,y) x + y"),
-%   3 = Res1(1,2),
-%   {_, [{a, Res2}]} = elixir:eval("a = do (x,y); x + y; end"),
-%   3 = Res2(1,2),
-%   {_, [{a, Res3}]} = elixir:eval("a = -> (\nx,\ny\n) x + y"),
-%   3 = Res3(1,2).
-% 
-% function_nested_assignment_test() ->
-%   {_, [{a, Res1}]} = elixir:eval("a = -> -> 1 + 2"),
-%   ResF1 = Res1(),
-%   3 = ResF1().
-% 
-% function_assignment_new_line_test() ->
-%   {_, [{a, Res2}]} = elixir:eval("a = do\n1 + 2\nend"),
-%   3 = Res2().
-% 
-% function_nested_assignment_new_line_test() ->
-%   {_, [{a, Res1}]} = elixir:eval("a = -> do\n1 + 2\nend "),
-%   ResF1 = Res1(),
-%   3 = ResF1(),
-%   {_, [{a, Res2}]} = elixir:eval("a = do\n-> 1 + 2\nend "),
-%   ResF2 = Res2(),
-%   3 = ResF2().
-% 
-% function_as_clojure_test() ->
-%   {_, [{a, Res1}|_]} = elixir:eval("b = 1; a = -> b + 2"),
-%   3 = Res1(),
-%   {_, [{a, Res2}|_]} = elixir:eval("b = 1; a = do\nb + 2\nend"),
-%   3 = Res2().
-% 
-% %% Function calls
+function_as_clojure_test() ->
+  {_, [{a, Res1}|_]} = elixir:eval("b = 1; a = fn { b + 2 }"),
+  3 = Res1().
+
+%% Function calls
 
 function_call_test() ->
   {3, _} = elixir:eval("x = fn(a,b, do: a + b)\nx.(1,2)").
@@ -79,23 +36,14 @@ function_call_without_arg_test() ->
 function_call_do_end_test() ->
   {[1,[{do,2},{else,3}]], _} = elixir:eval("x = fn(a, b){ [a, b] }\nx.(1) do\n2\nelse: 3\nend").
 
-% function_calls_with_arg_test() ->
-%   {3, _} = elixir:eval("b = 1; a = -> (a) a + b; a.(2)").
-% 
-% function_call_with_assignment_test() ->
-%   {3, [{a,_},{c, 3}]} = elixir:eval("a = -> (x) x + 2; c = a.(1)").
-% 
-% function_call_inside_another_function_test() ->
-%   {1, _} = elixir:eval("a = -> (x) x + 2; b = -> a.(1) - 2; b.()").
-% 
-% function_calls_with_multiple_args_test() ->
-%   {5, _} = elixir:eval("a = -> (a, b) a + b; a.(3, 2)").
-% 
-% function_calls_with_multiple_expressions_test() ->
-%   {26, _} = elixir:eval("a = -> (a, b) a + b; a.((3 + 4 - 1), (2 * 10))").
-% 
-% function_calls_with_multiple_args_with_line_breaks_test() ->
-%   {5, _} = elixir:eval("a = -> (a, b) a + b; a.(\n3,\n2\n)").
-% 
-% function_calls_with_parenthesis_test() ->
-%   {3, [{a,_},{b,1}]} = elixir:eval("(a = -> (x) x + 2).(b = 1)").
+function_call_with_assignment_test() ->
+  {3, [{a,_},{c, 3}]} = elixir:eval("a = fn(x){ x + 2 }; c = a.(1)").
+
+function_calls_with_multiple_expressions_test() ->
+  {26, _} = elixir:eval("a = fn(a, b){ a + b }; a.((3 + 4 - 1), (2 * 10))").
+
+function_calls_with_multiple_args_with_line_breaks_test() ->
+  {5, _} = elixir:eval("a = fn(a, b){ a + b }; a.(\n3,\n2\n)").
+
+function_calls_with_parenthesis_test() ->
+  {3, [{a,_},{b,1}]} = elixir:eval("a = fn(x){ x + 2 }.(b = 1)").
