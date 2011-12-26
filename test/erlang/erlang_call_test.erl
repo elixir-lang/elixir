a0f536434cce94e32eb893bf4b4bf994dc24054f
José Valim <jose.valim@gmail.com>
2011-12-26 15:20:47 +0100

    Add more dynamic calls tests.

--- test/erlang/erlang_call_test.erl
+++ test/erlang/erlang_call_test.erl
@@ -5,7 +5,13 @@
 match() -> abs.
 
 erlang_atoms_test() ->
-  {abs, []}   = elixir:eval("Erlang.abs").
+  {abs, []} = elixir:eval("Erlang.abs").
+
+erlang_tuple_test() ->
+  {[erlang,1,2], []} = elixir:eval("{ :erlang, 1, 2 }.tuple_to_list").
+
+erlang_local_test() ->
+  {1, []} = elixir:eval(":abs.(-1)").
 
 erlang_call_test() ->
   {1, []}   = elixir:eval("Erlang.erlang.abs(-1)"),
