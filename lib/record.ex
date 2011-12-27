ad32f30c167b26633d927d1db177cd6e7a435b8e
José Valim <jose.valim@gmail.com>
2011-12-27 08:55:00 +0100

    Add some docs to defrecord.

--- lib/record.ex
+++ lib/record.ex
@@ -1,19 +1,58 @@
 module Record
 
+# Define a record given by name and values. Example:
+#
+#     defrecord FileInfo, atime: nil, mtime: nil
+#
+# This macro will then define a module named FileInfo
+# which will contain getters and setters for each attribute
+# and initializers methods. Therefore, one can do:
+#
+#     file_info = FileInfo.new(a: now())
+#     file_info.a         #=> Returns the value of now()
+#     file_Info.a(now())  #=> Updates the value of now()
+#
 defmacro define(name, values) do
   functions = getters_and_setters(values, 1, [])
   functions = [initializers(name, values)|functions]
 
   quote do
+    # Use `module NAME, do: CONTENTS` syntax which is
+    # the same as `module NAME do CONTENTS end`. We need
+    # to wrap this in a block so this module declaration
+    # do not affect the outer module one.
     module unquote(name), do: unquote(functions)
   end
 end
 
 private
 
+# Define initializers methods. For a declaration like:
+#
+#     defrecord FileInfo, atime: nil, mtime: nil
+#
+# It will define three methods:
+#
+#     def new() do
+#       new([])
+#     end
+#
+#     def new([]) do
+#       { FileInfo, nil, nil }
+#     end
+#
+#     def new(opts) do
+#       { FileInfo, Orddict.fetch(opts, :atime, nil), Orddict.fetch(opts, :mtime, nil) }
+#     end
+#
 def initializers(name, values) do
+  # Get default values from the dictionary.
   defaults  = Orddict.values(values)
 
+  # For each value, define a piece of code that will receive
+  # an ordered dict of options (opts) and it will try to fetch
+  # the given key from the ordered dict, falling back to the
+  # default value if one does not exist.
   selective = List.map values, fn({k,v}) {
     quote { Orddict.fetch(opts, unquote(k), unquote(v)) }
   }
@@ -25,6 +64,35 @@ def initializers(name, values) do
   end
 end
 
+# Implement getters and setters for each attribute.
+# For a declaration like:
+#
+#     defrecord FileInfo, atime: nil, mtime: nil
+#
+# It will define four methods:
+#
+#     def :atime.(record) do
+#       element(2, record)
+#     end
+#
+#     def :atime.(record, value) do
+#       setelement(2, value, record)
+#     end
+#
+#     def :mtime.(record) do
+#       element(3, record)
+#     end
+#
+#     def :mtime.(record, value) do
+#       setelement(3, value, record)
+#     end
+#
+# `element` and `setelement` will simply get and set values
+# from the record tuple. Notice that `:atime.(record)` is just
+# a dynamic way to say `atime(record)`. We need to use this
+# syntax as `unquote(key)(record)` wouldn't be valid (as Elixir
+# allows you to parenthesis just on specific cases as `foo()`
+# and `foo.bar()`)
 def getters_and_setters([{ key, _ }|t], i, acc) do
   i = i + 1
 
