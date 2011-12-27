cb8a39651b044fee8f5a0c1bbd5a5b65a59f7e1a
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2011-12-27 14:50:34 +0100

    new version of iex merged with string interpolation fix

--- lib/elixir/iex.ex
+++ lib/elixir/iex.ex
@@ -1,14 +1,28 @@
 module Elixir::IEx
 
 def start do
-  Erlang.io.format("Interactive Elixir (#{Elixir::Code.version})\nRunning on")
-  function = fn { loop([], "") }
+  Erlang.io.format("Interactive Elixir (#{Elixir::Code.version})\nRunning on ")
+  function = fn { loop([], '') }
   Erlang.user_drv.start([:"tty_sl -c -e", {:erlang, :spawn, [function]}])
 end
 
 def loop(binding, code_cache) do
-  code = Erlang.io.get_line("iex> ")
-  {result, new_binding} = Erlang.elixir.eval(code, binding)
-  Erlang.io.format("~p~n", [result])
-  loop(new_binding, code_cache)
+  prompt = case code_cache do
+  match: []
+    "iex> "
+  match: _
+    "...> "
+  end
+
+  code = code_cache ++ Erlang.io.get_line(prompt)
+
+  {binding_to_return, code_cache_to_return} = try do
+    {result, new_binding} = Erlang.elixir.eval(code, binding)
+    Erlang.io.format("~p~n", [result])
+    {new_binding, ''}
+  catch: { :error, {:badsyntax, {_, _, _, []}}, _};
+    {binding, code}
+  end
+
+  loop(binding_to_return, code_cache_to_return)
 end
