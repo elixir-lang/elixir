e27d86af944ddeec05ad3a880cec84520b7c5f68
José Valim <jose.valim@gmail.com>
2011-12-29 13:42:38 +0100

    Simplify IO since Erlang IO can take care of both bins and lists.

--- lib/io.ex
+++ lib/io.ex
@@ -1,15 +1,7 @@
 module IO
 
-def print(device, list) when is_list(list) do
-  print(device, list_to_binary(list))
-end
-
 def print(device // :standard_io, item) do
   Erlang.io.format device, item, []
-end
-
-def puts(device, list) when is_list(list) do
-  puts(device, list_to_binary(list))
 end
 
 def puts(device // :standard_io, item) do
