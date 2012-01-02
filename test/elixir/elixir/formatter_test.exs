61ae0bd4ff4ee77d064c24bbe6d1ecd9a0300f47
José Valim <jose.valim@gmail.com>
2012-01-02 14:01:40 +0100

    Break tests into smaller ones.

--- test/elixir/elixir/formatter_test.exs
+++ test/elixir/elixir/formatter_test.exs
@@ -7,16 +7,25 @@ defmodule Elixir::FormatterTest do
 
   import Elixir::Formatter, only: [format_stacktrace: 1, format_catch: 2]
 
-  def test_format_stacktrace do
+  def test_format_stacktrace_earlier_erlang_releases do
     "::Foo.bar()" = format_stacktrace({::Foo, :bar, []})
     "::Foo.bar(1, 2, 3)" = format_stacktrace({::Foo, :bar, [1, 2, 3]})
     "::Foo.bar/1" = format_stacktrace({::Foo, :bar, 1})
+  end
+
+  def test_format_stacktrace_earlier_erlang_r15_release_with_no_file_or_line do
     "::Foo.bar(1, 2, 3)" = format_stacktrace({::Foo, :bar, [1, 2, 3], []})
     "::Foo.bar()" = format_stacktrace({::Foo, :bar, [], []})
     "::Foo.bar/1" = format_stacktrace({::Foo, :bar, 1, []})
+  end
+
+  def test_format_stacktrace_earlier_erlang_r15_release_with_file_and_line do
     "file.ex:10: ::Foo.bar()" = format_stacktrace({::Foo, :bar, [], [file: 'file.ex', line: 10]})
     "file.ex:10: ::Foo.bar(1, 2, 3)" = format_stacktrace({::Foo, :bar, [1, 2, 3], [file: 'file.ex', line: 10]})
     "file.ex:10: ::Foo.bar/1" = format_stacktrace({::Foo, :bar, 1, [file: 'file.ex', line: 10]})
+  end
+
+  def test_format_stacktrace_earlier_erlang_r15_release_with_file_no_line do
     "file.ex: ::Foo.bar()" = format_stacktrace({::Foo, :bar, [], [file: 'file.ex']})
     "file.ex: ::Foo.bar(1, 2, 3)" = format_stacktrace({::Foo, :bar, [1, 2, 3], [file: 'file.ex']})
     "file.ex: ::Foo.bar/1" = format_stacktrace({::Foo, :bar, 1, [file: 'file.ex']})
