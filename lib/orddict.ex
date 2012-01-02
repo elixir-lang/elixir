44ade7072ddc135c080e8a6fe8d7a49be99a75d3
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2012-01-02 11:40:51 +0100

    added missing orddict test, small refactorization

--- lib/orddict.ex
+++ lib/orddict.ex
@@ -1,32 +1,41 @@
 module Orddict do
+
+  # Provides conversion form list to the dictionary
   def from_list(pairs) do
-    List.foldl pairs, [], fn({k,v}, dict){ store(dict, k, v) }
+    List.foldl pairs, [], fn({k, v}, dict){ store(dict, k, v) }
   end
 
-  def fetch([{k,_}|_], key, default) when key < k, do: default
-  def fetch([{k,_}|d], key, default) when key > k, do: fetch(d, key, default)
-  def fetch([{_k,value}|_], _key, _default),    do: value
+  # Fetches value from the dictionary for specific key.
+  # If key not exist return default value
+  def fetch([{k, _}|_], key, default) when key < k, do: default
+  def fetch([{k, _}|d], key, default) when key > k, do: fetch(d, key, default)
+  def fetch([{_k, value}|_], _key, _default),    do: value
   def fetch([], _, default),                    do: default
 
-  def keys([{k,_}|t]), do: [k|keys(t)]
+  # Returns all keys of dictionary
+  def keys([{k, _}|t]), do: [k|keys(t)]
   def keys([]),        do: []
 
-  def values([{_,v}|t]), do: [v|values(t)]
+  # Returns all values of dictionary
+  def values([{_, v}|t]), do: [v|values(t)]
   def values([]),        do: []
 
-  def delete([{k,_}=e|dict], key) when key < k, do: [e|dict]
-  def delete([{k,_}=e|dict], key) when key > k, do: [e|delete(dict, key)]
-  def delete([{_k,_v}|dict], _key), do: dict
+  # Deletes key, value entry from dictionary for specific key
+  def delete([{k, _} = e|dict], key) when key < k, do: [e|dict]
+  def delete([{k, _} = e|dict], key) when key > k, do: [e|delete(dict, key)]
+  def delete([{_k, _v}|dict], _key), do: dict
   def delete([], _), do: []
 
-  def store([{k,_}=e|dict], key, new) when key < k, do: [{key,new},e|dict]
-  def store([{k,_}=e|dict], key, new) when key > k, do: [e|store(dict, key, new)]
-  def store([{_,_}|dict], key, new), do: [{key,new}|dict]
-  def store([], key, new), do: [{key,new}]
+  # Stores key, value entry in dictionary
+  def store([{k, _} = e|dict], key, value) when key < k, do: [{key, value},e|dict]
+  def store([{k, _} = e|dict], key, value) when key > k, do: [e|store(dict, key, value)]
+  def store([{_, _}|dict], key, value), do: [{key, value}|dict]
+  def store([], key, value), do: [{key, value}]
 
-  def merge([{k1,_}=e1|d1], [{k2,_}=e2|d2]) when k1 < k2, do: [e1|merge(d1, [e2|d2])];
-  def merge([{k1,_}=e1|d1], [{k2,_}=e2|d2]) when k1 > k2, do: [e2|merge([e1|d1], d2)];
-  def merge([{k1,_v1}|d1], [{_k2,v2}|d2]), do: [{k1,v2}|merge(d1, d2)];
+  # Merges two dictionaries in one
+  def merge([{k1, _} = e1|d1], [{k2, _} = e2|d2]) when k1 < k2, do: [e1|merge(d1, [e2|d2])];
+  def merge([{k1, _} = e1|d1], [{k2, _} = e2|d2]) when k1 > k2, do: [e2|merge([e1|d1], d2)];
+  def merge([{k1, _v1}|d1], [{_k2, v2}|d2]), do: [{k1, v2}|merge(d1, d2)];
   def merge([], d2), do: d2
   def merge(d1, []), do: d1
 end
---
+++ test/elixir/orddict_test.exs
@@ -0,0 +1,45 @@
+module OrddictTest do
+  use ExUnit::Case
+
+  def test_from_list do
+    [first_key: 1, second_key: 2] = Orddict.from_list([{:first_key, 1},{:second_key, 2}])
+  end
+
+  def test_fetch do
+    1         = Orddict.fetch(create_dict, :first_key, nil)
+    2         = Orddict.fetch(create_dict, :second_key, nil)
+    nil       = Orddict.fetch(create_dict, :other_key, nil)
+    "default" = Orddict.fetch(create_empty_dict, :first_key, "default")
+  end
+
+  def test_keys do
+    [:first_key, :second_key] = Orddict.keys(create_dict)
+    []                        = Orddict.keys(create_empty_dict)
+  end
+
+  def test_values do
+    [1, 2] = Orddict.values(create_dict)
+    []     = Orddict.values(create_empty_dict)
+  end
+
+  def test_delete do
+    [first_key: 1]                = Orddict.delete(create_dict, :second_key)
+    [first_key: 1, second_key: 2] = Orddict.delete(create_dict, :other_key)
+    [] = Orddict.delete(create_empty_dict, :other_key)
+  end
+
+  def test_store do
+    [first_key: 1]                = Orddict.store(create_empty_dict, :first_key, 1)
+    [first_key: 1, second_key: 2] = Orddict.store(create_dict, :first_key, 1)
+  end
+
+  def test_merge do
+    [first_key: 1, second_key: 2] = Orddict.merge(create_empty_dict, create_dict)
+    [first_key: 1, second_key: 2] = Orddict.merge(create_dict, create_empty_dict)
+    [first_key: 1, second_key: 2] = Orddict.merge(create_dict, create_dict)
+    [] = Orddict.merge(create_empty_dict, create_empty_dict)
+  end
+
+  defp create_empty_dict, do: create_dict([])
+  defp create_dict(list // [first_key: 1, second_key: 2]), do: Orddict.from_list(list)
+end
