1a421e5a23fcf29aaba0f4bc0041d0aee905df38
José Valim <jose.valim@gmail.com>
2012-01-07 10:40:53 +0100

    Add Module.compiled?

--- lib/module.ex
+++ lib/module.ex
@@ -1,8 +1,36 @@
 defmodule Module do
+  # Evalutes the quotes contents in the given module context.
+  #
+  # ## Examples
+  #
+  #     defmodule Foo do
+  #       contents = quote { def sum(a, b), do: a + b }
+  #       Module.eval_quoted __MODULE__, contents, [], __FILE__, __LINE__
+  #     end
+  #
+  #     Foo.sum(1, 2) #=> 3
+  #
   def eval_quoted(module, quoted, binding, filename, line) do
-    assert_table_exists!(:eval_quoted, module)
+    assert_already_compiled!(:eval_quoted, module)
     Erlang.elixir_module.eval_quoted(module, quoted, binding, to_list(filename), line)
   end
+
+  # Checks if the module is compiled or not.
+  #
+  # ## Examples
+  #
+  #     defmodule Foo do
+  #       Module.compiled?(__MODULE__) #=> false
+  #     end
+  #
+  #     Module.compiled?(Foo) #=> true
+  #
+  def compiled?(module) do
+    table = table_for(module)
+    table == Erlang.ets.info(table, :name)
+  end
+
+  ## Helpers
 
   defp to_list(list) when is_list(list),  do: list
   defp to_list(bin)  when is_binary(bin), do: binary_to_list(bin)
@@ -11,9 +39,8 @@ defmodule Module do
     list_to_atom Erlang.lists.concat([:a, module])
   end
 
-  defp assert_table_exists!(fun, module) do
-    table = table_for(module)
-    table == Erlang.ets.info(table, :name) orelse
+  defp assert_already_compiled!(fun, module) do
+    compiled?(module) orelse
       error { :module_already_compiled,
         "could not call #{fun} on module #{module} because it was already compiled" }
   end
