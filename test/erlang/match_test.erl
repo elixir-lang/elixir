d48b9106c8ecf6121302e162f74bfb04b92f77d7
José Valim <jose.valim@gmail.com>
2012-01-08 13:38:45 +0100

    Add more tests.

--- test/erlang/match_test.erl
+++ test/erlang/match_test.erl
@@ -17,7 +17,9 @@ not_single_assignment_test() ->
   {1, [{a, 1}]} = elixir:eval("{a,a} = :{}.(1, 1)\na"),
   {2, [{a, 2}]} = elixir:eval("a = 1\n{^a,a} = {1,2}\na"),
   ?assertError({badmatch, _}, elixir:eval("{a,a} = {1,2}")),
+  ?assertError({badmatch, _}, elixir:eval("{1 = a,a} = {1,2}")),
   ?assertError({badmatch, _}, elixir:eval("a = 0;{a,a} = {1,2}")),
+  ?assertError({badmatch, _}, elixir:eval("a = 0;{1 = a,a} = {1,2}")),
   ?assertError({badmatch, _}, elixir:eval("a = 1\n^a = 2")).
 
 duplicated_assignment_on_module_with_tuple_test() ->
