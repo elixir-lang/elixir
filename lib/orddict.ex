c51ef464b934fd5c7539734bc751ce46859fcb80
José Valim <jose.valim@gmail.com>
2012-01-16 22:26:36 +0100

    Improve docs for Orddict.

--- lib/orddict.ex
+++ lib/orddict.ex
@@ -7,22 +7,47 @@ defmodule Orddict do
     Enum.foldl pairs, [], fn({k, v}, dict){ store(dict, k, v) }
   end
 
-  # Fetches value from the dictionary for specific key.
-  # If key not exist return default value
+  # Gets value from the dictionary for specific key.
+  # If key not exist return default value (nil if no default value)
+  # exists.
+  #
+  # ## Examples
+  #
+  #     Orddict.get [a: 1], :a      #=> 1
+  #     Orddict.get [a: 1], :b      #=> nil
+  #     Orddict.get [a: 1], :b, 3   #=> 3
+  #
   def get([{k, _}|_], key, default) when key < k, do: default
   def get([{k, _}|d], key, default) when key > k, do: get(d, key, default)
   def get([{_k, value}|_], _key, _default),       do: value
   def get([], _, default // nil),                 do: default
 
-  # Returns all keys of dictionary
-  def keys([{k, _}|t]), do: [k|keys(t)]
-  def keys([]),        do: []
+  # Returns all keys of dictionary.
+  #
+  # ## Examples
+  #
+  #     Orddict.keys [a: 1, b: 2] #=> [:a,:b]
+  def keys(dict) do
+    lc { key, _ } in dict, do: key
+  end
 
-  # Returns all values of dictionary
-  def values([{_, v}|t]), do: [v|values(t)]
-  def values([]),        do: []
+  # Returns all values of dictionary.
+  #
+  # ## Examples
+  #
+  #     Orddict.values [a: 1, b: 2] #=> [1,2]
+  def values(dict) do
+    lc { _, value } in dict, do: value
+  end
 
-  # Deletes key, value entry from dictionary for specific key
+  # Deletes key, value entry from dictionary for specific key.
+  # If the key does not exist, returns the dictionary unchanged.
+  #
+  # ## Examples
+  #
+  #     Orddict.delete [a: 1, b: 2], :a   #=> [b: 2]
+  #     Orddict.delete [b: 2], :a         #=> [b: 2]
+  #
   def delete([{k, _} = e|dict], key) when key < k, do: [e|dict]
   def delete([{k, _} = e|dict], key) when key > k, do: [e|delete(dict, key)]
   def delete([{_k, _v}|dict], _key), do: dict
