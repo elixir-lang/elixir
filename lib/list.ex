b3522bd32f6c3225b4c1ea72bfbc11645233c772
José Valim <jose.valim@gmail.com>
2012-01-18 14:56:56 +0100

    Add tests and docs for List.

--- lib/list.ex
+++ lib/list.ex
@@ -5,10 +5,27 @@ defmodule List do
   # Bifs: member/2, reverse/2
   # Bifs: keymember/3, keysearch/3, keyfind/3
 
+  # Appends the list of lists all the given lists together.
+  #
+  # ## Examples
+  #
+  #     List.append [[1,[2],3], [4], [5,6]]
+  #     #=> [1,[2],3,4,5,6]
+  #
   def append(list) do
     Erlang.lists.append(list)
   end
 
+  # Appends the list on the right to the list on the left.
+  # If the list on the left contains only one element, the
+  # we simply add it as a head as an optimization. This
+  # function does the same as the `++` operator.
+  #
+  # ## Examples
+  #
+  #     List.append [1,2,3], [4,5,6]
+  #     #=> [1,2,3,4,5,6]
+  #
   def append([h], right) when is_list(right) do
     [h|right]
   end
@@ -17,46 +34,85 @@ defmodule List do
     left ++ right
   end
 
+  # Flattens the given `list` of lists. An optional
+  # tail can be given that will be added at the end of
+  # the flattened list.
+  #
+  # ## Examples
+  #
+  #     List.flatten [1,[[2],3]]
+  #     # => [1,2,3]
+  #
+  #     List.flatten [1,[[2],3]], [4,5]
+  #     # => [1,2,3,4,5]
+  #
   def flatten(list, tail // []) when is_list(list) andalso is_list(tail) do
-    _flatten(list, tail)
+    do_flatten(list, tail)
   end
 
+  # Reverses the given list. This function simply delegates
+  # to `lists:reverse` which is implemented in C for performance.
+  #
+  # ## Examples
+  #
+  #     List.reverse [1,2,3]
+  #     #=> [3,2,1]
+  #
   def reverse(list) when is_list(list) do
     Erlang.lists.reverse(list)
   end
 
-  def member?(list, thing) when is_list(list) do
-    Erlang.lists.member(thing, list)
+  # Checks if the given `term` is included in the list.
+  # This function simply delegates to `lists:member`
+  # which is implemented in C for performance.
+  #
+  # ## Examples
+  #
+  #     List.member? [1,2,3], 1
+  #     #=> true
+  #
+  #     List.member? [1,2,3], 0
+  #     #=> false
+  #
+  def member?(list, term) when is_list(list) do
+    Erlang.lists.member(term, list)
   end
 
+  # Returns a list without duplicated items.
+  #
+  # ## Examples
+  #
+  #     List.uniq [1,2,3,2,1]
+  #     #=> [1,2,3]
+  #
   def uniq(list) when is_list(list) do
-    _uniq(list, [])
+    do_uniq(list, [])
   end
 
   ## Private
 
-  defp _flatten([h|t], tail) when is_list(h) do
-    _flatten(h, _flatten(t, tail))
+  defp do_flatten([h|t], tail) when is_list(h) do
+    do_flatten(h, do_flatten(t, tail))
   end
 
-  defp _flatten([h|t], tail) do
-    [h|_flatten(t, tail)]
+  defp do_flatten([h|t], tail) do
+    [h|do_flatten(t, tail)]
   end
 
-  defp _flatten([], tail) do
+  defp do_flatten([], tail) do
     tail
   end
 
-  defp _uniq([h|t], acc) do
+  defp do_uniq([h|t], acc) do
     case Erlang.lists.member(h, acc) do
     match: true
-      _uniq(t, acc)
+      do_uniq(t, acc)
     match: false
-      _uniq(t, [h|acc])
+      [h|do_uniq(t, [h|acc])]
     end
   end
 
-  defp _uniq([], acc) do
-    Erlang.lists.reverse(acc)
+  defp do_uniq([], _acc) do
+    []
   end
 end
--- test/elixir/list_test.exs
+++ test/elixir/list_test.exs
@@ -12,15 +12,31 @@ defmodule ListTest do
     [] = List.flatten([[]])
   end
 
+  def test_flatten_with_tail do
+    [1,2,3,4,5] = List.flatten([1,2,3], [4,5])
+    [1,2,3,4,5] = List.flatten([1,[2],3], [4,5])
+    [1,2,3,4,5] = List.flatten([[1,[2],3]], [4,5])
+  end
+
   def test_member? do
     true  = List.member? [1,2,3], 1
     false = List.member? [1,2,3], 0
     false = List.member? [], 0
   end
 
-  def test_flatten_with_tail do
-    [1,2,3,4,5] = List.flatten([1,2,3], [4,5])
-    [1,2,3,4,5] = List.flatten([1,[2],3], [4,5])
-    [1,2,3,4,5] = List.flatten([[1,[2],3]], [4,5])
+  def test_append_1 do
+    [1,[2],3,4,5,6] = List.append [[1,[2],3], [4], [5,6]]
+  end
+
+  def test_append_2 do
+    [1,[2],3,4,5] = List.append [1,[2],3], [4,5]
+  end
+
+  def test_reverse do
+    [3,2,1] = List.reverse [1,2,3]
+  end
+
+  def test_uniq do
+    [1,2,3] = List.uniq [1,2,3,2,1]
   end
 end
