6af3aca860c31158aa0c9c7c7e36e6d5c9063582
José Valim <jose.valim@gmail.com>
2012-02-06 12:28:39 +0100

    Revert "Support implicit string concatenation, closes #105"
    This reverts commit 7efead6d79a3b05630b2576c012bf1c81f7169d4.
    
    Conflicts:
    
    	test/elixir/string_test.exs

--- test/elixir/string_test.exs
+++ test/elixir/string_test.exs
@@ -3,13 +3,6 @@ Code.require_file "../test_helper", __FILE__
 defmodule StringTest do
   use ExUnit::Case
 
-  def test_implicit_string_concat do
-    "foobar" = "foo" "bar"
-    "foobar" = "foo" "#{:bar}"
-    "foobar" = "foo" \
-      "#{:bar}"
-  end
-
   def test_string_concatenation do
     "foo" <> x = "foobar"
     "bar" = x
