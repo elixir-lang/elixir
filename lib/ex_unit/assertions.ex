4a111be79acbc4a43201e8556d0a0e0c63a75e3a
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-02-11 21:59:26 -0200

    Add assert, refute and assert_equal

--- lib/ex_unit/assertions.ex
+++ lib/ex_unit/assertions.ex
@@ -6,4 +6,22 @@ defmodule ExUnit::Assertions do
       raise ExUnit::AssertionError, message: "Expected #{inspect container} to include #{inspect base}"
     end
   end
+
+  def assert_equal(expected, received) do
+    if expected != received do
+      raise ExUnit::AssertionError, message: "Expected #{inspect received} to be equal to #{inspect expected}"
+    end
+  end
+
+  def assert(expected) do
+    unless expected do
+      raise ExUnit::AssertionError, message: "Expected #{inspect expected} to be true"
+    end
+  end
+
+  def refute(expected) do
+    if expected do
+      raise ExUnit::AssertionError, message: "Expected #{inspect expected} to be false"
+    end
+  end
 end
