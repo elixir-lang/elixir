30eee3dc6163a4ffcd2477983c4d6eb1e21314e3
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-02-21 20:56:52 -0200

    Use blank string as buffer

--- test/elixir/eex/engine_test.exs
+++ test/elixir/eex/engine_test.exs
@@ -5,10 +5,10 @@ defmodule EEx::EngineTest do
   require EEx::Engine, as: E
 
   test "handle text" do
-    assert_equal {:"<>", 0, [[], "foo"]}, E.handle_text([], "foo")
+    assert_equal {:"<>", 0, ["", "foo"]}, E.handle_text("", "foo")
   end
 
   test "handle equal expression" do
-    assert_equal {:"<>", 0, [[],{:to_binary,0,[:foo]}]}, E.handle_expr([], '=', :foo)
+    assert_equal {:"<>", 0, ["",{:to_binary,0,[:foo]}]}, E.handle_expr("", '=', :foo)
   end
 end
