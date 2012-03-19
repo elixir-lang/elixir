b7ecb0a177b6641bdfa1958c2272e3b7c3b5289a
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-03-19 11:53:41 -0300

    OptionParse parses key/value option

--- lib/option_parser.ex
+++ lib/option_parser.ex
@@ -11,4 +11,18 @@ defmodule OptionParser do
   defp do_parse('--' ++ option, dict) do
     Orddict.put dict, list_to_atom(option), true
   end
+
+  defp do_parse('false', dict) do
+    key = List.last Orddict.keys(dict)
+    Orddict.put dict, key, false
+  end
+
+  defp do_parse('true', dict) do
+    dict
+  end
+
+  defp do_parse(value, dict) do
+    key = List.last Orddict.keys(dict)
+    Orddict.put dict, key, value
+  end
 end
--- test/elixir/option_parser_test.exs
+++ test/elixir/option_parser_test.exs
@@ -10,4 +10,16 @@ defmodule OptionParserTest do
   test "parses more than one boolean option" do
     assert_equal [docs: true, compile: true], OptionParser.parse(['--docs', '--compile'])
   end
+
+  test "parses key/value option" do
+    assert_equal [source: 'form_docs/'], OptionParser.parse(['--source', 'form_docs/'])
+  end
+
+  test "parses key/value option when value is false" do
+    assert_equal [docs: false], OptionParser.parse(['--docs', 'false'])
+  end
+
+  test "parses key/value option when value is true" do
+    assert_equal [docs: true], OptionParser.parse(['--docs', 'true'])
+  end
 end
