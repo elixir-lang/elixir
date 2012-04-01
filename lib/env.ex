e864164321ac453f03e799f5d92d8b8f70f33d1e
Alexei Sholik <alcosholik@gmail.com>
2012-04-01 14:33:23 +0300

    Initial draft of the new Env module

---
+++ lib/env.ex
@@ -0,0 +1,43 @@
+defmodule Env do
+  Env.Compiler.generate
+end
+
+defmodule Env.Compiler do
+  @moduledoc false
+
+  def generate do
+    quote do
+      @moduledoc """
+      Provides functions for obtaining information about Elixir runtime.
+      """
+
+      @doc """
+      Returns a tuple { Elixir version, commit sha-1, build date }
+      """
+      def version do
+        { Code.version,
+          unquote(get_head_sha),
+          unquote(get_date) }
+      end
+
+      @doc """
+      Returns the list of command-line arguments passed to the program.
+      """
+      def argv do
+        Erlang.gen_server.call(:elixir_code_server, :argv)
+      end
+    end
+  end
+
+  defp get_head_sha do
+    trim :os.cmd 'git rev-parse HEAD'
+  end
+
+  defp get_date do
+    trim :os.cmd 'TZ=GMT date'
+  end
+
+  defp trim(string) do
+    :re.replace string, '\n' , '', [{:return,:list}]
+  end
+end
