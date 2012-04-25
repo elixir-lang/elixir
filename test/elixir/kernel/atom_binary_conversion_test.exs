b1c38a54c32ac5acdfee1efb541813a15110203f
José Valim <jose.valim@plataformatec.com.br>
2012-04-25 15:21:55 +0200

    Merge atom <-> binary oconversion tests into one file

--- test/elixir/kernel/atom_binary_conversion_test.exs
+++ test/elixir/kernel/atom_binary_conversion_test.exs
@@ -1,9 +1,17 @@
 Code.require_file "../../test_helper", __FILE__
 
-defmodule Kernel.BinaryToAtomTest do
+defmodule Kernel.AtomToBinaryTest do
   use ExUnit.Case
 
-  test :default_to_utf8 do
+  test :atom_to_binary_defaults_to_utf8 do
+    expected  = atom_to_binary :some_binary, :utf8
+    actual    = atom_to_binary :some_binary
+
+    assert_equal expected, actual
+    assert_equal "another_atom", atom_to_binary :another_atom
+  end
+
+  test :binary_to_atom_defaults_to_utf8 do
     expected  = binary_to_atom "some_binary", :utf8
     actual    = binary_to_atom "some_binary"
 
@@ -11,19 +19,17 @@ defmodule Kernel.BinaryToAtomTest do
     assert_equal :another_binary, binary_to_atom "another_binary"
   end
 
-  test :default_to_utf8_existing do
+  test :binary_to_existing_atom_defaults_to_utf8 do
     expected = binary_to_atom "existing_atom", :utf8
     actual   = binary_to_existing_atom "existing_atom"
 
     assert_equal expected, actual
-    
+
     :existing_atom
     assert_equal :existing_atom, binary_to_existing_atom "existing_atom"
-    
+
     assert_raises ArgumentError, fn ->
       binary_to_existing_atom "nonexisting_atom"
     end
-
   end
-
 end
