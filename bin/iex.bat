b2727077279a5a5130d7d971ee469f69c5d3c2c9
José Valim <jose.valim@plataformatec.com.br>
2012-05-24 20:23:57 +0200

    Fix issue with iex not booting elixir properly

--- bin/iex.bat
+++ bin/iex.bat
@@ -1,2 +1,2 @@
 @echo off
-erl -pa %~dp0\..\ebin -noshell %ELIXIR_ERL_OPTS% -s "__MAIN__.Elixir.IEx" simple_start -extra %*
+erl -pa %~dp0\..\ebin -noshell %ELIXIR_ERL_OPTS% -s elixir start_cli -extra --no-halt -e "Elixir.IEx.start" %*
