a594159b2eb7c6406a48d0736b0bad58a5183eb1
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-06-05 16:04:55 -0700

    Makes Record's accessors and new/0,1 overridable

--- test/elixir/record_test.exs
+++ test/elixir/record_test.exs
@@ -8,6 +8,38 @@ defrecord name, a: 0, b: 1
 
 defrecord RecordTest.WithNoField, []
 
+defrecord RecordTest.Override, a: 0 do
+
+  def new, do: new([a: 100])
+
+  def new(opts) do
+      opts = Keyword.put opts, :a, opts[:a] * 5
+      super(opts)
+  end
+
+  def a(value, r) do
+    super({value, :erlang.now}, r)
+  end
+  def a(r) do
+    {v, _} = super(r)
+    v
+  end
+  def update_a(fun, r) do
+    r.a(fun.(r.a))
+  end
+  def prepend_a(_, r) do
+    r.a(r.a)
+  end
+  def merge_a(_, r) do
+    r.a(r.a)
+  end
+  def increment_a(_, r) do
+      r
+  end
+
+  def raw_a(r), do: elem(r, 2)
+end
+
 defmodule RecordTest do
   use ExUnit.Case
 
@@ -47,6 +79,26 @@ defmodule RecordTest do
     record = RecordTest.DynamicName.new(a: "a", b: "b")
     assert record.to_keywords[:a] == "a"
     assert record.to_keywords[:b] == "b"
+  end
+
+  test :overridable do
+    record = RecordTest.Override.new
+    assert record.raw_a == 500
+    record = record.a(1)
+    assert {1, _} = record.raw_a
+    assert record.a == 1
+    record = record.update_a(fn(_) -> 2 end)
+    assert {2, _} = record.raw_a
+    assert record.a == 2
+    record = record.prepend_a(100)
+    assert {2, _} = record.raw_a
+    assert record.a == 2
+    record = record.merge_a(100)
+    assert {2, _} = record.raw_a
+    assert record.a == 2
+    record = record.increment_a(1)
+    assert {2, _} = record.raw_a
+    assert record.a == 2
   end
 
   defp file_info do
