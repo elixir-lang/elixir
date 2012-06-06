elixir-lang/elixir

commit 088eff4c19614101cea55dfef9966d4de89181e3

Rewrite access macro in Elixir \o/

lib/elixir/builtin.ex
@@ -1444,33 +1444,6 @@ defmodule Elixir.Builtin do
   end
 
   @doc """
-  Access the given element according the qualifier according
-  to the `Access` protocol. Many types implement the access
-  protocol, so check the protocol documentation for more
-  information.
-
-  It is important to notice the access protocol is also
-  allowed in function signatures when applying to references.
-  This is useful when working with records to allow to match
-  against an specific part of a record:
-
-      def uri_parse(Uri.Config[schema: :http])
-
-  In the example above, the schema clause will only match if
-  the config schema is `:http`. Using the access protocol with
-  a reference that does not point to a record module will
-  generate a compilation exception.
-
-  ## Examples
-
-      a = { :a, :b, :c }
-      a[1] #=> :a
-      access a, 1 #=> :a
-
-  """
-  defmacro access(element, qualifier)
-
-  @doc """
   Convert the argument to a string according to the Binary.Chars protocol.
   This is the function invoked when there is string interpolation.
 
@@ -2180,6 +2153,78 @@ defmodule Elixir.Builtin do
   end
 
   @doc """
+  Access the given element according the qualifier according
+  to the `Access` protocol. Many types implement the access
+  protocol, so check the protocol documentation for more
+  information.
+
+  It is important to notice the access protocol is also
+  allowed in function signatures when applying to references.
+  This is useful when working with records to allow to match
+  against an specific part of a record:
+
+      def uri_parse(Uri.Config[schema: :http])
+
+  In the example above, the schema clause will only match if
+  the config schema is `:http`. Using the access protocol with
+  a reference that does not point to a record module will
+  generate a compilation exception.
+
+  ## Examples
+
+      a = { :a, :b, :c }
+      a[1] #=> :a
+      access a, 1 #=> :a
+
+  """
+  defmacro access(element, keyword) do
+    caller = __CALLER__
+    atom = Macro.expand_aliases(element, caller)
+
+    case { atom, caller.in_match? } do
+      { nil, false } ->
+        quote do: Access.access(unquote(element), unquote(keyword))
+      { nil, true } ->
+        raise "invalid usage of access protocol in signature"
+      { _, in_match } ->
+        case is_orddict(keyword) do
+          true  -> nil
+          false -> raise "expected contents inside brackets to be a Keyword"
+        end
+
+        case :code.ensure_loaded(atom) do
+          { :error, _ } ->
+            raise "expected module #{inspect atom} to be loaded and defined"
+          _ -> nil
+        end
+
+        fields = try do
+          atom.__record__(:fields)
+        rescue
+          UndefinedFunctionError ->
+            raise "cannot use module #{inspect atom} in access protocol because it does not represent a record"
+        end
+
+        iterator = fn({field, default}, each_keyword) ->
+          new_fields = Keyword.get(each_keyword, field) || case(in_match) do
+            true  -> { :_, 0, nil }
+            false -> Macro.escape(default)
+          end
+          { new_fields, Keyword.delete(each_keyword, field) }
+        end
+
+        { match, remaining } = :lists.mapfoldl(iterator, keyword, fields)
+
+        case remaining do
+          [] -> { :{}, caller.line, [atom|match] }
+          _  ->
+            keys = lc { key, _ } in remaining, do: key
+            raise "record #{inspect atom} does not have the keys: #{inspect keys}"
+        end
+    end
+  end
+
+  @doc """
   Handles the sigil %B. It simples returns a binary
   without escaping characters and without interpolations.
 
@@ -2314,4 +2359,10 @@ defmodule Elixir.Builtin do
   end
 
   defp build_cond_clauses([], acc), do: acc
+
+  defp is_orddict(list) when is_list(list), do: :lists.all(is_orddict_tuple(&1), list)
+  defp is_orddict(_), do: false
+
+  defp is_orddict_tuple({ x, _ }) when is_atom(x), do: true
+  defp is_orddict_tuple(_), do: false
 end

src/elixir_compiler.erl
@@ -205,8 +205,8 @@ core_main() ->
     "lib/elixir/builtin.ex",
     "lib/macro/env.ex",
     "lib/macro.ex",
-    "lib/module.ex",
     "lib/keyword.ex",
+    "lib/module.ex",
     "lib/list.ex",
     "lib/protocol.ex",
     "lib/enum.ex",

src/elixir_dispatch.erl
@@ -319,7 +319,6 @@ in_erlang_macros() ->
     {'>',2},
     {'>=',2},
     {'@',1},
-    {access,2},
     {'and',2},
     {apply,2},
     {apply,3},

src/elixir_macros.erl
@@ -171,57 +171,6 @@ translate_macro({use, Line, [Raw, Args]}, S) ->
 
   translate_each(Call, S);
 
-%% Access
-
-translate_macro({ access, Line, [Element, Keyword] }, S) ->
-  case translate_each(Element, S) of
-    { { atom, _, Atom }, _ } -> Atom;
-    _ -> Atom = false
-  end,
-
-  case { S#elixir_scope.context == assign, Atom } of
-    { false, false } ->
-      Fallback = { { '.', Line, ['__MAIN__.Access', access] }, Line, [Element, Keyword] },
-      translate_each(Fallback, S);
-    { true, false } ->
-      syntax_error(Line, S#elixir_scope.filename, "invalid usage of access protocol in signature");
-    { Assign, _ } ->
-      case is_orddict(Keyword) of
-        true -> [];
-        false ->
-          Message0 = "expected contents inside brackets to be a Keyword",
-          syntax_error(Line, S#elixir_scope.filename, Message0)
-      end,
-
-      elixir_ref:ensure_loaded(Line, Atom, S),
-
-      try Atom:'__record__'(fields) of
-        Fields ->
-          { Match, Remaining } = lists:mapfoldl(fun({Field, Default}, KeywordEach) ->
-            { case orddict:find(Field, KeywordEach) of
-              { ok, Value } -> Value;
-              error ->
-                case Assign of
-                  true  -> { '_', Line, nil };
-                  false -> '__MAIN__.Macro':escape(Default)
-                end
-            end, orddict:erase(Field, KeywordEach) }
-          end, Keyword, Fields),
-
-          case Remaining of
-            [] -> translate_each({ '{}', Line, [Atom|Match] }, S);
-            _ ->
-              Keys = [Key || {Key,_} <- Remaining],
-              Message1 = "record ~s does not have some of the given keys: ~p",
-              syntax_error(Line, S#elixir_scope.filename, Message1, [elixir_errors:inspect(Atom), Keys])
-          end
-      catch
-        error:undef ->
-          Message2 = "cannot use module ~s in access protocol because it doesn't represent a record",
-          syntax_error(Line, S#elixir_scope.filename, Message2, [elixir_errors:inspect(Atom)])
-      end
-  end;
-
 %% Apply - Optimize apply by checking what doesn't need to be dispatched dynamically
 
 translate_macro({ apply, Line, [Left, Right, Args] }, S) when is_list(Args) ->
@@ -251,11 +200,6 @@ module_ref({ '__aliases__', _, [{ '__MAIN__', _, Atom }|_]}, Module, _Nesting) w
 
 module_ref(_F, Module, Nesting) ->
   elixir_ref:concat([Nesting, Module]).
-
-is_orddict(Keyword) -> is_list(Keyword) andalso lists:all(fun is_orddict_tuple/1, Keyword).
-
-is_orddict_tuple({X,_}) when is_atom(X) -> true;
-is_orddict_tuple(_) -> false.
 
 is_reserved_data(moduledoc) -> true;
 is_reserved_data(doc)       -> true;

test/elixir/elixir/errors_test.exs
@@ -147,32 +147,32 @@ defmodule Elixir.ErrorsTest do
   end
 
   test :invalid_access_protocol_not_reference do
-    assert "nofile:2: invalid usage of access protocol in signature" ==
+    assert "invalid usage of access protocol in signature" ==
       format_rescue 'defmodule Foo do\ndef sample(config[integer: 0]), do: true\nend'
   end
 
   test :invalid_access_protocol_not_available do
-    assert "nofile:2: module Unknown is not loaded and could not be found" ==
+    assert "expected module Unknown to be loaded and defined" ==
       format_rescue 'defmodule Foo do\ndef sample(Unknown[integer: 0]), do: true\nend'
   end
 
   test :invalid_access_protocol_not_record do
-    assert "nofile:2: cannot use module Elixir.ErrorsTest in access protocol because it doesn't represent a record" ==
+    assert "cannot use module Elixir.ErrorsTest in access protocol because it does not represent a record" ==
       format_rescue 'defmodule Foo do\ndef sample(Elixir.ErrorsTest[integer: 0]), do: true\nend'
   end
 
   test :invalid_access_protocol_not_keywords do
-    assert "nofile:2: expected contents inside brackets to be a Keyword" ==
+    assert "expected contents inside brackets to be a Keyword" ==
       format_rescue 'defmodule Foo do\ndef sample(Elixir.ErrorsTest.Config[0]), do: true\nend'
   end
 
   test :invalid_access_protocol_invalid_keywords do
-    assert "nofile:2: record Elixir.ErrorsTest.Config does not have some of the given keys: [foo]" ==
+    assert "record Elixir.ErrorsTest.Config does not have the keys: [:foo]" ==
       format_rescue 'defmodule Foo do\ndef sample(Elixir.ErrorsTest.Config[foo: :bar]), do: true\nend'
   end
 
   test :invalid_access_protocol_invalid_keywords_outside_assignment do
-    assert "nofile:1: record Elixir.ErrorsTest.Config does not have some of the given keys: [foo]" ==
+    assert "record Elixir.ErrorsTest.Config does not have the keys: [:foo]" ==
       format_rescue 'Elixir.ErrorsTest.Config[foo: :bar]'
   end
 

commit 893f012ffe695af575b7f4ff28c5de413fb2e127

Fixes regression in record access protocol handling introduced in 088eff4c19614101cea55dfef9966d4de89181e3
It was mistreating nil values for undefined keys

lib/elixir/builtin.ex
@@ -2237,9 +2237,11 @@ defmodule Elixir.Builtin do
         end
 
         iterator = fn({field, default}, each_keyword) ->
-          new_fields = Keyword.get(each_keyword, field) || case(in_match) do
-            true  -> { :_, 0, nil }
-            false -> Macro.escape(default)
+          new_fields = 
+          case {Keyword.key?(each_keyword, field), in_match} do
+             {true, _} -> Keyword.get(each_keyword, field)
+             {_, false} -> Macro.escape(default)
+             {_, true}  -> { :_, 0, nil }
           end
           { new_fields, Keyword.delete(each_keyword, field) }
         end

test/elixir/record_test.exs
@@ -11,6 +11,12 @@ defrecord RecordTest.WithNoField, []
 defmodule RecordTest do
   use ExUnit.Case
 
+  test :record_access_with_nil_keyword do
+    record = RecordTest.DynamicName.new(a: nil)
+    record_access = RecordTest.DynamicName[a: nil]
+    assert record == record_access
+  end
+
   test :record_constructor_with_dict do
     record   = RecordTest.FileInfo.new(type: :regular)
     assert record.type == :regular