b62ab9d541c26e162de82384ed05e4dbc7413d8d
José Valim <jose.valim@plataformatec.com.br>
2012-06-18 21:58:26 +0200

    Add some in range assertions

--- test/elixir/range_test.exs
+++ test/elixir/range_test.exs
@@ -15,4 +15,12 @@ defmodule RangeTest do
     assert (1..3).first == 1
     assert (1..3).last  == 3
   end
+
+  test :in do
+    refute 0 in 1..3, "raw range assertion"
+    assert 1 in 1..3, "raw range assertion"
+    assert 2 in 1..3, "raw range assertion"
+    assert 3 in 1..3, "raw range assertion"
+    refute 4 in 1..3, "raw range assertion"
+  end
 end
