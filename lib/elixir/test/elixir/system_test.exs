59c38454c7e5bd003b01b7da791c9cb5dd1a3787
José Valim <jose.valim@plataformatec.com.br>
2012-07-06 14:49:15 +0200

    Refactor code now that we have local macros

--- lib/elixir/test/elixir/system_test.exs
+++ lib/elixir/test/elixir/system_test.exs
@@ -2,18 +2,18 @@ Code.require_file "../test_helper", __FILE__
 
 require Erlang.os, as: OS
 
-defmodule System.ARGVTest do
+defmodule SystemTest do
   use ExUnit.Case
+
+  test :build_info do
+    assert { _, _, _ } = System.build_info
+  end
 
   test :argv do
     list = OS.cmd('../../bin/elixir -e "IO.inspect System.argv" -- -o opt arg1 arg2 --long-opt 10')
     { args, _ } = Code.eval list, []
     assert args == ["-o", "opt", "arg1", "arg2", "--long-opt", "10"]
   end
-end
-
-defmodule SystemTest do
-  use ExUnit.Case
 
   test :at_exit do
     output = OS.cmd('../../bin/elixir -e "System.at_exit(fn x -> IO.inspect x end)"')
