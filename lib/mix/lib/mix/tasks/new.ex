10eb8f4963fa7fef6f6ef5a74efaac1b82c62f1d
José Valim <jose.valim@plataformatec.com.br>
2012-07-28 14:38:24 +0200

    Split default template around

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -77,18 +77,19 @@ defmodule Mix.Tasks.New do
     use Mix.Project
 
     def project do
-      [
-        app: :<%= @app %>,
+      [ app: :<%= @app %>,
         version: "0.0.1",
-        deps: [
-          # Add dependencies here
-          # { :foo_bar, "0.1", git: "https://github.com/foo/bar.git" }
-        ]
-      ]
+        deps: deps ]
     end
 
     # Configuration for the OTP application
     def application do
+      []
+    end
+
+    # Returns the list of dependencies in the format:
+    # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
+    defp deps do
       []
     end
   end
