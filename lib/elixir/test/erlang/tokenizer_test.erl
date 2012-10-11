c977170331ae092b569d5f6222ee53177125702c
José Valim <jose.valim@plataformatec.com.br>
2012-10-11 13:48:46 +0200

    Fix failing erlang test

--- lib/elixir/test/erlang/tokenizer_test.erl
+++ lib/elixir/test/erlang/tokenizer_test.erl
@@ -41,7 +41,7 @@ op_atom_test() ->
   [{atom,1,[f0_1]}] = tokenize(":f0_1").
 
 kw_test() ->
-  [{kw_identifier,1,do}] = tokenize("do:").
+  [{kw_identifier,1,do}] = tokenize("do: ").
 
 integer_test() ->
   [{number, 1, 123}] = tokenize("123"),
