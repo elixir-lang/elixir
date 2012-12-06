5a1bc1e333ad00fb8871786e92f36e00457b872e
José Valim <jose.valim@plataformatec.com.br>
2012-12-06 10:02:39 +0100

    Improve coverage for Supervisor.Behaviour

--- lib/elixir/test/elixir/gen_server/behaviour_test.exs
+++ lib/elixir/test/elixir/gen_server/behaviour_test.exs
@@ -1,36 +1,36 @@
 Code.require_file "../../test_helper.exs", __FILE__
 
-defmodule GenServer.Sample do
-  use GenServer.Behaviour
-
-  # Callbacks
-
-  def handle_call(:pop, _from, [h|t]) do
-    { :reply, h, t }
-  end
-
-  def handle_call(:terminate, _from, config) do
-    { :stop, :normal, :ok, config }
-  end
-
-  def handle_call(_request, _from, _config) do
-    super
-  end
-
-  def handle_cast({ :push, item }, config) do
-    { :noreply, [item|config] }
-  end
-
-  def handle_cast(request, config) do
-    super(request, config)
-  end
-end
-
 defmodule GenServer.BehaviourTest do
   use ExUnit.Case, async: true
 
+  defmodule Sample do
+    use GenServer.Behaviour
+
+    # Callbacks
+
+    def handle_call(:pop, _from, [h|t]) do
+      { :reply, h, t }
+    end
+
+    def handle_call(:terminate, _from, config) do
+      { :stop, :normal, :ok, config }
+    end
+
+    def handle_call(_request, _from, _config) do
+      super
+    end
+
+    def handle_cast({ :push, item }, config) do
+      { :noreply, [item|config] }
+    end
+
+    def handle_cast(request, config) do
+      super(request, config)
+    end
+  end
+
   test :using do
-    assert { :ok, pid } = :gen_server.start_link(GenServer.Sample, [:hello], [])
+    assert { :ok, pid } = :gen_server.start_link(Sample, [:hello], [])
     assert :gen_server.call(pid, :pop) == :hello
     assert :gen_server.cast(pid, { :push, :world }) == :ok
     assert :gen_server.call(pid, :pop) == :world
---
+++ lib/elixir/test/elixir/supervisor/behaviour_test.exs
@@ -0,0 +1,91 @@
+Code.require_file "../../test_helper.exs", __FILE__
+
+defmodule Supervisor.BehaviourTest do
+  use ExUnit.Case, async: true
+
+  defmodule Server do
+    use GenServer.Behaviour
+
+    def start_link do
+      :gen_server.start_link(__MODULE__, [], [])
+    end
+  end
+
+  defmodule Sup do
+    use Supervisor.Behaviour
+
+    def init(:ok) do
+      supervise [worker(Server, [])], strategy: :one_for_one
+    end
+
+    def init(:noop) do
+      :ignore
+    end
+  end
+
+  import Supervisor.Behaviour
+
+  test :start_link do
+    assert :ignore = :supervisor.start_link(Sup, :noop)
+    assert { :ok, pid } = :supervisor.start_link(Sup, :ok)
+    assert is_pid(pid)
+  end
+
+  test :worker do
+    assert worker(Foo, [1,2,3]) == {
+      Foo,
+      { Foo, :start_link, [1,2,3] },
+      :permanent,
+      5000,
+      :worker,
+      [Foo]
+    }
+
+    opts = [id: :sample, function: :start, modules: :dynamic,
+      restart: :temporary, shutdown: :brutal_kill]
+
+    assert worker(Foo, [1,2,3], opts) == {
+      :sample,
+      { Foo, :start, [1,2,3] },
+      :temporary,
+      :brutal_kill,
+      :worker,
+      :dynamic
+    }
+  end
+
+  test :supervisor do
+    assert supervisor(Foo, [1,2,3]) == {
+      Foo,
+      { Foo, :start_link, [1,2,3] },
+      :permanent,
+      5000,
+      :supervisor,
+      [Foo]
+    }
+
+    opts = [id: :sample, function: :start, modules: :dynamic,
+      restart: :temporary, shutdown: :brutal_kill]
+
+    assert supervisor(Foo, [1,2,3], opts) == {
+      :sample,
+      { Foo, :start, [1,2,3] },
+      :temporary,
+      :brutal_kill,
+      :supervisor,
+      :dynamic
+    }
+  end
+
+  test :supervise do
+    assert supervise([], strategy: :one_for_one) == {
+      :ok, { { :one_for_one, 5, 5 }, [] }
+    }
+
+    opts = [strategy: :one_for_all, max_restarts: 1, max_seconds: 1]
+
+    assert supervise([:sample], opts) == {
+      :ok, { { :one_for_all, 1, 1 }, [:sample] }
+    }
+  end
+end
