4387ae1a91dcd71bbdbaf7d38a419d2049f74d53
José Valim <jose.valim@plataformatec.com.br>
2013-01-01 13:14:22 +0100

    Propagate configs from ParallelCompiler in ParallelRequire

--- lib/elixir/lib/kernel/parallel_require.ex
+++ lib/elixir/lib/kernel/parallel_require.ex
@@ -29,11 +29,24 @@ defmodule Kernel.ParallelRequire do
   defp spawn_requires([h|t], waiting, callback, schedulers, result) do
     parent = self
 
-    child  = spawn_link fn ->
+    compiler_pid    = :erlang.get(:elixir_compiler_pid)
+    ensure_compiled = :erlang.get(:elixir_ensure_compiled)
+    { :error_handler, handler } = :erlang.process_info(parent, :error_handler)
+
+    child = spawn_link fn ->
+      if compiler_pid != :undefined do
+        :erlang.put(:elixir_compiler_pid, compiler_pid)
+      end
+
+      if ensure_compiled != :undefined do
+        :erlang.put(:elixir_ensure_compiled, ensure_compiled)
+      end
+
+      :erlang.process_flag(:error_handler, handler)
+
       try do
         new = Code.require_file(h)
-        callback.(h)
-        parent <- { :required, self, new }
+        parent <- { :required, self, new, h }
       catch
         kind, reason ->
           parent <- { :failure, self, kind, reason, System.stacktrace }
@@ -45,7 +58,8 @@ defmodule Kernel.ParallelRequire do
 
   defp wait_for_messages(files, waiting, callback, schedulers, result) do
     receive do
-      { :required, child, new } ->
+      { :required, child, new, file } ->
+        callback.(file)
         spawn_requires(files, List.delete(waiting, child), callback, schedulers, (new || []) ++ result)
       { :failure, _child, kind, reason, stacktrace } ->
         :erlang.raise(kind, reason, stacktrace)
