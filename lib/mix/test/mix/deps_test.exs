a1c202e02e653afbb18d728fc04f3c32de40a720
José Valim <jose.valim@plataformatec.com.br>
2013-01-04 19:25:08 +0100

    Clean up Mix.project after setting it

--- lib/mix/test/mix/deps_test.exs
+++ lib/mix/test/mix/deps_test.exs
@@ -25,5 +25,7 @@ defmodule Mix.DepsTest do
       assert Enum.find deps, match?(Mix.Dep[app: :ok, status: { :ok, _ }], &1)
       assert Enum.find deps, match?(Mix.Dep[app: :invalidapp, status: { :invalidapp, _ }], &1)
     end
+  after
+    Mix.Project.pop
   end
 end
