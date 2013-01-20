119d48e093a34df24436aed1488ef76be712b5f4
Yurii Rashkovskii <yrashk@gmail.com>
2013-01-19 20:22:16 -0800

    Improve IO.ANSI test case so it can be run on non-terminals correctly

--- lib/elixir/test/elixir/io/ansi_test.exs
+++ lib/elixir/test/elixir/io/ansi_test.exs
@@ -4,19 +4,19 @@ defmodule IO.ANSITest do
   use ExUnit.Case, async: true
 
   test :escape_single do
-    assert IO.ANSI.escape("Hello, #[red]world!#[reset]") ==
+    assert IO.ANSI.escape("Hello, #[red]world!#[reset]", true) ==
            "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
-    assert IO.ANSI.escape("Hello, #[ red ]world!#[reset]") ==
+    assert IO.ANSI.escape("Hello, #[ red ]world!#[reset]", true) ==
            "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
 
   end
 
   test :escape_multiple do
-    assert IO.ANSI.escape("Hello, #[red,bright]world!#[reset]") ==
+    assert IO.ANSI.escape("Hello, #[red,bright]world!#[reset]", true) ==
            "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
-    assert IO.ANSI.escape("Hello, #[red, bright]world!#[reset]") ==
+    assert IO.ANSI.escape("Hello, #[red, bright]world!#[reset]", true) ==
            "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
-    assert IO.ANSI.escape("Hello, #[red , bright]world!#[reset]") ==
+    assert IO.ANSI.escape("Hello, #[red , bright]world!#[reset]", true) ==
            "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
   end
 
