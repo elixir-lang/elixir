6960f856f4d8131e5bc3de60644bdc8f2e922ebe
Yurii Rashkovskii <yrashk@gmail.com>
2013-01-19 20:17:59 -0800

    Put proper IO.ANSI tests in

--- lib/elixir/test/elixir/io/ansi_test.exs
+++ lib/elixir/test/elixir/io/ansi_test.exs
@@ -3,9 +3,26 @@ Code.require_file "../../test_helper.exs", __FILE__
 defmodule IO.ANSITest do
   use ExUnit.Case, async: true
 
-  test :boolean do
-    assert String.length(IO.ANSI.red(true)) > 0
-    assert IO.ANSI.red(false) == ""
+  test :escape_single do
+    assert IO.ANSI.escape("Hello, #[red]world!#[reset]") ==
+           "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello, #[ red ]world!#[reset]") ==
+           "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
+
+  end
+
+  test :escape_multiple do
+    assert IO.ANSI.escape("Hello, #[red,bright]world!#[reset]") ==
+           "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello, #[red, bright]world!#[reset]") ==
+           "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello, #[red , bright]world!#[reset]") ==
+           "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
+  end
+
+  test :no_emit do
+    assert IO.ANSI.escape("Hello, #[red,bright]world!#[reset]", false) ==
+           "Hello, world!"
   end
 
 end
