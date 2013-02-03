1dc4bf098963b4cb5e0ad033ca588b8b0685bcca
José Valim <jose.valim@plataformatec.com.br>
2013-02-02 21:48:06 -0700

    Use tag from VERSION on package.exs

--- package.exs
+++ package.exs
@@ -1,9 +1,11 @@
+version = String.strip(File.read!("VERSION"))
+
 Expm.Package.new(
   name: "elixir",
   description: "Elixir is a functional meta-programming aware language built on top of the Erlang VM",
-  version: String.strip(File.read!("VERSION")),
+  version: version,
   keywords: [],
   homepage: "http://elixir-lang.org/",
   maintainers: [[name: "José Valim", email: "jose.valim@plataformatec.com.br"]],
-  repositories: [[github: "elixir-lang/elixir", tag: "v0.8.0.dev"]]
+  repositories: [[github: "elixir-lang/elixir", tag: "v#{version}"]]
 )
