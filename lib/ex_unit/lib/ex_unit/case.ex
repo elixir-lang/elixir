369fd7506c990370aec0f9492f6629bdc48bfc47
José Valim <jose.valim@plataformatec.com.br>
2013-03-26 13:36:54 -0600

    Ensure unquotes are respected in the test macro

--- lib/ex_unit/lib/ex_unit/case.ex
+++ lib/ex_unit/lib/ex_unit/case.ex
@@ -88,7 +88,7 @@ defmodule ExUnit.Case do
       end
 
       def message, [unquote(Macro.escape var)], [], do:
-        unquote(Macro.escape contents)
+        unquote(Macro.escape contents, escape_unquote: false)
     end
   end
 end
