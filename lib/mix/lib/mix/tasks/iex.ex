53ee456fb744e92196ea107fc50a37720a1737a9
José Valim <jose.valim@plataformatec.com.br>
2013-04-20 19:22:13 -0600

    Remove deprecations

--- lib/mix/lib/mix/tasks/iex.ex
+++ lib/mix/lib/mix/tasks/iex.ex
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.Iex do
   @moduledoc false
 
   def run(_args) do
-    raise Mix.Error, message: "mix iex is deprecated. Due to the VM constraints, " <>
+    raise Mix.Error, message: "mix iex is disabled. Due to the VM constraints, " <>
       "IEx needs to be configured on boot. Use `iex -S mix` instead."
   end
 end
