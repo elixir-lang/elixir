elixir-lang/elixir

commit 18c4e2b8104990afb49a1020eee70ccccf957bbe

Don't output if there is no doc, closes #1025

lib/iex/lib/iex/introspection.ex
@@ -148,7 +148,7 @@ defmodule IEx.Introspection do
   defp print_doc({ { fun, _ }, _line, kind, args, doc }) do
     args = Enum.map_join(args, ", ", print_doc_arg(&1))
     IO.puts IO.ANSI.escape("%{yellow}* #{kind} #{fun}(#{args})\n")
-    IO.write IO.ANSI.escape_fragment("%{yellow}") <> doc <> IO.ANSI.escape_fragment("%{reset}")
+    if doc, do: IO.write IO.ANSI.escape("%{yellow} #{doc}")
   end
 
   defp print_doc_arg({ ://, _, [left, right] }) do

commit 7bbb6806e357e3fa3e92c19eb7f0e5c69956dba1

Fix regression introduced by 18c4e2b8104990afb49a1020eee70ccccf957bbe
It is important to avoid rendering ANSI codes in the documentation as

1) IO.ANSI's own documentation becomes effectively unreadable as one
   can't see the syntax format
2) Markdown is not expected to have ANSI codes rendered

lib/iex/lib/iex/introspection.ex
@@ -148,7 +148,7 @@ defmodule IEx.Introspection do
   defp print_doc({ { fun, _ }, _line, kind, args, doc }) do
     args = Enum.map_join(args, ", ", print_doc_arg(&1))
     IO.puts IO.ANSI.escape("%{yellow}* #{kind} #{fun}(#{args})\n")
-    if doc, do: IO.write IO.ANSI.escape("%{yellow} #{doc}")
+    if doc, do: IO.write IO.ANSI.escape_fragment("%{yellow}") <> doc <> IO.ANSI.escape_fragment("%{reset}")
   end
 
   defp print_doc_arg({ ://, _, [left, right] }) do