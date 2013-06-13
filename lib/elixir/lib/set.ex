37621d4a073b19f3fcd0712ac2105c09ddff81bc
josephwilk <joe@josephwilk.net>
2013-06-13 18:35:41 +0200

    Adding Set with simple ops just for bucket (small sets)

---
+++ lib/elixir/lib/set.ex
@@ -0,0 +1,90 @@
+defmodule Set do
+  # The ordered record contains a single bucket
+  @ordered_threshold 8
+
+  defrecordp :ordered,
+    size: 0,
+    bucket: []
+
+  # The bucketed record contains a series of buckets.
+  @expand_load 5
+  @contract_load 2
+  @node_bitmap 0b111
+  @node_shift 3
+  @node_size 8
+  @node_template :erlang.make_tuple(@node_size, [])
+
+  defrecordp :trie,
+    size: 0,
+    depth: 0,
+    expand_on: @node_size * @expand_load,
+    contract_on: @contract_load,
+    root: @node_template
+
+  def new() do
+    ordered()
+  end
+
+  def new(vals) do
+    Enum.reduce vals, ordered(), fn v, set ->
+      put(set, v)
+    end
+  end
+
+  def empty(_) do
+    ordered()
+  end
+
+  def size(set) do
+    elem(set, 1)
+  end
+
+  def to_list(ordered(bucket: bucket)) do
+    bucket
+  end
+
+  def reduce(ordered(bucket: bucket), acc, fun) do
+    :lists.foldl(fun, acc, bucket)
+  end
+
+  def put(set, value) do
+    { set, _ } = set_put(set, { :put, value })
+    set
+  end
+
+  defp set_put(ordered(size: size, bucket: bucket) = set, value) do
+    { new, count } = bucket_put(bucket, value)
+    { ordered(set, size: size + count, bucket: new), count }
+  end
+
+  ## Bucket helpers
+
+  defp bucket_put([v|_]=bucket, { :put, value }) when v > value do
+    { [value|bucket], 1 }
+  end
+
+  defp bucket_put([value|bucket], { :put, value }) do
+    { [value|bucket], 0 }
+  end
+
+  defp bucket_put([e|bucket], value) do
+    { rest, count } = bucket_put(bucket, value)
+    { [e|rest], count }
+  end
+
+  defp bucket_put([], { :put, value }) do
+    { [value], 1 }
+  end
+end
+
+defimpl Binary.Inspect, for: Set do
+  import Kernel, except: [inspect: 2]
+
+  def inspect(set, opts) do
+    "#Set<" <> Kernel.inspect(Set.to_list(set), opts) <> ">"
+  end
+end
+
+defimpl Enumerable, for: Set do
+  def reduce(set, acc, fun),  do: Set.reduce(set, acc, fun)
+end
---
+++ lib/elixir/test/elixir/set_test.exs
@@ -0,0 +1,36 @@
+Code.require_file "test_helper.exs", __DIR__
+
+defmodule SetTest do
+  use ExUnit.Case, async: true
+
+  test :empty do
+    assert Set.empty filled_set(8)   == Set.new
+    assert Set.empty filled_set(20)  == Set.new
+    assert Set.empty filled_set(120) == Set.new
+  end
+
+  test :to_list do
+    set = filled_set(8)
+    list = set |> Set.to_list
+    assert length(list) == 8
+    assert 1 in list
+    assert list == Enum.to_list(set)
+  end
+
+  test "a set removes duplicates" do
+    assert Set.new([1,1,2,3,3,3]) == Set.new([1,2,3])
+  end
+
+  test "a set comparison ignores the order" do
+    assert Set.new([3,2,1]) == Set.new([1,2,3])
+
+    assert Set.new([:c,:a,:b]) == Set.new([:a,:b,:c])
+
+    assert Set.new(["c","a","b"]) == Set.new(["b", "c", "a"])
+  end
+
+  defp filled_set(range) do
+    Enum.reduce 1..range, Set.new, Set.put(&2, &1)
+  end
+end
+
