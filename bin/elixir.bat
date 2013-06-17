c7a2442bc7186ce9592abfbf36c09bc14dda967e
Tom Janssens <D4Skunk@gmail.com>
2013-06-17 11:19:44 +0200

    Make sure all Erlang parameters are extracted and parsed in elixir.bat

--- bin/elixir.bat
+++ bin/elixir.bat
@@ -1,10 +1,10 @@
 @echo off
 set argc=0
 for %%x in (%*) do set /A argc+=1
-if %argc% == 0 (
+if  %argc%== 0 (
   goto documentation
 ) else (
-  goto run
+  goto parseopts
 )
 :documentation
 echo Usage: %~nx0 [options] [.exs file] [data]
@@ -21,5 +21,47 @@ echo.
 echo ** Options marked with (*) can be given more than once
 echo ** Options given after the .exs file or -- are passed down to the executed code
 echo ** Options can be passed to the erlang runtime using ELIXIR_ERL_OPTS
+goto :EOF
+
+:parseopts
+
+rem parameters for Erlang
+set parsErlang=
+
+rem make sure we keep a copy of all parameters
+set allPars=%*
+
+rem Recursive loop called for each parameter
+:startloop
+set par=%1
+shift
+if "%par%"=="" (
+  rem if no parameters defined
+  goto :run
+)
+for /f "usebackq" %%m in (`echo %par%^|findstr \--detached`) do (
+  set parsErlang=%parsErlang% -detached
+  goto:startloop
+)
+for /f "usebackq" %%m in (`echo %par%^|findstr \--hidden`) do (
+  set parsErlang=%parsErlang% -hidden
+  goto:startloop
+)
+for /f "usebackq" %%m in (`echo %par%^|findstr \--cookie`) do (
+  set parsErlang=%parsErlang% -setcookie %1
+  shift
+  goto:startloop
+)
+for /f "usebackq" %%m in (`echo %par%^|findstr \--sname`) do (
+  set parsErlang=%parsErlang% -sname %1
+  shift
+  goto:startloop
+)
+for /f "usebackq" %%m in (`echo %par%^|findstr \--name`) do (
+  set parsErlang=%parsErlang% -name %1
+  shift
+  goto:startloop
+)
+goto:startloop
 :run
-erl -env ERL_LIBS %ERL_LIBS%;"%~dp0\..\lib" -noshell %ELIXIR_ERL_OPTS% -s elixir start_cli -extra %*
+erl -env ERL_LIBS %ERL_LIBS%;"%~dp0\..\lib" -noshell %ELIXIR_ERL_OPTS% %parsErlang% -s elixir start_cli -extra %allPars%
