6c56bb8682f7b4c4c2cbe7464f0fed77c19a82fd
José Valim <jose.valim@plataformatec.com.br>
2013-06-30 15:52:10 +0200

    Put doc tests in alphabetical order

--- lib/elixir/test/doc_test.exs
+++ lib/elixir/test/doc_test.exs
@@ -2,28 +2,29 @@ ExUnit.start []
 
 defmodule KernelTest do
   use ExUnit.Case, async: true
-  doctest Kernel
-  doctest Kernel.SpecialForms
+
   doctest Access.List
   doctest Binary.Chars.List
   doctest Binary.Inspect.Atom
   doctest Binary.Inspect.BitString
   doctest Binary.Inspect.List
-  doctest Binary.Inspect.Tuple
   doctest Binary.Inspect.Number
   doctest Binary.Inspect.Regex
+  doctest Binary.Inspect.Tuple
   doctest Bitwise
   doctest Code
   doctest Dict
-  doctest Set
   doctest Enum
   doctest IO.ANSI
-  doctest Regex
-  doctest String
+  doctest Keyword
+  doctest Kernel
+  doctest Kernel.SpecialForms
+  doctest List
+  doctest Macro
+  doctest Module
   doctest OptionParser
   doctest Path
-  doctest Module
-  doctest Macro
-  doctest List
-  doctest Keyword
+  doctest Regex
+  doctest Set
+  doctest String
 end
