8ca28496e49c3ff818c99bf7f9d02460ee4a881f
José Valim <jose.valim@plataformatec.com.br>
2013-06-30 09:36:14 +0200

    Add Stream.reject/2 and Stream.(drop|take)*/2

--- lib/elixir/test/elixir/stream_test.exs
+++ lib/elixir/test/elixir/stream_test.exs
@@ -31,16 +31,58 @@ defmodule StreamTest do
     assert Enum.take(stream, 5) == [1,2,3,1,2]
   end
 
-  test :map do
-    stream = Stream.map([1,2,3], &1 * 2)
+  test :drop do
+    stream = Stream.drop(1..10, 5)
     assert is_lazy(stream)
-    assert Enum.to_list(stream) == [2,4,6]
+    assert Enum.to_list(stream) == [6,7,8,9,10]
+
+    assert Enum.to_list(Stream.drop(1..5, 0)) == [1,2,3,4,5]
+    assert Enum.to_list(Stream.drop(1..3, 5)) == []
+  end
+
+  test :drop_while do
+    stream = Stream.drop_while(1..10, &1 <= 5)
+    assert is_lazy(stream)
+    assert Enum.to_list(stream) == [6,7,8,9,10]
+
+    assert Enum.to_list(Stream.drop_while(1..5, &1 <= 0)) == [1,2,3,4,5]
+    assert Enum.to_list(Stream.drop_while(1..3, &1 <= 5)) == []
   end
 
   test :filter do
     stream = Stream.filter([1,2,3], fn(x) -> rem(x, 2) == 0 end)
     assert is_lazy(stream)
     assert Enum.to_list(stream) == [2]
+  end
+
+  test :map do
+    stream = Stream.map([1,2,3], &1 * 2)
+    assert is_lazy(stream)
+    assert Enum.to_list(stream) == [2,4,6]
+  end
+
+  test :reject do
+    stream = Stream.reject([1,2,3], fn(x) -> rem(x, 2) == 0 end)
+    assert is_lazy(stream)
+    assert Enum.to_list(stream) == [1,3]
+  end
+
+  test :take do
+    stream = Stream.take(1..1000, 5)
+    assert is_lazy(stream)
+    assert Enum.to_list(stream) == [1,2,3,4,5]
+
+    assert Enum.to_list(Stream.take(1..1000, 0)) == []
+    assert Enum.to_list(Stream.take(1..3, 5)) == [1,2,3]
+  end
+
+  test :take_while do
+    stream = Stream.take_while(1..1000, &1 <= 5)
+    assert is_lazy(stream)
+    assert Enum.to_list(stream) == [1,2,3,4,5]
+
+    assert Enum.to_list(Stream.take_while(1..1000, &1 <= 0)) == []
+    assert Enum.to_list(Stream.take_while(1..3, &1 <= 5)) == [1,2,3]
   end
 
   test :with_index do
