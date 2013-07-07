eb4dda0b1e3c28ba8d41d569bbc41089242ae4ca
Alexei Sholik <alcosholik@gmail.com>
2013-07-08 00:01:30 +0300

    Refactor IEx.Options and raise on bad key

--- lib/iex/test/iex/options_test.exs
+++ lib/iex/test/iex/options_test.exs
@@ -2,7 +2,7 @@ Code.require_file "../test_helper.exs", __DIR__
 
 defmodule IEx.OptionsTest do
   use IEx.Case
-  
+
   @doc """
   Hello, I have %{red}ANSI%{reset} escapes.
   """
@@ -11,7 +11,7 @@ defmodule IEx.OptionsTest do
     test "color" do
       opts = [colors: [enabled: true, eval_result: "red"]]
       assert capture_iex("1 + 2", opts) == "\e[31m3\e[0m"
-  
+
       # Sanity checks
       assert capture_iex("IO.ANSI.escape(\"%{blue}hello\", true)", opts)
              == "\e[31m\"\\e[34mhello\\e[0m\"\e[0m"
@@ -19,19 +19,19 @@ defmodule IEx.OptionsTest do
              == "\e[34mhello\e[0m\n\e[31m:ok\e[0m"
       assert capture_iex("IO.puts IO.ANSI.escape(\"%{blue}hello\", true)", [colors: [enabled: false]])
              == "\e[34mhello\e[0m\n:ok"
-  
+
       # Test that ANSI escapes in the docs are left alone
       opts = [colors: [enabled: true, info: "red", eval_result: "red"]]
       assert capture_iex("h IEx.OptionsTest.ansi_escapes", opts)
              == "\e[31m* def ansi_escapes()\n\e[0m\n\e[31mHello, I have %{red}ANSI%{reset} escapes.\n\e[0m\n\e[31m:ok\e[0m"
-  
+
       # Test that ANSI escapes in iex output are left alone
       assert capture_iex("\"%{red} %{blue}\"", opts) == "\e[31m\"%{red} %{blue}\"\e[0m"
       assert capture_iex("IO.puts IEx.color(:info, \"%{red} %{blue}\")", opts)
              == "\e[31m%{red} %{blue}\e[0m\n\e[31m:ok\e[0m"
     end
   end
-	
+
   test "inspect opts" do
     opts = [inspect: [limit: 3, raw: true]]
     assert capture_iex("[1,2,3,4,5]\nArgumentError[]", opts) ==
@@ -47,5 +47,17 @@ defmodule IEx.OptionsTest do
     assert "1\n2\n3\n4\n** (RuntimeError) Out of bounds" <> _ = capture_iex("1\n2\n3\n4\nv(1)", opts)
     assert "1\n2\n3\n4\n** (RuntimeError) Out of bounds" <> _ = capture_iex("1\n2\n3\n4\nv(-4)", opts)
     assert "1\n2\n3\n4\n2\n** (RuntimeError) Out of bounds" <> _ = capture_iex("1\n2\n3\n4\nv(2)\nv(2)", opts)
+  end
+
+  test "bad option" do
+    assert_raise ArgumentError, fn ->
+      IEx.Options.set :nonexistent_option, nil
+    end
+  end
+
+  test "bad key" do
+    assert_raise ArgumentError, fn ->
+      IEx.Options.set :colors, nonexistent_color_name: "red"
+    end
   end
 end
