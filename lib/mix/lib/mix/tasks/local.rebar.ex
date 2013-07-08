5e5bf1ddb154974ce251b246ac9ed0f3771d602b
José Valim <jose.valim@plataformatec.com.br>
2013-07-08 21:48:56 +0200

    Install rebar from elixir repo which is more up to date

--- lib/mix/lib/mix/tasks/local.rebar.ex
+++ lib/mix/lib/mix/tasks/local.rebar.ex
@@ -3,13 +3,14 @@ defmodule Mix.Tasks.Local.Rebar do
 
   import Mix.Generator, only: [create_file: 2]
 
-  @rebar_url "https://github.com/downloads/basho/rebar/rebar"
+  @rebar_url "https://github.com/elixir-lang/elixir/raw/3cf4c21ad1f4627585b80394c37ac9f4d39d3aa8/rebar"
   @shortdoc  "Install rebar locally"
 
   @moduledoc """
-  Fetch a copy of rebar from the given path or url, defaulting to
-  #{@rebar_url} if no parameter
-  given. The local copy is stored in your MIX_HOME (defaults to ~/.mix).
+  Fetch a copy of rebar from the given path or url. It defaults to a
+  rebar copy that ships with Elixir source if available or fetches it from
+  #{@rebar_url}.
+The local copy is stored in your MIX_HOME (defaults to ~/.mix).
 
   This version of rebar will be used as required by mix deps.compile
   """
@@ -17,9 +18,14 @@ defmodule Mix.Tasks.Local.Rebar do
   def run(argv) do
     { _, argv } = OptionParser.parse(argv)
     do_install(case argv do
-      []       -> @rebar_url
+      []       -> rebar_path
       [path|_] -> path
     end)
+  end
+
+  defp rebar_path do
+    bundled_with_elixir = Path.join(:code.lib_dir(:mix), "../../rebar")
+    if File.regular?(bundled_with_elixir), do: bundled_with_elixir, else: @rebar_url
   end
 
   defp do_install(path) do
