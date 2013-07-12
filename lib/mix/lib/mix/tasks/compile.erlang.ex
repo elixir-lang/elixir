e820fb27b14fa917f24d5edd3741ddc1ecc82159
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-07-12 14:23:32 +0200

    Add manifest files to erlang compiler
    Makes sure that the correct generated files are deleted and
    cleaned even when the source files are moved or renamed.

--- lib/mix/lib/mix/tasks/compile.erlang.ex
+++ lib/mix/lib/mix/tasks/compile.erlang.ex
@@ -8,6 +8,7 @@ defmodule Mix.Tasks.Compile.Erlang do
   @hidden true
   @shortdoc "Compile Erlang source files"
   @recursive true
+  @manifest ".compile.erlang"
 
   @moduledoc """
   A task to compile Erlang source files.
@@ -79,7 +80,7 @@ defmodule Mix.Tasks.Compile.Erlang do
       files = Enum.filter(files, requires_compilation?(compile_path, &1))
     end
 
-    if files == [] do
+    if files == [] and not opts[:force] do
       :noop
     else
       File.mkdir_p! compile_path
@@ -155,16 +156,27 @@ defmodule Mix.Tasks.Compile.Erlang do
   end
 
   defp compile_files(files, compile_path, erlc_options) do
+    manifest_path = Path.join(compile_path, @manifest)
+    previous = Mix.Utils.read_manifest(manifest_path)
+    Enum.each(previous, fn(entry) ->
+      Path.join(compile_path, entry <> ".beam") |> File.rm
+    end)
+
     File.mkdir_p!(compile_path)
-    Enum.each files, compile_file(&1, erlc_options)
+    results = Enum.map(files, compile_file(&1, erlc_options))
+
+    modules = Enum.filter_map(results, match?({:ok, _}, &1), elem(&1, 1))
+    Mix.Utils.update_manifest(manifest_path, modules)
   end
 
   defp compile_file(erl, erlc_options) do
-    file = to_erl_file Path.rootname(erl.file, ".erl")
-    interpret_result file, :compile.file(file, erlc_options), ".erl"
+    file = to_erl_file(Path.rootname(erl.file, ".erl"))
+    result = :compile.file(file, erlc_options)
+    interpret_result(file, result, ".erl")
+    result
   end
 
-  ## Helpers shared accross erlang compilers
+  ## Helpers shared across erlang compilers
 
   @doc """
   Extract stale pairs considering the set of directories
