af454d3f90a480800deee0eff02031b1886fbd4c
José Valim <jose.valim@plataformatec.com.br>
2013-07-18 08:24:56 +0200

    Support for multiple callbacks in parallel compiler

--- lib/elixir/lib/kernel/parallel_compiler.ex
+++ lib/elixir/lib/kernel/parallel_compiler.ex
@@ -5,8 +5,6 @@ defmodule Kernel.ParallelCompiler do
   A module responsible for compiling files in parallel.
   """
 
-  defmacrop default_callback, do: quote(do: fn x -> x end)
-
   @doc """
   Compiles the given files.
 
@@ -22,24 +20,40 @@ defmodule Kernel.ParallelCompiler do
   A callback that will be invoked every time a file is compiled
   with its name can be optionally given as an argument.
   """
-  def files(files, callback // default_callback) do
-    spawn_compilers(files, nil, callback)
+  def files(files, opts // [])
+
+  def files(files, callback) when is_function(callback) do
+    IO.write "[WARNING] Kernel.ParallelCompiler.files(files, callback) is deprecated, " <>
+      "please use Kernel.ParallelCompiler.files(files, each_file: callback) instead\n#{Exception.format_stacktrace}"
+    spawn_compilers(files, nil, each_file: callback)
+  end
+
+  def files(files, callbacks) do
+    spawn_compilers(files, nil, callbacks)
   end
 
   @doc """
   Compiles the given files to the given path.
   Read `files/2` for more information.
   """
-  def files_to_path(files, path, callback // default_callback) when is_binary(path) do
-    spawn_compilers(files, path, callback)
+  def files_to_path(files, path, opts // [])
+
+  def files_to_path(files, path, callback) when is_function(callback) do
+    IO.write "[WARNING] Kernel.ParallelCompiler.files_to_path(files, path, callback) is deprecated, " <>
+      "please use Kernel.ParallelCompiler.files_to_path(files, path, each_file: callback) instead\n#{Exception.format_stacktrace}"
+    spawn_compilers(files, path, each_file: callback)
   end
 
-  defp spawn_compilers(files, path, callback) do
+  def files_to_path(files, path, callbacks) when is_binary(path) do
+    spawn_compilers(files, path, callbacks)
+  end
+
+  defp spawn_compilers(files, path, callbacks) do
     Code.ensure_loaded(Kernel.ErrorHandler)
     :elixir_code_server.cast(:reset_warnings)
     schedulers = max(:erlang.system_info(:schedulers_online), 2)
 
-    result = spawn_compilers(files, path, callback, [], [], schedulers, [])
+    result = spawn_compilers(files, path, callbacks, [], [], schedulers, [])
 
     # In case --warning-as-errors is enabled and there was a warning,
     # compilation status will be set to error and we fail with Kernel.CompilationError
@@ -50,13 +64,13 @@ defmodule Kernel.ParallelCompiler do
   end
 
   # We already have 4 currently running, don't spawn new ones
-  defp spawn_compilers(files, output, callback, waiting, queued, schedulers, result) when
+  defp spawn_compilers(files, output, callbacks, waiting, queued, schedulers, result) when
       length(queued) - length(waiting) >= schedulers do
-    wait_for_messages(files, output, callback, waiting, queued, schedulers, result)
+    wait_for_messages(files, output, callbacks, waiting, queued, schedulers, result)
   end
 
   # Spawn a compiler for each file in the list until we reach the limit
-  defp spawn_compilers([h|t], output, callback, waiting, queued, schedulers, result) do
+  defp spawn_compilers([h|t], output, callbacks, waiting, queued, schedulers, result) do
     parent = self()
 
     child  = spawn_link fn ->
@@ -77,40 +91,43 @@ defmodule Kernel.ParallelCompiler do
       end
     end
 
-    spawn_compilers(t, output, callback, waiting, [{child, h}|queued], schedulers, result)
+    spawn_compilers(t, output, callbacks, waiting, [{child, h}|queued], schedulers, result)
   end
 
   # No more files, nothing waiting, queue is empty, we are done
-  defp spawn_compilers([], _output, _callback, [], [], _schedulers, result), do: result
+  defp spawn_compilers([], _output, _callbacks, [], [], _schedulers, result), do: result
 
   # Queued x, waiting for x: POSSIBLE ERROR! Release processes so we get the failures
-  defp spawn_compilers([], output, callback, waiting, queued, schedulers, result) when length(waiting) == length(queued) do
+  defp spawn_compilers([], output, callbacks, waiting, queued, schedulers, result) when length(waiting) == length(queued) do
     Enum.each queued, fn { child, _ } -> child <- { :release, self() } end
-    wait_for_messages([], output, callback, waiting, queued, schedulers, result)
+    wait_for_messages([], output, callbacks, waiting, queued, schedulers, result)
   end
 
   # No more files, but queue and waiting are not full or do not match
-  defp spawn_compilers([], output, callback, waiting, queued, schedulers, result) do
-    wait_for_messages([], output, callback, waiting, queued, schedulers, result)
+  defp spawn_compilers([], output, callbacks, waiting, queued, schedulers, result) do
+    wait_for_messages([], output, callbacks, waiting, queued, schedulers, result)
   end
 
   # Wait for messages from child processes
-  defp wait_for_messages(files, output, callback, waiting, queued, schedulers, result) do
+  defp wait_for_messages(files, output, callbacks, waiting, queued, schedulers, result) do
     receive do
       { :compiled, child, file } ->
-        callback.(file)
+        if callback = Keyword.get(callbacks, :each_file) do
+          callback.(file)
+        end
+
         new_queued  = List.keydelete(queued, child, 0)
         # Sometimes we may have spurious entries in the waiting
         # list because someone invoked try/rescue UndefinedFunctionError
         new_waiting = List.keydelete(waiting, child, 0)
-        spawn_compilers(files, output, callback, new_waiting, new_queued, schedulers, result)
+        spawn_compilers(files, output, callbacks, new_waiting, new_queued, schedulers, result)
       { :module_available, _child, module, binary } ->
         new_waiting = release_waiting_processes(module, waiting)
         new_result  = [{module, binary}|result]
-        wait_for_messages(files, output, callback, new_waiting, queued, schedulers, new_result)
+        wait_for_messages(files, output, callbacks, new_waiting, queued, schedulers, new_result)
       { :waiting, child, on } ->
         new_waiting = OrdDict.store(child, on, waiting)
-        spawn_compilers(files, output, callback, new_waiting, queued, schedulers, result)
+        spawn_compilers(files, output, callbacks, new_waiting, queued, schedulers, result)
       { :failure, child, kind, reason, stacktrace } ->
         if many_missing?(child, files, waiting, queued) do
           IO.puts "== Compilation failed =="
