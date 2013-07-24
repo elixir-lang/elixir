182e11f0598c28103bbb826fc8e70ee71c8c3bc8
José Valim <jose.valim@plataformatec.com.br>
2013-07-24 23:48:39 +0200

    Update new app docs

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -154,7 +154,7 @@ defmodule Mix.Tasks.New do
     end
 
     # Returns the list of dependencies in the format:
-    # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
+    # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
     defp deps do
       []
     end
@@ -171,7 +171,7 @@ defmodule Mix.Tasks.New do
     end
 
     # Returns the list of dependencies in the format:
-    # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
+    # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
     # These dependencies are not accessible from child applications
     defp deps do
       []
