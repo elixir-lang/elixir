954310ae2c99e25c30214c0bfefdaaf309ff95ab
José Valim <jose.valim@plataformatec.com.br>
2013-07-25 11:02:29 +0200

    Get rid of Mix.Tasks.IEx

--- lib/mix/lib/mix/tasks/iex.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Iex do
-  use Mix.Task
-  @moduledoc false
-
-  def run(_args) do
-    raise Mix.Error, message: "mix iex is disabled. Due to the VM constraints, " <>
-      "IEx needs to be configured on boot. Use `iex -S mix` instead."
-  end
-end
