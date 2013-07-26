2b0121a733af0eb53e19441b1f313f2b1bee7c20
Tom Janssens <Tom@corebvba.be>
2013-07-26 18:57:43 +0200

    Moved and adjusted test_color_elixir.bat

--- windows/wac/test_color_elixir.bat
+++ windows/wac/test_color_elixir.bat
@@ -1,5 +1,5 @@
 @echo off
 echo There is still a problem with exclamation marks!
-echo The following 2 lines should be identical, except for the exclamation
+echo The following 2 lines should be identical, except for the exclamation mark
 call elixir -e "IO.puts \"zz #{IO.ANSI.green}Ahh ! Finally #{IO.ANSI.red}Colors work?""
 call elixir -e "IO.puts \"zz #{IO.ANSI.green}Ahh ? Finally #{IO.ANSI.red}Colors work?""
