b710ebd5e661a5a736f1cc4dea2f529783e9656d
vanstee <patrick@vanstee.me>
2013-08-12 23:10:48 -0400

    Return an empty string from slice with valid index
    To keep in line with ruby semantics of
    [`String#slice`](http://ruby-doc.org/core-2.0/String.html#method-i-slice)
    slice should return an empty string instead of nil when the starting
    index is valid even if the length is 0.
    
    Fixes #1591

--- lib/elixir/test/elixir/string_test.exs
+++ lib/elixir/test/elixir/string_test.exs
@@ -249,6 +249,9 @@ defmodule StringTest do
     assert String.slice("あいうえお", 6, 2) == nil
     assert String.slice("ειξήριολ", 8, 1) == ""
     assert String.slice("ειξήριολ", 9, 1) == nil
+    assert String.slice("elixir", 0, 0) == ""
+    assert String.slice("elixir", 5, 0) == ""
+    assert String.slice("elixir", -5, 0) == ""
     assert String.slice("", 0, 1) == ""
     assert String.slice("", 1, 1) == nil
   end
