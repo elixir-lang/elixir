2deaa02f189fbc8b7ce959f3b0f62e221738261c
pragdave <dave@pragprog.com>
2013-09-11 22:04:00 -0500

    respond to review. make internal methods private. Add tests for external methods.

--- lib/iex/lib/iex/ansi_docs.ex
+++ lib/iex/lib/iex/ansi_docs.ex
@@ -1,4 +1,4 @@
-defmodule FormatDocs do
+defmodule IEx.ANSIDocs do
   @moduledoc """
   Take a Markdown formatted docstring and attempt to make it a little 
   prettier on the terminal. We support only a minimal subset of Markdown,
@@ -15,16 +15,16 @@ defmodule FormatDocs do
   * :underline "underline",
   """
 
-  def doc_heading(string) do
-    if IO.ANSI.terminal? do
+  def doc_heading(string, use_ansi // IO.ANSI.terminal?) do
+    if use_ansi do
       write_doc_heading(string)
     else
       IO.puts "* #{string}\n"
     end
   end
 
-  def write(doc) do
-    if IO.ANSI.terminal? do
+  def format(doc, use_ansi // IO.ANSI.terminal?) do
+    if use_ansi do
       doc
       |> String.split(["\r\n","\n"], trim: false)
       |> Enum.map(&String.rstrip/1)
@@ -35,114 +35,108 @@ defmodule FormatDocs do
     end
   end
 
-  @doc "Bring lines back to a common left margin"
-  def bring_to_margin([]), do: []
-  def bring_to_margin(lines) do
+  # Bring lines back to a common left margin
+  defp bring_to_margin([]), do: []
+  defp bring_to_margin(lines) do
     case (lines |> Enum.map(&get_leading_spaces/1) |> Enum.min) do
       0 -> lines
       n -> Enum.map(lines, &strip_leading_spaces(&1, n))
     end
   end
 
-  def get_leading_spaces(""), do: 999
-  def get_leading_spaces(line) do
+  defp get_leading_spaces(""), do: 999
+  defp get_leading_spaces(line) do
      [{ _, leading_spaces }] = Regex.run(%r{^\s*}, line, return: :index)
      leading_spaces
   end
 
-  def strip_leading_spaces("", _n),  do: ""
-  def strip_leading_spaces(line, n), do: String.slice(line, n, 999)
+  defp strip_leading_spaces("", _n),  do: ""
+  defp strip_leading_spaces(line, n), do: String.slice(line, n, 999)
 
 
+  defp process([], _indent), do: nil
 
-  def process([], _indent), do: nil
-
-  def process([<< "# " :: utf8, heading :: binary>> | rest], _indent) do
+  defp process([<< "# " :: utf8, heading :: binary>> | rest], _indent) do
     write_h1(String.strip(heading))
     process(rest, "")
   end
 
-  def process([<< "## " :: utf8, heading :: binary>> | rest], _indent) do
+  defp process([<< "## " :: utf8, heading :: binary>> | rest], _indent) do
     write_h2(String.strip(heading))
     process(rest, "")
   end
 
-  def process([<< "### " :: utf8, heading :: binary>> | rest], indent) do
+  defp process([<< "### " :: utf8, heading :: binary>> | rest], indent) do
     write_h3(String.strip(heading), indent)
     process(rest, indent)
   end
 
-  def process([<< "* " :: utf8, line :: binary >> | rest], indent) do
+  defp process([<< "* " :: utf8, line :: binary >> | rest], indent) do
     { list_content, rest } = split_out_list(rest)
     list_content = bring_to_margin(list_content)
     process_ul(list_content, [line], indent <> "  ")
     process(rest, indent)
   end
 
-  def process([ "" | rest ], indent) do
+  defp process([ "" | rest ], indent) do
     process(rest, indent)
   end
 
-  def process( [ << "    " :: utf8, line :: binary>> | rest], indent) do
+  defp process( [ << "    " :: utf8, line :: binary>> | rest], indent) do
     process_code(rest, [line], indent)
   end
 
-  def process([ line | rest ], indent) do
+  defp process([ line | rest ], indent) do
     process_para(rest, [line], indent)
   end
 
-  def process_para(doc=[ "" | _rest], para, indent) do
+  defp process_para(doc=[ "" | _rest], para, indent) do
     write_para(para, indent)
     process(doc, indent)
   end
 
-  def process_para([], para, indent) do
+  defp process_para([], para, indent) do
     write_para(para, indent)
   end
 
-  def process_para([line | rest], para, indent) do
+  defp process_para([line | rest], para, indent) do
     process_para(rest, [ line | para ], indent)
   end
 
-
-
-  def process_code([], code, indent) do
+  defp process_code([], code, indent) do
     write_code(code, indent)
   end
 
   # blank line between code blocks
-  def process_code([ "", << "    " :: utf8, line :: binary>> | rest ], code, indent) do
+  defp process_code([ "", << "    " :: utf8, line :: binary>> | rest ], code, indent) do
     process_code(rest, [line, "" | code], indent)
   end
 
-  def process_code([ << "    " :: utf8, line :: binary>> | rest ], code, indent) do
+  defp process_code([ << "    " :: utf8, line :: binary>> | rest ], code, indent) do
     process_code(rest, [line|code], indent)
   end
 
-  def process_code(rest, code, indent) do
+  defp process_code(rest, code, indent) do
     write_code(code, indent)
     process(rest, indent)
   end
 
-  # def process_ul([ << "  " :: utf8, line :: binary>> | rest], list, indent) do
-  #   process_ul(rest, [ line | list ], indent)
-  # end
 
-  def process_ul([], result, indent) do
+  defp process_ul([], result, indent) do
     write_ul(result, indent)
   end
 
-  def process_ul(["" | rest], result, indent) do
+  defp process_ul(["" | rest], result, indent) do
     write_ul(result, indent)
     IO.puts ""
     process(rest, indent) 
   end
 
-  def process_ul([line|rest], result, indent) do
+  defp process_ul([line|rest], result, indent) do
     process_ul(rest, [line|result], indent)
   end
 
-  def write_doc_heading(heading) do
+  defp write_doc_heading(heading) do
     IO.puts IO.ANSI.reset
     width = column_width("")
     padding = div(width + String.length(heading), 2)
@@ -150,20 +144,20 @@ defmodule FormatDocs do
     write(:doc_heading, heading)
   end
 
-  def write_h1(heading) do
+  defp write_h1(heading) do
     write_h2(String.upcase(heading))
   end
 
-  def write_h2(heading) do
+  defp write_h2(heading) do
     write(:headings, heading)
   end
 
-  def write_h3(heading, indent) do
+  defp write_h3(heading, indent) do
     IO.write(indent)
     write(:headings, heading)
   end
 
-  def write_para(para, indent) do
+  defp write_para(para, indent) do
     para = para |> Enum.reverse |> Enum.join(" ")
     para = Regex.replace(%r{\[(.*?)\]\((.*?)\)}, para, "\\1 (\\2)")
     words = para |> String.split(%r{\s})
@@ -173,28 +167,27 @@ defmodule FormatDocs do
     IO.puts ""
   end
 
-  def write_code(code, indent) do
+  defp write_code(code, indent) do
     write(:code, "#{indent}┃ #{Enum.join(Enum.reverse(code), "\n#{indent}┃ ")}")
   end
 
-  def write_ul(list, indent) do
+  defp write_ul(list, indent) do
     list = list |> Enum.reverse |> Enum.join(" ") |> String.split(%r{\s})
     IO.write("• ")
     width = column_width(indent)
     write_with_wrap(list, width, width, indent)
   end
 
-  def write(style, string) do
+  defp write(style, string) do
     IO.puts IEx.color(style, string)
     IO.puts IO.ANSI.reset
   end
 
-
-  def write_with_wrap([], left, max, _indent) do
+  defp write_with_wrap([], left, max, _indent) do
     unless left == max, do: IO.write("\n")
   end
 
-  def write_with_wrap([word|words], left_on_line, max_columns, indent) do
+  defp write_with_wrap([word|words], left_on_line, max_columns, indent) do
     { word, leader, trailer, punc } = look_for_markup(word)
 
     word_length = String.length(word)
@@ -216,7 +209,7 @@ defmodule FormatDocs do
     write_with_wrap(words, left_on_line - word_length - 1, max_columns, indent)
   end
 
-  def look_for_markup(word) do
+  defp look_for_markup(word) do
     leader = nil
     trailer = nil
     punc    = nil
@@ -244,25 +237,24 @@ defmodule FormatDocs do
   # divide the lines into the leading portion that can be part of
   # the list and the rest. The first group is lines with at least 2
   # leading spaces (which we remove)
-  def split_out_list(lines) do
+  defp split_out_list(lines) do
     { list, rest } = Enum.split_while lines, fn line ->
       String.starts_with?(line, " ") || line == ""
     end
     { Enum.map(list, &chop(&1, 2)), rest }
   end
 
-  def chop(line, n) when size(line) >= n do
+  defp chop(line, n) when size(line) >= n do
     String.slice(line, n, 999)
   end
 
-  def chop(_, _), do: ""
+  defp chop(_, _), do: ""
 
+  defp color_name_for(?`), do: :inline_code
+  defp color_name_for(?_), do: :underline
+  defp color_name_for(?*), do: :bold
 
-  def color_name_for(?`), do: :inline_code
-  def color_name_for(?_), do: :underline
-  def color_name_for(?*), do: :bold
-
-  def color(color_name) do
+  defp color(color_name) do
     colors = IEx.Options.get(:colors)
     if colors[:enabled] do
       IO.ANSI.escape_fragment("%{#{colors[color_name]}}")
@@ -271,8 +263,10 @@ defmodule FormatDocs do
     end
   end
 
-  def column_width(indent) do
-    { :ok, width } = :io.columns
-    width - String.length(indent) - 1
+  defp column_width(indent) do
+    case :io.columns do
+      { :ok, width } -> width
+      _              -> 80
+    end
   end
 end
--- lib/iex/lib/iex/introspection.ex
+++ lib/iex/lib/iex/introspection.ex
@@ -12,8 +12,8 @@ defmodule IEx.Introspection do
         if function_exported?(module, :__info__, 1) do
           case module.__info__(:moduledoc) do
             { _, binary } when is_binary(binary) ->
-              FormatDocs.doc_heading(inspect module)
-              FormatDocs.write(binary)
+              IEx.ANSIDocs.doc_heading(inspect module)
+              IEx.ANSIDocs.format(binary)
             { _, _ } ->
               nodocs(inspect module)
             _ ->
@@ -158,8 +158,8 @@ defmodule IEx.Introspection do
 
   defp print_doc({ { fun, _ }, _line, kind, args, doc }) do
     args = Enum.map_join(args, ", ", print_doc_arg(&1))
-    FormatDocs.doc_heading("#{kind} #{fun}(#{args})")
-    if doc, do: FormatDocs.write(doc)
+    IEx.ANSIDocs.doc_heading("#{kind} #{fun}(#{args})")
+    if doc, do: IEx.ANSIDocs.format(doc)
   end
 
   defp print_doc_arg({ ://, _, [left, right] }) do
