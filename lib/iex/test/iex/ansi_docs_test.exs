4b2f94bd473f52209dab7c7e0c42eb34c89cea29
pragdave <dave@pragprog.com>
2013-09-30 13:10:13 -0500

    add a few extra tests

--- lib/iex/test/iex/ansi_docs_test.exs
+++ lib/iex/test/iex/ansi_docs_test.exs
@@ -13,39 +13,66 @@ defmodule IEx.AnsiDocsTest do
 
   @opts [colors: @colors]
 
+  def format(str, opts // [], use_ansi // true) do
+    cmd = "IEx.ANSIDocs.format(#{inspect str}, #{inspect use_ansi})"
+    capture_iex(cmd, opts)
+  end
+
   test "non-ansi heading just uses an asterisk" do
-    assert capture_iex("IEx.ANSIDocs.doc_heading(\"wibble\", false)", []) == "* wibble\n\n:ok"
+    assert capture_iex("IEx.ANSIDocs.doc_heading(\"wibble\", false)", []) == "* wibble"
   end
 
   test "ansi heading is formatted" do
     result = capture_iex("IEx.ANSIDocs.doc_heading(\"wibble\", true)", @opts)
     assert String.starts_with?(result, "\e[0m\n\e[7m\e[33m\e[1m")
-    assert String.ends_with?(result, "\e[0m\n\e[0m\n\e[33m:ok\e[0m")
+    assert String.ends_with?(result, "\e[0m\n\e[0m")
     assert String.contains?(result, " wibble ")
   end
 
   test "first level heading is converted" do
-    result = capture_iex("IEx.ANSIDocs.format(\"# wibble\n\ntext\n\", true)", @opts)
-    assert result == "\e[33m\e[1mWIBBLE\e[0m\n\e[0m\ntext \n\n\e[33mnil\e[0m"
+    result = format("# wibble\n\ntext\n", @opts)
+    assert result == "\e[33m\e[1mWIBBLE\e[0m\n\e[0m\ntext"
   end
 
   test "second level heading is converted" do
-    result = capture_iex("IEx.ANSIDocs.format(\"## wibble\n\ntext\n\", true)", @opts)
-    assert result == "\e[33m\e[1mwibble\e[0m\n\e[0m\ntext \n\n\e[33mnil\e[0m"
+    result = format("## wibble\n\ntext\n", @opts)
+    assert result == "\e[33m\e[1mwibble\e[0m\n\e[0m\ntext"
   end
 
   test "third level heading is converted" do
-    result = capture_iex("IEx.ANSIDocs.format(\"## wibble\n\ntext\n\", true)", @opts)
-    assert result == "\e[33m\e[1mwibble\e[0m\n\e[0m\ntext \n\n\e[33mnil\e[0m"
+    result = format("## wibble\n\ntext\n", @opts)
+    assert result == "\e[33m\e[1mwibble\e[0m\n\e[0m\ntext"
   end
 
   test "code block is converted" do
-    result = capture_iex("IEx.ANSIDocs.format(\"line\n\n    code\n    code2\n\nline2\n\", true)", @opts)
-    assert result == "line \n\n\e[36m\e[1m┃ code\n┃ code2\e[0m\n\e[0m\nline2 \n\n\e[33mnil\e[0m"
+    result = format("line\n\n    code\n    code2\n\nline2\n", @opts)
+    assert result == "line\n\n\e[36m\e[1m┃ code\n┃ code2\e[0m\n\e[0m\nline2"
   end
 
   test "list is converted" do
-    result = capture_iex("IEx.ANSIDocs.format(\"* one\n* two\n* three\n\", true)", @opts)
-    assert result == "• one \n• two \n• three \n\n\e[33mnil\e[0m"
+    result = format("* one\n* two\n* three\n", @opts)
+    assert result == "• one\n• two\n• three"
   end
+
+  test "paragraphs are split" do
+    result = format("para1\n\npara2")
+    assert result == "para1\n\npara2"
+  end
+
+  test "extra whitespace is ignored between paras" do
+    result = format("para1\n   \npara2")
+    assert result == "para1\n\npara2"
+  end
+
+  test "extra whitespace doesn't mess up a following list" do
+    result = format("para1\n   \n* one\n* two")
+    assert result == "para1\n\n• one\n• two"
+  end
+
+
+  test "bold and italic work" do
+    result = format("baz _foo_ bar\n", @opts)
+    assert result == "foo bar"
+  end
+
 end
