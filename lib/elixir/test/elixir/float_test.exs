80997548984307be4c9279d5d1cc031e5d1b5081
Carlos Brito Lage <carlos@carloslage.net>
2013-10-19 15:13:28 -0700

    Added extra test coverage for float parsing

--- lib/elixir/test/elixir/float_test.exs
+++ lib/elixir/test/elixir/float_test.exs
@@ -8,11 +8,17 @@ defmodule FloatTest do
     assert Float.parse("-12") === {-12.0, ""}
     assert Float.parse("123456789") === {123456789.0, ""}
     assert Float.parse("12.5") === {12.5, ""}
+    assert Float.parse("12.524235") === {12.524235, ""}
     assert Float.parse("-12.5") === {-12.5, ""}
+    assert Float.parse("-12.524235") === {-12.524235, ""}
     assert Float.parse("7.5e3") === {7.5e3, ""}
     assert Float.parse("7.5e-3") === {7.5e-3, ""}
     assert Float.parse("12x") === {12.0, "x"}
     assert Float.parse("12.5x") === {12.5, "x"}
+    assert Float.parse("-12.32453e10") === {-1.232453e11, ""}
+    assert Float.parse("-12.32453e-10") === {-1.232453e-9, ""}
+    assert Float.parse("0.32453e-10") === {3.2453e-11, ""}
+    assert Float.parse("1.32453e-10") === {1.32453e-10, ""}
     assert Float.parse("pi") === :error
   end
 end
