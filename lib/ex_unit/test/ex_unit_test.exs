90f9162e3fa518395ea7b83700981c0c3cdd070b
vanstee <patrick@vanstee.me>
2013-12-01 11:45:10 -0500

    Write test exercising case filtering

--- lib/ex_unit/test/ex_unit_test.exs
+++ lib/ex_unit/test/ex_unit_test.exs
@@ -28,6 +28,54 @@ defmodule ExUnit.NilFormatter do
   end
 end
 
+defmodule ExUnit.TestsCounterFormatter do
+  @timeout 30_000
+  @behaviour ExUnit.Formatter
+
+  use GenServer.Behaviour
+
+  def suite_started(opts) do
+    { :ok, pid } = :gen_server.start_link(__MODULE__, opts, [])
+    pid
+  end
+
+  def suite_finished(id, _run_us, _load_us) do
+    :gen_server.call(id, { :suite_finished }, @timeout)
+  end
+
+  def case_started(_id, _test_case) do
+    :ok
+  end
+
+  def case_finished(_id, _test_case) do
+    :ok
+  end
+
+  def test_started(_id, _test) do
+    :ok
+  end
+
+  def test_finished(id, test) do
+    :gen_server.cast(id, { :test_finished, test })
+  end
+
+  def init(_opts) do
+    { :ok, 0 }
+  end
+
+  def handle_call({ :suite_finished }, _from, tests_counter) do
+    { :stop, :normal, tests_counter, tests_counter }
+  end
+
+  def handle_cast({ :test_finished, ExUnit.Test[state: { :skip, _ }] }, tests_counter) do
+    { :noreply, tests_counter }
+  end
+
+  def handle_cast({ :test_finished, _ }, tests_counter) do
+    { :noreply, tests_counter + 1 }
+  end
+end
+
 defmodule ExUnitTest do
   use ExUnit.Case, async: false
 
@@ -47,5 +95,29 @@ defmodule ExUnitTest do
     end
 
     assert ExUnit.run == 1
+  end
+
+  test "filtering cases with tags" do
+    ExUnit.configure(filter: [run: true], formatter: ExUnit.TestsCounterFormatter)
+
+    defmodule FilteredTest do
+      use ExUnit.Case, async: false
+
+      test "without any tags" do
+        assert false
+      end
+
+      @tag run: true
+      test "with matching tag" do
+        assert false
+      end
+
+      @tag run: false
+      test "without matching tag" do
+        assert false
+      end
+    end
+
+    assert ExUnit.run == 2
   end
 end
