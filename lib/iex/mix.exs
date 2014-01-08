d6de0979fb620e45641d81d302d3a8fd21d12779
José Valim <jose.valim@plataformatec.com.br>
2014-01-08 15:04:19 +0100

    Ensure Elixir own .app files include all lib modules

--- lib/iex/mix.exs
+++ lib/iex/mix.exs
@@ -8,26 +8,30 @@ defmodule IEx.Mixfile do
   def application do
     [ env: [
         after_spawn: [],
-        colors: [ enabled: true,
-
-                  # Used by default on evaluation cycle
-                  eval_interrupt: "yellow",
-                  eval_result: "yellow",
-                  eval_error: "red",
-                  eval_info: "normal",
-
-                  # Used by ls
-                  ls_directory: "blue",
-                  ls_device: "green",
-
-                  # Used by ansi docs
-                  doc_bold: "bright",
-                  doc_code: "cyan,bright",
-                  doc_headings: "yellow,bright",
-                  doc_inline_code: "cyan",
-                  doc_underline: "underline",
-                  doc_title: "reverse,yellow,bright" ],
+        colors: colors,
         inspect: [limit: 50, raw: false, pretty: true],
         history_size: 20 ] ]
+  end
+
+  defp colors do
+    [ enabled: true,
+
+      # Used by default on evaluation cycle
+      eval_interrupt: "yellow",
+      eval_result: "yellow",
+      eval_error: "red",
+      eval_info: "normal",
+
+      # Used by ls
+      ls_directory: "blue",
+      ls_device: "green",
+
+      # Used by ansi docs
+      doc_bold: "bright",
+      doc_code: "cyan,bright",
+      doc_headings: "yellow,bright",
+      doc_inline_code: "cyan",
+      doc_underline: "underline",
+      doc_title: "reverse,yellow,bright" ]
   end
 end
