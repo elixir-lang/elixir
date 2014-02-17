db7686630930fa9b2069540f4df4c707a154bc0b
José Valim <jose.valim@plataformatec.com.br>
2014-02-17 20:24:37 +0100

    Minor style changes

--- lib/elixir/lib/kernel/typespec.ex
+++ lib/elixir/lib/kernel/typespec.ex
@@ -498,7 +498,7 @@ defmodule Kernel.Typespec do
 
   defp abstract_code(module) do
     case :beam_lib.chunks(abstract_code_beam(module), [:abstract_code]) do
-      {:ok, { _, [{ :abstract_code, { _raw_abstract_v1, abstract_code } }] } } ->
+      { :ok, { _, [{ :abstract_code, { _raw_abstract_v1, abstract_code } }] } } ->
         { :ok, abstract_code }
       _ ->
         :error
