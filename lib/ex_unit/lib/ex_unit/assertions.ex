dde8a2ac125e8fab85c15155c218d7e3bab92ea2
pragdave <dave@pragprog.com>
2014-02-28 22:39:47 +0530

    Spruce up the output format of ex_unit

--- lib/ex_unit/lib/ex_unit/assertions.ex
+++ lib/ex_unit/lib/ex_unit/assertions.ex
@@ -1,20 +1,15 @@
-defexception ExUnit.AssertionError,  message: "assertion failed"
+defmodule ExUnit.NoValueSupplied do
+  def no_value, do: {:__no__, :__meaningful__, :__value__}
+end
 
-defexception ExUnit.ExpectationError, expected: nil, actual: nil, assertion: "",
-    negation: false, prelude: "Expected", expr: nil do
-  def message(exception) do
-    if desc = exception.expr do
-      "#{exception.prelude} #{desc} #{exception.full_assertion} " <>
-        "#{exception.expected}. Instead got #{exception.actual}"
-    else
-      "#{exception.prelude} #{exception.expected} " <>
-        "#{exception.full_assertion} #{exception.actual}"
-    end
-  end
+defexception ExUnit.AssertionError, [
+                    left:      ExUnit.NoValueSupplied.no_value,
+                    right:     ExUnit.NoValueSupplied.no_value,
+                    value:     ExUnit.NoValueSupplied.no_value,
+                    message:   ExUnit.NoValueSupplied.no_value,
+                    operator:  ExUnit.NoValueSupplied.no_value,
+                    expr:      "missing failing expression" ] do
 
-  def full_assertion(exception) do
-    "to" <> if(exception.negation, do: " not ", else: " ") <> exception.assertion
-  end
 end
 
 defmodule ExUnit.Assertions do
@@ -28,7 +23,10 @@ defmodule ExUnit.Assertions do
   For example, `assert some_fun() == 10` will fail (assuming
   `some_fun()` returns 13):
 
-      Expected 10 to be equal to 13
+     Comparison (using ==) failed in:
+     code: some_fun() == 10
+     lhs:  13
+     rhs:  10
 
   This module also provides other convenience functions
   like `assert_in_delta` and `assert_raise` to easily handle other
@@ -36,87 +34,45 @@ defmodule ExUnit.Assertions do
   """
 
   @doc """
-  Asserts the `expected` value is true.
+  Asserts it's argument is true.
 
-  `assert` in general tries to be smart and provide good
-  reporting whenever there is a failure. For example,
-  `assert 10 > 15` is going to fail with the message:
+  `assert` tries to be smart and provide good
+  reporting whenever there is a failure. In particular, if
+  given a match expression, it will report any failure in terms
+  of that match. Given 
+ 
+      assert [one] = [two]
 
-      Expected 10 to be more than 15
+  you'll see:
 
-  ## Examples
+      match (=) failed
+      code: [one] = [two]
+      rhs:  [2]
 
-      assert true
+  If the expression is a comparison operator, the message
+  will show the values of the two sides. The assertion
 
+      assert 1+2+3+4 > 15
+
+   will fail with the message:
+
+      Comparison (using >) failed in:
+      code: 1+2+3+4 > 15
+      lhs:  10
+      rhs:  15
   """
-  defmacro assert(expected) do
-    case translate_assertion(expected) do
-      nil ->
-        # Default message in case no transform was performed
-        quote do
-          value = unquote(expected)
-
-          unless value do
-            raise ExUnit.ExpectationError,
-              expr: unquote(Macro.to_string(expected)),
-              assertion: "be",
-              expected: "true",
-              actual: inspect(value)
-          end
-
-          value
-        end
-
-      value -> value
-    end
-  end
-
-  @doc """
-  Refutes the `expected` value is true.
-
-  `refute` in general tries to be smart and provide good
-  reporting whenever there is a failure.
-
-  ## Examples
-
-      refute false
-
-  """
-  defmacro refute(expected) do
-    case translate_assertion({ :!, [], [expected] }) do
-      nil ->
-        # Default message in case no transform was performed
-        quote do
-          value = unquote(expected)
-
-          if value do
-            raise ExUnit.ExpectationError,
-              expr: unquote(Macro.to_string(expected)),
-              assertion: "be",
-              expected: "false",
-              actual: inspect(value)
-          end
-
-          false
-        end
-
-      value -> { :!, [], [value] }
-    end
-  end
-
-  ## START HELPERS
-
-  defp translate_assertion({ :=, _, [left, right] }) do
+  defmacro assert(assertion = { :=, _, [left, right] }) do
+    code = Macro.to_string(assertion)
     { :case, meta, args } =
       quote do
         case right do
           unquote(left) ->
             right
           _ ->
-            raise ExUnit.ExpectationError,
-              expected: inspect(right),
-              actual: unquote(Macro.to_string(left)),
-              assertion: "match pattern (=)"
+            raise ExUnit.AssertionError,
+              right: Macro.to_string(right),
+              expr: unquote(code),
+              message: "match (=) failed"
         end
       end
 
@@ -126,47 +82,98 @@ defmodule ExUnit.Assertions do
     end
   end
 
-  defp translate_assertion({ :==, _, [left, right] }) do
-    assert_operator :==, left, right, "be equal to (==)"
+
+  defmacro assert(assertion) do
+    case translate_assertion(assertion) do
+      nil ->
+        # Default message in case no transform was performed
+        quote do
+          value = unquote(assertion)
+
+          unless value do
+            raise ExUnit.AssertionError,
+              expr:  unquote(Macro.to_string(assertion)),
+              message: "#{inspect value} is not truthy"
+          end
+
+          value
+        end
+
+      value -> 
+        value
+    end
   end
 
-  defp translate_assertion({ :<, _, [left, right] }) do
-    assert_operator :<, left, right, "be less than"
+  @doc """
+  This is a negative assertion, failing if its parameter
+  is truthy.
+
+  ## Examples
+
+      refute age < 0
+
+  """
+
+  defmacro refute(assertion = { :=, _, [left, right] }) do
+    code = Macro.to_string(assertion)
+    { :case, meta, args } =
+      quote do
+        case right do
+          unquote(left) ->
+            raise ExUnit.AssertionError,
+              right: Macro.to_string(right),
+              expr: unquote(code),
+              message: "match (=) succeeded, but should have failed"
+          _ ->
+            right
+        end
+      end
+
+    quote do
+      right = unquote(right)
+      unquote({ :case, [{:export_head,true}|meta], args })
+    end
   end
 
-  defp translate_assertion({ :>, _, [left, right] }) do
-    assert_operator :>, left, right, "be more than"
+
+  defmacro refute(assertion) do
+    case translate_assertion({ :!, [], [assertion] }) do
+      nil ->
+        # Default message in case no transform was performed
+        quote do
+          value = unquote(assertion)
+
+          if value do
+            raise ExUnit.AssertionError,
+              expr:  unquote(Macro.to_string(assertion)),
+              message: "#{inspect value} should be false or nil"
+          end
+
+          value
+        end
+
+      value -> 
+        { :!, [], [value] }
+    end
   end
 
-  defp translate_assertion({ :<=, _, [left, right] }) do
-    assert_operator :<=, left, right, "be less than or equal to"
+  ## START HELPERS
+
+
+
+  defp translate_assertion(expr = { operator, _, [left, right] }) 
+  when operator in [ :==, :<, :>, :<=, :>=, :===, :=~, :!==, :!= ]  do
+    assert_operator operator, left, right, expr
   end
 
-  defp translate_assertion({ :>=, _, [left, right] }) do
-    assert_operator :>=, left, right, "be more than or equal to"
-  end
 
-  defp translate_assertion({ :===, _, [left, right] }) do
-    assert_operator :===, left, right, "be equal to (===)"
-  end
-
-  defp translate_assertion({ :!==, _, [left, right] }) do
-    assert_operator :!==, left, right, "be not equal to (!==)"
-  end
-
-  defp translate_assertion({ :!=, _, [left, right] }) do
-    assert_operator :!=, left, right, "be not equal to (!=)"
-  end
-
-  defp translate_assertion({ :=~, _, [left, right] }) do
-    assert_operator :=~, left, right, "match (=~)"
-  end
-
-  defp translate_assertion({ :in, _, [left, right] }) do
+  defp translate_assertion(expr = { :in, _, [left, right] }) do
+    code = Macro.to_string(expr)
     quote do
       left  = unquote(left)
       right = unquote(right)
-      assert Enum.member?(right, left), left, right, assertion: "be in"
+      assert_internal Enum.member?(right, left), 
+                           left: left, right: right, expr: unquote(code)
     end
   end
 
@@ -177,7 +184,7 @@ defmodule ExUnit.Assertions do
       right = unquote(right)
       case right do
         unquote(left) ->
-          raise ExUnit.ExpectationError,
+          raise ExUnit.AssertionError,
             expected: inspect(right),
             actual: unquote(Macro.to_string(left)),
             assertion: "match pattern (=)",
@@ -188,20 +195,20 @@ defmodule ExUnit.Assertions do
     end
   end
 
-  defp translate_assertion({ :!, _, [{ :=~, _, [left, right] }] }) do
+  defp translate_assertion(expr = { negation, _, [{ :in, _, [left, right] }] }) 
+  when negation in [:!, :not] do
+    code = Macro.to_string(expr)
     quote do
       left  = unquote(left)
       right = unquote(right)
-      assert !(left =~ right), left, right, assertion: "match (=~)", negation: true
+      assert_internal !Enum.member?(right, left), 
+                           left: left, right: right, expr: unquote(code)
     end
   end
 
-  defp translate_assertion({ negation, _, [{ :in, _, [left, right] }] }) when negation in [:!, :not] do
-    quote do
-      left  = unquote(left)
-      right = unquote(right)
-      assert !Enum.member?(right, left), left, right, assertion: "be in", negation: true
-    end
+  defp translate_assertion(expr = {:!, [], [{ operator, _, [left, right] }]}) 
+  when operator in [ :==, :<, :>, :<=, :>=, :===, :=~, :!==, :!= ]  do
+    refute_operator operator, left, right, expr
   end
 
   ## Fallback
@@ -210,47 +217,63 @@ defmodule ExUnit.Assertions do
     nil
   end
 
-  defp assert_operator(operator, expected, actual, text) do
-    quote do
-      left  = unquote(expected)
-      right = unquote(actual)
-      assert unquote(operator)(left, right), left, right, unquote(text)
+  defp assert_operator(operator, left, right, expr) do
+    expr = Macro.to_string(expr)
+    quote location: :keep do
+      left  = unquote(left)
+      right = unquote(right)
+      assert_internal unquote(operator)(left, right), left: left, right: right, expr: unquote(expr), operator: to_string(unquote(operator))
     end
   end
 
+  defp refute_operator(operator, left, right, expr) do
+    expr = Macro.to_string(expr)
+    quote location: :keep do
+      left  = unquote(left)
+      right = unquote(right)
+      assert_internal not(unquote(operator)(left, right)), left: left, right: right, expr: unquote(expr), operator: to_string(unquote(operator))
+    end
+  end
+
+  def assert_internal(successful, opts) do
+    unless successful, do: raise(ExUnit.AssertionError, opts)
+    true
+  end
   ## END HELPERS
 
   @doc """
-  Asserts the `expected` value is true.
-  If it fails, raises the given `message`.
+  Asserts `value` is true, displaying the given `message` otherwise.
 
   ## Examples
 
       assert false, "it will never be true"
 
   """
-  def assert(expected, message) when is_binary(message) do
-    unless expected, do: raise(ExUnit.AssertionError, message: message)
-    true
+  def assert(value, message) when is_binary(message) do
+    assert_internal(value, message: message, expr: ExUnit.NoValueSupplied.no_value)
   end
 
   @doc """
-  Asserts the `expected` value is true.
+  Asserts `value` is true.
   If it fails, it raises an expectation error
-  using the given `expected` and `actual` values.
+  using the given `left` and `right` values.
+
+  You probably don't need to use this—the regular `assert` function
+  handles this for you.
 
   ## Examples
 
       assert this > that, this, that, "more than"
 
   """
-  def assert(value, expected, actual, content) when is_binary(content) do
-    assert(value, expected, actual, assertion: content)
+  def assert(value, left, right, message) when is_binary(message) do
+    assert_internal(value, left: left, right: right, 
+                    message: message, expr: ExUnit.NoValueSupplied.no_value)
   end
 
   def assert(value, expected, actual, opts) do
     unless value do
-      raise ExUnit.ExpectationError,
+      raise ExUnit.AssertionError,
         Keyword.merge([expected: inspect(expected), actual: inspect(actual)], opts)
     end
     true
@@ -260,7 +283,7 @@ defmodule ExUnit.Assertions do
   Asserts a message was or is going to be received. Unlike
   `assert_received`, it has a default timeout of 100 milliseconds.
 
-  The given `expected` argument has to be a pattern.
+  The `expected` argument is a pattern.
 
   ## Examples
 
@@ -286,7 +309,7 @@ defmodule ExUnit.Assertions do
   Asserts a message was received and is in the current process' mailbox.
   Timeout is set to 0, so there is no waiting time.
 
-  The given `expected` argument has to be a pattern.
+  The `expected` argument is a pattern.
 
   ## Examples
 
@@ -312,7 +335,7 @@ defmodule ExUnit.Assertions do
           unquote(expected) = received -> received
         after
           unquote(timeout) ->
-            flunk unquote(message) || "Expected to have received message matching #{unquote binary}"
+            flunk unquote(message) || "No message matching #{unquote binary}"
         end
       end
 
@@ -321,7 +344,8 @@ defmodule ExUnit.Assertions do
 
   @doc """
   Asserts the `exception` is raised during `function` execution with
-  the `expected_message`. Returns the rescued exception, fails otherwise.
+  the `expected_message`. Returns the rescued exception, fails otherwise. 
+  The expected record can be a string or a regular expression.
 
   ## Examples
 
@@ -337,8 +361,8 @@ defmodule ExUnit.Assertions do
       Regex.regex?(message) -> error.message =~ message
     end
 
-    assert is_match, message, error.message,
-      prelude: "Expected error message", assertion: "match"
+    msg = "Wrong message for #{inspect exception}. Expected #{inspect message}, got #{inspect error.message}"
+    assert_internal is_match, message: msg, expr:  ExUnit.NoValueSupplied.no_value
 
     error
   end
@@ -357,22 +381,23 @@ defmodule ExUnit.Assertions do
   def assert_raise(exception, function) when is_function(function) do
     try do
       function.()
-      flunk "Expected #{inspect exception} exception but nothing was raised"
+      flunk "Expected exception '#{inspect exception}' but nothing was raised"
     rescue
       error in [exception] -> error
       error ->
         name = error.__record__(:name)
 
-        if name in [ExUnit.AssertionError, ExUnit.ExpectationError] do
+        if name in [ExUnit.AssertionError, ExUnit.AssertionError] do
           raise(error)
         else
-          flunk "Expected exception #{inspect exception}, got #{inspect name} (#{error.message})"
+          flunk "Expected exception '#{inspect exception}' but got #{inspect name}(#{error.message})"
         end
     end
   end
 
   @doc """
-  Asserts the `expected` and `received` are within `delta`.
+  Asserts that `val1` and `val2` differ by no more than `delta`.
+
 
   ## Examples
 
@@ -380,15 +405,16 @@ defmodule ExUnit.Assertions do
       assert_in_delta 10, 15, 4
 
   """
-  def assert_in_delta(expected, received, delta, message \\ nil) do
-    diff = abs(expected - received)
+  def assert_in_delta(val1, val2, delta, message \\ nil) do
+    diff = abs(val1 - val2)
     message = message ||
-      "Expected |#{inspect expected} - #{inspect received}| (#{inspect diff}) to be < #{inspect delta}"
+      "Expected the difference between #{inspect val1} and " <>
+      "#{inspect val2} (#{inspect diff}) to be less than #{inspect delta}"
     assert diff < delta, message
   end
 
   @doc """
-  Asserts the given `expression` will throw a value.
+  Asserts `expression` will throw a value.
   Returns the thrown value or fails otherwise.
 
   ## Examples
@@ -401,7 +427,7 @@ defmodule ExUnit.Assertions do
   end
 
   @doc """
-  Asserts the given `expression` will exit.
+  Asserts `expression` will exit.
   Returns the exit status/message or fails otherwise.
 
   ## Examples
@@ -414,7 +440,7 @@ defmodule ExUnit.Assertions do
   end
 
   @doc """
-  Asserts the given `expression` will cause an error.
+  Asserts `expression` will cause an error.
   Returns the error or fails otherwise.
 
   ## Examples
@@ -432,7 +458,7 @@ defmodule ExUnit.Assertions do
         unquote(expr)
         flunk "Expected to catch #{unquote(kind)}, got nothing"
       rescue
-        e in [ExUnit.AssertionError, ExUnit.ExpectationError] -> raise(e)
+        e in [ExUnit.AssertionError, ExUnit.AssertionError] -> raise(e)
       catch
         unquote(kind), what_we_got -> what_we_got
       end
@@ -440,23 +466,24 @@ defmodule ExUnit.Assertions do
   end
 
   @doc """
-  Asserts the `not_expected` value is `nil` or `false`.
-  In case it is a truthy value, raises the given message.
+  Asserts `value` is `nil` or `false` (that is, `value` is not truthy).
 
   ## Examples
 
       refute true, "This will obviously fail"
 
   """
-  def refute(not_expected, message) do
-    not assert(!not_expected, message)
+  def refute(value, message) do
+    not assert(!value, message)
   end
 
   @doc """
-  Asserts a message was not received and won't be within
+      refute_receive message, timeout \\ 100, message \\ nil
+
+  Asserts `message` was not received (and won't be received) within
   the `timeout` period.
 
-  The `not_expected` argument must be a match pattern.
+  The `not_expected` argument is a match pattern.
 
   ## Examples
 
@@ -493,7 +520,7 @@ defmodule ExUnit.Assertions do
     quote do
       receive do
         unquote(not_expected) = actual ->
-          flunk unquote(message) || "Expected to not have received message matching #{unquote binary}, got #{inspect actual}"
+          flunk unquote(message) || "Unexpectedly received message #{inspect actual} (which matched #{unquote binary})"
       after
         unquote(timeout) -> false
       end
@@ -501,7 +528,10 @@ defmodule ExUnit.Assertions do
   end
 
   @doc """
-  Asserts the `expected` and `received` are not within `delta`.
+  Asserts `val1` and `val2` are not within `delta`.
+
+  If you supply `message`, information about the values will
+  automatically be appended to it.
 
   ## Examples
 
@@ -509,10 +539,15 @@ defmodule ExUnit.Assertions do
       refute_in_delta 10, 11, 2
 
   """
-  def refute_in_delta(expected, received, delta, message \\ nil) do
-    diff = abs(expected - received)
-    message = message ||
-      "Expected |#{inspect expected} - #{inspect received}| (#{inspect diff}) to not be < #{inspect delta}"
+  def refute_in_delta(val1, val2, delta, message \\ nil) do
+    diff = abs(val1 - val2)
+    message = if message do
+      message <> " (difference between #{inspect val1} " <>
+      "and #{inspect val2} is less than #{inspect delta})"
+    else
+      "Expected the difference between #{inspect val1} and " <>
+      "#{inspect val2} (#{inspect diff}) to be more than #{inspect delta}"
+    end
     refute diff < delta, message
   end
 
@@ -527,6 +562,6 @@ defmodule ExUnit.Assertions do
   @spec flunk :: no_return
   @spec flunk(String.t) :: no_return
   def flunk(message \\ "Flunked!") do
-    raise ExUnit.AssertionError, message: message
+    assert_internal false, message: message, expr:  ExUnit.NoValueSupplied.no_value
   end
 end
