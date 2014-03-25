8001ddcadcf1ef302f8b99d2d285c1a654159c3a
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-03-25 08:47:22 -0500

    Add from_hex

--- lib/elixir/lib/digest.ex
+++ lib/elixir/lib/digest.ex
@@ -2,7 +2,6 @@ defmodule Digest do
   @moduledoc """
   This module provides the framework for message digest libraries.
   """
-  use Bitwise
 
   @doc """
   Hash a string using the MD5 hash algorithm.
@@ -36,8 +35,34 @@ defmodule Digest do
     bc <<x :: size(4), y :: size(4)>> inbits string do <<hex_char(x), hex_char(y)>> end |> String.upcase
   end
 
+  @doc """
+  Converts a hexadecimal-encoded string to it's unencoded representation.
+
+  ## Examples
+
+    iex> Digest.from_hex("74657374")
+    "test"
+    iex> Digest.from_hex("C3A6C39F")
+    "æß"
+  """
+  @spec from_hex(String.t) :: String.t | no_return
+  def from_hex(""), do: ""
+  def from_hex(string) when is_binary(string) do
+    unhex_binary(string, <<>>)
+  end
+
+
   # Get the hex character for a given character within the range 0-16
   defp hex_char(n) when n < 10, do: ?0 + n
   defp hex_char(n) when n < 16, do: ?a + (n - 10)
+  # Get the integer value of a hex character in the range 0-F
+  defp unhex_char(c) when c in ?0..?9, do: c - ?0
+  defp unhex_char(c) when c in ?A..?F, do: (c - ?A) + 10
+  defp unhex_char(c) when c in ?a..?f, do: (c - ?a) + 10
+  # Recursively convert a hexadecimal string to it's unencoded representation
+  defp unhex_binary(<<>>, acc), do: acc
+  defp unhex_binary(<<c1 :: utf8, c2 :: utf8, rest :: binary>>, acc) do
+    unhex_binary(rest, <<acc :: binary, unhex_char(c1) :: 4, unhex_char(c2) :: 4>>)
+  end
 
 end
