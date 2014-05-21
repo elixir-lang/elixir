ce9b381f67ec7b5d62715dca9c8930ccd0e7a4ec
José Valim <jose.valim@plataformatec.com.br>
2014-05-21 10:47:37 +0200

    Unify mix run tests

--- lib/mix/test/mix/tasks/run_test.exs
+++ lib/mix/test/mix/tasks/run_test.exs
@@ -21,11 +21,11 @@ defmodule Mix.Tasks.RunTest do
     git_repo = fixture_path("git_repo/lib/git_repo.ex")
 
     in_fixture "no_mixfile", fn ->
-      Mix.Tasks.Run.run ["-r", git_repo, "-e", "Mix.shell.info GitRepo.hello"]
-      assert_received {:mix_shell, :info, ["World"]}
+      Mix.Tasks.Run.run ["-r", git_repo, "-e", "send self, {:hello, GitRepo.hello}"]
+      assert_received {:hello, "World"}
 
-      Mix.Tasks.Run.run ["-pr", git_repo, "-e", "Mix.shell.info GitRepo.hello"]
-      assert_received {:mix_shell, :info, ["World"]}
+      Mix.Tasks.Run.run ["-pr", git_repo, "-e", "send self, {:hello, GitRepo.hello}"]
+      assert_received {:hello, "World"}
     end
   after
     purge [GitRepo]
@@ -33,30 +33,32 @@ defmodule Mix.Tasks.RunTest do
 
   test "run rewrites System.argv" do
     in_fixture "no_mixfile", fn ->
-      File.write! file = "argv.exs", "send self, {:system_argv, System.argv}"
+      file = "argv.exs"
+
+      File.write! file, "send self, {:argv, System.argv}"
       unload_file = fn ->
         Code.unload_files [Path.expand(file)]
       end
 
       Mix.Tasks.Run.run [file]
-      assert_received {:system_argv, []}
+      assert_received {:argv, []}
 
       unload_file.()
       Mix.Tasks.Run.run [file, "foo", "-e", "bar"]
-      assert_received {:system_argv, ["foo", "-e", "bar"]}
+      assert_received {:argv, ["foo", "-e", "bar"]}
 
       unload_file.()
-      Mix.Tasks.Run.run ["-e", "send self, {:system_argv, System.argv}", file, "foo", "-x", "bar"]
-      assert_received {:system_argv, [^file, "foo", "-x", "bar"]}
+      Mix.Tasks.Run.run ["-e", "send self, {:argv, System.argv}", file, "foo", "-x", "bar"]
+      assert_received {:argv, [^file, "foo", "-x", "bar"]}
 
       unload_file.()
       Mix.Tasks.Run.run [
         "-e", "send self, :evaled",
-        "-e", "send self, {:system_argv, System.argv}",
+        "-e", "send self, {:argv, System.argv}",
         "--no-compile", file, "-x", "bar"
       ]
       assert_received :evaled
-      assert_received {:system_argv, [^file, "-x", "bar"]}
+      assert_received {:argv, [^file, "-x", "bar"]}
     end
   end
 end
