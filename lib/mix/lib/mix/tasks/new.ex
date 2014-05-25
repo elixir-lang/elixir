b4c0826467be917043abd1ee635be1c10e599755
José Valim <jose.valim@plataformatec.com.br>
2014-05-25 12:41:57 +0200

    Update repo links

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -296,7 +296,7 @@ defmodule Mix.Tasks.New do
   defmodule <%= @mod %> do
     use Application
 
-    # See http://elixir-lang.org/docs/stable/Application.html
+    # See http://elixir-lang.org/docs/stable/elixir/Application.html
     # for more information on OTP Applications
     def start(_type, _args) do
       import Supervisor.Spec, warn: false
@@ -306,7 +306,7 @@ defmodule Mix.Tasks.New do
         # worker(<%= @mod %>.Worker, [arg1, arg2, arg3])
       ]
 
-      # See http://elixir-lang.org/docs/stable/Supervisor.html
+      # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
       # for other strategies and supported options
       opts = [strategy: :one_for_one, name: <%= @mod %>.Supervisor]
       Supervisor.start_link(children, opts)
