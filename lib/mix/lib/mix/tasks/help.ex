b7897b224dd24eb76ec4138e5185623123c35d31
Paul <pwoolcoc@gmail.com>
2014-05-29 13:38:15 -0400

    enable passing --names to `mix help` to only display command names

--- lib/mix/lib/mix/tasks/help.ex
+++ lib/mix/lib/mix/tasks/help.ex
@@ -58,6 +58,14 @@ defmodule Mix.Tasks.Help do
     display_iex_task_doc(max)
   end
 
+  def run(["--names"]) do
+    Mix.Task.load_all
+    tasks = for module <- Enum.sort(Mix.Task.all_modules), do: Mix.Task.task_name(module)
+    Enum.each tasks, fn task ->
+      Mix.shell.info "#{task}"
+    end
+  end
+
   def run([task]) do
     module = Mix.Task.get!(task)
     doc    = Mix.Task.moduledoc(module) || "There is no documentation for this task"
