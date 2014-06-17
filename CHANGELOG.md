38aa5c57e56eb1c8b05487a6983dd0352059869c
José Valim <jose.valim@plataformatec.com.br>
2014-06-17 16:56:27 +0200

    Add Node.start/3 and Node.stop/0, closes #2406

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 
 * Enhancements
   * [ExUnit] The test process now exits with `:shutdown` reason
+  * [Node] Add `Node.start/3` and `Node.stop/0`
   * [Task] Log when tasks crash
 
 * Bug fixes
---
+++ lib/elixir/test/elixir/node_test.exs
@@ -0,0 +1,11 @@
+Code.require_file "test_helper.exs", __DIR__
+
+defmodule NodeTest do
+  use ExUnit.Case
+
+  test "start/3 and stop/0" do
+    assert Node.stop == {:error, :not_found}
+    assert {:ok, _} = Node.start(:hello, :shortnames, 15000)
+    assert Node.stop() == :ok
+  end
+end
