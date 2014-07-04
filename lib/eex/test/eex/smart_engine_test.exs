d015ad5f2d3bd51817bbb7f0ec9233236ee277c2
José Valim <jose.valim@plataformatec.com.br>
2014-07-04 11:54:07 +0200

    Ensure line numbers are properly preserved in EEx, closes #2485

--- lib/eex/test/eex/smart_engine_test.exs
+++ lib/eex/test/eex/smart_engine_test.exs
@@ -19,6 +19,16 @@ defmodule EEx.SmartEngineTest do
     assert_eval "1\n2\n3\n", "<%= for x <- [1, 2, 3] do %><%= x %>\n<% end %>"
   end
 
+  test "compiled preserved line numbers" do
+    result = EEx.compile_string("<%= @hello %>", engine: EEx.SmartEngine)
+    Macro.prewalk(result, fn
+      {_left, meta, _right} ->
+        assert Keyword.get(meta, :line, 0) in [0, 1]
+      _ ->
+        :ok
+    end)
+  end
+
   defp assert_eval(expected, actual, binding \\ []) do
     result = EEx.eval_string(actual, binding, file: __ENV__.file)
     assert result == expected
