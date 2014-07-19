6538f33825a3aae309837935e78d267027d6ffc5
Thomaz Leite <i@thomazleite.com>
2014-07-19 13:26:53 -0300

    Bring back original tests for IO.ANSI
    To avoid conflict and preserve compatibilty, the new tests use the
    functions `IO.ANSI.format/2` and `IO.ANSI.format_fragment/2`.

--- lib/elixir/test/elixir/io/ansi_test.exs
+++ lib/elixir/test/elixir/io/ansi_test.exs
@@ -3,70 +3,119 @@ Code.require_file "../test_helper.exs", __DIR__
 defmodule IO.ANSITest do
   use ExUnit.Case, async: true
 
-  test :escape_ansicode do
-    assert IO.ANSI.escape(:green, true) ==
+  test :format_ansicode do
+    assert IO.ANSI.format(:green, true) ==
            "#{IO.ANSI.green}#{IO.ANSI.reset}"
-    assert IO.ANSI.escape(:green, false) ==
+    assert IO.ANSI.format(:green, false) ==
            ""
   end
 
-  test :escape_binary do
-    assert IO.ANSI.escape("Hello, world!", true) ==
+  test :format_binary do
+    assert IO.ANSI.format("Hello, world!", true) ==
            "Hello, world!"
-    assert IO.ANSI.escape("This is a map: %{foo: :bar}", false) ==
+    assert IO.ANSI.format("This is a map: %{foo: :bar}", false) ==
            "This is a map: %{foo: :bar}"
   end
 
-  test :escape_empty_list do
-    assert IO.ANSI.escape([], true) ==
+  test :format_empty_list do
+    assert IO.ANSI.format([], true) ==
            ""
-    assert IO.ANSI.escape([], false) ==
+    assert IO.ANSI.format([], false) ==
            ""
   end
 
-  test :escape_ansicode_list do
-    assert IO.ANSI.escape([:red, :bright], true) ==
+  test :format_ansicode_list do
+    assert IO.ANSI.format([:red, :bright], true) ==
            "#{IO.ANSI.red}#{IO.ANSI.bright}#{IO.ANSI.reset}"
-    assert IO.ANSI.escape([:red, :bright], false) ==
+    assert IO.ANSI.format([:red, :bright], false) ==
            ""
   end
 
-  test :escape_binary_list do
-    assert IO.ANSI.escape(["Hello, ", "world!"], true) ==
+  test :format_binary_list do
+    assert IO.ANSI.format(["Hello, ", "world!"], true) ==
            "Hello, world!"
-    assert IO.ANSI.escape(["Hello, ", "world!"], false) ==
-           "Hello, world!"
-  end
-
-  test :escape_char_list do
-    assert IO.ANSI.escape('Hello, world!', true) ==
-           "Hello, world!"
-    assert IO.ANSI.escape('Hello, world!', false) ==
+    assert IO.ANSI.format(["Hello, ", "world!"], false) ==
            "Hello, world!"
   end
 
-  test :escape_mixed_list do
-    assert IO.ANSI.escape(["Hello", ?,, 32, :red, "world!"], true) ==
+  test :format_char_list do
+    assert IO.ANSI.format('Hello, world!', true) ==
+           "Hello, world!"
+    assert IO.ANSI.format('Hello, world!', false) ==
+           "Hello, world!"
+  end
+
+  test :format_mixed_list do
+    assert IO.ANSI.format(["Hello", ?,, 32, :red, "world!"], true) ==
            "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
-    assert IO.ANSI.escape(["Hello", ?,, 32, :red, "world!"], false) ==
+    assert IO.ANSI.format(["Hello", ?,, 32, :red, "world!"], false) ==
            "Hello, world!"
   end
 
-  test :escape_nested_list do
-    assert IO.ANSI.escape(["Hello, ", ["nested", 32, :red, "world!"]], true) ==
+  test :format_nested_list do
+    assert IO.ANSI.format(["Hello, ", ["nested", 32, :red, "world!"]], true) ==
            "Hello, nested #{IO.ANSI.red}world!#{IO.ANSI.reset}"
-    assert IO.ANSI.escape(["Hello, ", ["nested", 32, :red, "world!"]], false) ==
+    assert IO.ANSI.format(["Hello, ", ["nested", 32, :red, "world!"]], false) ==
            "Hello, nested world!"
   end
 
-  test :escape_fragment do
-    assert IO.ANSI.escape_fragment([:red, "Hello!"], true) ==
+  test :format_fragment do
+    assert IO.ANSI.format_fragment([:red, "Hello!"], true) ==
            "#{IO.ANSI.red}Hello!"
   end
 
-  test :invalid_sequence do
+  test :format_invalid_sequence do
     assert_raise ArgumentError, "invalid ANSI sequence specification: brigh", fn ->
-      IO.ANSI.escape([:brigh, "Hello!"], true)
+      IO.ANSI.format([:brigh, "Hello!"], true)
+    end
+  end
+
+  test :escape_single do
+    assert IO.ANSI.escape("Hello, %{red}world!", true) ==
+           "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello, %{red}world!", true) ==
+           "Hello, #{IO.ANSI.red}world!#{IO.ANSI.reset}"
+  end
+
+  test :escape_non_attribute do
+    assert IO.ANSI.escape("Hello %{clear}world!", true) ==
+           "Hello #{IO.ANSI.clear}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello %{home}world!", true) ==
+           "Hello #{IO.ANSI.home}world!#{IO.ANSI.reset}"
+  end
+
+  test :escape_multiple do
+    assert IO.ANSI.escape("Hello, %{red,bright}world!", true) ==
+           "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello, %{red, bright}world!", true) ==
+           "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
+    assert IO.ANSI.escape("Hello, %{red , bright}world!", true) ==
+           "Hello, #{IO.ANSI.red}#{IO.ANSI.bright}world!#{IO.ANSI.reset}"
+  end
+
+  test :escape_no_emit do
+    assert IO.ANSI.escape("Hello, %{}world!", false) ==
+           "Hello, world!"
+
+    assert IO.ANSI.escape("Hello, %{red,bright}world!", false) ==
+           "Hello, world!"
+  end
+
+  test :escape_fragment do
+    assert IO.ANSI.escape("%{red}", true) == "#{IO.ANSI.red}#{IO.ANSI.reset}"
+    assert IO.ANSI.escape_fragment("", true) == ""
+  end
+
+  test :escape_noop do
+    assert IO.ANSI.escape("") == ""
+  end
+
+  test :escape_invalid do
+    assert_raise ArgumentError, "invalid ANSI sequence specification: brigh", fn ->
+      IO.ANSI.escape("%{brigh}, yes")
+    end
+    assert_raise ArgumentError, "invalid ANSI sequence specification: brigh", fn ->
+      IO.ANSI.escape("%{brigh,red}, yes")
     end
   end
 end
