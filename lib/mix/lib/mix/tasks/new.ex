6c604697d8de1b38baeae6798083fe47de2ac276
José Valim <jose.valim@plataformatec.com.br>
2014-07-20 11:06:12 +0200

    Improve config.exs docs

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -269,13 +269,14 @@ defmodule Mix.Tasks.New do
 
   embed_template :config, ~S"""
   # This file is responsible for configuring your application
-  # and its dependencies. The Mix.Config module provides functions
-  # to aid in doing so.
+  # and its dependencies with the aid of the Mix.Config module.
   use Mix.Config
 
-  # Note this file is loaded before any dependency and is restricted
+  # This configuration is loaded before any dependency and is restricted
   # to this project. If another project depends on this project, this
-  # file won't be loaded nor affect the parent project.
+  # file won't be loaded nor affect the parent project. For this reason,
+  # if you want to provide default values for your application, it should
+  # be done in your mix.exs file.
 
   # Sample configuration:
   #
@@ -296,7 +297,7 @@ defmodule Mix.Tasks.New do
 
   # Finally, note that configuration defined in children projects
   # inside apps/ are not automatically available to the umbrella parent.
-  # They can, however, be easily imported as well:
+  # They can, however, be easily imported:
   #
   #     import_config "../apps/foo/config/config.exs"
   #     import_config "../apps/bar/config/config.exs"
