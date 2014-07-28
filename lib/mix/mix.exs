957e389fe78de6ac0dbe3db47206fa7a32ded6aa
Martin Schürrer <martin@schuerrer.org>
2014-07-27 22:45:40 -0500

    Update mix's mix.exs to fix escript.build

--- lib/mix/mix.exs
+++ lib/mix/mix.exs
@@ -5,7 +5,7 @@ defmodule Mix.Mixfile do
     [app: :mix,
      build_per_environment: false,
      version: System.version,
-     escript_main_module: Mix.CLI]
+     escript: [main_module: Mix.CLI]]
   end
 
   def application do
