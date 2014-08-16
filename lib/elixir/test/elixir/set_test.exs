dd788b3db8e43735a9592a27ec6b318690d7013e
José Valim <jose.valim@plataformatec.com.br>
2014-08-16 15:46:56 +0200

    Deprecate Collectable.empty and Enum.traverse
    The current implementation has three issues:
    
    1. It is slow as we can't reuse the data structure but rather
       build a new one from scratch
    
    2. Due to 1., trees can't be traversed
    
    3. empty/1 is unable to deal with resources

--- lib/elixir/test/elixir/set_test.exs
+++ lib/elixir/test/elixir/set_test.exs
@@ -158,7 +158,6 @@ defmodule SetTest.Common do
       test "is collectable" do
         assert Set.equal?(new_set([1, 1, 2, 3, 3, 3]), new_set([1, 2, 3]))
         assert Set.equal?(new_set([1, 1, 2, 3, 3, 3], &(&1 * 2)), new_set([2, 4, 6]))
-        assert Collectable.empty(new_set([1, 2, 3])) == new_set
       end
 
       test "is zippable" do
