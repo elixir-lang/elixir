eee921bf7b2c14e6c76b9d8793224e6143ab903a
Paulo Almeida <paulo.almeida@timeboxed.co.ao>
2014-10-06 19:07:32 +0100

    Support mix help --search PATTERN
    Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

--- lib/mix/lib/mix/tasks/help.ex
+++ lib/mix/lib/mix/tasks/help.ex
@@ -8,10 +8,11 @@ defmodule Mix.Tasks.Help do
 
   ## Arguments
 
-      mix help         - prints all tasks and their shortdoc
-      mix help TASK    - prints full docs for the given task
-      mix help --names - prints all task names and aliases
-                         (useful for autocompleting)
+      mix help                  - prints all tasks and their shortdoc
+      mix help TASK             - prints full docs for the given task
+      mix help --search PATTERN - prints all tasks that contain PATTERN in the name
+      mix help --names          - prints all task names and aliases
+                                  (useful for autocompleting)
 
   ## Colors
 
@@ -40,24 +41,12 @@ defmodule Mix.Tasks.Help do
   def run([]) do
     loadpaths!
 
-    shell   = Mix.shell
     modules = Mix.Task.load_all()
 
-    docs = for module <- modules,
-               doc = Mix.Task.shortdoc(module) do
-      {"mix " <> Mix.Task.task_name(module), doc}
-    end
-
-    max = Enum.reduce docs, 0, fn({task, _}, acc) ->
-      max(byte_size(task), acc)
-    end
+    {docs, max} = build_task_doc_list(modules)
 
     display_default_task_doc(max)
-
-    Enum.each Enum.sort(docs), fn({task, doc}) ->
-      shell.info format_task(task, max, doc)
-    end
-
+    display_task_doc_list(docs, max)
     display_iex_task_doc(max)
   end
 
@@ -76,6 +65,22 @@ defmodule Mix.Tasks.Help do
     for info <- Enum.sort(aliases ++ tasks) do
       Mix.shell.info info
     end
+  end
+
+  def run(["--search", pattern]) do
+    loadpaths!
+
+    modules =
+      Mix.Task.load_all()
+      |> Enum.filter(&(String.contains?(Mix.Task.task_name(&1), pattern)))
+
+    {docs, max} = build_task_doc_list(modules)
+
+    display_task_doc_list(docs, max)
+  end
+
+  def run(["--search"]) do
+    Mix.raise "Unexpected arguments, expected `mix help --search PATTERN`"
   end
 
   def run([task]) do
@@ -146,5 +151,23 @@ defmodule Mix.Tasks.Help do
   defp display_iex_task_doc(max) do
     Mix.shell.info format_task("iex -S mix", max,
                     "Start IEx and run the default task")
+  end
+
+  defp display_task_doc_list(docs, max) do
+    Enum.each Enum.sort(docs), fn({task, doc}) ->
+      Mix.shell.info format_task(task, max, doc)
+    end
+  end
+
+  defp build_task_doc_list(modules) do
+    Enum.reduce modules, {[], 0}, fn module, {docs, max} ->
+      doc = Mix.Task.shortdoc(module)
+      if doc do
+        task = "mix " <> Mix.Task.task_name(module)
+        docs = [{task, doc} | docs]
+        max  = max(byte_size(task), max)
+      end
+      {docs, max}
+    end
   end
 end
