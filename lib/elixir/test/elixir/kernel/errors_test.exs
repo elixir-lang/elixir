7e645cea3f7fc62918e4bf0ad0f7aebe55279828
José Valim <jose.valim@plataformatec.com.br>
2015-01-15 15:19:20 +0100

    Do not repeat the module name

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -420,7 +420,7 @@ defmodule Kernel.ErrorsTest do
 
   test :macro_with_undefined_local do
     assert_compile_fail UndefinedFunctionError,
-      "undefined function: ErrorsTest.unknown/1 (function unknown/1 is not available from ErrorsTest)",
+      "undefined function: ErrorsTest.unknown/1 (function unknown/1 is not available)",
       '''
       defmodule ErrorsTest do
         defmacrop bar, do: unknown(1)
@@ -431,7 +431,7 @@ defmodule Kernel.ErrorsTest do
 
   test :private_macro do
     assert_compile_fail UndefinedFunctionError,
-      "undefined function: ErrorsTest.foo/0 (function foo/0 is not available from ErrorsTest)",
+      "undefined function: ErrorsTest.foo/0 (function foo/0 is not available)",
       '''
       defmodule ErrorsTest do
         defmacrop foo, do: 1
