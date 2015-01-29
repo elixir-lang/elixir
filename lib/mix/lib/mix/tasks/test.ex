529af51417177a1356a1b81f19c1f25d5901c760
Aleksei Magusev <lexmag@me.com>
2015-01-29 13:12:27 +0100

    Suppress warning in `Mix.Tasks.Test`

--- lib/mix/lib/mix/tasks/test.ex
+++ lib/mix/lib/mix/tasks/test.ex
@@ -193,7 +193,7 @@ defmodule Mix.Tasks.Test do
     System.at_exit fn _ ->
       # Make sure all messages the tests might have sent to the
       # Logger are printed before we shut down the VM.
-      if pid = Process.whereis(Logger), do: Logger.flush()
+      if Process.whereis(Logger), do: Logger.flush()
       if failures > 0, do: exit({:shutdown, 1})
     end
   end
