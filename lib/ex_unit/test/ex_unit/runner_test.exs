bfe86e5899e61b45677b6d3ada0bac248ed3acd9
Lauro Caetano <laurocaetano1@gmail.com>
2015-02-06 19:55:00 -0200

    Refactor the runner configuration to make it testable

---
+++ lib/ex_unit/test/ex_unit/runner_test.exs
@@ -0,0 +1,23 @@
+Code.require_file "../test_helper.exs", __DIR__
+
+defmodule ExUnit.RunnerTest do
+  use ExUnit.Case
+
+  test "it set the timeout to infinity and max cases to one with trace enabled" do
+    opts = Keyword.merge(ExUnit.configuration, trace: true)
+
+    {_, config} = ExUnit.Runner.configure(opts)
+
+    assert config[:timeout] == :infinity
+    assert config[:max_cases] == 1
+  end
+
+  test "it does not set timeout to infinity and the max cases to 1 with trace disabled" do
+    opts = Keyword.merge(ExUnit.configuration, trace: false)
+
+    {_, config} = ExUnit.Runner.configure(opts)
+
+    assert config[:max_cases] != 1
+    assert config[:timeout] == 30_000
+  end
+end
