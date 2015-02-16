d996a7bf1aed75f5b4a968ceb153ba09ee891c75
José Valim <jose.valim@plataformatec.com.br>
2015-02-16 16:53:23 +0100

    Support R18 standard error

--- lib/elixir/src/elixir.erl
+++ lib/elixir/src/elixir.erl
@@ -29,9 +29,12 @@ start(_Type, _Args) ->
     end,
 
   ok = io:setopts(standard_io, Opts),
+
   %% Must use undocument {unicode, true} to set unicode on standard_error, more
   %% info: http://erlang.org/pipermail/erlang-bugs/2014-April/004310.html
-  ok = io:setopts(standard_error, [{unicode,true}]),
+  _ = io:setopts(standard_error, [{unicode,true}]),  %% R17
+  _ = io:setopts(standard_error, [{encoding,utf8}]), %% R18
+
   case file:native_name_encoding() of
     latin1 ->
       io:format(standard_error,
