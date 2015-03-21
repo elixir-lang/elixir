da4f02e4a4f7d36491e0030571a678bcf0804508
Aleksei Magusev <lexmag@me.com>
2015-03-21 23:33:21 +0100

    Make IEx `b` helper to accept single module argument

--- lib/iex/test/iex/helpers_test.exs
+++ lib/iex/test/iex/helpers_test.exs
@@ -49,10 +49,19 @@ defmodule IEx.HelpersTest do
     assert capture_io(fn -> h __info__ end) == "No documentation for __info__ was found\n"
   end
 
-  test "b helper" do
+  test "b helper module" do
+    assert capture_io(fn -> b Mix end) == "No callbacks for Mix were found\n"
+    assert capture_io(fn -> b NoMix end) == "Could not load module NoMix, got: nofile\n"
+    assert capture_io(fn -> b Mix.SCM end) =~ """
+           defcallback accepts_options(app :: atom(), opts()) :: opts() | nil
+           defcallback checked_out?(opts()) :: boolean()
+      """
+  end
+
+  test "b helper function" do
     assert capture_io(fn -> b Mix.Task.stop end) == "No documentation for Mix.Task.stop was found\n"
     assert capture_io(fn -> b Mix.Task.run end) =~ "* defcallback run([binary()]) :: any()\n\nA task needs to implement `run`"
-    assert capture_io(fn -> b NoMix.run end) =~ "Could not load module NoMix, got: nofile\n"
+    assert capture_io(fn -> b NoMix.run end) == "Could not load module NoMix, got: nofile\n"
     assert capture_io(fn -> b Exception.message/1 end) == "* defcallback message(t()) :: String.t()\n\n\n"
   end
 
