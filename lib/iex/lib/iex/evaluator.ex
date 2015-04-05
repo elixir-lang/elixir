4fab06ecc5190fec341c61abd40f8f0eaaf703d5
Aleksei Magusev <lexmag@me.com>
2015-04-05 19:56:14 +0200

    Keep IEx history state in the evaluator process

--- lib/iex/lib/iex/evaluator.ex
+++ lib/iex/lib/iex/evaluator.ex
@@ -11,15 +11,12 @@ defmodule IEx.Evaluator do
 
   """
   def start(server, leader) do
-    {:ok, history} = IEx.History.start_link
     old_leader = Process.group_leader
     Process.group_leader(self, leader)
-    Process.put(:iex_history_pid, history)
 
     try do
-      loop(server, history)
+      loop(server, IEx.History.init)
     after
-      IEx.History.reset(history)
       Process.group_leader(self, old_leader)
     end
   end
@@ -27,10 +24,10 @@ defmodule IEx.Evaluator do
   defp loop(server, history) do
     receive do
       {:eval, ^server, code, state} ->
-        send server, {:evaled, self, eval(code, state, history)}
+        {result, history} = eval(code, state, history)
+        send server, {:evaled, self, result}
         loop(server, history)
       {:done, ^server} ->
-        IEx.History.reset(history)
         :ok
     end
   end
@@ -94,40 +91,42 @@ defmodule IEx.Evaluator do
     catch
       kind, error ->
         print_error(kind, error, System.stacktrace)
-        %{state | cache: ''}
+        {%{state | cache: ''}, history}
     end
   end
 
-  defp do_eval(@break_trigger, state=%IEx.State{cache: ''}, _) do
-    state
+  defp do_eval(@break_trigger, %IEx.State{cache: ''} = state, history) do
+    {state, history}
   end
 
-  defp do_eval(@break_trigger, state, _) do
+  defp do_eval(@break_trigger, state, _history) do
     :elixir_errors.parse_error(state.counter, "iex", "incomplete expression", "")
   end
 
   defp do_eval(latest_input, state, history) do
     code = state.cache ++ latest_input
     line = state.counter
+    Process.put(:iex_history, history)
     handle_eval(Code.string_to_quoted(code, [line: line, file: "iex"]), code, line, state, history)
+  after
+    Process.delete(:iex_history)
   end
 
   defp handle_eval({:ok, forms}, code, line, state, history) do
-    {result, new_binding, env, scope} =
+    {result, binding, env, scope} =
       :elixir.eval_forms(forms, state.binding, state.env, state.scope)
     unless result == IEx.dont_display_result, do: io_inspect(result)
-    update_history(line, code, result, history)
-    %{state | env: env,
-              cache: '',
-              scope: scope,
-              binding: new_binding,
-              counter: state.counter + 1}
+    state =
+      %{state | env: env, cache: '',
+                scope: scope, binding: binding,
+                counter: state.counter + 1}
+    {state, update_history(history, line, code, result)}
   end
 
-  defp handle_eval({:error, {_, _, ""}}, code, _line, state, _) do
+  defp handle_eval({:error, {_, _, ""}}, code, _line, state, history) do
     # Update state.cache so that IEx continues to add new input to
     # the unfinished expression in `code`
-    %{state | cache: code}
+    {%{state | cache: code}, history}
   end
 
   defp handle_eval({:error, {line, error, token}}, _code, _line, _state, _) do
@@ -135,7 +134,7 @@ defmodule IEx.Evaluator do
     :elixir_errors.parse_error(line, "iex", error, token)
   end
 
-  defp update_history(counter, cache, result, history) do
+  defp update_history(history, counter, cache, result) do
     IEx.History.append(history, {counter, cache, result}, IEx.Config.history_size)
   end
 
