02f9d8a724d6438c1e7a3efc49a0938d5204d6c0
Alexei Sholik <alcosholik@gmail.com>
2015-04-23 18:20:30 +0300

    Handle :undefined in Exception.format_arity/1
    We get :undefined when a temporary worker of a simple_one_for_one
    supervisor crashes.
    
    Thanks to @fishcakez for feedback

--- lib/elixir/lib/exception.ex
+++ lib/elixir/lib/exception.ex
@@ -446,6 +446,10 @@ defmodule Exception do
     "/" <> Integer.to_string(arity)
   end
 
+  defp format_arity(:undefined) do
+    "/?"
+  end
+
   @doc """
   Formats the given file and line as shown in stacktraces.
   If any of the values are `nil`, they are omitted.
