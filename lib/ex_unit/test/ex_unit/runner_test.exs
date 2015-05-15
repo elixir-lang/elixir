29bad60ff95136d1e370d3e01b702b20fbd88071
José Valim <jose.valim@plataformatec.com.br>
2015-05-15 11:16:05 +0200

    Fix runner test

--- lib/ex_unit/test/ex_unit/runner_test.exs
+++ lib/ex_unit/test/ex_unit/runner_test.exs
@@ -3,13 +3,14 @@ Code.require_file "../test_helper.exs", __DIR__
 defmodule ExUnit.RunnerTest do
   use ExUnit.Case
 
-  test "it set the timeout to infinity and max cases to one with trace enabled" do
+  test "it sets max cases to one with trace enabled" do
     opts = Keyword.merge(ExUnit.configuration, trace: true)
 
     {_, config} = ExUnit.Runner.configure(opts)
 
-    assert config[:timeout] == :infinity
-    assert config[:max_cases] == 1
+    assert config.trace
+    assert config.timeout == 60_000
+    assert config.max_cases == 1
   end
 
   test "it does not set timeout to infinity and the max cases to 1 with trace disabled" do
@@ -17,7 +18,8 @@ defmodule ExUnit.RunnerTest do
 
     {_, config} = ExUnit.Runner.configure(opts)
 
-    assert config[:max_cases] == :erlang.system_info(:schedulers_online)
-    assert config[:timeout] == 60_000
+    refute config.trace
+    assert config.max_cases == :erlang.system_info(:schedulers_online)
+    assert config.timeout == 60_000
   end
 end
