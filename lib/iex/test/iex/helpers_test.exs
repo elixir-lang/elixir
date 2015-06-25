c3f8c3d45281018bcdb2c98e287416125d06e816
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-06-25 04:20:50 +0200

    Fix map typespec in helpers test

--- lib/iex/test/iex/helpers_test.exs
+++ lib/iex/test/iex/helpers_test.exs
@@ -96,7 +96,7 @@ defmodule IEx.HelpersTest do
     assert capture_io(fn -> s Enum.all?/1 end) ==
            "@spec all?(t()) :: boolean()\n"
     assert capture_io(fn -> s struct end) ==
-           "@spec struct(module() | map(), Enum.t()) :: %{}\n"
+           "@spec struct(module() | map(), Enum.t()) :: map()\n"
   end
 
   test "v helper" do
