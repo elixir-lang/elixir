27a69942ac67037d4188c978544c1e386a013980
Dan Munckton <dan@cultivatehq.com>
2015-06-29 16:14:05 +0100

    Move the two `assert_file` helpers out of test_new to test_support

--- lib/mix/test/mix/tasks/new_test.exs
+++ lib/mix/test/mix/tasks/new_test.exs
@@ -152,18 +152,4 @@ defmodule Mix.Tasks.NewTest do
       end
     end
   end
-
-  defp assert_file(file) do
-    assert File.regular?(file), "Expected #{file} to exist, but does not"
-  end
-
-  defp assert_file(file, match) do
-    cond do
-      Regex.regex?(match) ->
-        assert_file file, &(assert &1 =~ match)
-      is_function(match, 1) ->
-        assert_file(file)
-        match.(File.read!(file))
-    end
-  end
 end
