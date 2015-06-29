d570f30344022c9303bc8379f1b2cb0d16c4b35d
Dan Munckton <dan@cultivatehq.com>
2015-06-29 16:17:57 +0100

    Add module docs to the generate task

--- lib/mix/lib/mix/tasks/generate.ex
+++ lib/mix/lib/mix/tasks/generate.ex
@@ -4,6 +4,21 @@ defmodule Mix.Tasks.Generate do
   import Mix.Generator
   import Mix.Templates
 
+  @shortdoc "Generate boilerplate code from templates"
+
+  @moduledoc """
+  Generate boilerplate code from templates
+
+    mix generate GENERATOR CAMELISED_NAME
+
+  Where GENERATOR is one of:
+
+    module   Generate a module
+
+  And CAMELISED_NAME is a module name like MyModuleName or
+  MyNameSpace.MyModuleName.
+  """
+
   def run(["module", module_name]) do
     create_file module_file_path(module_name), lib_template([mod: module_name])
   end
