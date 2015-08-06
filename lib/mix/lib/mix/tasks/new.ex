3078c26cc8be294e7ed43b953071866c8205f807
José Valim <jose.valim@plataformatec.com.br>
2015-08-06 17:03:56 +0200

    Improve mix.exs code sample

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -334,17 +334,21 @@ defmodule Mix.Tasks.New do
   # This configuration is loaded before any dependency and is restricted
   # to this project. If another project depends on this project, this
   # file won't be loaded nor affect the parent project. For this reason,
-  # if you want to provide default values for your application for third-
+  # if you want to provide default values for your application for 3rd-
   # party users, it should be done in your mix.exs file.
 
-  # Sample configuration:
+  # You can configure for your application as:
   #
-  #     config :logger,
-  #       level: :info
+  #     config :<%= @app %>, key: :value
   #
-  #     config :logger, :console,
-  #       format: "$date $time [$level] $metadata$message\n",
-  #       metadata: [:user_id]
+  # And access this configuration in your application as:
+  #
+  #     Application.get_env(:<%= @app %>, :key)
+  #
+  # Or configure a 3rd-party app:
+  #
+  #     config :logger, level: :info
+  #
 
   # It is also possible to import configuration files, relative to this
   # directory. For example, you can emulate configuration per environment
