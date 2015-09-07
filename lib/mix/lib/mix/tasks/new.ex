d38db94f0000622c9083a293dab7c704190530f6
eksperimental <eksperimental@users.noreply.github.com>
2015-09-07 21:22:25 +0700

    Avoid splitting words that use hyphen
    For the ones in the documentation, a space will be added in after the hyphen.
    Just for consistency and to ease detection, I have replaced the ones in
    comments as well.
    
    Command to detect these lines:
    
        ag "\w+-\n"

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -336,8 +336,8 @@ defmodule Mix.Tasks.New do
   # This configuration is loaded before any dependency and is restricted
   # to this project. If another project depends on this project, this
   # file won't be loaded nor affect the parent project. For this reason,
-  # if you want to provide default values for your application for 3rd-
-  # party users, it should be done in your mix.exs file.
+  # if you want to provide default values for your application for
+  # 3rd-party users, it should be done in your mix.exs file.
 
   # You can configure for your application as:
   #
