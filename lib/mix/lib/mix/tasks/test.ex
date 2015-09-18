4f544c34db04f895f873e93aaec6ed17e746b538
Gustaf Andersson <gustaf.andersson@infomaker.se>
2015-09-18 23:28:18 +0200

    New config, :warn_test_pattern Display warning for files matching :warn_test_pattern that are not matching :test_pattern. Default config,"*_test.ex".

--- lib/mix/lib/mix/tasks/test.ex
+++ lib/mix/lib/mix/tasks/test.ex
@@ -108,6 +108,9 @@ defmodule Mix.Tasks.Test do
 
     * `:test_pattern` - a pattern to load test files, defaults to `*_test.exs`.
 
+    * `:warn_test_pattern` - a pattern to match potentially missed test files 
+      and display a warning, defaults to `*_test.ex`.
+
     * `:test_coverage` - a set of options to be passed down to the coverage
       mechanism.
 
@@ -186,8 +189,15 @@ defmodule Mix.Tasks.Test do
     # Finally parse, require and load the files
     test_files   = parse_files(files, test_paths)
     test_pattern = project[:test_pattern] || "*_test.exs"
+    warn_test_pattern = project[:warn_test_pattern] || "*_test.ex"
 
-    case Mix.Utils.extract_files(test_files, test_pattern) do
+    matched_test_files = Mix.Utils.extract_files(test_files, test_pattern)
+    matched_warn_test_files = 
+      Mix.Utils.extract_files(test_files, warn_test_pattern) -- matched_test_files
+    
+    display_warn_test_pattern(matched_warn_test_files, test_pattern)
+    
+    case matched_test_files do
       [] ->
         Mix.shell.error "Test patterns did not match any file: " <> Enum.join(files, ", ")
       test_files ->
@@ -200,6 +210,12 @@ defmodule Mix.Tasks.Test do
         System.at_exit fn _ ->
           if failures > 0, do: exit({:shutdown, 1})
         end
+    end
+  end
+
+  defp display_warn_test_pattern(files, pattern) do
+    for file <- files do
+      Mix.shell.info "Warning: #{file} does not match \"#{pattern}\" and won't be run"
     end
   end
 
