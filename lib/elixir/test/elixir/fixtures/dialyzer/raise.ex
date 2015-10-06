6d061329dadd50c9507266de54ab96c97bb0826c
James Fish <james@fishcakez.com>
2015-10-06 17:29:16 +0100

    Add dialyzer tests for generated AST

---
+++ lib/elixir/test/elixir/fixtures/dialyzer/raise.ex
@@ -0,0 +1,23 @@
+defmodule DialyzerRaise do
+
+  @dialyzer :no_undefined_callbacks
+  defexception [:message]
+
+  def exception_var() do
+    e = %DialyzerRaise{}
+    raise e
+  end
+
+  def exception_var(e = %DialyzerRaise{}) do
+    raise e
+  end
+
+  def string_var() do
+    string = "hello"
+    raise string
+  end
+
+  def string_var(string) when is_binary(string) do
+    raise string
+  end
+end
