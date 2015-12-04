01b4f2fdab7493f95c102f13d0e3ccf226970369
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-12-04 16:23:34 +0100

    Remove improper checking from update and get
    It was incorrect because we were doing the check before loading deps
    and writing lock. It needs to happen after.
    
    Related #3795.

--- lib/mix/lib/mix/dep/fetcher.ex
+++ lib/mix/lib/mix/dep/fetcher.ex
@@ -82,11 +82,7 @@ defmodule Mix.Dep.Fetcher do
   defp out_of_date?(%Mix.Dep{status: {:unavailable, _}}),  do: true
   defp out_of_date?(%Mix.Dep{}),                           do: false
 
-
   defp do_finalize({all_deps, apps, new_lock}, old_lock, opts) do
-    not_available = Enum.filter(all_deps, &not Mix.Dep.available?(&1))
-    show_not_available!(not_available)
-
     # Let's get the loaded versions of deps
     deps = Mix.Dep.loaded_by_name(apps, all_deps, opts)
 
@@ -147,21 +143,5 @@ defmodule Mix.Dep.Fetcher do
     Enum.map(given, fn(app) ->
       if is_binary(app), do: String.to_atom(app), else: app
     end)
-  end
-
-  defp show_not_available!([]) do
-    :ok
-  end
-
-  defp show_not_available!(deps) do
-    shell = Mix.shell
-    shell.error "Error fetching dependencies:"
-
-    Enum.each deps, fn(dep) ->
-      shell.error "* #{Mix.Dep.format_dep dep}"
-      shell.error "  #{Mix.Dep.format_status dep}"
-    end
-
-    Mix.raise "Can't continue due to errors on dependencies"
   end
 end
