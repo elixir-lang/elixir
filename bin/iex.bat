77b681c354d6d794d4f1a7614f4b288caff1161a
Brett N. DiFrischia <orderthruchaos@gmail.com>
2015-12-22 11:16:40 -0600

    Add environment variable to iex.bat so user can default to --werl if desired.

--- bin/iex.bat
+++ bin/iex.bat
@@ -1,2 +1,2 @@
 @if defined ELIXIR_CLI_ECHO (@echo on) else  (@echo off)
-call "%~dp0\elixir.bat" +iex --erl "-user Elixir.IEx.CLI" --no-halt %*
+call "%~dp0\elixir.bat" +iex --erl "-user Elixir.IEx.CLI" --no-halt %ELIXIR_IEX_FLAGS% %*
