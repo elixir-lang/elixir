a23873d6a6d5d8f8639b417a006cfce6b710af6a
Timur Yanberdin <yanberdint@gmail.com>
2016-01-02 01:02:27 +0500

    specify syntax highlighting for mix README.md template

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -203,15 +203,19 @@ defmodule Mix.Tasks.New do
 
     1. Add <%= @app %> to your list of dependencies in `mix.exs`:
 
-          def deps do
-            [{:<%= @app %>, "~> 0.0.1"}]
-          end
+      ```elixir
+      def deps do
+        [{:<%= @app %>, "~> 0.0.1"}]
+      end
+      ```
 
     2. Ensure <%= @app %> is started before your application:
 
-          def application do
-            [applications: [:<%= @app %>]]
-          end
+      ```elixir
+      def application do
+        [applications: [:<%= @app %>]]
+      end
+      ```
   <% end %>
   """
 
