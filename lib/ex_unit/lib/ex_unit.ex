9b12738706dba784a58cec673ac7a836a66f913b
Paul Hieromnimon <paul.hieromnimon@gmail.com>
2016-03-13 11:55:36 -0700

    Clarify ExUnit async docs

--- lib/ex_unit/lib/ex_unit.ex
+++ lib/ex_unit/lib/ex_unit.ex
@@ -14,7 +14,8 @@ defmodule ExUnit do
       # 2) Create a new test module (test case) and use "ExUnit.Case".
       defmodule AssertionTest do
         # 3) Notice we pass "async: true", this runs the test case
-        #    concurrently with other test cases
+        #    concurrently with other test cases. The individual tests
+        #    within each test case are still run serially.
         use ExUnit.Case, async: true
 
         # 4) Use the "test" macro instead of "def" for clarity.
