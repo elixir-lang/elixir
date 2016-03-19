6030370aa1d2a24ade72ff2b91a0d3a4beb23302
José Valim <jose.valim@plataformatec.com.br>
2016-03-19 15:26:40 +0100

    Add :archives option to Mix.Project
    Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

--- lib/mix/lib/mix/tasks/clean.ex
+++ lib/mix/lib/mix/tasks/clean.ex
@@ -47,7 +47,7 @@ defmodule Mix.Tasks.Clean do
 
   # Loadpaths without checks because compilers may be defined in deps.
   defp loadpaths! do
-    Mix.Task.run "loadpaths", ["--no-elixir-version-check", "--no-deps-check"]
+    Mix.Task.run "loadpaths", ["--no-elixir-version-check", "--no-deps-check", "--no-archives-check"]
     Mix.Task.reenable "loadpaths"
   end
 end
