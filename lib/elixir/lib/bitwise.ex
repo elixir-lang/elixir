00e146074707c971d273c3c298129044d98bb5c6
José Valim <jose.valim@gmail.com>
2016-03-31 12:18:48 +0200

    Update bitwise.ex

--- lib/elixir/lib/bitwise.ex
+++ lib/elixir/lib/bitwise.ex
@@ -1,31 +1,35 @@
 defmodule Bitwise do
   @moduledoc """
-  This module provides macro-based operators that perform calculations
-  on (sets of) bits.
+  A set of macros that perform calculations on bits.
 
-  In general, you should `use` the Bitwise module as a whole:
+  The macros in this module come in two flavors: named or
+  operators. For example:
 
       iex> use Bitwise
-      iex> bnot 1
+      iex> bnot 1   # named
       -2
-      iex> 1 &&& 1
+      iex> 1 &&& 1  # operator
       1
 
-  When used, it accepts the following options:
+  If you prefer to use only operators or skip them, you can
+  pass the following options:
 
     * `:only_operators` - include only operators
     * `:skip_operators` - skip operators
 
+  For example:
 
       iex> use Bitwise, only_operators: true
       iex> 1 &&& 1
       1
 
+  When invoked with no options, `use Bitwise` is equivalent
+  to `import Bitwise`.
 
-  These macros can be used in guards:
+  All bitwise macros can be used in guards:
 
       iex> use Bitwise
-      iex> odd? = fn(int) when band(int, 1) == 1 -> true; (_) -> false end
+      iex> odd? = fn int when band(int, 1) == 1 -> true; _ -> false end
       iex> odd?.(1)
       true
 
@@ -38,7 +42,8 @@ defmodule Bitwise do
         [bnot: 1, band: 2, bor: 2, bxor: 2, bsl: 2, bsr: 2]
       Keyword.get(options, :skip_operators) ->
         [~~~: 1, &&&: 2, |||: 2, ^^^: 2, <<<: 2, >>>: 2]
-      true -> []
+      true ->
+        []
     end
 
     quote do
