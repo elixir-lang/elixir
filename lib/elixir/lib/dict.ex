ca856d774e00715768d294eab3dc220d17d2d183
José Valim <jose.valim@plataformatec.com.br>
2016-04-03 13:52:15 +0200

    Spell out our deprecation policy

--- lib/elixir/lib/dict.ex
+++ lib/elixir/lib/dict.ex
@@ -14,7 +14,6 @@ defmodule Dict do
   @type t :: list | map
 
   # TODO: Deprecate every function by 1.4
-  # TODO: Remove by 2.0
   defmacro __using__(_) do
     # Use this import to guarantee proper code expansion
     import Kernel, except: [size: 1]
