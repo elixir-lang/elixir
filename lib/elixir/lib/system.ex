7ce93f6c8ba8c8fb1e7da6a057d9c12f87ad091f
José Valim <jose.valim@plataformatec.com.br>
2016-04-05 17:21:33 +0200

    Normalize and document time and time units

--- lib/elixir/lib/system.ex
+++ lib/elixir/lib/system.ex
@@ -1,32 +1,88 @@
 defmodule System do
   @moduledoc """
-  The System module provides access to variables used or
-  maintained by the VM and to functions that interact directly
+  The System module provides functions that interact directly
   with the VM or the host system.
+
+  ## Time
+
+  The System module also provides functions that work with time.
+  Returning different times kept by the system as well as support
+  for different time units.
+
+  One of the complexities in relying on system time is that they
+  may be adjust. For example, when you enter and leave daylight
+  saving time, the system clock will be adjusted, often adding
+  or removing one hour. We call such changes "time warps". In
+  order to understand how such changes may be harmful, imagine
+  the following code:
+
+      ## DO NOT DO THIS
+      prev = System.os_time()
+      # ... execute some code ...
+      next = System.os_time()
+      diff = next - prev
+
+  If, while the code is executing, the system clock changes,
+  some code that executed in 1 second may be reported as taking
+  over 1 hour! To address such concerns, the VM provides a
+  monotonic time via `System.monotonic_time/0` which never
+  decreases and does not leap:
+
+      ## DO THIS
+      prev = System.monotonic_time()
+      # ... execute some code ...
+      next = System.monotonic_time()
+      diff = next - prev
+
+
+  Generally speaking, the VM provides three time measurements:
+
+    * `os_time/0` - the time reported by the OS. This time may be
+      adjusted forwards or backwards in time with no limitation;
+
+    * `system_time/0` - the VM view of the `os_time/0`. They may
+      not match in case of time warps although the VM works towards
+      aligning them. This time is not monotonic (i.e. it may decrease)
+      as its behaviour is configured [by the VM time warp
+      mode](http://erlang.org/doc/apps/erts/time_correction.html#Time_Warp_Modes);
+
+    * `monotonic_time/0` - a monotonically increasing time provided
+      by the Erlang VM
+
+  The time functions in this module works in the `:native` unit,
+  which is OS dependent. Most of the time, all calculations are
+  done in the `:native` unit, to avoid loss of precision, with
+  `convert_time_unit/3` being invoked at the end to convert to
+  milli, micro or nanoseconds. See the `t:time_unit/0` type for
+  more information.
+
+  For a more complete rundown on the VM support for different
+  times, see the [chapter on time and time
+  correction](http://www.erlang.org/doc/apps/erts/time_correction.html)
+  in the Erlang docs.
   """
 
   @typedoc """
-  The time unit that can be passed to functions like `System.system_time/1`
-  and similar.
+  The time unit to be passed to functions like `monotonic_time/1` and others.
 
-  The `:seconds`, `:milli_seconds`, `:micro_seconds`, and `:nano_seconds` time
-  units are self-explanatory: the return value of the functions that accept a
-  time unit will be in the given time unit.
+  The `:seconds`, `:milliseconds`, `:microseconds` and `:nanoseconds` time
+  units controls the return value of the functions that accept a time unit.
 
   A time unit can also be a strictly positive integer. In this case, it
   represents the "parts per second": the time will be returned in `1 /
-  parts_per_second` seconds. For example, using the `:milli_seconds` time unit
+  parts_per_second` seconds. For example, using the `:milliseconds` time unit
   is equivalent to using `1000` as the time unit (as the time will be returned
   in 1/1000 seconds - milliseconds).
 
-  See also the [documentation on the `time_unit` Erlang
-  type](http://erlang.org/doc/man/erlang.html#type-time_unit).
+  Keep in mind the Erlang API will use `:milli_seconds`, `:micro_seconds`
+  and `:nano_seconds` as time units although Elixir normalizes their spelling
+  to match the SI convention.
   """
   @type time_unit ::
     :seconds
-    | :milli_seconds
-    | :micro_seconds
-    | :nano_seconds
+    | :milliseconds
+    | :microseconds
+    | :nanoseconds
     | pos_integer
 
   @base_dir     :filename.join(__DIR__, "../../..")
@@ -550,12 +606,8 @@ defmodule System do
   @doc """
   Returns the current monotonic time in the `:native` time unit.
 
-  This time is monotonically increasing and starts in an unspecified point in
-  time.
-
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
+  This time is monotonically increasing and starts in an unspecified
+  point in time.
 
   Inlined by the compiler into `:erlang.monotonic_time/0`.
   """
@@ -567,23 +619,20 @@ defmodule System do
   @doc """
   Returns the current monotonic time in the given time unit.
 
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
-
-  Inlined by the compiler into `:erlang.monotonic_time/1`.
+  This time is monotonically increasing and starts in an unspecified
+  point in time.
   """
   @spec monotonic_time(time_unit) :: integer
   def monotonic_time(unit) do
-    :erlang.monotonic_time(unit)
+    :erlang.monotonic_time(convert_time_unit(unit))
   end
 
   @doc """
   Returns the current system time in the `:native` time unit.
 
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
+  It is the VM view of the `os_time/0`. They may not match in
+  case of time warps although the VM works towards aligning
+  them. This time is not monotonic.
 
   Inlined by the compiler into `:erlang.system_time/0`.
   """
@@ -595,38 +644,36 @@ defmodule System do
   @doc """
   Returns the current system time in the given time unit.
 
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
-
-  Inlined by the compiler into `:erlang.system_time/1`.
+  It is the VM view of the `os_time/0`. They may not match in
+  case of time warps although the VM works towards aligning
+  them. This time is not monotonic.
   """
   @spec system_time(time_unit) :: integer
   def system_time(unit) do
-    :erlang.system_time(unit)
+    :erlang.system_time(convert_time_unit(unit))
   end
 
   @doc """
-  Converts `time` from time unit `from_unit` to time unit `to_unit`. The result
-  is rounded via the floor function.
+  Converts `time` from time unit `from_unit` to time unit `to_unit`.
 
-  `convert_time_unit/3` accepts an additional time unit (other than the ones in
-  the `time_unit` type): `:native`. `:native` is the time unit used by the
-  Erlang runtime system. It's determined when the runtime starts and stays the
-  same until the runtime is stopped. To determine what the `:native` unit
-  amounts to in a system, you can call this function to convert 1 second to the
-  `:native` time unit (i.e., `System.convert_time_unit(1, :seconds, :native)`).
+  The result is rounded via the floor function.
 
-  Inlined by the compiler into `:erlang.convert_time_unit/3`.
+  `convert_time_unit/3` accepts an additional time unit (other than the
+  ones in the `time_unit` type) called `:native`. `:native` is the time
+  unit used by the Erlang runtime system. It's determined when the runtime
+  starts and stays the same until the runtime is stopped. To determine what
+  the `:native` unit amounts to in a system, you can call this function to
+  convert 1 second to the `:native` time unit (i.e.,
+  `System.convert_time_unit(1, :seconds, :native)`).
   """
   @spec convert_time_unit(integer, time_unit | :native, time_unit | :native) :: integer
   def convert_time_unit(time, from_unit, to_unit) do
-    :erlang.convert_time_unit(time, from_unit, to_unit)
+    :erlang.convert_time_unit(time, convert_time_unit(from_unit), convert_time_unit(to_unit))
   end
 
   @doc """
-  Returns the current time offset between the Erlang monotonic time and the
-  Erlang system time.
+  Returns the current time offset between the Erlang VM monotonic
+  time and the Erlang VM system time.
 
   The result is returned in the `:native` time unit.
 
@@ -640,22 +687,17 @@ defmodule System do
   end
 
   @doc """
-  Returns the current time offset between the Erlang monotonic time and the
-  Erlang system time.
+  Returns the current time offset between the Erlang VM monotonic
+  time and the Erlang VM system time.
 
-  The result is returned in the given time unit `unit`. The returned offset,
-  added to an Erlang monotonic time (e.g., obtained with `monotonic_time/1`),
-  gives the Erlang system time that corresponds to that monotonic time.
-
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
-
-  Inlined by the compiler into `:erlang.time_offset/1`.
+  The result is returned in the given time unit `unit`. The returned
+  offset, added to an Erlang monotonic time (e.g., obtained with
+  `monotonic_time/1`), gives the Erlang system time that corresponds
+  to that monotonic time.
   """
   @spec time_offset(time_unit) :: integer
   def time_offset(unit) do
-    :erlang.time_offset(unit)
+    :erlang.time_offset(convert_time_unit(unit))
   end
 
   @doc """
@@ -663,11 +705,8 @@ defmodule System do
 
   The result is returned in the `:native` time unit.
 
-  This time **does not** increase monotonically.
-
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
+  This time may be adjusted forwards or backwards in time
+  with no limitation and is not monotonic.
 
   Inlined by the compiler into `:os.system_time/0`.
   """
@@ -679,17 +718,12 @@ defmodule System do
   @doc """
   Returns the current OS time in the given time `unit`.
 
-  This time **does not** increase monotonically.
-
-  For more information, see the [chapter on time and time
-  correction](http://www.erlang.org/doc/apps/erts/time_correction.html) in the
-  Erlang docs.
-
-  Inlined by the compiler into `:os.system_time/1`.
+  This time may be adjusted forwards or backwards in time
+  with no limitation and is not monotonic.
   """
   @spec os_time(time_unit) :: integer
   def os_time(unit) do
-    :os.system_time(unit)
+    :os.system_time(convert_time_unit(unit))
   end
 
   @doc """
@@ -718,5 +752,22 @@ defmodule System do
   @spec unique_integer([:positive | :monotonic]) :: integer
   def unique_integer(modifiers \\ []) do
     :erlang.unique_integer(modifiers)
+  end
+
+  defp convert_time_unit(:native),       do: :seconds
+  defp convert_time_unit(:seconds),      do: :seconds
+  defp convert_time_unit(:milliseconds), do: :milli_seconds
+  defp convert_time_unit(:microseconds), do: :micro_seconds
+  defp convert_time_unit(:nanoseconds),  do: :nano_seconds
+
+  # TODO: Warn on Elixir 1.4
+  defp convert_time_unit(erlang)
+      when erlang in [:milli_seconds, :micro_seconds, :nano_seconds] do
+    erlang
+  end
+
+  defp convert_time_unit(other) do
+    raise ArgumentError, "unsupported time unit. Expected :seconds, :milliseconds, " <>
+                         ":microseconds or :nanoseconds, got #{inspect other}"
   end
 end
