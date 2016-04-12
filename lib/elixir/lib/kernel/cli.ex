elixir-lang/elixir

commit 8b035311d1b2cadd982491d92bf091ade45d3976

Use System.build_info[:build] consistently in Mix and Elixir

lib/elixir/lib/kernel/cli.ex
@@ -147,15 +147,9 @@ defmodule Kernel.CLI do
 
   defp parse_shared([opt|_t], _config) when opt in ["-v", "--version"] do
     if function_exported?(IEx, :started?, 0) and IEx.started? do
-      IO.puts "IEx #{System.version}"
+      IO.puts "IEx " <> System.build_info[:build]
     else
-      IO.puts :erlang.system_info(:system_version)
-      {:ok, v} = Version.parse(System.version)
-
-      case v.pre do
-        [] -> IO.puts "Elixir #{System.version}"
-        _  -> IO.puts "Elixir #{System.version} (#{System.build_info().revision})"
-      end
+      IO.puts "Elixir " <> System.build_info[:build]
     end
 
     System.halt 0

lib/mix/lib/mix/cli.ex
@@ -114,7 +114,8 @@ defmodule Mix.CLI do
   end
 
   defp display_version() do
-    IO.puts "Mix #{System.version}"
+    IO.puts :erlang.system_info(:system_version)
+    IO.puts "Mix " <> System.build_info[:build]
   end
 
   # Check for --help or --version in the args

commit 7d7e6f7a6a7a208f515ff888082489d90aa5cb03

Fix 'elixir -v' to display Erlang :system_version info
It fixes a bug accidentally introduced in 8b035311d1b2cadd982491d92bf091ade45d3976
that removed a line of code

lib/elixir/lib/kernel/cli.ex
@@ -149,6 +149,7 @@ defmodule Kernel.CLI do
     if function_exported?(IEx, :started?, 0) and IEx.started? do
       IO.puts "IEx " <> System.build_info[:build]
     else
+      IO.puts :erlang.system_info(:system_version)
       IO.puts "Elixir " <> System.build_info[:build]
     end
 