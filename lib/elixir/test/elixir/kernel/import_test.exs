12a051fd0ded72f23e230d9b41919921863316b1
José Valim <jose.valim@plataformatec.com.br>
2016-05-15 17:55:29 +0200

    Add assertions that match on the imported value

--- lib/elixir/test/elixir/kernel/import_test.exs
+++ lib/elixir/test/elixir/kernel/import_test.exs
@@ -13,25 +13,25 @@ defmodule Kernel.ImportTest do
   end
 
   test "multi-call" do
-    import Elixir.{List, unquote(:String)}
+    [List, String] = import Elixir.{List, unquote(:String)}
     assert keymember?([a: 1], :a, 0)
     assert valid?("ø")
   end
 
   test "blank multi-call" do
-    import List.{}
+    [] = import List.{}
     # Buggy local duplicate is untouched
     assert duplicate([1], 2) == [1]
   end
 
   test "multi-call with options" do
-    import Elixir.{List}, only: []
+    [List] = import Elixir.{List}, only: []
     # Buggy local duplicate is untouched
     assert duplicate([1], 2) == [1]
   end
 
   test "import all" do
-    import :lists
+    :lists = import :lists
     assert flatten([1, [2], 3]) == [1, 2, 3]
   end
 
