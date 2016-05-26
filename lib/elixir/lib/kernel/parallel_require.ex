6ee71ed66b7ac80658e374c1ca35740061493593
José Valim <jose.valim@plataformatec.com.br>
2016-05-26 20:22:54 +0200

    Break apart parallel require and compiler tests

--- lib/elixir/lib/kernel/parallel_require.ex
+++ lib/elixir/lib/kernel/parallel_require.ex
@@ -32,7 +32,7 @@ defmodule Kernel.ParallelRequire do
       :ok ->
         result
       :error ->
-        IO.puts :stderr, "Compilation failed due to warnings while using the --warnings-as-errors option"
+        IO.puts :stderr, "\nLoading files failed due to warnings while using the --warnings-as-errors option"
         exit({:shutdown, 1})
     end
   end
