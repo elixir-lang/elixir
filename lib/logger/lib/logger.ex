a81e21a73b9d3d3f7200fc3106e8de87657a5f57
José Valim <jose.valim@plataformatec.com.br>
2016-05-30 19:00:03 +0200

    Translate messages on logger backend only when necessary

--- lib/logger/lib/logger.ex
+++ lib/logger/lib/logger.ex
@@ -476,13 +476,8 @@ defmodule Logger do
           level: min_level, utc_log: utc_log?} = Logger.Config.__data__
 
         if compare_levels(level, min_level) != :lt do
-          truncated = truncate(chardata_or_fn, truncate)
+          message = truncate(chardata_or_fn, truncate)
           metadata = [pid: self()] ++ Keyword.merge(pdict, metadata)
-          message =
-            case :unicode.characters_to_binary(truncated) do
-              {_, good, bad} -> [good | Logger.Formatter.prune(bad)]
-              good -> good
-            end
 
           tuple = {Logger, message, Logger.Utils.timestamp(utc_log?), metadata}
 
