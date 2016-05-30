13983bb25d0f9ab4c610ab63d7adf097e6e191d1
Olafur Arason <olafura@olafura.com>
2016-05-30 11:12:59 -0400

    Better logger for console with direct access (#4720)

--- lib/logger/lib/logger/backends/console.ex
+++ lib/logger/lib/logger/backends/console.ex
@@ -76,11 +76,13 @@ defmodule Logger.Backends.Console do
     output =
       format_event(level, msg, ts, md, state)
       |> color_event(level, colors)
-    try do
-      IO.write(device, output)
-    rescue
-      ArgumentError ->
-        IO.write(device, Logger.Formatter.prune(output))
+    case :unicode.characters_to_binary(output) do
+      {:incomplete, good, bad} ->
+        send(device, {:io_request, self(), self(), {:put_chars, :unicode, [good | Logger.Formatter.prune(bad)]}})
+      {:error, good, bad} ->
+        send(device, {:io_request, self(), self(), {:put_chars, :unicode, [good | Logger.Formatter.prune(bad)]}})
+      good ->
+        send(device, {:io_request, self(), self(), {:put_chars, :unicode, good}})
     end
   end
 
