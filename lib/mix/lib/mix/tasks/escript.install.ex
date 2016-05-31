f57ee03ad12bbe37c40aeaca2eb0ddde92d5454c
Eric Entin <eric@ericent.in>
2016-05-31 17:51:07 -0400

    Add mix escript.install git/github

--- lib/mix/lib/mix/tasks/escript.install.ex
+++ lib/mix/lib/mix/tasks/escript.install.ex
@@ -2,6 +2,7 @@ defmodule Mix.Tasks.Escript.Install do
   use Mix.Task
 
   @shortdoc "Installs an escript locally"
+  @preferred_cli_env :prod
 
   @moduledoc """
   Installs an escript locally.
@@ -12,11 +13,20 @@ defmodule Mix.Tasks.Escript.Install do
 
       mix do escript.build, escript.install
 
-  If an argument is provided, it should be a local path or a URL to a prebuilt escript.
+  If an argument is provided, it should be a local path, a URL to a prebuilt escript,
+  a git repository, or a github repository.
 
       mix escript.install escript
       mix escript.install path/to/escript
       mix escript.install https://example.com/myescript
+      mix escript.install git https://path/to/git/repo
+      mix escript.install git https://path/to/git/repo branch git_branch
+      mix escript.install git https://path/to/git/repo tag git_tag
+      mix escript.install git https://path/to/git/repo ref git_ref
+      mix escript.install github user/project
+      mix escript.install github user/project branch git_branch
+      mix escript.install github user/project tag git_tag
+      mix escript.install github user/project ref git_ref
 
   After installation, the escript can be invoked as
 
@@ -28,10 +38,14 @@ defmodule Mix.Tasks.Escript.Install do
 
   ## Command line options
 
-    * `--sha512` - checks the escript matches the given SHA-512 checksum
+    * `--sha512` - checks the escript matches the given SHA-512 checksum. Only
+      applies to installations via URL or local path.
 
     * `--force` - forces installation without a shell prompt; primarily
       intended for automation in build systems like make
+
+    * `--submodules` - fetch repository submodules before building escript from
+      git or github
 
   """
 
@@ -39,10 +53,94 @@ defmodule Mix.Tasks.Escript.Install do
 
   @escript_file_mode 0o555 # only read and execute permissions
 
-  @switches [force: :boolean, sha512: :string]
+  @switches [force: :boolean, sha512: :string, submodules: :boolean]
   @spec run(OptionParser.argv) :: boolean
   def run(argv) do
-    Mix.Local.Installer.install({__MODULE__, :escript}, argv, @switches)
+    {opts, args} = OptionParser.parse!(argv, strict: @switches)
+
+    case args do
+      ["git" | rest] ->
+        raise_if_sha512("git", opts)
+        do_git_install(rest, opts)
+      ["github" | rest] ->
+        raise_if_sha512("github", opts)
+        do_github_install(rest, opts)
+      _ ->
+        Mix.Local.Installer.install({__MODULE__, :escript}, argv, @switches)
+    end
+  end
+
+  defp raise_if_sha512(name, opts) do
+    if opts[:sha512] do
+      Mix.raise "--sha512 is not supported for escript.install from #{name}"
+    end
+  end
+
+  defp do_github_install([], _opts) do
+    Mix.raise "escript.install github expects a repository"
+  end
+
+  defp do_github_install([repo | rest], opts) do
+    url = "https://github.com/#{repo}.git"
+    do_git_install([url | rest], opts)
+  end
+
+  defp do_git_install([], _opts) do
+    Mix.raise "escript.install git expects a URL"
+  end
+
+  defp do_git_install([url], opts) do
+    do_git_install([url, "branch", "master"], opts)
+  end
+
+  defp do_git_install([url, ref_type, ref], opts) do
+    if opts[:sha512] do
+      Mix.raise "--sha512 is not supported for given "
+    end
+
+    in_tmp_dir fn tmp_path ->
+      checkout_opts =
+        ref_to_config(ref_type, ref) ++
+        [dest: tmp_path, git: url, submodules: opts[:submodules]]
+
+      Mix.SCM.Git.checkout(checkout_opts)
+      do_escript_build_and_install(tmp_path, opts)
+    end
+  end
+
+  defp do_git_install([_url | rest], _opts) do
+    Mix.raise "escript.install received invalid git checkout spec: #{Enum.join(rest, " ")}"
+  end
+
+  defp ref_to_config("branch", branch), do: [branch: branch]
+
+  defp ref_to_config("tag", tag), do: [tag: tag]
+
+  defp ref_to_config("ref", ref), do: [ref: ref]
+
+  defp ref_to_config(ref_type, _) do
+    Mix.raise "escript.install expected one of \"branch\", \"tag\", or \"ref\". Got: \"#{ref_type}\""
+  end
+
+  defp in_tmp_dir(fun) do
+    unique = :crypto.strong_rand_bytes(4) |> Base.url_encode64(padding: false)
+    tmp_path = Path.join(System.tmp_dir!(), "mix-escript-install-" <> unique)
+
+    try do
+      fun.(tmp_path)
+    after
+      File.rm_rf!(tmp_path)
+    end
+  end
+
+  defp do_escript_build_and_install(path, opts) do
+    install_opts = if opts[:force], do: ["--force"], else: []
+
+    Mix.Project.in_project :new_escript, path, fn _mixfile ->
+      Mix.Task.run("deps.get", [])
+      Mix.Task.run("escript.build", [])
+      Mix.Task.run("escript.install", install_opts)
+    end
   end
 
   ### Mix.Local.Installer callbacks
