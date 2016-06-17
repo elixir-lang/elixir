f2ac02166835d1e1df416325b3bb8e8407df62ff
Andrea Leopardi <an.leopardi@gmail.com>
2016-06-17 15:07:45 +0200

    Fix the name of the ExUnit.DescribeTest module
    It was ExUnit.BundleTest, in memory of ancient times when "describe" was
    called "bundle".

--- lib/ex_unit/test/ex_unit/describe_test.exs
+++ lib/ex_unit/test/ex_unit/describe_test.exs
@@ -1,6 +1,6 @@
 Code.require_file "../test_helper.exs", __DIR__
 
-defmodule ExUnit.BundleTest do
+defmodule ExUnit.DescribeTest do
   use ExUnit.Case, async: true
 
   @moduletag [attribute_tag: :from_module]
