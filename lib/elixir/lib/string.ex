22f319b481c56788143302298737930b63648f65
Eksperimental <eksperimental@users.noreply.github.com>
2016-06-23 16:25:33 -0500

    String.splitter/3: add doctest example (#4867)

--- lib/elixir/lib/string.ex
+++ lib/elixir/lib/string.ex
@@ -370,6 +370,18 @@ defmodule String do
   ## Options
 
     * :trim - when `true`, does not emit empty patterns
+
+  ## Examples
+
+    iex> String.splitter("1,2 3,4 5,6 7,8,...,99999", [" ", ","]) |> Enum.take(4)
+    ["1", "2", "3", "4"]
+
+    iex> String.splitter("abcd", "") |> Enum.take(10)
+    ["a", "b", "c", "d", ""]
+
+    iex> String.splitter("abcd", "", trim: true) |> Enum.take(10)
+    ["a", "b", "c", "d"]
+
   """
   @spec splitter(t, pattern, Keyword.t) :: Enumerable.t
   def splitter(string, pattern, options \\ []) do
