e9ee720caec40581f8b65834f85eb7eb33455536
Aleksei Magusev <lexmag@me.com>
2016-06-29 00:46:55 +0200

    Make Mix.Tasks.Do.gather_commands/3 private

--- lib/mix/lib/mix/tasks/do.ex
+++ lib/mix/lib/mix/tasks/do.ex
@@ -26,21 +26,21 @@ defmodule Mix.Tasks.Do do
 
   @doc false
   def gather_commands(args) do
-    gather_commands args, [], []
+    gather_commands(args, [], [])
   end
 
-  def gather_commands([head | rest], current, acc)
+  defp gather_commands([head | rest], current, acc)
       when binary_part(head, byte_size(head), -1) == "," do
     part    = binary_part(head, 0, byte_size(head) - 1)
     current = Enum.reverse([part | current])
-    gather_commands rest, [], [current | acc]
+    gather_commands(rest, [], [current | acc])
   end
 
-  def gather_commands([head | rest], current, acc) do
-    gather_commands rest, [head | current], acc
+  defp gather_commands([head | rest], current, acc) do
+    gather_commands(rest, [head | current], acc)
   end
 
-  def gather_commands([], current, acc) do
+  defp gather_commands([], current, acc) do
     Enum.reverse [Enum.reverse(current) | acc]
   end
 end
