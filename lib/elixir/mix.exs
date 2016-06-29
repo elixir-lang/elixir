17a8527137d1478fd1e65b8b0e17ad2ac39d3652
Chicker <vadim.agishev@gmail.com>
2016-06-29 15:57:56 +0500

    Remove benchmarking of the `unicode.ex`

--- lib/elixir/mix.exs
+++ lib/elixir/mix.exs
@@ -4,12 +4,6 @@ defmodule Elixir.Mixfile do
   def project do
     [app: :elixir,
      version: System.version,
-     build_per_environment: false,
-     deps: deps,
-     elixirc_paths: ["lib", "unicode"]]
-  end
-  
-  defp deps do
-    [{:benchfella, "~> 0.3.0"}]
+     build_per_environment: false]
   end
 end
