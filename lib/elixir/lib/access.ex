d10f2318d09b396f80a16faec93bfef6c311f6a9
Qqwy <qqwy@gmx.com>
2016-07-05 09:44:51 +0200

    Adds documentation for Access behaviour Callbacks (#4939)

--- lib/elixir/lib/access.ex
+++ lib/elixir/lib/access.ex
@@ -113,15 +113,78 @@ defmodule Access do
 
   See the functions `key/1`, `key!/1`, `elem/1` and `all/0` for the current
   accessors.
+
+  ## Adding the Access behaviour to your own data structures
+  
+  If you want to be able to use the Access protocol 
+  with your own data structures, you can do so by adding:
+
+    @behaviour Access
+
+  inside the module that contains the struct representing your data structure.
+  You will then need to define implementations of 
+  `fetch/2`, `get/3`, `get_and_update/3` and `pop/2`,
+  which will be invoked whenever one of the Access functions is used. 
+
   """
 
   @type t :: list | map | nil
   @type key :: any
   @type value :: any
 
+  @doc """
+  Invoked when someone wants to access the value stored under `key` in your structure.
+
+  This function is supposed to return the tuple `{:ok, value}` if it succeeded,
+  or `:error` if the key does not exist in the structure.
+
+  Many of the functions defined in the `Access` module internally call this function.
+
+  See `Map.fetch/2` and `Keyword.fetch/2` for example implementations.
+
+  """
   @callback fetch(t, key) :: {:ok, value} | :error
+
+  @doc """
+  Invoked when someone wants to access the value stored under `key` in your structure. 
+  If `key` is not found, the second argument is returned.
+
+  This is the method that is used when someone calls `your_structure[some_key]`.
+
+  For most data structures, this can be implemented using `fetch/2` internally.
+
+  See `Map.get/3` and `Keyword.get/3` for example implementations.
+  """
   @callback get(t, key, value) :: value
+
+  @doc """
+  Invoked when someone calls `Access.get_and_update/3`.
+  
+  The implementation should invoke the passed function
+  on the value inside key `key` in the passed structure.
+  This function will return a tuple in the form of `{received_value, new_value}`,
+  or it will return `:pop`.
+  
+  When the function returned a tuple,  `new_value` should then
+  be used to create an updated structure,
+  where `new_value` is stored under `key` instead of its original value.
+  Finally, the implementation should return the tuple `{received_value, updated_structure}`.
+
+  When the function returned `:pop`, the implementation should instead 
+  remove the value from the structure. (making it behave similar to `pop/2`.)
+
+  See `Map.get_and_update/3` or `Keyword.get_and_update/3` for example implementations.
+  """
   @callback get_and_update(t, key, (value -> {value, value} | :pop)) :: {value, t}
+  
+  @doc """
+  Invoked when someone calls `Access.pop/2`, and also used internally by some functions.
+
+  The implementation should remove `key` and whatever is inside it from the passed structure.
+  The result value should be the tuple `{removed_value, updated_structure}`.
+
+  See `Map.pop/2` or `Keyword.pop/2` for example implementations.
+  """
   @callback pop(t, key) :: {value, t}
 
   defmacrop raise_undefined_behaviour(e, struct, top) do
