3aa2c2936ffed4b615fd0e9edfa956d23d0f1dbf
Eugene Pirogov <iamexile@gmail.com>
2016-07-11 12:58:52 +0300

    Improve error for bad return value in Keyword.get_and_update(!)/3 (#4992)

--- lib/elixir/test/elixir/keyword_test.exs
+++ lib/elixir/test/elixir/keyword_test.exs
@@ -26,4 +26,22 @@ defmodule KeywordTest do
     assert Map.__info__(:functions) -- Keyword.__info__(:functions) ==
            [from_struct: 1]
   end
+
+  test "get_and_update/3 raises on bad return value from the argument function" do
+    assert_raise RuntimeError, "the given function must return a two-element tuple or :pop, got: 1", fn ->
+      Keyword.get_and_update([a: 1], :a, fn value -> value end)
+    end
+  end
+
+  test "get_and_update/3 raises on bad return value from the argument function (given empty keyword list)" do
+    assert_raise RuntimeError, "the given function must return a two-element tuple or :pop, got: nil", fn ->
+      Keyword.get_and_update([], :a, fn value -> value end)
+    end
+  end
+
+  test "get_and_update!/3  raises on bad return value from the argument function" do
+    assert_raise RuntimeError, "the given function must return a two-element tuple or :pop, got: 1", fn ->
+      Keyword.get_and_update([a: 1], :a, fn value -> value end)
+    end
+  end
 end
