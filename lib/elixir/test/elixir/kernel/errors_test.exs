916f104031b508f7b056d0f78c16c9a0fcc646c3
ottolin <ottolin@gmail.com>
2016-08-10 19:55:03 +0800

    Enhancing error message for token matching failure (#5123)

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -681,7 +681,7 @@ defmodule Kernel.ErrorsTest do
 
   test "interpolation error" do
     assert_compile_fail SyntaxError,
-      "nofile:1: unexpected token: \")\". \"do\" starting at line 1 is missing terminator \"end\"",
+      "nofile:1: \"do\" is missing terminator \"end\". unexpected token: \")\" at line 1.",
       '"foo\#{case 1 do )}bar"'
   end
 
