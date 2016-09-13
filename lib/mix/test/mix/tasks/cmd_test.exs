f274c9e2b5305a1a450bdad19edd4322ccd6914e
Justin Schneck <jschneck@mac.com>
2016-09-13 14:47:39 -0400

    Allow a list of apps to be passed to mix cmd

--- lib/mix/test/mix/tasks/cmd_test.exs
+++ lib/mix/test/mix/tasks/cmd_test.exs
@@ -15,4 +15,17 @@ defmodule Mix.Tasks.CmdTest do
       end)
     end
   end
+
+  test "only run the cmd for specified apps" do
+    in_fixture "umbrella_dep/deps/umbrella", fn ->
+      Mix.Project.in_project(:umbrella, ".", fn _ ->
+        Mix.Task.run "cmd", ["--only", "bar", "echo", "hello"]
+        nl = os_newline()
+        assert_received {:mix_shell, :info, ["==> bar"]}
+        assert_received {:mix_shell, :run, ["hello" <> ^nl]}
+        refute_received {:mix_shell, :info, ["==> foo"]}
+        refute_received {:mix_shell, :run, ["hello" <> ^nl]}
+      end)
+    end
+  end
 end
