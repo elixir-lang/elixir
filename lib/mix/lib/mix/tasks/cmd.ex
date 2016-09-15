76b355a9d90989987174a87957622ecc372add00
Justin Schneck <jschneck@mac.com>
2016-09-15 08:27:14 -0400

    change mix cmd to use --app instead of --only (#5220)

--- lib/mix/lib/mix/tasks/cmd.ex
+++ lib/mix/lib/mix/tasks/cmd.ex
@@ -13,9 +13,9 @@ defmodule Mix.Tasks.Cmd do
       mix cmd echo pwd
 
   You can limit which apps the cmd runs in by passing the app names
-  before the cmd using --only:
+  before the cmd using --app:
 
-      mix cmd --only app1 --only app2 echo pwd
+      mix cmd --app app1 --app app2 echo pwd
 
   Aborts when a command exits with a non-zero status.
   """
@@ -33,7 +33,7 @@ defmodule Mix.Tasks.Cmd do
 
   defp parse_apps(args, apps) do
     case args do
-      ["--only", app | tail] ->
+      ["--app", app | tail] ->
         parse_apps(tail, [String.to_atom(app) | apps])
       args -> {args, apps}
     end
