elixir-lang/elixir

commit 904e89782dcde18463b1306e6fe47a0798d55e7f

Fixing spelling mistakes of specially -> especially. (#5235)

lib/elixir/lib/inspect.ex
@@ -429,7 +429,7 @@ defimpl Inspect, for: Regex do
     do: escape(rest, buf <> <<?\\, term>>, term)
 
   # the list of characters is from "String.printable?" impl
-  # minus characters treated specially by regex: \s, \d, \b, \e
+  # minus characters treated especially by regex: \s, \d, \b, \e
 
   defp escape(<<?\n>> <> rest, buf, term),
     do: escape(rest, <<buf::binary, ?\\, ?n>>, term)

lib/elixir/src/elixir_tokenizer.erl
@@ -554,7 +554,7 @@ handle_dot([$., T | Rest], Line, Column, DotColumn, Scope, Tokens) when
     ?rel_op(T); ?match_op(T); ?pipe_op(T); T == $% ->
   handle_call_identifier(Rest, Line, Column + 1, DotColumn, 1, list_to_atom([T]), Scope, Tokens);
 
-% ## Exception for .( as it needs to be treated specially in the parser
+% ## Exception for .( as it needs to be treated especially in the parser
 handle_dot([$., $( | Rest], Line, Column, DotColumn, Scope, Tokens) ->
   tokenize([$( | Rest], Line, Column + 2, Scope, add_token_with_nl({dot_call_op, {Line, DotColumn, DotColumn + 1}, '.'}, Tokens));
 
@@ -791,7 +791,7 @@ tokenize_comment([_ | Rest])         -> tokenize_comment(Rest);
 tokenize_comment([])                 -> [].
 
 %% Atoms
-%% Handle atoms specially since they support @
+%% Handle atoms especially since they support @
 
 tokenize_atom(T, Acc) ->
   tokenize_atom(T, Acc, 0).

lib/elixir/lib/keyword.ex
@@ -25,7 +25,7 @@ defmodule Keyword do
   The functions in Keyword do not guarantee any property when
   it comes to ordering. However, since a keyword list is simply a
   list, all the operations defined in `Enum` and `List` can be
-  applied too, specially when ordering is required.
+  applied too, especially when ordering is required.
   """
 
   @compile :inline_list_funcs

commit dd78780363bba0fa33d95375ef9eb55a9014471f

Fix a few typos introduced in 904e89782dcde18463b1306e6fe47a0798d55e7f

lib/elixir/lib/inspect.ex
@@ -429,7 +429,7 @@ defimpl Inspect, for: Regex do
     do: escape(rest, buf <> <<?\\, term>>, term)
 
   # the list of characters is from "String.printable?" impl
-  # minus characters treated especially by regex: \s, \d, \b, \e
+  # minus characters treated specially by regex: \s, \d, \b, \e
 
   defp escape(<<?\n>> <> rest, buf, term),
     do: escape(rest, <<buf::binary, ?\\, ?n>>, term)

lib/elixir/src/elixir_tokenizer.erl
@@ -554,7 +554,7 @@ handle_dot([$., T | Rest], Line, Column, DotColumn, Scope, Tokens) when
     ?rel_op(T); ?match_op(T); ?pipe_op(T); T == $% ->
   handle_call_identifier(Rest, Line, Column + 1, DotColumn, 1, list_to_atom([T]), Scope, Tokens);
 
-% ## Exception for .( as it needs to be treated especially in the parser
+% ## Exception for .( as it needs to be treated specially in the parser
 handle_dot([$., $( | Rest], Line, Column, DotColumn, Scope, Tokens) ->
   tokenize([$( | Rest], Line, Column + 2, Scope, add_token_with_nl({dot_call_op, {Line, DotColumn, DotColumn + 1}, '.'}, Tokens));
 
@@ -791,7 +791,7 @@ tokenize_comment([_ | Rest])         -> tokenize_comment(Rest);
 tokenize_comment([])                 -> [].
 
 %% Atoms
-%% Handle atoms especially since they support @
+%% Handle atoms specially since they support @
 
 tokenize_atom(T, Acc) ->
   tokenize_atom(T, Acc, 0).