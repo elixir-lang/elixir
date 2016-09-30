b68bb4f24b3d47ef61d0c08673990795381403d8
James Fish <james@fishcakez.com>
2016-10-01 00:02:46 +0100

    Flush error_logger before capturing

--- lib/ex_unit/lib/ex_unit/capture_log.ex
+++ lib/ex_unit/lib/ex_unit/capture_log.ex
@@ -37,7 +37,14 @@ defmodule ExUnit.CaptureLog do
   Returns the binary which is the captured output.
 
   This function mutes the `:console` backend
-  and captures any log messages sent to Logger.
+  and captures any log messages sent to Logger from the calling
+  processes. It is possible to ensure explicit log messages from other
+  processes are captured by waiting for their exit or monitor
+  signal. This does not guarantee to capture VM generated log messages
+  when a process spawned using a `spawn` BIF does not catch an exception.
+  A `Task`, or other OTP process, will send explicit logs that are sent
+  before its exit or monitor signal and will not cause VM generated log
+  messages.
 
   Note that when the `async` is set to `true`,
   the messages from another test might be captured.
@@ -59,6 +66,7 @@ defmodule ExUnit.CaptureLog do
     {:ok, string_io} = StringIO.open("")
 
     try do
+      _ = :gen_event.which_handlers(:error_logger)
       :ok = add_capture(string_io, opts)
       ref = ExUnit.CaptureServer.log_capture_on(self())
 
