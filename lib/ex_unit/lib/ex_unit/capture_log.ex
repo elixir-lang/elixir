aa10dcf5e321be7bdc492e42a68669e5acb69442
James Fish <james@fishcakez.com>
2016-10-01 00:21:14 +0100

    Add more explicit description of cause of VM error log

--- lib/ex_unit/lib/ex_unit/capture_log.ex
+++ lib/ex_unit/lib/ex_unit/capture_log.ex
@@ -42,9 +42,10 @@ defmodule ExUnit.CaptureLog do
   processes are captured by waiting for their exit or monitor
   signal. This does not guarantee to capture VM generated log messages
   when a process is spawned using a low level `Kernel` spawn function (e.g.
-  `Kernel.spawn/1`) does not catch an exception. A `Task`, or other OTP
-  process, will send explicit logs that are sent before its exit or monitor
-  signal and will not cause VM generated log messages.
+  `Kernel.spawn/1`) raises an exception that isn't rescued or a thow that
+  isn't caught. A `Task`, orther OTP process, will send explicit logs that
+  are sent before its exit or monitor signal and will not cause VM generated
+  log messages.
 
   Note that when the `async` is set to `true`,
   the messages from another test might be captured.
