3bbccf7e415d6e7b18e9e9eac6a42ab3017e246d
José Valim <jose.valim@gmail.com>
2016-10-08 12:32:18 +0200

    Add @moduledoc false to application callback module

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -433,10 +433,12 @@ defmodule Mix.Tasks.New do
 
   embed_template :lib_app, """
   defmodule <%= @mod %>.Application do
-    use Application
-
     # See http://elixir-lang.org/docs/stable/elixir/Application.html
     # for more information on OTP Applications
+    @moduledoc false
+
+    use Application
+
     def start(_type, _args) do
       import Supervisor.Spec, warn: false
 
