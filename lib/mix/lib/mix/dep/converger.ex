3d1560f6ccfe5c544f1b804c46ee96554de9962a
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-11-04 15:55:38 +0100

    Add RemoteConverger.post_converge/0 callback (#5074)

--- lib/mix/lib/mix/dep/converger.ex
+++ lib/mix/lib/mix/dep/converger.ex
@@ -48,6 +48,7 @@ defmodule Mix.Dep.Converger do
   """
   def converge(acc, lock, opts, callback) do
     {deps, acc, lock} = all(acc, lock, opts, callback)
+    if remote = Mix.RemoteConverger.get, do: remote.post_converge()
     {topsort(deps), acc, lock}
   end
 
