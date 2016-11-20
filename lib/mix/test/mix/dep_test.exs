388c01d10bba2d1e3f2587a0265fb1ea19d2bcb5
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-11-20 14:57:03 +0100

    Fix converger order (#5452)
    Only reverse at the end instead of doing multiple reverses that will
    mess with the full order of dependencies.

--- lib/mix/test/mix/dep_test.exs
+++ lib/mix/test/mix/dep_test.exs
@@ -247,7 +247,8 @@ defmodule Mix.DepTest do
   defmodule IdentityRemoteConverger do
     @behaviour Mix.RemoteConverger
 
-    def remote?(_app), do: true
+    def remote?(%Mix.Dep{app: :deps_repo}), do: false
+    def remote?(%Mix.Dep{}), do: true
     def deps(_dep, _lock), do: []
     def post_converge, do: :ok
 
@@ -280,10 +281,11 @@ defmodule Mix.DepTest do
 
   test "pass dependencies to remote converger in defined order" do
     deps = [
-      {:ok,         "0.1.0", path: "deps/ok"},
+      {:ok, "0.1.0", path: "deps/ok"},
       {:invalidvsn, "0.2.0", path: "deps/invalidvsn"},
+      {:deps_repo, "0.1.0", path: "custom/deps_repo"},
       {:invalidapp, "0.1.0", path: "deps/invalidapp"},
-      {:noappfile,  "0.1.0", path: "deps/noappfile"}
+      {:noappfile, "0.1.0", path: "deps/noappfile"}
     ]
 
     with_deps deps, fn ->
@@ -293,7 +295,7 @@ defmodule Mix.DepTest do
         Mix.Tasks.Deps.Get.run([])
 
         deps = Process.get(:remote_converger) |> Enum.map(& &1.app)
-        assert deps == [:ok, :invalidvsn, :invalidapp, :noappfile]
+        assert deps == [:ok, :invalidvsn, :deps_repo, :invalidapp, :noappfile, :git_repo]
       end
     end
   after
