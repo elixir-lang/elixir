b54a5cd31d48a3d304f59974e3e4043023ee5158
Eksperimental <eksperimental@users.noreply.github.com>
2016-11-29 17:41:09 -0500

    Use https for tools.ietf.org links (#5491)

--- lib/elixir/lib/base.ex
+++ lib/elixir/lib/base.ex
@@ -3,7 +3,7 @@ defmodule Base do
 
   @moduledoc """
   This module provides data encoding and decoding functions
-  according to [RFC 4648](http://tools.ietf.org/html/rfc4648).
+  according to [RFC 4648](https://tools.ietf.org/html/rfc4648).
 
   This document defines the commonly used base 16, base 32, and base
   64 encoding schemes.
