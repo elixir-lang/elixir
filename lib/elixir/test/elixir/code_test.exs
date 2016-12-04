4024b937e2d61b2d31b8679e2e96198e048376a1
Andrea Leopardi <an.leopardi@gmail.com>
2016-12-04 20:52:50 +0100

    Tidy up tests for Code (#5531)
    * Better test names
    * Removed a few duplicate tests
    * Used a few describe/2 blocks to organize tests a bit more

--- lib/elixir/test/elixir/code_test.exs
+++ lib/elixir/test/elixir/code_test.exs
@@ -21,13 +21,50 @@ defmodule CodeTest do
 
   Code.eval_quoted contents, [], file: "sample.ex", line: 13
 
-  test "eval string" do
-    assert Code.eval_string("1 + 2") == {3, []}
-    assert {3, _} = Code.eval_string("a + b", [a: 1, b: 2], Macro.Env.location(__ENV__))
-  end
+  describe "eval_string/1-3" do
+    test "correctly evaluates a string of code" do
+      assert Code.eval_string("1 + 2") == {3, []}
+      assert Code.eval_string("two = 1 + 1") == {2, [two: 2]}
+    end
 
-  test "eval string with other context" do
-    assert Code.eval_string("var!(a, Sample) = 1") == {1, [{{:a, Sample}, 1}]}
+    test "supports a %Macro.Env{} struct as the third argument" do
+      assert {3, _} = Code.eval_string("a + b", [a: 1, b: 2], __ENV__)
+    end
+
+    test "can return bindings from a different context" do
+      assert Code.eval_string("var!(a, Sample) = 1") == {1, [{{:a, Sample}, 1}]}
+    end
+
+    test "supports unnamed scopes" do
+      assert {%RuntimeError{}, [a: %RuntimeError{}]} =
+        Code.eval_string("a = (try do (raise \"hello\") rescue e -> e end)")
+    end
+
+    test "supports the :requires option" do
+      assert Code.eval_string("Kernel.if true, do: :ok", [], requires: [Z, Kernel]) == {:ok, []}
+    end
+
+    test "with many options" do
+      options = [
+        functions: [{Kernel, [is_atom: 1]}],
+        macros: [{Kernel, [and: 2]}],
+        aliases: [{K, Kernel}],
+        requires: [Kernel],
+      ]
+
+      code = "is_atom(:foo) and K.is_list([])"
+
+      assert Code.eval_string(code, [], options) == {true, []}
+    end
+
+    test "yields the correct stacktrace" do
+      try do
+        Code.eval_string("<<a::size(b)>>", a: :a, b: :b)
+      rescue
+        _ ->
+          assert System.stacktrace |> Enum.any?(&(elem(&1, 0) == __MODULE__))
+      end
+    end
   end
 
   test "eval binary errors" do
@@ -40,43 +77,17 @@ defmodule CodeTest do
     end
   end
 
-  test "eval with unnamed scopes" do
-    assert {%RuntimeError{}, [a: %RuntimeError{}]} =
-           Code.eval_string("a = (try do (raise \"hello\") rescue e -> e end)")
-  end
-
-  test "eval options" do
-    assert Code.eval_string("is_atom(:foo) and K.is_list([])", [],
-                            functions: [{Kernel, [is_atom: 1]}],
-                            macros: [{Kernel, [..: 2, and: 2]}],
-                            aliases: [{K, Kernel}],
-                            requires: [Kernel]) == {true, []}
-  end
-
-  test "eval stacktrace" do
-    try do
-      Code.eval_string("<<a :: size(b)>>", a: :a, b: :b)
-    rescue
-      _ ->
-        assert System.stacktrace |> Enum.any?(&(elem(&1, 0) == __MODULE__))
-    end
-  end
-
-  test "eval with requires" do
-    assert Code.eval_string("Kernel.if true, do: :ok", [], requires: [Z, Kernel]) == {:ok, []}
-  end
-
-  test "eval quoted" do
+  test "eval_quoted/1" do
     assert Code.eval_quoted(quote(do: 1 + 2)) == {3, []}
     assert CodeTest.Sample.eval_quoted_info() == {CodeTest.Sample, "sample.ex", 13}
   end
 
-  test "eval quoted with env" do
+  test "eval_quoted/2 with a %Macro.Env{} struct as the second argument" do
     alias :lists, as: MyList
     assert Code.eval_quoted(quote(do: MyList.flatten [[1, 2, 3]]), [], __ENV__) == {[1, 2, 3], []}
   end
 
-  test "eval file" do
+  test "eval_file/1" do
     assert Code.eval_file(fixture_path("code_sample.exs")) == {3, [var: 3]}
 
     assert_raise Code.LoadError, fn ->
@@ -84,7 +95,7 @@ defmodule CodeTest do
     end
   end
 
-  test "require" do
+  test "require_file/1" do
     Code.require_file fixture_path("code_sample.exs")
     assert fixture_path("code_sample.exs") in Code.loaded_files
     assert Code.require_file(fixture_path("code_sample.exs")) == nil
@@ -94,23 +105,12 @@ defmodule CodeTest do
     assert Code.require_file(fixture_path("code_sample.exs")) != nil
   end
 
-  test "string to quoted" do
-    assert Code.string_to_quoted("1 + 2")  == {:ok, {:+, [line: 1], [1, 2]}}
-    assert Code.string_to_quoted!("1 + 2") == {:+, [line: 1], [1, 2]}
-
-    assert Code.string_to_quoted("a.1") ==
-           {:error, {1, "syntax error before: ", "1"}}
-
-    assert_raise SyntaxError, fn ->
-      Code.string_to_quoted!("a.1")
-    end
+  test "string_to_quoted/1" do
+    assert Code.string_to_quoted("1 + 2") == {:ok, {:+, [line: 1], [1, 2]}}
+    assert Code.string_to_quoted("a.1") == {:error, {1, "syntax error before: ", "1"}}
   end
 
-  test "string to quoted existing atoms only" do
-    assert :badarg = catch_error(Code.string_to_quoted!(":there_is_no_such_atom", existing_atoms_only: true))
-  end
-
-  test "string_to_quoted!" do
+  test "string_to_quoted!/1 works as string_to_quoted/1 but raises on errors" do
     assert Code.string_to_quoted!("1 + 2") == {:+, [line: 1], [1, 2]}
 
     assert_raise SyntaxError, fn ->
@@ -122,6 +122,10 @@ defmodule CodeTest do
     end
   end
 
+  test "string_to_quoted!/2 raises with the :existing_atoms_only option" do
+    assert catch_error(Code.string_to_quoted!(":there_is_no_such_atom", existing_atoms_only: true)) == :badarg
+  end
+
   test "compile source" do
     assert __MODULE__.__info__(:compile)[:source] == String.to_charlist(__ENV__.file)
   end
@@ -131,33 +135,28 @@ defmodule CodeTest do
     assert Keyword.get(compile, :source) != nil
   end
 
-  test "compile string works accross lexical scopes" do
-    assert [{CompileCrossSample, _}] = Code.compile_string("CodeTest.genmodule CompileCrossSample")
-  after
-    :code.purge CompileCrossSample
-    :code.delete CompileCrossSample
+  describe "compile_string/1" do
+    test "compiles the given string" do
+      assert [{CompileStringSample, _}] = Code.compile_string("defmodule CompileStringSample, do: :ok")
+    after
+      :code.purge CompileSimpleSample
+      :code.delete CompileSimpleSample
+    end
+
+    test "works across lexical scopes" do
+      assert [{CompileCrossSample, _}] = Code.compile_string("CodeTest.genmodule CompileCrossSample")
+    after
+      :code.purge CompileCrossSample
+      :code.delete CompileCrossSample
+    end
   end
 
-  test "compile string" do
-    assert [{CompileStringSample, _}] = Code.compile_string("defmodule CompileStringSample, do: :ok")
-  after
-    :code.purge CompileSimpleSample
-    :code.delete CompileSimpleSample
-  end
-
-  test "compile quoted" do
-    assert [{CompileQuotedSample, _}] = Code.compile_string("defmodule CompileQuotedSample, do: :ok")
-  after
-    :code.purge CompileQuotedSample
-    :code.delete CompileQuotedSample
-  end
-
-  test "ensure_loaded?" do
+  test "ensure_loaded?/1" do
     assert Code.ensure_loaded?(__MODULE__)
     refute Code.ensure_loaded?(Code.NoFile)
   end
 
-  test "ensure_compiled?" do
+  test "ensure_compiled?/1" do
     assert Code.ensure_compiled?(__MODULE__)
     refute Code.ensure_compiled?(Code.NoFile)
   end
