704fdf25d943e39ec05d8fc4b29a18203e3cbf38
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-12-07 02:44:38 +0000

    add Port.stream!/2

--- lib/elixir/test/elixir/port_test.exs
+++ lib/elixir/test/elixir/port_test.exs
@@ -2,6 +2,7 @@ Code.require_file "test_helper.exs", __DIR__
 
 defmodule PortTest do
   use ExUnit.Case, async: true
+  import PathHelpers
 
   test "info/1,2 with registered name" do
     {:ok, port} = :gen_udp.open(0)
@@ -17,5 +18,62 @@ defmodule PortTest do
 
     assert Port.info(port, :registered_name) == nil
     assert Port.info(port) == nil
+  end
+
+  test "stream map" do
+    stream = Port.stream!({:spawn, "echo"}, [:stream])
+
+    assert %Port.Stream{} = stream
+    assert stream.name == {:spawn, "echo"}
+    assert stream.settings == [:exit_status, :stream]
+    assert stream.raw == false
+
+    stream = Port.stream!({:spawn, "echo"}, [:stream], true)
+    assert stream.raw == true
+  end
+
+  test "stream simple" do
+    {name, settings} = case windows?() do
+      true  -> {{:spawn, "cmd /c echo hello"}, []}
+      false -> {{:spawn, "echo hello"}, []}
+    end
+
+    messages = Port.stream!(name, settings) |> Enum.into([])
+    assert messages == ['hello\n']
+  end
+
+  test "stream binary" do
+    {name, settings} = case windows?() do
+      true  -> {{:spawn, "cmd /c echo hello"}, [:binary]}
+      false -> {{:spawn, "echo hello"}, [:binary]}
+    end
+
+    stream = Port.stream!(name, settings)
+
+    transformed = Enum.map stream, fn(data) ->
+      String.replace(data, "e", "a")
+    end
+
+    assert transformed == ["hallo\n"]
+  end
+
+  test "stream line" do
+    {name, settings} = case windows?() do
+      true  -> {{:spawn, "cmd /c echo 'hello\nworld"}, [:binary, {:line, 2048}]}
+      false -> {{:spawn, "echo 'hello\nworld'"}, [:binary, {:line, 2048}]}
+    end
+
+    lines = Port.stream!(name, settings) |> Enum.into([])
+    assert lines == ["hello", "world"]
+  end
+
+  test "stream raw and line" do
+    {name, settings} = case windows?() do
+      true  -> {{:spawn, "cmd /c echo hello"}, [{:line, 2}]}
+      false -> {{:spawn, "echo hello"}, [{:line, 2}]}
+    end
+
+    messages = Port.stream!(name, settings, true) |> Enum.into([])
+    assert messages == [{:noeol, 'he'}, {:noeol, 'll'}, {:eol, 'o'}]
   end
 end
