194d212d0573b6e9ebea2fe8d34d069c958d921a
nowakp01 <Patryk.Nowak@stepstone.com>
2017-01-16 11:25:25 +0100

    enum.flat_map/2 description rephrased

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -931,11 +931,11 @@ defmodule Enum do
   end
 
   @doc """
-  Returns a new enumerable appending the result of invoking `fun` on
-  each corresponding item of `enumerable` and flattening
-  the given results (only one level deep).
+  Maps the given `fun` over `enumerable` and flattens the result.
 
-  The given function must return an enumerable.
+  This function returns a new enumerable built by appending the result of invoking `fun`
+  on each element of `enumerable` together; conceptually, this is similar to a
+  combination of `map/3` and `concat/1`.
 
   ## Examples
 
