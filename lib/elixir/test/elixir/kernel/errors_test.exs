7286ae50005d1d316d6f9cb4d0bd44b76be4bf64
Amos L King <amos.l.king@gmail.com>
2017-01-16 09:15:10 -0600

    Clarify invalid pattern in match error
    It was pointed out that `invalid expression in match` was unclear
    because the "expression" that is referred to should is not actually an
    expression. Since `case` can only match against a pattern it is clearer
    to say `invalid pattern in match`.
    
    Addresses Issue #5649
    
    Amos King @adkron <amos@binarynoggin.com>

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -223,7 +223,7 @@ defmodule Kernel.ErrorsTest do
 
   test "invalid match pattern" do
     assert_compile_fail CompileError,
-    "nofile:2: invalid expression in match",
+    "nofile:2: invalid pattern in match",
     '''
     case true do
       true && true -> true
