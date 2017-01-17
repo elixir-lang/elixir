2d29283a11421b2daedd004f6af772713259c054
James Fish <james@fishcakez.com>
2017-01-17 15:41:04 +0000

    Add logging to GenServer loop

--- lib/elixir/lib/gen_server/loop.ex
+++ lib/elixir/lib/gen_server/loop.ex
@@ -50,7 +50,12 @@ defmodule GenServer.Loop do
   end
 
   def system_terminate(reason, _, _, {mod, state, _}) do
-    handle_stop(reason, mod, state)
+    try do
+      throw(:terminate)
+    catch
+      :throw, :terminate ->
+        handle_exception(:exit, reason, System.stacktrace(), nil, mod, state)
+    end
   end
 
   ## Helpers
@@ -95,26 +100,33 @@ defmodule GenServer.Loop do
   end
 
   defp handle({:"$gen_call", from, req}, parent, dbg, mod, state, _) do
-    handle_sync(req, from, parent, dbg, mod, state)
+    sync(req, from, parent, dbg, mod, state)
   end
   defp handle({:"$gen_cast", req}, parent, dbg, mod, state, _) do
-    handle_async(:handle_cast, req, parent, dbg, mod, state)
+    async(:handle_cast, req, parent, dbg, mod, state)
   end
   defp handle({:system, from, req}, parent, dbg, mod, state, await) do
     misc = {mod, state, await}
     :sys.handle_system_msg(req, from, parent, __MODULE__, dbg, misc)
   end
+  defp handle({:EXIT, parent, reason}, parent, _, mod, state, _) do
+    try do
+      throw(:EXIT)
+    catch
+      :throw, :EXIT ->
+        handle_exception(:exit, reason, System.stacktrace, nil, mod, state)
+    end
+  end
   defp handle(msg, parent, dbg, mod, state, _) do
-    handle_async(:handle_info, msg, parent, dbg, mod, state)
+    async(:handle_info, msg, parent, dbg, mod, state)
   end
 
-  defp handle_sync(req, from, parent, dbg, mod, state) do
+  defp sync(req, from, parent, dbg, mod, state) do
     try do
       apply(mod, :handle_call, [req, from, state])
     catch
       kind, reason ->
-        reason = exit_reason(kind, reason, System.stacktrace())
-        handle_stop(reason, mod, state)
+        handle_exception(kind, reason, System.stacktrace(), req, mod, state)
     else
       {:reply, resp, state} ->
         :gen.reply(from, resp)
@@ -124,48 +136,86 @@ defmodule GenServer.Loop do
         loop(parent, dbg, mod, state, await)
       {:stop, reason, resp, state} ->
         :gen.reply(from, resp)
-        handle_stop(reason, mod, state)
+        handle_stop(reason, req, mod, state)
       other ->
-        handle_async(other, parent, dbg, mod, state)
+        handle_async(other, req, parent, dbg, mod, state)
     end
   end
 
-  defp handle_async(fun, msg, parent, dbg, mod, state) do
+  defp async(fun, msg, parent, dbg, mod, state) do
     try do
       apply(mod, fun, [msg, state])
     catch
       kind, reason ->
-        reason = exit_reason(kind, reason, System.stacktrace())
-        handle_stop(reason, mod, state)
+        handle_exception(kind, reason, System.stacktrace(), msg, mod, state)
     else
       return ->
-        handle_async(return, parent, dbg, mod, state)
+        handle_async(return, msg, parent, dbg, mod, state)
     end
   end
 
-  defp handle_async({:noreply, state}, parent, dbg, mod, _) do
+  defp handle_async({:noreply, state}, _, parent, dbg, mod, _) do
     loop(parent, dbg, mod, state, :infinity)
   end
-  defp handle_async({:noreply, state, await}, parent, dbg, mod, _) do
+  defp handle_async({:noreply, state, await}, _, parent, dbg, mod, _) do
     loop(parent, dbg, mod, state, await)
   end
-  defp handle_async({:stop, reason, state}, _, _, mod, _) do
-    handle_stop(reason, mod, state)
+  defp handle_async({:stop, reason, state}, msg, _, _, mod, _) do
+    handle_stop(reason, msg, mod, state)
   end
-  defp handle_async(other, _, _, mod, state) do
-    handle_stop({:bad_return_value, other}, mod, state)
+  defp handle_async(other, msg, _, _, mod, state) do
+    try do
+      throw(:bad_return_value)
+    catch
+      :bad_return_value ->
+        reason = {:bad_return_value, other}
+        handle_exception(:exit, reason, System.stacktrace(), msg, mod, state)
+    end
   end
 
-  defp handle_stop(reason, mod, state) do
+  defp handle_exception(kind, reason, stack, msg, mod, state) do
+    exit_reason = exit_reason(kind, reason, stack)
+    log_reason = log_reason(kind, reason, stack)
+    terminate(exit_reason, log_reason, msg, mod, state)
+  end
+
+  defp handle_stop(reason, msg, mod, state) do
+    terminate(reason, reason, msg, mod, state)
+  end
+
+  defp log_reason(:exit, reason, stack),  do: {reason, stack}
+  defp log_reason(:error, reason, stack), do: {reason, stack}
+  defp log_reason(:throw, value, stack),  do: {{:nocatch, value}, stack}
+
+  defp terminate(exit_reason, log_reason, msg, mod, state) do
     try do
-      apply(mod, :terminate, [reason, state])
+      apply(mod, :terminate, [exit_reason, state])
     catch
       kind, reason ->
-        reason = exit_reason(kind, reason, System.stacktrace())
-        exit(reason)
+        stack = System.stacktrace()
+        exit_reason = exit_reason(kind, reason, stack)
+        log_exit(exit_reason, log_reason(kind, reason, stack), msg, state)
     else
       _ ->
-        exit(reason)
+        handle_exit(exit_reason, log_reason, msg, state)
     end
+  end
+
+  defp handle_exit(:normal, _, _, _),                   do: exit(:normal)
+  defp handle_exit(:shutdown, _, _, _),                 do: exit(:shutdown)
+  defp handle_exit(shutdown = {:shutdown, _}, _, _, _), do: exit(shutdown)
+  defp handle_exit(exit_reason, log_reason, msg, state) do
+    log_exit(exit_reason, log_reason, msg, state)
+  end
+
+  defp log_exit(exit_reason, log_reason, msg, state) do
+    format = '** Generic server ~p terminating~n' ++
+             '** Last message in was ~p~n' ++
+             '** When Server state == ~p~n' ++
+             '** Reason for termination == ~n** ~p~n'
+    # TODO: log name
+    # TODO: format status on state
+    :error_logger.format(format, [self(), msg, state, log_reason])
+    exit(exit_reason)
   end
 end
