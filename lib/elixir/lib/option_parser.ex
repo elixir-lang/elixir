elixir-lang/elixir

commit 435c431b04195dd9d5834319d42a9922d2c827c8

Use clearer comment in OptionParser example (#5839)

lib/elixir/lib/option_parser.ex
@@ -130,7 +130,7 @@ defmodule OptionParser do
   never used anywhere:
 
       OptionParser.parse(["--option-parser-example"])
-      # Does nothing more...
+      # :option_parser_example atom is nowhere used bellow
 
   However, the code below does since the `:option_parser_example` atom is used
   at some point later (or earlier) on:

commit 27895f034e39cdf4a5278a03291609577cfb4933

Fix some documentation in OptionParser
[ci skip]

A typo-ed comment was introduced in 435c431b04195dd9d5834319d42a9922d2c827c8.

lib/elixir/lib/option_parser.ex
@@ -125,12 +125,12 @@ defmodule OptionParser do
   set to `true`.
 
   Since Elixir converts switches to atoms, the dynamic mode will only parse
-  switches that translates to atoms used by the runtime. Therefore, the code below
+  switches that translate to atoms used by the runtime. Therefore, the code below
   likely won't parse the given option since the `:option_parser_example` atom is
   never used anywhere:
 
       OptionParser.parse(["--option-parser-example"])
-      # :option_parser_example atom is nowhere used bellow
+      # The :option_parser_example atom is not used anywhere below
 
   However, the code below does since the `:option_parser_example` atom is used
   at some point later (or earlier) on: