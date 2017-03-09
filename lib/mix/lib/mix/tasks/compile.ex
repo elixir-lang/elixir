c55f92d78b04baaa274fb2d54bea557c444165d4
José Valim <jose.valim@plataformatec.com.br>
2017-03-09 01:31:27 +0100

    Clarify build_embedded

--- lib/mix/lib/mix/tasks/compile.ex
+++ lib/mix/lib/mix/tasks/compile.ex
@@ -17,8 +17,10 @@ defmodule Mix.Tasks.Compile do
       consolidation via the `compile.protocols` task. The default
       value is `true`.
 
-    * `:build_embedded` - when `true`, does not generate symlinks in
-      builds. Defaults to `false`.
+    * `:build_embedded` - when `true`, prepares the `_build` directory
+      for production by not using symlinks and optimizing paths. Once
+      enabled, implicit compilation is disabled and explicit calls
+      to `mix compile` is required. Defaults to `false`.
 
     * `:build_path` - the directory where build artifacts
       should be written to. This option is intended only for
