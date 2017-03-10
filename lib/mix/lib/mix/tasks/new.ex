1168b908fb1f4375f2266d4759a034ee1349b193
Gary Rennie <gazler@gmail.com>
2017-03-10 09:53:46 +0000

    Implicitly document dependency sources (#5858)

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -275,8 +275,8 @@ defmodule Mix.Tasks.New do
     # Run "mix help deps" to learn about dependencies.
     defp deps do
       [
-        # {:dep_example, "~> 0.3.0"},
-        # {:dep_example, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+        # {:dep_from_hexpm, "~> 0.3.0"},
+        # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       ]
     end
   end
@@ -310,8 +310,8 @@ defmodule Mix.Tasks.New do
     # Run "mix help deps" to learn about dependencies.
     defp deps do
       [
-        # {:dep_example, "~> 0.3.0"},
-        # {:dep_example, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+        # {:dep_from_hexpm, "~> 0.3.0"},
+        # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
         # {:sibling_app_in_umbrella, in_umbrella: true},
       ]
     end
