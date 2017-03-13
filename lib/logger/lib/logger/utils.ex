3f44012870e42c9e7d78985b956bb755dddf068f
Ben Johnson <bjohnson@binarylogic.com>
2017-03-13 13:41:48 -0500

    Format microseconds properly

--- lib/logger/lib/logger/utils.ex
+++ lib/logger/lib/logger/utils.ex
@@ -251,7 +251,7 @@ defmodule Logger.Utils do
   Formats time as chardata.
   """
   def format_time({hh, mi, ss, ms}) do
-    [pad2(hh), ?:, pad2(mi), ?:, pad2(ss), ?., pad3(ms)]
+    [pad2(hh), ?:, pad2(mi), ?:, pad2(ss), ?., ms]
   end
 
   @doc """
@@ -260,10 +260,6 @@ defmodule Logger.Utils do
   def format_date({yy, mm, dd}) do
     [Integer.to_string(yy), ?-, pad2(mm), ?-, pad2(dd)]
   end
-
-  defp pad3(int) when int < 10,  do: [?0, ?0, Integer.to_string(int)]
-  defp pad3(int) when int < 100, do: [?0, Integer.to_string(int)]
-  defp pad3(int), do: Integer.to_string(int)
 
   defp pad2(int) when int < 10, do: [?0, Integer.to_string(int)]
   defp pad2(int), do: Integer.to_string(int)
