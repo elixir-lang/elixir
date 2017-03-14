a040d7af8491d01de56386660b5fab570d5303ec
Thijs Klaver <TBK145@gmail.com>
2017-03-14 10:33:03 +0100

    Typo in uri.ex

--- lib/elixir/lib/uri.ex
+++ lib/elixir/lib/uri.ex
@@ -4,7 +4,7 @@ defmodule URI do
 
   This module provides functions for working with URIs (for example, parsing
   URIs or encoding query strings). The functions in this are implemented
-  acrroding to [RFC 3986](https://tools.ietf.org/html/rfc3986).
+  according to [RFC 3986](https://tools.ietf.org/html/rfc3986).
   """
 
   defstruct scheme: nil, path: nil, query: nil,
