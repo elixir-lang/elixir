e775cb3a3cd7ff8e38896d59fd0b45e5fdfd3ab0
eksperimental <eksperimental@users.noreply.github.com>
2017-03-30 21:06:45 +0700

    Add spec for edge-case in Integer.gcd/2

--- lib/elixir/lib/integer.ex
+++ lib/elixir/lib/integer.ex
@@ -404,6 +404,7 @@ defmodule Integer do
       0
 
   """
+  @spec gcd(0, 0) :: 0
   @spec gcd(integer, integer) :: pos_integer
   def gcd(int1, int2) when is_integer(int1) and is_integer(int2) do
     gcd_positive(abs(int1), abs(int2))
