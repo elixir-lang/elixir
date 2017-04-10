23f1fc5febf622ed02fc6207e0c492cbf18b089c
eksperimental <eksperimental@users.noreply.github.com>
2017-04-10 11:52:22 +0700

    Align descriptions of options in elixirc bin files
    It is aligned the same way as the elixir command.
    
    - It leaves 30 chars to the left.
    - Options should start with 2 white-spaces.

--- bin/elixirc.bat
+++ bin/elixirc.bat
@@ -14,12 +14,12 @@ goto run
 :documentation
 echo Usage: %~nx0 [elixir switches] [compiler switches] [.ex files]
 echo.
-echo  -o                   The directory to output compiled files
-echo  --no-docs            Does not attach documentation to compiled modules
-echo  --no-debug-info      Does not attach debug info to compiled modules
-echo  --verbose            Prints compilation status
-echo  --warnings-as-errors Treats warnings as errors and returns non-zero exit code
-echo  --ignore-module-conflict
+echo   -o                          The directory to output compiled files
+echo   --no-docs                   Does not attach documentation to compiled modules
+echo   --no-debug-info             Does not attach debug info to compiled modules
+echo   --verbose                   Prints compilation status
+echo   --warnings-as-errors        Treats warnings as errors and returns non-zero exit code
+echo   --ignore-module-conflict    Does not emit warnings if a module was previously defined
 echo.
 echo ** Options given after -- are passed down to the executed code
 echo ** Options can be passed to the Erlang runtime using ELIXIR_ERL_OPTIONS
