c8301e8cbeff894f5641eefb46e599cb1ab33baa
Aleksei Magusev <lexmag@me.com>
2017-04-23 14:23:08 +0200

    Improve docs for archive.install, and escript.install Mix tasks

--- lib/mix/lib/mix/tasks/archive.install.ex
+++ lib/mix/lib/mix/tasks/archive.install.ex
@@ -34,22 +34,23 @@ defmodule Mix.Tasks.Archive.Install do
 
       mix some_task
 
-  Note that installing via git/github/hex fetches the source of the archive
+  Note that installing via Git/GitHub/Hex fetches the source of the archive
   and builds it, while using URL/local path fetches a pre-built archive.
 
   ## Command line options
 
     * `--sha512` - checks the archive matches the given SHA-512 checksum. Only
-      applies to installations via URL or local path.
+      applies to installations via URL or local path
 
     * `--force` - forces installation without a shell prompt; primarily
-      intended for automation in build systems like `make`
+      intended for automation in build systems like Make
 
     * `--submodules` - fetches repository submodules before building archive from
-      git or github
+      Git or GitHub
 
     * `--app` - specifies a custom app name to be used for building the archive
-      from git, github, or hex
+      from Git, GitHub, or Hex
+
   """
 
   @behaviour Mix.Local.Installer
