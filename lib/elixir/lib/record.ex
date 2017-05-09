290e002ab8ff2eda3d536c1c32097326b96d02ee
Andrea Leopardi <an.leopardi@gmail.com>
2017-05-09 21:31:50 +0200

    Properly escape fields passed to defrecord (#6086)

--- lib/elixir/lib/record.ex
+++ lib/elixir/lib/record.ex
@@ -319,6 +319,7 @@ defmodule Record do
       Keyword.keyword?(args) ->
         create(tag, fields, args, caller)
       true ->
+        fields = Macro.escape(fields)
         case Macro.expand(args, caller) do
           {:{}, _, [^tag | list]} when length(list) == length(fields) ->
             record = List.to_tuple([tag | list])
