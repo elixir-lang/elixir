dced1f2360a034a95404407ac391ac28ad3d5d69
Eksperimental <eksperimental@users.noreply.github.com>
2017-05-14 14:33:12 -0500

    Use inspect when displaying file paths and extensions (#6106)
    Related to https://github.com/elixir-lang/elixir/pull/6007

--- lib/mix/lib/mix/local.ex
+++ lib/mix/lib/mix/local.ex
@@ -128,7 +128,7 @@ defmodule Mix.Local do
       |> find_latest_eligible_version
     else
       Mix.raise "Could not install #{name} because Mix could not verify authenticity " <>
-                "of metadata file at #{path}. This may happen because a proxy or some " <>
+                "of metadata file at #{inspect(path)}. This may happen because a proxy or some " <>
                 "entity is interfering with the download or because you don't have a " <>
                 "public key to verify the download.\n\nYou may try again later or check " <>
                 "if a new public key has been released in our public keys page: #{@public_keys_html}"
