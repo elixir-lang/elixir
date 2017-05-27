f8a7f0a5ae5a268ebf27c57ad8da348b09c55f7a
Justin DeMaris <justin.demaris@gmail.com>
2017-05-27 04:32:51 -0400

    Clarify Enum.uniq_by/2 function parameter (#6156)

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -2584,8 +2584,9 @@ defmodule Enum do
   Enumerates the `enumerable`, by removing the elements for which
   function `fun` returned duplicate items.
 
-  The function `fun` maps every element to a term which is used to
-  determine if two elements are duplicates.
+  The function `fun` maps every element to a term. Two elements are
+  considered duplicates if the return value of `fun` is equal for
+  both of them.
 
   The first occurrence of each element is kept.
 
