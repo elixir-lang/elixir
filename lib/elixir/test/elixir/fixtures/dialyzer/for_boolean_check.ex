1955ab1adc7fc7139bb2820ce4d2f744c847a4ce
James Fish <james@fishcakez.com>
2017-05-30 19:53:47 +0100

    Add for with boolean check dialyzer test

---
+++ lib/elixir/test/elixir/fixtures/dialyzer/for_boolean_check.ex
@@ -0,0 +1,9 @@
+defmodule Dialyzer.ForBooleanCheck do
+  def foo(enum, potential) when is_binary(potential) do
+    for element <- enum,
+        string = Atom.to_string(element),
+        string == potential do
+      element
+    end
+  end
+end
