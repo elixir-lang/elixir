5281ff4fb6c1db7a6f7d5c75df5c30a6ab953c04
José Valim <jose.valim@plataformatec.com.br>
2017-06-08 17:33:52 +0200

    Do not leak EXIT messages on Registry dispatch

--- lib/elixir/test/elixir/registry_test.exs
+++ lib/elixir/test/elixir/registry_test.exs
@@ -221,6 +221,8 @@ defmodule RegistryTest do
       end
 
       test "dispatches to multiple keys", %{registry: registry} do
+        Process.flag(:trap_exit, true)
+
         assert Registry.dispatch(registry, "hello", fn _ ->
           raise "will never be invoked"
         end) == :ok
@@ -244,6 +246,8 @@ defmodule RegistryTest do
         refute_received {:dispatch, :value1}
         refute_received {:dispatch, :value2}
         assert_received {:dispatch, :value3}
+
+        refute_received {:EXIT, _, _}
       end
 
       test "allows process unregistering", %{registry: registry} do
