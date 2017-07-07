384b5c618799eb4513d851f7ed4e3731eea00fe2
uohzxela <uohzxela@gmail.com>
2017-07-08 06:58:50 +0800

    Break to next line after equality operator

--- lib/elixir/test/elixir/code_test.exs
+++ lib/elixir/test/elixir/code_test.exs
@@ -101,16 +101,16 @@ defmodule CodeTest do
   end
 
   test "string_to_quoted/1 for presence of sigils terminators" do
-    assert Code.string_to_quoted("~r/foo/") == {:ok, {:sigil_r, [terminator: '/', line: 1],
-                                                [{:<<>>, [line: 1], ["foo"]}, []]}}
-    assert Code.string_to_quoted("~r[foo]") == {:ok, {:sigil_r, [terminator: '[', line: 1],
-                                                [{:<<>>, [line: 1], ["foo"]}, []]}}
-    assert Code.string_to_quoted("~r\"foo\"") == {:ok, {:sigil_r, [terminator: '"', line: 1],
-                                                  [{:<<>>, [line: 1], ["foo"]}, []]}}
-    assert Code.string_to_quoted("~S\"\"\"\nsigil heredoc\n\"\"\"") == {:ok, {:sigil_S, [terminator: '"""', line: 1],
-                                                                        [{:<<>>, [line: 1], ["sigil heredoc\n"]}, []]}}
-    assert Code.string_to_quoted("~S'''\nsigil heredoc\n'''") == {:ok, {:sigil_S, [terminator: '\'\'\'', line: 1],
-                                                                  [{:<<>>, [line: 1], ["sigil heredoc\n"]}, []]}}
+    assert Code.string_to_quoted("~r/foo/") ==
+           {:ok, {:sigil_r, [terminator: '/', line: 1], [{:<<>>, [line: 1], ["foo"]}, []]}}
+    assert Code.string_to_quoted("~r[foo]") ==
+           {:ok, {:sigil_r, [terminator: '[', line: 1], [{:<<>>, [line: 1], ["foo"]}, []]}}
+    assert Code.string_to_quoted("~r\"foo\"") ==
+           {:ok, {:sigil_r, [terminator: '"', line: 1], [{:<<>>, [line: 1], ["foo"]}, []]}}
+    assert Code.string_to_quoted("~S\"\"\"\nsigil heredoc\n\"\"\"") ==
+           {:ok, {:sigil_S, [terminator: '"""', line: 1], [{:<<>>, [line: 1], ["sigil heredoc\n"]}, []]}}
+    assert Code.string_to_quoted("~S'''\nsigil heredoc\n'''") ==
+           {:ok, {:sigil_S, [terminator: '\'\'\'', line: 1], [{:<<>>, [line: 1], ["sigil heredoc\n"]}, []]}}
   end
 
   test "string_to_quoted!/1 works as string_to_quoted/1 but raises on errors" do
