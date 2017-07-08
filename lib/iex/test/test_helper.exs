f3f22335a349f34fd857549b30c6fff0fdd54e14
José Valim <jose.valim@plataformatec.com.br>
2017-07-08 14:05:37 +0200

    Add IEx.Helpers.open/0 and open/1

--- lib/iex/test/test_helper.exs
+++ lib/iex/test/test_helper.exs
@@ -2,6 +2,8 @@ assert_timeout = String.to_integer(
   System.get_env("ELIXIR_ASSERT_TIMEOUT") || "500"
 )
 
+System.put_env("ELIXIR_EDITOR", "echo")
+
 :ok = Application.start(:iex)
 IEx.configure([colors: [enabled: false]])
 ExUnit.start [trace: "--trace" in System.argv, assert_receive_timeout: assert_timeout]
