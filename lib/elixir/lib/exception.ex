5f735de4ca86be1b519bd1ade26fc1ead578f380
José Valim <jose.valim@plataformatec.com.br>
2017-07-13 15:46:50 +0200

    Do not pass kind to definition_scope

--- lib/elixir/lib/exception.ex
+++ lib/elixir/lib/exception.ex
@@ -230,7 +230,7 @@ defmodule Exception do
          {_, kind, _, clauses} <- List.keyfind(defs, {function, arity}, 0) do
       clauses =
         for {meta, ex_args, guards, _block} <- clauses do
-          scope = :elixir_erl.definition_scope(meta, kind, "nofile")
+          scope = :elixir_erl.definition_scope(meta, "nofile")
           {erl_args, scope} =
             :elixir_erl_clauses.match(&:elixir_erl_pass.translate_args/2, ex_args, scope)
           {args, binding} =
