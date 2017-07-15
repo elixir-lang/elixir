b764d94ce4c87ca09f7dea327e34e37562072522
Wojtek Mach <wojtekmach@users.noreply.github.com>
2017-07-16 01:31:47 +0200

    Minor updates to Calendar.ISO and Calendar.Holocene fixture (#6331)
    * Implement remaining callbacks for Calendar.Holocene
    * Add "@impl true" to Calendar.ISO
    * Add "@impl true" to Calendar.Holocene

--- lib/elixir/test/elixir/fixtures/calendar/holocene.exs
+++ lib/elixir/test/elixir/fixtures/calendar/holocene.exs
@@ -3,6 +3,8 @@ defmodule Calendar.Holocene do
   # It implements the Holocene calendar, which is based on the
   # Propleptic Gregorian calendar with every year + 10000.
 
+  @behaviour Calendar
+
   def date(year, month, day) do
     %Date{year: year, month: month, day: day, calendar: __MODULE__}
   end
@@ -12,33 +14,63 @@ defmodule Calendar.Holocene do
                    microsecond: microsecond, calendar: __MODULE__}
   end
 
+  @impl true
   def date_to_string(year, month, day) do
     "#{year}-#{month}-#{day} (HE)"
   end
 
+  @impl true
   def naive_datetime_to_string(year, month, day, hour, minute, second, microsecond) do
-    "#{year}-#{month}-#{day} #{Calendar.ISO.time_to_string(hour, minute, second, microsecond)} (HE)"
+    "#{year}-#{month}-#{day}" <>
+      Calendar.ISO.time_to_string(hour, minute, second, microsecond) <> " (HE)"
   end
 
+  @impl true
+  def datetime_to_string(year, month, day,
+                         hour, minute, second, microsecond,
+                         _time_zone, zone_abbr, _utc_offset, _std_offset) do
+    "#{year}-#{month}-#{day}" <>
+      Calendar.ISO.time_to_string(hour, minute, second, microsecond) <> " #{zone_abbr} (HE)"
+  end
+
+  @impl true
   defdelegate time_to_string(hour, minute, second, microsecond), to: Calendar.ISO
 
+  @impl true
   def day_rollover_relative_to_midnight_utc(), do: {0, 1}
 
+  @impl true
   def naive_datetime_from_iso_days(entry) do
     {year, month, day, hour, minute, second, microsecond} =
       Calendar.ISO.naive_datetime_from_iso_days(entry)
     {year + 10000, month, day, hour, minute, second, microsecond}
   end
 
+  @impl true
   def naive_datetime_to_iso_days(year, month, day, hour, minute, second, microsecond) do
     Calendar.ISO.naive_datetime_to_iso_days(year - 10000, month, day, hour, minute, second, microsecond)
   end
 
+  @impl true
   defdelegate time_from_day_fraction(day_fraction), to: Calendar.ISO
 
+  @impl true
   defdelegate time_to_day_fraction(hour, minute, second, microsecond), to: Calendar.ISO
 
+  @impl true
   defdelegate leap_year?(year), to: Calendar.ISO
 
+  @impl true
   defdelegate days_in_month(year, month), to: Calendar.ISO
+
+  @impl true
+  defdelegate day_of_week(year, month, day), to: Calendar.ISO
+
+  @impl true
+  def valid_date?(year, month, day) do
+    :calendar.valid_date(year, month, day)
+  end
+
+  @impl true
+  defdelegate valid_time?(hour, minute, second, microsecond), to: Calendar.ISO
 end
