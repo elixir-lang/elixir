92a3bf76a396988af0acf02fde6aa3c6d85a4d70
Michał Muskała <michal@muskala.eu>
2017-07-16 12:50:21 +0200

    Use orelse instead of or when expanding in operator (#6314)
    orelse may lead to better code in many occasions - Since we're comparing
    to literals, there's no semantic difference.
    
    This also means, the compiler is able to figure out more "will always match"
    checks. For example in a situation like the following a warning will be emitted.
    This pull request also fixes such warnings in the test suite.
    
        if true do
          IO.puts("true")
        end
    
    One problem with using `orelse` is that it will short-circuit and not evaluate
    some side-effecting things, for example:
    
        1 in [1, send(self(), :foo)]
    
    To remedy this, we'll traverse the right side of `in` and extract any things
    that are not simple literals to variables, so that the above example expands
    to something like the following:
    
        var0 = send(seld(), :foo)
        var = 1
        var === 1 or var === var0
    
    This guarantees all side-effects are performed and we still retain benefits of
    the short-circuiting comparisons in the end.

--- lib/elixir/test/erlang/control_test.erl
+++ lib/elixir/test/erlang/control_test.erl
@@ -206,7 +206,7 @@ optimized_andand_test() ->
   {'case', _, _,
     [{clause, _,
       [{var, _, Var}],
-      [[{op, _, 'or', _, _}]],
+      [[{op, _, 'orelse', _, _}]],
       [{var, _, Var}]},
     {clause, _, [{var, _, '_'}], [], [{atom, 0, done}]}]
   } = to_erl("is_list([]) && :done").
@@ -215,7 +215,7 @@ optimized_oror_test() ->
   {'case', _, _,
     [{clause, 1,
       [{var, 1, _}],
-      [[{op, 1, 'or', _, _}]],
+      [[{op, 1, 'orelse', _, _}]],
       [{atom, 0, done}]},
     {clause, 1, [{var, 1, Var}], [], [{var, 1, Var}]}]
   } = to_erl("is_list([]) || :done").
