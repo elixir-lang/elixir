b27db522e0ed90728585e0bc3a2f74aba885a210
José Valim <jose.valim@plataformatec.com.br>
2017-07-26 16:44:33 +0200

    Make sure both old and new specs can play together at the supervisor level

--- lib/elixir/lib/supervisor/default.ex
+++ lib/elixir/lib/supervisor/default.ex
@@ -1,10 +1,6 @@
 defmodule Supervisor.Default do
   @moduledoc false
 
-  def init({[{_, _, _, _, _, _} | _] = children, opts}) do
-    Supervisor.Spec.supervise(children, opts)
-  end
-
   def init({children, opts}) do
     Supervisor.init(children, opts)
   end
