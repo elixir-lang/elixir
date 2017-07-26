1edc5ec2555c0fa0a61de40280b30cce70b144aa
ohr486 <ohr486@gmail.com>
2017-07-26 22:35:38 +0900

    Fix small typo in GenServer,Supervisor,Task doc (#6384)

--- lib/elixir/lib/gen_server.ex
+++ lib/elixir/lib/gen_server.ex
@@ -67,7 +67,7 @@ defmodule GenServer do
   defined module to be put under a supervision tree. The generated
   `child_spec/1` can be customized with the following options:
 
-    * `:id` - the child specification id, defauts to the current module
+    * `:id` - the child specification id, defaults to the current module
     * `:start` - how to start the child process (defaults to calling `__MODULE__.start_link/1`)
     * `:restart` - when the child should be restarted, defaults to `:permanent`
     * `:shutdown` - how to shut down the child
