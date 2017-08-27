a3781546529ec7b096651fa3f442041b896e8eb5
José Valim <jose.valim@plataformatec.com.br>
2017-08-27 12:39:40 +0200

    Make terminator binaries

--- lib/elixir/src/elixir_errors.erl
+++ lib/elixir/src/elixir_errors.erl
@@ -84,6 +84,8 @@ parse_error(Line, File, <<"syntax error before: ">>, <<"eol">>) ->
 parse_error(Line, File, <<"syntax error before: ">>, <<"'end'">>) ->
   raise(Line, File, 'Elixir.SyntaxError', <<"unexpected token: end">>);
 
+%% TODO: Remove workarounds for sigils, aliases, binaries and interpolation.
+
 %% Produce a human-readable message for errors before a sigil
 parse_error(Line, File, <<"syntax error before: ">>, <<"{sigil,", _Rest/binary>> = Full) ->
   {sigil, _, Sigil, [Content | _], _, _} = parse_erl_term(Full),
