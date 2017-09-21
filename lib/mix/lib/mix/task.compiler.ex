767d690adc8c8130c5e746ac7392059a26c60b0f
Jake Becker <jake@jakebecker.org>
2017-09-21 14:11:59 -0700

    Define typespec for compiler diagnostics and return them from Mix.Compilers.Elixir
    Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

--- lib/mix/lib/mix/task.compiler.ex
+++ lib/mix/lib/mix/task.compiler.ex
@@ -14,6 +14,12 @@ defmodule Mix.Task.Compiler do
         end
       end
 
+  The `run/1` function returns an atom indicating the status of the
+  compilation, and optionally can also return a list of "diagnostics"
+  such as warnings or compilation errors. Doing this enables code
+  editors to display issues inline without having to analyze the
+  command-line output.
+
   If the compiler uses manifest files to track stale sources, it should
   define `manifests/0`, and if it writes any output to disk it should
   also define `clean/0`.
@@ -22,11 +28,65 @@ defmodule Mix.Task.Compiler do
   documentation as a regular Mix task. See `Mix.Task` for more information.
   """
 
+  defmodule Diagnostic do
+    @moduledoc """
+    Diagnostic information such as a warning or compilation error.
+    """
+
+    @type t :: %__MODULE__{
+      file: Path.t,
+      severity: severity,
+      message: String.t,
+      position: position,
+      compiler_name: String.t,
+      details: any
+    }
+
+    @typedoc """
+    Severity of a diagnostic:
+
+      * `:error` - An issue that caused compilation to fail
+
+      * `:warning` - An issue that did not cause failure but suggests the
+        programmer may have made a mistake
+
+      * `:hint` - A suggestion for style or good practices that is not as
+        severe as a warning
+
+      * `:information` - Any other information relevant to compilation that
+        does not fit into the above categories
+
+    """
+    @type severity :: :error | :warning | :information | :hint
+
+    @typedoc """
+    Where in a file the diagnostic applies. Can be either a line number,
+    a range specified as `{start_line, start_col, end_line, end_col}`,
+    or `nil` if unknown.
+
+    Line numbers are 1-based, and column numbers in a range are 0-based and refer
+    to the cursor position at the start of the character at that index. For example,
+    to indicate that a diagnostic applies to the first `n` characters of the
+    first line, the range would be `{1, 0, 1, n}`.
+    """
+    @type position ::
+            nil
+            | pos_integer
+            | {pos_integer, non_neg_integer, pos_integer, non_neg_integer}
+
+    @enforce_keys [:file, :severity, :message, :position, :compiler_name]
+    defstruct [:file, :severity, :message, :position, :compiler_name, :details]
+  end
+
   @doc """
-  Receives command-line arguments and performs compilation. Returns `:noop`
-  if nothing is stale and no compilation is needed, `:ok` if successful.
+  Receives command-line arguments and performs compilation. If it
+  produces errors, warnings, or any other diagnostic information,
+  it should return a tuple with the status and a list of diagnostics.
   """
-  @callback run([binary]) :: :ok | :noop
+  @callback run([binary]) ::
+              :ok
+              | :noop
+              | {:ok | :noop | :error, [Diagnostic.t]}
 
   @doc """
   Lists manifest files for the compiler.
