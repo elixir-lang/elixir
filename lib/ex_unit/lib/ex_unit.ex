62d97b152282ea9c5b65e89d3db5c53a5a523891
Glauber Campinho <ggcampinho@gmail.com>
2017-09-26 11:55:57 +0200

    Use a different random seed for each ExUnit test (#6593)

--- lib/ex_unit/lib/ex_unit.ex
+++ lib/ex_unit/lib/ex_unit.ex
@@ -222,7 +222,10 @@ defmodule ExUnit do
     * `:refute_receive_timeout` - the timeout to be used on `refute_receive`
       calls, defaults to `100` milliseconds;
 
-    * `:seed` - an integer seed value to randomize the test suite;
+    * `:seed` - an integer seed value to randomize the test suite. This seed
+      is also mixed with the test module and name to create a new unique seed
+      on every test, which is automatically fed into the `:rand` module. This
+      provides randomness between tests, but predictable and reproducible results;
 
     * `:slowest` - prints timing information for the N slowest tests. Running
       ExUnit with slow test reporting automatically runs in `trace` mode. It
