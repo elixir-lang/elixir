443679c31466bd9502ae5ba8f7b2e8060dda9b40
José Valim <jose.valim@plataformatec.com.br>
2017-10-07 14:40:32 +0200

    Fix bootstraping

--- lib/elixir/lib/code.ex
+++ lib/elixir/lib/code.ex
@@ -305,10 +305,8 @@ defmodule Code do
   """
   def format_string!(string, opts \\ []) when is_binary(string) and is_list(opts) do
     line_length = Keyword.get(opts, :line_length, 98)
-
-    string
-    |> Code.Formatter.to_algebra!(opts)
-    |> Inspect.Algebra.format(line_length)
+    algebra = Code.Formatter.to_algebra!(string, opts)
+    Inspect.Algebra.format(algebra, line_length)
   end
 
   @doc """
@@ -318,14 +316,9 @@ defmodule Code do
   available options.
   """
   def format_file!(file, opts \\ []) when is_binary(file) and is_list(opts) do
-    file
-    |> File.read!
-    |> format_string!([file: file, line: 1] ++ opts)
-    |> cons_newline()
-  end
-
-  defp cons_newline(arg) do
-    [arg, ?\n]
+    string = File.read!(file)
+    formatted = format_string!(string, [file: file, line: 1] ++ opts)
+    [formatted, ?\n]
   end
 
   @doc """
