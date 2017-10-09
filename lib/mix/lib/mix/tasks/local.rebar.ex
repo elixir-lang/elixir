da2db9ae4ea6d43ba5606ab0483960170c920e5a
Fumihiro Xue <xfumihiro@gmail.com>
2017-10-09 06:49:24 -0500

    Run the code formatter on Mix.Tasks.Local.Rebar (#6665)

--- lib/mix/lib/mix/tasks/local.rebar.ex
+++ lib/mix/lib/mix/tasks/local.rebar.ex
@@ -1,12 +1,12 @@
 defmodule Mix.Tasks.Local.Rebar do
   use Mix.Task
 
-  @rebar2_list_url     "/installs/rebar-1.x.csv"
-  @rebar2_escript_url  "/installs/[ELIXIR_VERSION]/rebar-[REBAR_VERSION]"
-  @rebar3_list_url     "/installs/rebar3-1.x.csv"
-  @rebar3_escript_url  "/installs/[ELIXIR_VERSION]/rebar3-[REBAR_VERSION]"
+  @rebar2_list_url "/installs/rebar-1.x.csv"
+  @rebar2_escript_url "/installs/[ELIXIR_VERSION]/rebar-[REBAR_VERSION]"
+  @rebar3_list_url "/installs/rebar3-1.x.csv"
+  @rebar3_escript_url "/installs/[ELIXIR_VERSION]/rebar3-[REBAR_VERSION]"
 
-  @shortdoc  "Installs Rebar locally"
+  @shortdoc "Installs Rebar locally"
 
   @moduledoc """
   Fetches a copy of `rebar` or `rebar3` from the given path or URL.
@@ -47,15 +47,20 @@ defmodule Mix.Tasks.Local.Rebar do
 
     case argv do
       ["rebar", path | _] ->
-       install_from_path(:rebar, path, opts)
+        install_from_path(:rebar, path, opts)
+
       ["rebar3", path | _] ->
-       install_from_path(:rebar3, path, opts)
+        install_from_path(:rebar3, path, opts)
+
       [] ->
         install_from_s3(:rebar, @rebar2_list_url, @rebar2_escript_url, opts)
         install_from_s3(:rebar3, @rebar3_list_url, @rebar3_escript_url, opts)
+
       _ ->
-        Mix.raise "Invalid arguments given to mix local.rebar. " <>
-                  "To find out the proper call syntax run \"mix help local.rebar\""
+        Mix.raise(
+          "Invalid arguments given to mix local.rebar. " <>
+            "To find out the proper call syntax run \"mix help local.rebar\""
+        )
     end
   end
 
@@ -68,13 +73,16 @@ defmodule Mix.Tasks.Local.Rebar do
           File.mkdir_p!(Path.dirname(local))
           File.write!(local, binary)
           File.chmod!(local, 0o755)
-          Mix.shell.info [:green, "* creating ", :reset, Path.relative_to_cwd(local)]
+          Mix.shell().info([:green, "* creating ", :reset, Path.relative_to_cwd(local)])
+
         :badpath ->
-          Mix.raise "Expected #{inspect path} to be a URL or a local file path"
+          Mix.raise("Expected #{inspect(path)} to be a URL or a local file path")
+
         {:local, message} ->
-          Mix.raise message
+          Mix.raise(message)
+
         {kind, message} when kind in [:remote, :checksum] ->
-          Mix.raise """
+          Mix.raise("""
           #{message}
 
           Could not fetch #{manager} at:
@@ -84,7 +92,7 @@ defmodule Mix.Tasks.Local.Rebar do
           Please download the file above manually to your current directory and run:
 
               mix local.rebar #{manager} ./#{Path.basename(local)}
-          """
+          """)
       end
     end
 
@@ -92,7 +100,7 @@ defmodule Mix.Tasks.Local.Rebar do
   end
 
   defp install_from_s3(manager, list_url, escript_url, opts) do
-    hex_mirror = Mix.Hex.mirror
+    hex_mirror = Mix.Hex.mirror()
     list_url = hex_mirror <> list_url
 
     {elixir_version, rebar_version, sha512} =
