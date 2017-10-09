5b06cf406f9e42bdc26a6746c4504a424993d7d9
Dave Lucia <davelucianyc@gmail.com>
2017-10-09 17:08:59 -0400

    Run code formatter on lib/mix/test/fixtures/test_stale/mix.exs (#6764)

--- lib/mix/test/fixtures/test_stale/mix.exs
+++ lib/mix/test/fixtures/test_stale/mix.exs
@@ -2,8 +2,10 @@ defmodule TestStale.MixProject do
   use Mix.Project
 
   def project do
-    [app: :test_stale,
-     version: "0.0.1",
-     test_pattern: "*_test_stale.exs"]
+    [
+      app: :test_stale,
+      version: "0.0.1",
+      test_pattern: "*_test_stale.exs"
+    ]
   end
 end
