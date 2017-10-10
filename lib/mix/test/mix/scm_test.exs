fbf429749abaa1963c9a36ccd7b819a9cad2397f
José Valim <jose.valim@plataformatec.com.br>
2017-10-10 18:09:59 +0200

    Format more low hanging fruit files

--- lib/mix/test/mix/scm_test.exs
+++ lib/mix/test/mix/scm_test.exs
@@ -1,21 +1,21 @@
-Code.require_file "../test_helper.exs", __DIR__
+Code.require_file("../test_helper.exs", __DIR__)
 
 defmodule Mix.SCMTest do
   use MixTest.Case
 
   setup do
-    available = Mix.SCM.available
-    on_exit fn -> Mix.State.put(:scm, available) end
+    available = Mix.SCM.available()
+    on_exit(fn -> Mix.State.put(:scm, available) end)
     :ok
   end
 
   test "prepends an SCM" do
     Mix.SCM.prepend(Hello)
-    assert Enum.at(Mix.SCM.available, 0) == Hello
+    assert Enum.at(Mix.SCM.available(), 0) == Hello
   end
 
   test "appends an SCM" do
     Mix.SCM.append(Hello)
-    assert Enum.at(Mix.SCM.available, -1) == Hello
+    assert Enum.at(Mix.SCM.available(), -1) == Hello
   end
 end
--- lib/mix/test/mix_test.exs
+++ lib/mix/test/mix_test.exs
@@ -1,22 +1,22 @@
-Code.require_file "test_helper.exs", __DIR__
+Code.require_file("test_helper.exs", __DIR__)
 
 defmodule MixTest do
   use MixTest.Case
 
   test "shell" do
-    assert Mix.shell == Mix.Shell.Process
+    assert Mix.shell() == Mix.Shell.Process
   end
 
   test "env" do
-    assert Mix.env == :dev
+    assert Mix.env() == :dev
     Mix.env(:prod)
-    assert Mix.env == :prod
+    assert Mix.env() == :prod
   end
 
   test "debug" do
-    refute Mix.debug?
+    refute Mix.debug?()
     Mix.debug(true)
-    assert Mix.debug?
+    assert Mix.debug?()
     Mix.debug(false)
   end
 end
