6bb5e79680d2c5384c6e1e562bb59c55fb534df3
Jean Matheus Souto <jeanmatheussouto@gmail.com>
2017-10-10 13:54:20 -0300

    Run the code formatter on lib/mix/lib/mix/tasks/compile.yecc.ex (#6825)

--- lib/mix/lib/mix/tasks/compile.yecc.ex
+++ lib/mix/lib/mix/tasks/compile.yecc.ex
@@ -44,30 +44,33 @@ defmodule Mix.Tasks.Compile.Yecc do
   def run(args) do
     {opts, _, _} = OptionParser.parse(args, switches: @switches)
 
-    project = Mix.Project.config
+    project = Mix.Project.config()
 
     source_paths = project[:erlc_paths]
     Mix.Compilers.Erlang.assert_valid_erlc_paths(source_paths)
     mappings = Enum.zip(source_paths, source_paths)
 
     options = project[:yecc_options] || []
+
     unless is_list(options) do
-      Mix.raise ":yecc_options should be a list of options, got: #{inspect(options)}"
+      Mix.raise(":yecc_options should be a list of options, got: #{inspect(options)}")
     end
 
-    Erlang.compile(manifest(), mappings, :yrl, :erl, opts, fn
-      input, output ->
-        Erlang.ensure_application!(:parsetools, input)
-        options = options ++ @forced_opts ++ [parserfile: Erlang.to_erl_file(output)]
-        :yecc.file(Erlang.to_erl_file(input), options)
-    end)
+    yecc_file = fn input, output ->
+      Erlang.ensure_application!(:parsetools, input)
+      options = options ++ @forced_opts ++ [parserfile: Erlang.to_erl_file(output)]
+
+      :yecc.file(Erlang.to_erl_file(input), options)
+    end
+
+    Erlang.compile(manifest(), mappings, :yrl, :erl, opts, yecc_file)
   end
 
   @doc """
   Returns Yecc manifests.
   """
   def manifests, do: [manifest()]
-  defp manifest, do: Path.join(Mix.Project.manifest_path, @manifest)
+  defp manifest, do: Path.join(Mix.Project.manifest_path(), @manifest)
 
   @doc """
   Cleans up compilation artifacts.
