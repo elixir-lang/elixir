b0e38e7ae8aeb3f00c9e33e4dec9a261fef55f90
Cohen Carlisle <Cohen-Carlisle@users.noreply.github.com>
2017-10-09 23:01:34 -0400

    Specify group_by values are ordered but map is not
    There was some confusion in #6759 over exactly what was ordered by group_by

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -1078,9 +1078,9 @@ defmodule Enum do
   @doc """
   Splits the enumerable into groups based on `key_fun`.
 
-  The result is a map where each key is given by `key_fun` and each
-  value is a list of elements given by `value_fun`. Ordering is preserved.
-
+  The result is a map where each key is given by `key_fun` and each value is a
+  list of elements given by `value_fun`. The order of each value is preserved
+  from the enumerable. However, maps themselves are unordered.
   ## Examples
 
       iex> Enum.group_by(~w{ant buffalo cat dingo}, &String.length/1)
