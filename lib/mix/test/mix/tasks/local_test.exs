bf7bb254a965a0790f82b4baa070aac19823a832
Andrea Leopardi <an.leopardi@gmail.com>
2017-10-10 10:38:31 +0200

    Run the code formatter on Mix.Tasks.LocalTest

--- lib/mix/test/mix/tasks/local_test.exs
+++ lib/mix/test/mix/tasks/local_test.exs
@@ -1,23 +1,25 @@
-Code.require_file "../../test_helper.exs", __DIR__
+Code.require_file("../../test_helper.exs", __DIR__)
 
 defmodule Mix.Tasks.LocalTest do
   use MixTest.Case
 
   test "MIX_PATH" do
-    File.rm_rf! tmp_path("mixpath")
-    System.put_env "MIX_PATH", tmp_path("mixpath/ebin")
+    File.rm_rf!(tmp_path("mixpath"))
+    System.put_env("MIX_PATH", tmp_path("mixpath/ebin"))
 
-    File.mkdir_p! tmp_path("mixpath/ebin")
-    Mix.Local.append_paths
+    File.mkdir_p!(tmp_path("mixpath/ebin"))
+    Mix.Local.append_paths()
 
     # Install on MIX_PATH manually
-    File.copy! fixture_path("archive/ebin/Elixir.Mix.Tasks.Local.Sample.beam"),
-               tmp_path("mixpath/ebin/Elixir.Mix.Tasks.Local.Sample.beam")
+    File.copy!(
+      fixture_path("archive/ebin/Elixir.Mix.Tasks.Local.Sample.beam"),
+      tmp_path("mixpath/ebin/Elixir.Mix.Tasks.Local.Sample.beam")
+    )
 
     # Run it
-    Mix.Task.run "local.sample"
+    Mix.Task.run("local.sample")
     assert_received {:mix_shell, :info, ["sample"]}
   after
-    purge [Mix.Tasks.Local.Sample]
+    purge([Mix.Tasks.Local.Sample])
   end
 end
