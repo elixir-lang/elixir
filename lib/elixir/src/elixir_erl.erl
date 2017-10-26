e26128a6306e5e18fe8edc564b3b0827cfd03554
José Valim <jose.valim@plataformatec.com.br>
2017-10-27 00:45:35 +0200

    Discard malformed lines in metadata
    Closes #6980.

--- lib/elixir/src/elixir_erl.erl
+++ lib/elixir/src/elixir_erl.erl
@@ -45,7 +45,7 @@ get_ann(Opts) when is_list(Opts) ->
   get_ann(Opts, false, 0).
 
 get_ann([{generated, true} | T], _, Line) -> get_ann(T, true, Line);
-get_ann([{line, Line} | T], Gen, _) -> get_ann(T, Gen, Line);
+get_ann([{line, Line} | T], Gen, _) when is_integer(Line) -> get_ann(T, Gen, Line);
 get_ann([_ | T], Gen, Line) -> get_ann(T, Gen, Line);
 get_ann([], Gen, Line) -> erl_anno:set_generated(Gen, Line).
 
