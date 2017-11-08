2fb92a2a5f2031548a4e84e31201980a3f7963c8
Wojtek Mach <wojtek@wojtekmach.pl>
2017-11-08 09:32:13 +0100

    Add Time.from_iso8601!/2 to match from_iso8601/2

--- lib/elixir/lib/calendar/time.ex
+++ lib/elixir/lib/calendar/time.ex
@@ -252,9 +252,9 @@ defmodule Time do
       iex> Time.from_iso8601!("2015:01:23 23-50-07")
       ** (ArgumentError) cannot parse "2015:01:23 23-50-07" as time, reason: :invalid_format
   """
-  @spec from_iso8601!(String.t()) :: t
-  def from_iso8601!(string) do
-    case from_iso8601(string) do
+  @spec from_iso8601!(String.t(), Calendar.calendar()) :: t
+  def from_iso8601!(string, calendar \\ Calendar.ISO) do
+    case from_iso8601(string, calendar) do
       {:ok, value} ->
         value
 
