69c168ab143d65a5df13c7b6131e892cb439f615
Glauber Campinho <glauber.campinho@soundcloud.com>
2017-11-12 18:23:26 +0100

    fix format

--- lib/elixir/test/elixir/kernel/expansion_test.exs
+++ lib/elixir/test/elixir/kernel/expansion_test.exs
@@ -647,7 +647,9 @@ defmodule Kernel.ExpansionTest do
     end
 
     test "uniq option is boolean" do
-      assert_raise CompileError, ~r":uniq option for comprehension only accept boolean, got: x", fn ->
+      message = ~r":uniq option for comprehension only accept boolean, got: x"
+
+      assert_raise CompileError, message, fn ->
         expand(quote(do: for(x <- 1..2, uniq: x, do: x)))
       end
     end
