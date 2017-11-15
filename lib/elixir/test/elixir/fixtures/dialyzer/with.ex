ed9c9b16016e716403143ac7b59e0a20594f5d3f
Glauber Campinho <ggcampinho@gmail.com>
2017-11-15 20:53:57 +0100

    Remove dialyzer warnings from else in with clauses (#7031)
    Add generated: true to guard clauses from else in with clauses
    
    Resolves #6426

--- lib/elixir/test/elixir/fixtures/dialyzer/with.ex
+++ lib/elixir/test/elixir/fixtures/dialyzer/with.ex
@@ -1,11 +1,25 @@
 defmodule Dialyzer.With do
   def with_else do
     with :ok <- ok_or_error(),
-         :ok <- ok_or_other_error() do
+         :ok <- ok_or_other_error(),
+         :ok <- ok_or_tuple_error(),
+         :ok <- ok_or_tuple_list_error() do
       :ok
     else
-      :error -> :error
-      :other_error -> :other_error
+      :error ->
+        :error
+
+      :other_error ->
+        :other_error
+
+      {:error, msg} when is_list(msg) or is_tuple(msg) ->
+        :error
+
+      {:error, msg} when is_list(msg) when is_tuple(msg) ->
+        :error
+
+      {:error, _msg} ->
+        :error
     end
   end
 
@@ -17,5 +31,15 @@ defmodule Dialyzer.With do
   @spec ok_or_other_error() :: :ok | :other_error
   defp ok_or_other_error do
     Enum.random([:ok, :other_error])
+  end
+
+  @spec ok_or_tuple_error() :: :ok | {:error, :err}
+  defp ok_or_tuple_error do
+    Enum.random([:ok, {:error, :err}])
+  end
+
+  @spec ok_or_tuple_list_error() :: :ok | {:error, [:err]}
+  defp ok_or_tuple_list_error do
+    Enum.random([:ok, {:error, [:err]}])
   end
 end
