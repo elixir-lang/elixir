91c09e02d5e524cf1514c549230978efd2c427b3
José Valim <jose.valim@plataformatec.com.br>
2017-11-21 13:54:24 -0200

    Attempt to soft_purge modules, closes #7047

--- lib/elixir/src/elixir_compiler.erl
+++ lib/elixir/src/elixir_compiler.erl
@@ -76,19 +76,14 @@ compile(Forms, Vars, #{line := Line, file := File} = E) ->
 
   {Module, Binary} = elixir_erl_compiler:noenv_forms(Form, File, [nowarn_nomatch]),
   code:load_binary(Module, "", Binary),
+  dispatch(Module, Fun, Args, I, EE).
 
-  Purgeable = beam_lib:chunks(Binary, [labeled_locals]) ==
-              {ok, {Module, [{labeled_locals, []}]}},
-  dispatch(Module, Fun, Args, Purgeable, I, EE).
-
-dispatch(Module, Fun, Args, Purgeable, I, E) ->
+dispatch(Module, Fun, Args, I, E) ->
   Res = Module:Fun(Args),
   code:delete(Module),
-  if Purgeable ->
-      code:purge(Module),
-      return_compiler_module(I);
-     true ->
-       ok
+  case code:soft_purge(Module) of
+    true -> return_compiler_module(I);
+    false -> ok
   end,
   {Res, E}.
 
