b639f572eeec28597992fc905eb6b284a97b115c
Dave Lucia <davelucianyc@gmail.com>
2017-11-28 09:42:34 -0500

    add example

--- lib/elixir/lib/kernel/special_forms.ex
+++ lib/elixir/lib/kernel/special_forms.ex
@@ -1375,7 +1375,11 @@ defmodule Kernel.SpecialForms do
       nil
 
   Note that if a "bare expression" fails to match, it will raise a `MatchError`
-  instead of returning the non-matched value.
+  instead of returning the non-matched value. For example, the following will raise:
+
+      with :foo = :bar, do: :ok
+      ** (MatchError) no match of right hand side value: :bar
+
 
   An `else` option can be given to modify what is being returned from
   `with` in the case of a failed match:
