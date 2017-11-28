ecdced7fb0c65a4db532e9e991eccb0886f90f9e
Glauber Campinho <glauber.campinho@soundcloud.com>
2017-11-29 00:17:00 +0100

    Fix env on boolean check expressions
    Fixes the issue raised on https://github.com/elixir-lang/elixir/pull/7069#issuecomment-347490983
    returning environment from the left side to the next expressions on boolean checks (`and`, `or`)

--- lib/elixir/src/elixir_expand.erl
+++ lib/elixir/src/elixir_expand.erl
@@ -388,6 +388,12 @@ expand({Atom, Meta, Args}, E) when is_atom(Atom), is_list(Meta), is_list(Args) -
 
 %% Remote calls
 
+expand({{'.', _, [erlang, 'andalso']}, _, [_, _]} = Expr, #{context := nil} = E) ->
+  expand_boolean_check(Expr, E);
+
+expand({{'.', _, [erlang, 'orelse']}, _, [_, _]} = Expr, #{context := nil} = E) ->
+  expand_boolean_check(Expr, E);
+
 expand({{'.', DotMeta, [Left, Right]}, Meta, Args}, E)
     when (is_tuple(Left) orelse is_atom(Left)), is_atom(Right), is_list(Meta), is_list(Args) ->
   {ELeft, EL} = expand(Left, E),
@@ -644,6 +650,13 @@ allowed_in_context(_, _Arity, guard) ->
   false;
 allowed_in_context(_, _, _) ->
   true.
+
+%% Boolean checks (`orelse`, `andalso`)
+
+expand_boolean_check({DotExpr, Meta, [Left, Right]}, E) ->
+  {ELeft, EL} = expand(Left, E),
+  {ERight, ER} = expand(Right, EL),
+  {{DotExpr, Meta, [ELeft, ERight]}, elixir_env:mergea(ER, EL)}.
 
 %% Lexical helpers
 
