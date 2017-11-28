5d86a5f0c2e3b625fd898ed3b82fe032a74b0524
Dave Lucia <davelucianyc@gmail.com>
2017-11-28 10:34:36 -0500

    Explain bare expression behavior in with docs (#7067)

--- lib/elixir/lib/kernel/special_forms.ex
+++ lib/elixir/lib/kernel/special_forms.ex
@@ -1374,6 +1374,12 @@ defmodule Kernel.SpecialForms do
       iex> width
       nil
 
+  Note that if a "bare expression" fails to match, it will raise a `MatchError`
+  instead of returning the non-matched value:
+
+      with :foo = :bar, do: :ok
+      #=> ** (MatchError) no match of right hand side value: :bar
+
   An `else` option can be given to modify what is being returned from
   `with` in the case of a failed match:
 
