6283d3d7758452e6e4d1e9f022fa693c1270af34
José Valim <jose.valim@plataformatec.com.br>
2018-01-16 11:56:22 +0100

    Do not allow stabs mixed with expressions

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -19,6 +19,16 @@ defmodule Kernel.ErrorsTest do
                       'fn x, y \\\\ 1 -> x + y end'
   end
 
+  test "invalid fn" do
+    assert_eval_raise SyntaxError,
+                      "nofile:1: expected anonymous functions to be defined with -> inside: 'fn'",
+                      'fn 1 end'
+
+    assert_eval_raise SyntaxError,
+                      ~r"nofile:2: unexpected operator ->. If you want to define multiple clauses, ",
+                      'fn 1\n2 -> 3 end'
+  end
+
   test "invalid token" do
     assert_eval_raise SyntaxError,
                       "nofile:1: unexpected token: \"\u200B\" (column 7, codepoint U+200B)",
@@ -92,12 +102,6 @@ defmodule Kernel.ErrorsTest do
 
       assert_eval_raise SyntaxError, message, :unicode.characters_to_nfd_list("foó")
     end
-  end
-
-  test "invalid fn" do
-    assert_eval_raise SyntaxError,
-                      "nofile:1: expected clauses to be defined with -> inside: 'fn'",
-                      'fn 1 end'
   end
 
   test "kw missing space" do
