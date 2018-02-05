56b03d3097b32824ff776a7671d249b123eb2d32
Eric Zhang <i@qinix.com>
2018-02-05 11:42:09 +0800

    Fix typo

--- lib/logger/lib/logger.ex
+++ lib/logger/lib/logger.ex
@@ -169,11 +169,11 @@ defmodule Logger do
     * `:discard_threshold_for_error_logger` - if `:error_logger` has more than
       `discard_threshold` messages in its inbox, messages will be dropped
       until the message queue goes down to `discard_threshold * 0.75`
-      entries. The threshold will be checked once again after 10% of thereshold
+      entries. The threshold will be checked once again after 10% of threshold
       messages are processed, to avoid messages from being constantly dropped.
-      For exmaple, if the thereshold is 500 (the default) and the inbox has
+      For exmaple, if the threshold is 500 (the default) and the inbox has
       600 messages, 250 messages will dropped, bringing the inbox down to
-      350 (0.75 * threshold) entries and 50 (0.1 * theshold) messages will
+      350 (0.75 * threshold) entries and 50 (0.1 * threshold) messages will
       be processed before the threshold is checked once again.
 
   For example, to configure `Logger` to redirect all
