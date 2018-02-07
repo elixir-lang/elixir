4602ea6d6f8790c88ca654380ab4258515fa1ab1
Nils Jonsson <nils@alumni.rice.edu>
2018-02-07 12:41:02 -0600

    Conform the spec for String.slice/3 to its guard clauses

--- lib/elixir/lib/string.ex
+++ lib/elixir/lib/string.ex
@@ -1736,7 +1736,7 @@ defmodule String do
       ""
 
   """
-  @spec slice(t, integer, integer) :: grapheme
+  @spec slice(t, integer, non_neg_integer) :: grapheme
 
   def slice(_, _, 0) do
     ""
