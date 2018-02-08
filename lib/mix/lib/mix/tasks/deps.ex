d894dcca3380b2a37a72e940103ae8eeb42e540e
José Valim <jose.valim@gmail.com>
2018-02-08 09:55:14 +0100

    Update deps.ex

--- lib/mix/lib/mix/tasks/deps.ex
+++ lib/mix/lib/mix/tasks/deps.ex
@@ -108,7 +108,7 @@ defmodule Mix.Tasks.Deps do
       as your Mix dependency. Search "sparse git checkouts" for more information.
 
   If your Git repository requires authentication, such as basic username:password
-  HTTP authentication via URLs, it can be achieve via git configuration, keeping
+  HTTP authentication via URLs, it can be achieved via Git configuration, keeping
   the access rules outside of source control.
 
       git config --global url."https://YOUR_USER:YOUR_PASS@example.com/".insteadOf "https://example.com/"
