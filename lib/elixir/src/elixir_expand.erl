9f702049f3c97e52a9822ba72c56883762e58858
Glauber Campinho <ggcampinho@gmail.com>
2018-03-25 20:44:36 +0200

    Check arguments only inside matches

--- lib/elixir/src/elixir_expand.erl
+++ lib/elixir/src/elixir_expand.erl
@@ -715,7 +715,7 @@ expand_remote(Receiver, DotMeta, Right, Meta, Args, #{context := Context} = E, E
   {EArgs, EA} = expand_args(Args, E),
   Rewritten = elixir_rewrite:rewrite(Receiver, DotMeta, Right, Meta, EArgs),
   maybe_warn_struct_comparison(Rewritten, Args, E),
-  assert_valid_args_for_remote(Rewritten, E),
+  assert_valid_args_for_remote(Rewritten, Context, E),
   case allowed_in_context(Rewritten, Arity, Context) of
     true ->
       {Rewritten, elixir_env:mergev(EL, EA)};
@@ -734,14 +734,9 @@ allowed_in_context(_, _Arity, guard) ->
 allowed_in_context(_, _, _) ->
   true.
 
-assert_valid_args_for_remote({{'.', _, [erlang, '++']}, Meta, [{_, _} = Left, _Right]}, E) ->
-  form_error(Meta, ?key(E, file), ?MODULE, {invalid_arg_for_union, Left});
-assert_valid_args_for_remote({{'.', _, [erlang, '++']}, Meta, [{:{}, _, _} = Left, _Right]}, E) ->
-  form_error(Meta, ?key(E, file), ?MODULE, {invalid_arg_for_union, Left});
-assert_valid_args_for_remote({{'.', _, [erlang, '++']}, Meta, [Left, _Right]}, E)
-  when is_binary(Left); is_float(Left); is_integer(Left); is_atom(literal) ->
-  form_error(Meta, ?key(E, file), ?MODULE, {invalid_arg_for_union, Left});
-assert_valid_args_for_remote(_, _) ->
+assert_valid_args_for_remote({{'.', _, [erlang, '++']}, Meta, [Left, _Right]}, match, E) when not is_list(Left) ->
+  form_error(Meta, ?key(E, file), ?MODULE, {invalid_arg_for_lists_concatenation, Left});
+assert_valid_args_for_remote(_, _, _) ->
   ok.
 
 maybe_warn_struct_comparison({{'.', _, [erlang, Op]}, Meta, [ELeft, ERight]}, [Left, Right], E)
@@ -983,7 +978,7 @@ format_error(wrong_number_of_args_for_super) ->
 format_error({invalid_arg_for_pin, Arg}) ->
   io_lib:format("invalid argument for unary operator ^, expected an existing variable, got: ^~ts",
                 ['Elixir.Macro':to_string(Arg)]);
-format_error({invalid_arg_for_union, Arg}) ->
+format_error({invalid_arg_for_lists_concatenation, Arg}) ->
   io_lib:format("invalid argument for ++ operator, expected a list, got: ~ts",
                 ['Elixir.Macro':to_string(Arg)]);
 format_error({pin_outside_of_match, Arg}) ->
