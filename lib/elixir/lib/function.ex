bad11b6f39280808058fe09185d8e8ba6806b69c
José Valim <jose.valim@gmail.com>
2018-03-28 10:01:51 +0200

    Update function.ex

--- lib/elixir/lib/function.ex
+++ lib/elixir/lib/function.ex
@@ -26,17 +26,11 @@ defmodule Function do
 
   ## Examples
 
-    iex> Function.capture(String, :length, 1)
-    &String.length/1
-
-  Useful when you want to capture a function when having variables.
-
-    iex> {module, function_name, arity} = {String, :length, 1}
-    iex> Function.capture(module, function_name, arity)
-    &String.length/1
+      iex> Function.capture(String, :length, 1)
+      &String.length/1
 
   """
-  @spec capture(module, atom, integer) :: (... -> any)
+  @spec capture(module, atom, integer) :: fun
   def capture(module, function_name, arity)
       when is_atom(module) and is_atom(function_name) and is_integer(arity) and arity in 0..255 do
     :erlang.make_fun(module, function_name, arity)
@@ -46,37 +40,42 @@ defmodule Function do
   Returns a keyword list with information about a function.
 
   The `{key, value}`s will include the following:
-  - `:type` - `:local` (for anonymous functions) or `:external` (for
+  
+    * `:type` - `:local` (for anonymous functions) or `:external` (for
   named functions)
-    - `:module` - an atom - the module where the function is defined when
+    * `:module` - an atom - the module where the function is defined when
     anonymous or the module which the function refers to when it's a named one.
-    - `:arity` - the number of arguments the function is to be called with
-    - `:name` - the name of the functions
-    - `:env` - a list of the environment or free variables. For named
-    functions, the returned list is always empty.
+    * `:arity` - the number of arguments the function is to be called with
+    * `:name` - the name of the functions
+    * `:env` - a list of the environment or free variables. For named
+      functions, the returned list is always empty.
 
   When it is an anonymous function it will also return info about:
-  - `:pid` - process identifier of the process that originally created
+  
+    * `:pid` - process identifier of the process that originally created
   the funciton
-    - `:index` - an integer - is an index into the module function table.
-    - `:new_index` - an integer - is an index into the module function table.
-    - `:new_uniq` - a binary-  it's a unique value for this function. It is
+    * `:index` - an integer - is an index into the module function table.
+    * `:new_index` - an integer - is an index into the module function table.
+    * `:new_uniq` - a binary-  it's a unique value for this function. It is
     calculated from the compiled code for the entire module.
-    - `:uniq` - an integer, a unique value for this function. As from Erlang/OTP
+    * `:uniq` - an integer, a unique value for this function. As from Erlang/OTP
     R15, this integer is calculated from the compiled code for the entire
     module. Before Erlang/OTP R15, this integer was based on only the body
     of the function.
 
+  **Note**: this function must be used only for debugging purposes.
 
   ## Examples
-    iex> fun = fn x -> x end
-    iex> info = Function.info(fun)
-    iex> info |> Keyword.get(:arity)
-    1
-    iex> info |> Keyword.get(:type)
-    :local
+
+      iex> fun = fn x -> x end
+      iex> info = Function.info(fun)
+      iex> Keyword.get(info, :arity)
+      1
+      iex> Keyword.get(info, :type)
+      :local
+
   """
-  @spec info((... -> any)) :: [{information, value}]
+  @spec info(fun) :: [{information, value}]
   def info(fun) when is_function(fun), do: :erlang.fun_info(fun)
 
   @doc """
@@ -91,19 +90,19 @@ defmodule Function do
   For a named function, the value of any of these items is always the
   atom `:undefined`.
 
-  For more information on each of the value returned - check the docs for
+  For more information on each of the value returned. Check the docs for
   `Function.info/1`.
 
   ## Examples
 
-    iex> f = fn x -> x end
-    iex> Function.info(f, :arity)
-    {:arity, 1}
-    iex> Function.info(f, :type)
-    {:type, :local}
+      iex> f = fn x -> x end
+      iex> Function.info(f, :arity)
+      {:arity, 1}
+      iex> Function.info(f, :type)
+      {:type, :local}
 
   """
-  @spec info((... -> any)) :: {information, value}
+  @spec info(fun) :: {information, value}
   def info(fun, item) when is_function(fun) and is_atom(item) do
     :erlang.fun_info(fun, item)
   end
