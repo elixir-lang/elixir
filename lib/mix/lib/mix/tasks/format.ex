38da9eca616dab2b1464e4f6164213c1bbe6b5cc
Fernando Tapia Rico <fertapric@gmail.com>
2018-05-19 21:00:04 +0200

    Match files starting with dot in "mix format" (#7697)
    Here is the example `.formatter.exs` file given in the
    documentation of `mix format`:
    
        [
          inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
        ]
    
    With the previous implementation, the `.formatter.exs` file would
    not be formatted because internally `Path.wildcard/2` was used
    without expanding files starting with dot ".":
    
        iex> Path.wildcard("{mix,.formatter}.exs")
        ["mix.exs"]
    
        iex> Path.wildcard("{mix,.formatter}.exs", match_dot: true)
        [".formatter.exs", "mix.exs"]

--- lib/mix/lib/mix/tasks/format.ex
+++ lib/mix/lib/mix/tasks/format.ex
@@ -358,7 +358,7 @@ defmodule Mix.Tasks.Format do
 
     map =
       for input <- List.wrap(formatter_opts[:inputs]),
-          file <- Path.wildcard(Path.join(prefix ++ [input])),
+          file <- Path.wildcard(Path.join(prefix ++ [input]), match_dot: true),
           do: {file, formatter_opts},
           into: %{}
 
@@ -381,7 +381,7 @@ defmodule Mix.Tasks.Format do
   end
 
   defp stdin_or_wildcard("-"), do: [:stdin]
-  defp stdin_or_wildcard(path), do: Path.wildcard(path)
+  defp stdin_or_wildcard(path), do: Path.wildcard(path, match_dot: true)
 
   defp read_file(:stdin) do
     {IO.stream(:stdio, :line) |> Enum.to_list() |> IO.iodata_to_binary(), file: "stdin"}
