ef48738e9e5fec5d6acb5f27dd5ed92bb928e95b
Michał Muskała <michal@muskala.eu>
2018-06-12 14:51:09 +0200

    Fix documentation of GenServer
    The second example was missing the init/2 callback and produced warnings.
    
    [CI SKIP]

--- lib/elixir/lib/gen_server.ex
+++ lib/elixir/lib/gen_server.ex
@@ -90,6 +90,11 @@ defmodule GenServer do
         end
 
         # Server (callbacks)
+        
+        @impl true
+        def init(stack) do
+          {:ok, stack}
+        end
 
         @impl true
         def handle_call(:pop, _from, [head | tail]) do
