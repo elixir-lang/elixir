bc278341bf05a98259da3fcf01424c4c3eb5497c
English3000 <english3000.org@gmail.com>
2018-07-01 19:55:47 +0300

    Reorganized info in tuple.ex
    While this `@moduledoc` has the proper information, it was organized in a manner conducive to TLDR. I've tried to put the main tripping points for beginners at the top.
    
    Honestly, the remaining content is somewhat redundant. Implementation details are good to include here but an explanation of a tuple and its most basic functionality is best reserved for the elixir-lang.org documentation. It does not add value for beginners and the functions' documentation are sufficient to explain what they do.

--- lib/elixir/lib/tuple.ex
+++ lib/elixir/lib/tuple.ex
@@ -1,6 +1,32 @@
 defmodule Tuple do
   @moduledoc """
   Functions for working with tuples.
+  
+  Please note the following functions for tuples are found in `Kernel`:
+  
+    * `elem/2` - for accessing a tuple by index
+    * `put_elem/3`
+    * `tuple_size/3`
+    
+  When deciding between whether to use a tuple or a list, please bear in mind
+  that tuples are not meant to be used as a "collection" type but rather as a
+  fixed-size container for multiple elements. That's why it is not possible
+  to traverse a tuple dynamically using the functions in the `Enum` module.
+  
+  The functions that add and remove elements from tuples, changing their size, 
+  are rarely used in practice, as they typically imply tuples are being used as 
+  collections. Even if you have a tuple `{:ok, atom}` and you want to append 
+  another element to it, such as an empty map, it is preferrable to rely on 
+  pattern matching and create a new tuple than manipulating it dynamically:
+
+      tuple = {:ok, :example}
+
+      # Avoid
+      Tuple.insert_at(tuple, 2, %{}}
+
+      # Prefer
+      {:ok, atom} = tuple
+      {:ok, atom, %{}}
 
   Tuples are composite data types with a fixed number of elements. Tuples
   can contain elements of any type, and a tuple can contain elements of
@@ -34,36 +60,12 @@ defmodule Tuple do
   shallow copy of the whole tuple. The tuple elements are still shared thanks
   to immutability.
 
-  Tuples are not meant to be used as a "collection" type but rather as a
-  fixed-size container for multiple elements. That's why it is not possible
-  to traverse a tuple dynamically using the functions in the `Enum` module.
-
   For example, tuples are often used to have functions return "enriched"
   values: a common pattern is for functions to return `{:ok, value}` for
   successful cases and `{:error, reason}` for unsuccessful cases. This is
   exactly what `File.read/1` does: it returns `{:ok, contents}` if reading
   the given file is successful, or `{:error, reason}` otherwise, such as
   when the file does not exist.
-
-  The most common operations performed on tuples are available in `Kernel`
-  (`Kernel.tuple_size/1`, `Kernel.elem/2`, `Kernel.put_elem/3`, and others)
-  and are automatically imported into your code. The functions in this module
-  cover other cases, such as dynamic creation of tuples (`Tuple.duplicate/2`)
-  and conversion to list (`Tuple.to_list/1`). The functions that add and remove
-  elements from tuples, changing their size, are rarely used in practice, as
-  they typically imply tuples are being used as collections. Even if you have
-  a tuple `{:ok, atom}` and you want to append another element to it, such as
-  an empty map, it is preferrable to rely on pattern matching and create a new
-  tuple than manipulating it dynamically:
-
-      tuple = {:ok, :example}
-
-      # Avoid
-      Tuple.insert_at(tuple, 2, %{}}
-
-      # Prefer
-      {:ok, atom} = tuple
-      {:ok, atom, %{}}
 
   """
 
