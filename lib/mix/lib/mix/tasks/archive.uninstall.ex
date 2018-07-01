7355f0ad94f424024ec06c6f2d9881799b5d72fb
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-07-01 10:04:23 -0700

    Remove incorrect code comment and add newlines per PR feedback

--- lib/mix/lib/mix/tasks/archive.uninstall.ex
+++ lib/mix/lib/mix/tasks/archive.uninstall.ex
@@ -9,16 +9,14 @@ defmodule Mix.Tasks.Archive.Uninstall do
       mix archive.uninstall archive.ez
 
   ## Command line options
-
-    * `--sha512` - checks the archive matches the given SHA-512 checksum. Only
-      applies to installations via URL or local path
-
     * `--force` - forces uninstallation without a shell prompt; primarily
       intended for automation
   """
+
   @switches [
     force: :boolean
   ]
+
   def run(argv) do
     Mix.Local.Installer.uninstall(Mix.Local.path_for(:archive), "archive", argv, @switches)
   end
