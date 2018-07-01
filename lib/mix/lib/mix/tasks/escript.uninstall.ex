29e2f6ffd74cd06bba1df84bf2bf950c24f71486
Anil Kulkarni <AnilRedshift@users.noreply.github.com>
2018-07-01 15:51:20 -0700

    Add a --force flag to mix archive.uninstall (#7818)

--- lib/mix/lib/mix/tasks/escript.uninstall.ex
+++ lib/mix/lib/mix/tasks/escript.uninstall.ex
@@ -8,10 +8,18 @@ defmodule Mix.Tasks.Escript.Uninstall do
 
       mix escript.uninstall escript_name
 
+  ## Command line options
+    * `--force` - forces uninstallation without a shell prompt; primarily
+      intended for automation
   """
 
+  @switches [
+    force: :boolean
+  ]
+
   def run(argv) do
-    if path = Mix.Local.Installer.uninstall(Mix.Local.path_for(:escript), "escript", argv) do
+    if path =
+         Mix.Local.Installer.uninstall(Mix.Local.path_for(:escript), "escript", argv, @switches) do
       File.rm(path <> ".bat")
     end
   end
