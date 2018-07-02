c1d6bd3e6075248eb3f732ad331effe28b48a886
José Valim <jose.valim@plataformatec.com.br>
2018-07-02 15:09:47 +0200

    Add a section about pattern matching on struct names

--- lib/elixir/lib/kernel/special_forms.ex
+++ lib/elixir/lib/kernel/special_forms.ex
@@ -75,7 +75,7 @@ defmodule Kernel.SpecialForms do
   defmacro unquote(:%{})(args), do: error!([args])
 
   @doc """
-  Creates a struct.
+  Matches on or builds a struct.
 
   A struct is a tagged map that allows developers to provide
   default values for keys, tags to be used in polymorphic
@@ -96,15 +96,24 @@ defmodule Kernel.SpecialForms do
 
       %User{} == %{__struct__: User, name: "john", age: 27}
 
-  A struct also validates that the given keys are part of the defined
-  struct. The example below will fail because there is no key
-  `:full_name` in the `User` struct:
+  The struct fields can be given when building the struct:
 
-      %User{full_name: "john doe"}
+      %User{age: 31}
+      #=> %{__struct__: User, name: "john", age: 31}
+
+  Or also on pattern matching to extract values out:
+
+      %User{age: age} = user
 
   An update operation specific for structs is also available:
 
       %User{user | age: 28}
+
+  The advantage of structs is that they validate that the given
+  keys are part of the defined struct. The example below will fail
+  because there is no key `:full_name` in the `User` struct:
+
+      %User{full_name: "john doe"}
 
   The syntax above will guarantee the given keys are valid at
   compilation time and it will guarantee at runtime the given
@@ -116,6 +125,24 @@ defmodule Kernel.SpecialForms do
   can be used with protocols for polymorphic dispatch. Also
   see `Kernel.struct/2` and `Kernel.struct!/2` for examples on
   how to create and update structs dynamically.
+
+  ## Pattern matching on struct names
+
+  Besides allowing pattern matching on struct fields, such as:
+
+      %User{age: age} = user
+
+  Structs also allow pattern matching on the struct name:
+
+      %struct_name{} = user
+      struct_name #=> User
+
+  You can also assign the struct name to `_` when you want to
+  check if something is a struct but you are not interested in
+  its name:
+
+      %_{} = user
+
   """
   defmacro unquote(:%)(struct, map), do: error!([struct, map])
 
