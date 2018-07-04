266285f13cb7685b74d699566307db1c2cf6ab21
José Valim <jose.valim@gmail.com>
2018-07-05 00:37:43 +0200

    Update task.ex

--- lib/mix/lib/mix/task.ex
+++ lib/mix/lib/mix/task.ex
@@ -122,10 +122,11 @@ defmodule Mix.Task do
 
   Returns the moduledoc or `nil`.
   """
-  @spec moduledoc(task_module) :: String.t() | nil
+  @spec moduledoc(task_module) :: String.t() | nil | false
   def moduledoc(module) when is_atom(module) do
     case Code.fetch_docs(module) do
       {:docs_v1, _, _, _, %{"en" => moduledoc}, _, _} -> moduledoc
+      {:docs_v1, _, _, _, :hidden, _, _} -> false
       _ -> nil
     end
   end
