2a024ef3784bf93f91ca1b27e07dbe1988212ae1
eksperimental <eksperimental@users.noreply.github.com>
2018-07-08 00:30:43 +0700

    Improve specs and documentation for List.wrap/1

--- lib/elixir/lib/list.ex
+++ lib/elixir/lib/list.ex
@@ -390,10 +390,10 @@ defmodule List do
   end
 
   @doc """
-  Wraps the argument in a list.
+  Wraps `term` in a list if this is not list.
 
-  If the argument is already a list, returns the list.
-  If the argument is `nil`, returns an empty list.
+  If `term` is already a list, it returns the list.
+  If `term` is `nil`, it returns an empty list.
 
   ## Examples
 
@@ -407,7 +407,11 @@ defmodule List do
       []
 
   """
-  @spec wrap(list | any) :: list
+  @spec wrap(nil) :: []
+  @spec wrap(list) :: list when list: maybe_improper_list()
+  @spec wrap(term) :: nonempty_list(term) when term: any()
+  def wrap(term)
+
   def wrap(list) when is_list(list) do
     list
   end
