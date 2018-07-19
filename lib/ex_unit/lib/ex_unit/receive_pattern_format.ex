3c19e76d30748c59711d2d6484171c9890a47ff5
jowens <jeremyo@companykitchen.com>
2018-07-19 10:48:13 -0500

    Change how the match? and = are formatted

--- lib/ex_unit/lib/ex_unit/receive_pattern_format.ex
+++ lib/ex_unit/lib/ex_unit/receive_pattern_format.ex
@@ -16,14 +16,18 @@ defmodule ExUnit.ReceivePatternFormat do
 
   @no_value :ex_unit_no_meaningful_value
 
+  # JOWENS
+  # Create context function that includes vars & pins from `left`
   def script(left, right) do
     # IO.inspect(binding(), label: "script")
 
     _diff =
       left
       |> ExUnit.PatternDiff.cmp(right)
+      |> IO.inspect(label: "Compare")
       |> format
       |> Enum.reject(&(&1 == ""))
+      |> IO.inspect(label: "Diff")
   end
 
   def format(diff, ctx \\ nil)
@@ -33,7 +37,7 @@ defmodule ExUnit.ReceivePatternFormat do
         %{keys: key_type, missing: false, comma: comma} = ctx
       )
       when key_type == :atom_keys or key_type == :non_atom_keys do
-    # IO.inspect("Container Map")
+    IO.inspect("Container Map")
     [h | rest] = items
 
     [
@@ -47,7 +51,7 @@ defmodule ExUnit.ReceivePatternFormat do
   end
 
   def format(%ContainerDiff{type: :map, items: _items} = diff, ctx) do
-    # IO.inspect(ctx, label: "Container Map - ")
+    IO.inspect(ctx, label: "Container Map - ")
     #    IO.inspect(_items, label: "items")
     map_ctx = create_context(diff)
 
@@ -65,7 +69,7 @@ defmodule ExUnit.ReceivePatternFormat do
         %ContainerDiff{type: :list, items: items},
         %{keys: :list, missing: false} = ctx
       ) do
-    # IO.inspect("Container list")
+    IO.inspect("Container list")
     [h | rest] = items
 
     [
@@ -78,13 +82,19 @@ defmodule ExUnit.ReceivePatternFormat do
   end
 
   def format(%ContainerDiff{type: :when, items: [l, r]}, %{comma: comma} = ctx) do
-    # IO.inspect("Container when")
+    IO.inspect("Container when")
 
     [comma, format(l, ctx), " ", format(r, ctx)]
     |> List.flatten()
   end
 
+  def format(%ContainerDiff{type: :list, items: []} = diff, %{comma: comma}) do
+    [comma, "[]"]
+  end
+
   def format(%ContainerDiff{type: :list, items: _items} = diff, %{comma: comma}) do
+    IO.inspect("Cointainer diff list")
+
     ctx = create_context(diff)
 
     [comma, format(diff, ctx)]
@@ -94,7 +104,7 @@ defmodule ExUnit.ReceivePatternFormat do
   end
 
   def format(%ContainerDiff{type: :tuple, items: items}, %{keys: :none} = ctx) do
-    # IO.inspect("Container tuple")
+    IO.inspect("Container tuple")
     [h | rest] = items
 
     [
@@ -107,8 +117,8 @@ defmodule ExUnit.ReceivePatternFormat do
   end
 
   def format(%ContainerDiff{type: :tuple, items: [l, r]}, %{keys: :non_atom_keys, comma: comma}) do
-    # IO.inspect(l, label: "Container non-atom tuple pair: L")
-    # IO.inspect(r, label: "Container non-atom tuple pair: R")
+    IO.inspect(l, label: "Container non-atom tuple pair: L")
+    IO.inspect(r, label: "Container non-atom tuple pair: R")
 
     left =
       case l do
@@ -141,7 +151,7 @@ defmodule ExUnit.ReceivePatternFormat do
   end
 
   def format(%ContainerDiff{type: :tuple, items: [l, r]}, %{keys: :atom_keys, comma: comma}) do
-    # IO.inspect("Container keyword tuple")
+    IO.inspect("Container keyword tuple")
 
     left =
       case l do
@@ -172,18 +182,19 @@ defmodule ExUnit.ReceivePatternFormat do
   end
 
   def format(diff, nil) do
-    # IO.inspect("Format with a nil context")
+    IO.inspect("Format with a nil context")
     # Jowens - extract context to it's own deal, this is repeated 3 times now
     format(diff, %{comma: "", keys: :none, missing: false, print_when?: true})
   end
 
   def format(%PatternDiff{diff_result: :eq, rh: rh}, %{comma: comma}) do
-    # IO.inspect("pattern equal")
+    IO.inspect("pattern equal")
     [comma, match(inspect(rh))]
   end
 
   def format(%PatternDiff{diff_result: :neq, rh: @no_value, lh: lh}, %{comma: comma} = ctx) do
-    # IO.inspect(lh, label: "pattern not equal, missing right")
+    IO.inspect(lh, label: "pattern not equal, missing right")
+    IO.inspect(ctx, label: "context")
     [comma, insert(textify_ast(lh, ctx))]
   end
 
@@ -191,7 +202,7 @@ defmodule ExUnit.ReceivePatternFormat do
         comma: comma,
         keys: :atom_keys
       }) do
-    # IO.inspect("pattern not equal, missing left")
+    IO.inspect("pattern not equal, missing left")
     [comma, match("#{to_string(key)}: #{inspect(value)}")]
   end
 
@@ -199,17 +210,17 @@ defmodule ExUnit.ReceivePatternFormat do
         comma: comma,
         keys: :non_atom_keys
       }) do
-    # IO.inspect("pattern not equal, missing left")
+    IO.inspect("pattern not equal, missing left")
     [comma, "#{inspect(key)} => #{inspect(value)}"]
   end
 
   def format(%PatternDiff{diff_result: :neq, rh: rh, lh: _lh}, %{comma: comma} = ctx) do
-    # IO.inspect(ctx, label: "pattern not equal, but not part of match")
+    IO.inspect(ctx, label: "pattern not equal, but not part of match")
     [comma, delete(inspect(rh))]
   end
 
   def format(%WhenDiff{result: res, op: :and, bindings: keys}, ctx) do
-    # IO.inspect("When Diff")
+    IO.inspect("When Diff")
     [left, right] = keys
     prefix = if ctx.print_when?, do: "when ", else: ""
     ctx = %{ctx | print_when?: false}
@@ -277,16 +288,22 @@ defmodule ExUnit.ReceivePatternFormat do
 
   defp textify_ast(%{ast: ast}, ctx), do: textify_ast(ast, ctx)
 
+  defp textify_ast({:^, _, [{var, _, _}]}, ctx) do
+    IO.inspect(var, label: "var")
+    IO.inspect(ctx, label: "ctx")
+    "^#{var}"
+  end
+
   defp textify_ast({:%{}, _, elements}, ctx) do
     inspect(Map.new(Enum.map(elements, &textify_ast(&1, ctx))))
   end
 
-  defp textify_ast({key, value}, %{keys: :atom_keys}) when is_atom(key) do
-    "#{key}: #{inspect(value)}"
+  defp textify_ast({key, value}, %{keys: :atom_keys} = ctx) when is_atom(key) do
+    "#{key}: #{textify_ast(value, ctx)}"
   end
 
-  defp textify_ast({key, value}, %{keys: :non_atom_keys}) when is_atom(key) do
-    "#{key} => #{inspect(value)}"
+  defp textify_ast({key, value}, %{keys: :non_atom_keys} = ctx) when is_atom(key) do
+    "#{key} => #{textify_ast(value, ctx)}"
   end
 
   defp textify_ast(elem, _ctx) when is_atom(elem), do: elem
