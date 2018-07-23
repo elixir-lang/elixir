86c46d744f560c8f3f785c6c0788174778d5474b
Fernando Tapia Rico <fertapric@gmail.com>
2018-07-23 21:04:15 +0200

    (fixup) Avoid warnings for unused priv functions

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -722,25 +722,28 @@ defmodule Kernel.ErrorsTest do
   test "@on_load attribute with undefined function" do
     assert_eval_raise CompileError,
                       "nofile:1: @on_load function foo/0 is undefined",
-                      'defmodule UndefinedOnLoadAttribute do @on_load :foo end'
+                      'defmodule UndefinedOnLoadFunction do @on_load :foo end'
   end
 
   test "wrong kind for @on_load attribute" do
-    # Capture warning: function foo/0 is unused
-    ExUnit.CaptureIO.capture_io(:stderr, fn ->
-      assert_eval_raise CompileError,
-                        "nofile:1: expected @on_load function foo/0 to be defined as \"def\", " <>
-                          "got \"defp\"",
-                        '''
-                        defmodule PrivateOnLoadAttribute do
-                          @on_load :foo
+    assert_eval_raise CompileError,
+                      "nofile:1: expected @on_load function foo/0 to be defined as \"def\", " <>
+                        "got \"defp\"",
+                      '''
+                      defmodule PrivateOnLoadFunction do
+                        @on_load :foo
 
-                          defp foo do
-                            :ok
-                          end
+
+                        defp foo do
+                          :ok
                         end
-                        '''
-    end)
+
+                        # To avoid warning: function foo/0 is unused
+                        def bar do
+                          foo()
+                        end
+                      end
+                      '''
   end
 
   test "interpolation error" do
