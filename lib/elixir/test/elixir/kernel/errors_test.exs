1da90928e4e81422926135dd9ad5e74059674c8c
Fernando Tapia Rico <fertapric@gmail.com>
2018-07-26 16:57:35 +0200

    Validate @on_load function in Elixir (#7973)

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -719,6 +719,33 @@ defmodule Kernel.ErrorsTest do
     end
   end
 
+  test "@on_load attribute with undefined function" do
+    assert_eval_raise CompileError,
+                      "nofile:1: @on_load function foo/0 is undefined",
+                      'defmodule UndefinedOnLoadFunction do @on_load :foo end'
+  end
+
+  test "wrong kind for @on_load attribute" do
+    assert_eval_raise CompileError,
+                      "nofile:1: expected @on_load function foo/0 to be defined as \"def\", " <>
+                        "got \"defp\"",
+                      '''
+                      defmodule PrivateOnLoadFunction do
+                        @on_load :foo
+
+
+                        defp foo do
+                          :ok
+                        end
+
+                        # To avoid warning: function foo/0 is unused
+                        def bar do
+                          foo()
+                        end
+                      end
+                      '''
+  end
+
   test "interpolation error" do
     assert_eval_raise SyntaxError,
                       "nofile:1: unexpected token: ). The \"do\" at line 1 is missing terminator \"end\"",
