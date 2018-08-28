d92687bd891b023864ee6a246cba3358694468ae
José Valim <jose.valim@gmail.com>
2018-08-28 17:27:16 +0200

    Update iso_test.exs

--- lib/elixir/test/elixir/calendar/iso_test.exs
+++ lib/elixir/test/elixir/calendar/iso_test.exs
@@ -5,7 +5,7 @@ defmodule Calendar.ISOTest do
   doctest Calendar.ISO
 
   describe "date_from_iso_days" do
-    test "positive dates" do
+    test "with positive dates" do
       assert {0, 1, 1} == iso_day_roundtrip(0, 1, 1)
       assert {0, 12, 31} == iso_day_roundtrip(0, 12, 31)
       assert {1, 12, 31} == iso_day_roundtrip(1, 12, 31)
@@ -17,7 +17,7 @@ defmodule Calendar.ISOTest do
       assert {9996, 1, 1} == iso_day_roundtrip(9996, 1, 1)
     end
 
-    test "negative dates" do
+    test "with negative dates" do
       assert {-1, 1, 1} == iso_day_roundtrip(-1, 1, 1)
       assert {-1, 12, 31} == iso_day_roundtrip(-1, 12, 31)
       assert {-1, 12, 31} == iso_day_roundtrip(-1, 12, 31)
@@ -32,18 +32,6 @@ defmodule Calendar.ISOTest do
 
       assert {-9996, 12, 31} == iso_day_roundtrip(-9996, 12, 31)
       assert {-9996, 1, 1} == iso_day_roundtrip(-9996, 1, 1)
-    end
-  end
-
-  describe "date_from_iso_days symptoms" do
-    test "date add" do
-      assert ~D[-0004-01-01] == Date.add(~D[-0004-01-01], 0)
-    end
-
-    test "naive_datetime_from_iso_days" do
-      {iso_days, parts} = Calendar.ISO.naive_datetime_to_iso_days(-4, 1, 1, 0, 0, 0, {0, 6})
-      naive_dt = Calendar.ISO.naive_datetime_from_iso_days({iso_days, parts})
-      assert {-4, 1, 1, 0, 0, 0, {0, 6}} == naive_dt
     end
   end
 
