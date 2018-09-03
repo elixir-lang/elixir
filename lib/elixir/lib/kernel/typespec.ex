5535c22d948c1f118c3b7515bbad89e921e413a2
Fernando Tapia Rico <fertapric@gmail.com>
2018-09-03 15:36:40 +0200

    (fixup) Feedback

--- lib/elixir/lib/kernel/typespec.ex
+++ lib/elixir/lib/kernel/typespec.ex
@@ -535,29 +535,28 @@ defmodule Kernel.Typespec do
   end
 
   # Handle type operator
-  defp typespec({:::, meta, [var, expr]} = orig, vars, caller) do
-    case var do
-      {var_name, _meta, context} when is_atom(var_name) and is_atom(context) ->
-        case typespec(expr, vars, caller) do
-          {:ann_type, _, _} ->
-            message =
-              "invalid type annotation. Type annotations cannot be nested: " <>
-                "#{Macro.to_string(orig)}"
-
-            compile_error(caller, message)
-
-          right ->
-            left = typespec(var, [elem(var, 0) | vars], caller)
-            {:ann_type, line(meta), [left, right]}
-        end
-
-      _ ->
+  defp typespec({:::, meta, [{var_name, _meta, context} = var, expr]} = ann_type, vars, caller)
+       when is_atom(var_name) and is_atom(context) do
+    case typespec(expr, vars, caller) do
+      {:ann_type, _, _} ->
         message =
-          "invalid type annotation. When using the | operator to represent the union of " <>
-            "types, make sure to wrap type annotations in parentheses: #{Macro.to_string(orig)}"
+          "invalid type annotation. Type annotations cannot be nested: " <>
+            "#{Macro.to_string(ann_type)}"
 
         compile_error(caller, message)
+
+      right ->
+        left = typespec(var, [var_name | vars], caller)
+        {:ann_type, line(meta), [left, right]}
     end
+  end
+
+  defp typespec({:::, _meta, _args} = ann_type, _vars, caller) do
+    message =
+      "invalid type annotation. When using the | operator to represent the union of types, " <>
+        "make sure to wrap type annotations in parentheses: #{Macro.to_string(ann_type)}"
+
+    compile_error(caller, message)
   end
 
   # Handle unary ops
