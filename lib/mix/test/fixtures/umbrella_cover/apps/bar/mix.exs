bbf8a5868b3d256d7068a506b9a045e50b307ab6
Gabe Jackson <gj@mail.co.de>
2018-09-08 23:48:10 -0400

    Use a unique test_pattern for the umbrella app fixture

--- lib/mix/test/fixtures/umbrella_cover/apps/bar/mix.exs
+++ lib/mix/test/fixtures/umbrella_cover/apps/bar/mix.exs
@@ -4,7 +4,10 @@ defmodule Bar.MixProject do
   def project do
     [
       app: :bar,
-      version: "0.1.0"
+      version: "0.1.0",
+      # Choose something besides *_test.exs so that these test files don't
+      # get accidentally swept up into the actual Mix test suite.
+      test_pattern: "*_tests.exs"
     ]
   end
 end
