507c5ded9c8491fe4b240345d7b52f69c42e8dc8
Gabe Jackson <gj@mail.co.de>
2018-09-08 22:47:54 -0400

    Ensure that mix --cover displays correct coverage in an umbrella app
    `:cover.start/0` initializes the internal database where coverage data
    on all compiled modules is stored. Prior to this change, the database
    wasn't cleared out between sub-apps in an umbrella app. When
    `:cover.analyse/2` is called, it runs against the entire contents of the
    internal database. In an umbrella app, the coverage report for App1
    would show App1, the report for App2 would show App1 and App2, App3
    would have App1, App2, and App3, and so on.
    
    Closes #8086

---
+++ lib/mix/test/fixtures/umbrella_cover/apps/bar/mix.exs
@@ -0,0 +1,10 @@
+defmodule Bar.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bar,
+      version: "0.1.0"
+    ]
+  end
+end
---
+++ lib/mix/test/fixtures/umbrella_cover/apps/foo/mix.exs
@@ -0,0 +1,10 @@
+defmodule Foo.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :foo,
+      version: "0.1.0"
+    ]
+  end
+end
