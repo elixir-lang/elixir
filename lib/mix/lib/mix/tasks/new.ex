0429cdd95f10abe2f5b3d188c2c3574132cae474
Wojtek Mach <wojtekmach@users.noreply.github.com>
2018-09-12 23:58:38 +0200

    Improve wording in generated umbrella config.exs (#8195)

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -422,10 +422,13 @@ defmodule Mix.Tasks.New do
   use Mix.Config
 
   # By default, the umbrella project as well as each child
-  # application will require this configuration file, ensuring
-  # they all use the same configuration. While one could
-  # configure all applications here, we prefer to delegate
-  # back to each application for organization purposes.
+  # application will require this configuration file, as
+  # configuration and dependencies are shared in an umbrella
+  # project. While one could configure all applications here,
+  # we prefer to keep the configuration of each individual
+  # child application in their own app, but all other
+  # dependencies, regardless if they belong to one or multiple
+  # apps, should be configured in the umbrella to avoid confusion.
   import_config "../apps/*/config/config.exs"
 
   # Sample configuration (overrides the imported configuration above):
