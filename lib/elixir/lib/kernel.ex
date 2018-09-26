43df2fa34a3ea1474adab7a54b90773ade66bc80
Gabe Jackson <gj@mail.co.de>
2018-09-26 15:03:57 -0400

    Use :erlang.length/1

--- lib/elixir/lib/kernel.ex
+++ lib/elixir/lib/kernel.ex
@@ -5021,7 +5021,7 @@ defmodule Kernel do
       for fun <- List.wrap(funs) do
         {name, args, as, as_args} = Kernel.Utils.defdelegate(fun, opts)
 
-        @doc delegate_to: {target, as, length(as_args)}
+        @doc delegate_to: {target, as, :erlang.length(as_args)}
 
         def unquote(name)(unquote_splicing(args)) do
           unquote(target).unquote(as)(unquote_splicing(as_args))
