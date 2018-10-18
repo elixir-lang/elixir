e2fc049fc5318afcae54fd964a4dc1454f2a73f4
Gabe Jackson <gj@mail.co.de>
2018-10-18 16:16:27 -0400

    Fix typo in bad_pipe/2 private function
    [skip ci]

--- lib/elixir/lib/macro.ex
+++ lib/elixir/lib/macro.ex
@@ -162,7 +162,7 @@ defmodule Macro do
 
   defp bad_pipe(expr, call_args) do
     "cannot pipe #{to_string(expr)} into #{to_string(call_args)}, " <>
-      "can only pipe into local calls foo(), remote calls Foo.bar() or anonymous functions calls foo.()"
+      "can only pipe into local calls foo(), remote calls Foo.bar() or anonymous function calls foo.()"
   end
 
   @doc """
