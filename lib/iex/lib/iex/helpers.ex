eb2ddbac6a4cc45260742afd7d3c99ef49ee6cd1
José Valim <jose.valim@gmail.com>
2018-10-22 21:25:45 +0200

    Update helpers.ex

--- lib/iex/lib/iex/helpers.ex
+++ lib/iex/lib/iex/helpers.ex
@@ -143,6 +143,8 @@ defmodule IEx.Helpers do
 
   It expects a list of files to compile and an optional path to write
   the compiled code to. By default files are in-memory compiled.
+  To write compiled files to a current directory use empty string "" for
+  the path. 
 
   It returns the names of the compiled modules.
 
@@ -150,15 +152,14 @@ defmodule IEx.Helpers do
 
   ## Examples
 
-      To write compiled files to a current directory use empty string "" for the path. 
-      In the example below, we pass a directory to where the c helper will 
-      write the .beam files to. This directory is typically named "ebin" for 
-      Erlang/Elixir systems. 
- 
+  In the example below, we pass a directory to where the `c/2` function will 
+  write the compiled `.beam` files to. This directory is typically named "ebin"
+  in Erlang/Elixir systems:
+
       iex> c(["foo.ex", "bar.ex"], "ebin")
       [Foo, Bar]
 
-      When compiling one file, there is no need to wrap it in a list.
+  When compiling one file, there is no need to wrap it in a list.
 
       iex> c("baz.ex")
       [Baz]
