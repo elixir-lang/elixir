926a133f673bcd0a543a8a9ae9bdba439ef63378
Lars Wikman <lars.wikman@gmail.com>
2018-11-21 10:47:47 +0100

    Added an additional missing space to an error message.

--- lib/elixir/src/elixir_expand.erl
+++ lib/elixir/src/elixir_expand.erl
@@ -1120,7 +1120,7 @@ format_error({invalid_quoted_expr, Expr}) ->
   Message =
     "invalid quoted expression: ~ts\n\n"
     "Please make sure your quoted expressions are made of valid AST nodes. "
-    "If you would like to introduce a value into the AST, such as a four-element"
+    "If you would like to introduce a value into the AST, such as a four-element "
     "tuple or a map, make sure to call Macro.escape/1 before",
   io_lib:format(Message, ['Elixir.Kernel':inspect(Expr, [])]);
 format_error({invalid_local_invocation, Context, {Name, _, Args} = Call}) ->
