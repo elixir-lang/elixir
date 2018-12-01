35d162c307b46ba6a0064848b2252c38bd438953
Lau Taarnskov <lau@taarnskov.com>
2018-12-01 14:59:40 -0300

    Add Calendar.datetime type and allow it in DateTime functions (#8444)
    Instead of requiring a DateTime struct, allow a map containing at least
    those fields. In the same way Calendar.naive_datetime is used in
    NaiveDateTime (and Calendar.time/Time, Calendar.date/Date).

--- lib/elixir/lib/calendar.ex
+++ lib/elixir/lib/calendar.ex
@@ -90,6 +90,23 @@ defmodule Calendar do
           microsecond: microsecond
         }
 
+  @typedoc "Any map/struct that contains the datetime fields"
+  @type datetime :: %{
+          optional(any) => any,
+          calendar: calendar,
+          year: year,
+          month: month,
+          day: day,
+          hour: hour,
+          minute: minute,
+          second: second,
+          microsecond: microsecond,
+          time_zone: time_zone,
+          zone_abbr: zone_abbr,
+          utc_offset: utc_offset,
+          std_offset: std_offset
+        }
+
   @typedoc """
   Specifies the time zone database for calendar operations.
 
