2fd7cc216a270a421eb2022d38f4306f63510252
eksperimental <eksperimental@users.noreply.github.com>
2018-12-09 00:50:29 +0700

    Elaborate on which characters are considered printable

--- lib/elixir/lib/list.ex
+++ lib/elixir/lib/list.ex
@@ -451,10 +451,24 @@ defmodule List do
   @doc """
   Checks if a list is a charlist made only of printable ASCII characters.
 
-  A printable charlist in Elixir contains only ASCII characters.
-
   Takes an optional `limit` as a second argument. `ascii_printable?/2` only
   checks the printability of the list up to the `limit`.
+
+  A printable charlist in Elixir contains only the printable characters in the
+  standard seven-bit ASCII character encoding, which are characters ranging from
+  32 to 126 in decimal notation, plus  the following control characters:
+
+  - `?\\a` - Bell
+  - `?\\b` - Backspace
+  - `?\\t` - Horizontal tab
+  - `?\\n` - Line feed
+  - `?\\v` - Vertical tab
+  - `?\\f` - Form feed
+  - `?\\r` - Carriage return
+  - `?\\e` - Escape
+
+  For more information read the [Character groups](https://en.wikipedia.org/wiki/ASCII#Character_groups)
+  section in the Wikipedia article about [ASCII](https://en.wikipedia.org/wiki/ASCII) standard.
 
   ## Examples
 
