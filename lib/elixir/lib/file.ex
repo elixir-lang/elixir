elixir-lang/elixir

commit 499446737ea0008bbb1c69a3e2f953fc7e1fedae

Allow posix time on touch and document proper defaults
This fixes issue #8450 which was caused by the user of
`system_time` when `os_time` should have been used.

This commit improves the touch API by also allowing
posix times in `File.touch/2` and also document its
defaults. With this change, we phase out the remaining
use of Erlang's `:calendar` module in Elixir.

lib/elixir/lib/file.ex
@@ -114,6 +114,12 @@ defmodule File do
           | {:read_ahead, pos_integer | false}
           | {:delayed_write, non_neg_integer, non_neg_integer}
 
+  @type erlang_time ::
+          {{year :: non_neg_integer(), month :: 1..12, day :: 1..13},
+           {hour :: 0..23, minute :: 0..59, second :: 0..59}}
+
+  @type posix_time :: integer()
+
   @doc """
   Returns `true` if the path is a regular file.
 
@@ -522,7 +528,9 @@ defmodule File do
   Updates modification time (mtime) and access time (atime) of
   the given file.
 
-  The file is created if it doesn’t exist. Requires datetime in UTC.
+  The file is created if it doesn’t exist. Requires datetime in UTC
+  (as returned by `:erlang.universaltime()`) or  or an integer
+  representing the POSIX timestamp (as returned by `System.os_time(:second)`).
 
   ## Examples
 
@@ -531,29 +539,36 @@ defmodule File do
       File.touch("/fakedir/b.txt", {{2018, 1, 30}, {13, 59, 59}})
       {:error, :enoent}
 
+      File.touch("/tmp/a.txt", 1544519753)
+      #=> :ok
+
   """
-  @spec touch(Path.t(), :calendar.datetime()) :: :ok | {:error, posix}
-  def touch(path, time \\ :calendar.universal_time()) do
+  @spec touch(Path.t(), erlang_time() | posix_time()) :: :ok | {:error, posix}
+  def touch(path, time \\ System.os_time(:second))
+
+  def touch(path, time) when is_tuple(time) do
     path = IO.chardata_to_string(path)
 
-    case :elixir_utils.change_universal_time(path, time) do
-      {:error, :enoent} -> touch_new(path, time)
-      other -> other
-    end
+    with {:error, :enoent} <- :elixir_utils.change_universal_time(path, time),
+         :ok <- write(path, "", [:append]),
+         do: :elixir_utils.change_universal_time(path, time)
   end
 
-  defp touch_new(path, time) do
-    case write(path, "", [:append]) do
-      :ok -> :elixir_utils.change_universal_time(path, time)
-      {:error, _reason} = error -> error
-    end
+  def touch(path, time) when is_integer(time) do
+    path = IO.chardata_to_string(path)
+
+    with {:error, :enoent} <- :elixir_utils.change_posix_time(path, time),
+         :ok <- write(path, "", [:append]),
+         do: :elixir_utils.change_posix_time(path, time)
   end
 
   @doc """
   Same as `touch/2` but raises a `File.Error` exception if it fails.
   Returns `:ok` otherwise.
 
-  Requires datetime in UTC.
+  The file is created if it doesn’t exist. Requires datetime in UTC
+  (as returned by `:erlang.universaltime()`) or  or an integer
+  representing the POSIX timestamp (as returned by `System.os_time(:second)`).
 
   ## Examples
 
@@ -562,9 +577,11 @@ defmodule File do
       File.touch!("/fakedir/b.txt", {{2018, 1, 30}, {13, 59, 59}})
       #=> ** (File.Error) could not touch "/fakedir/b.txt": no such file or directory
 
+      File.touch!("/tmp/a.txt", 1544519753)
+
   """
-  @spec touch!(Path.t(), :calendar.datetime()) :: :ok
-  def touch!(path, time \\ :calendar.universal_time()) do
+  @spec touch!(Path.t(), erlang_time() | posix_time()) :: :ok
+  def touch!(path, time \\ System.os_time(:second)) do
     case touch(path, time) do
       :ok ->
         :ok

lib/elixir/test/elixir/file_test.exs
@@ -1886,11 +1886,25 @@ defmodule FileTest do
     end
   end
 
-  test "touch with timestamp" do
-    fixture = tmp_path("tmp_test.txt")
+  test "touch with erlang timestamp" do
+    fixture = tmp_path("tmp_erlang_touch.txt")
 
     try do
-      assert File.touch!(fixture) == :ok
+      assert File.touch!(fixture, :erlang.universaltime()) == :ok
+      stat = File.stat!(fixture)
+
+      assert File.touch!(fixture, last_year()) == :ok
+      assert stat.mtime > File.stat!(fixture).mtime
+    after
+      File.rm(fixture)
+    end
+  end
+
+  test "touch with posix timestamp" do
+    fixture = tmp_path("tmp_posix_touch.txt")
+
+    try do
+      assert File.touch!(fixture, System.os_time(:second)) == :ok
       stat = File.stat!(fixture)
 
       assert File.touch!(fixture, last_year()) == :ok
@@ -1909,11 +1923,7 @@ defmodule FileTest do
     assert io_error?(File.touch(fixture))
   end
 
-  test "touch! with success" do
-    assert File.touch!(fixture_path()) == :ok
-  end
-
-  test "touch! with failure" do
+  test "touch! raises" do
     fixture = fixture_path("file.txt/bar")
 
     message =
@@ -2019,15 +2029,7 @@ defmodule FileTest do
   end
 
   defp last_year do
-    last_year(:calendar.local_time())
-  end
-
-  defp last_year({{year, 2, 29}, time}) do
-    {{year - 1, 2, 28}, time}
-  end
-
-  defp last_year({{year, month, day}, time}) do
-    {{year - 1, month, day}, time}
+    System.os_time(:second) - 365 * 24 * 60 * 60
   end
 
   defp io_error?(result) do

lib/mix/lib/mix/compilers/elixir.ex
@@ -33,7 +33,7 @@ defmodule Mix.Compilers.Elixir do
     # We fetch the time from before we read files so any future
     # change to files are still picked up by the compiler. This
     # timestamp is used when writing BEAM files and the manifest.
-    timestamp = :calendar.universal_time()
+    timestamp = System.os_time(:second)
     all_paths = MapSet.new(Mix.Utils.extract_files(srcs, exts))
 
     {all_modules, all_sources} = parse_manifest(manifest, dest)

lib/mix/lib/mix/utils.ex
@@ -176,7 +176,7 @@ defmodule Mix.Utils do
   (1970-01-01 00:00:00).
   """
   def last_modified_and_size(path) do
-    now = System.system_time(:second)
+    now = System.os_time(:second)
 
     case :elixir_utils.read_posix_mtime_and_size(path) do
       {:ok, mtime, size} when mtime > now ->
@@ -184,8 +184,7 @@ defmodule Mix.Utils do
           "warning: mtime (modified time) for #{inspect(path)} was set to the future, resetting to now"
 
         Mix.shell().error(message)
-
-        :elixir_utils.change_posix_time(path, now)
+        File.touch(path, now)
         {mtime, size}
 
       {:ok, mtime, size} ->

commit 42a3fb6c83e67e370b404b2627d1e999b6cefe16

Fix day in t:File.erlang_time
Bug recently introduced in 499446737ea0008bbb1c69a3e2f953fc7e1fedae

lib/elixir/lib/file.ex
@@ -115,7 +115,7 @@ defmodule File do
           | {:delayed_write, non_neg_integer, non_neg_integer}
 
   @type erlang_time ::
-          {{year :: non_neg_integer(), month :: 1..12, day :: 1..13},
+          {{year :: non_neg_integer(), month :: 1..12, day :: 1..31},
            {hour :: 0..23, minute :: 0..59, second :: 0..59}}
 
   @type posix_time :: integer()