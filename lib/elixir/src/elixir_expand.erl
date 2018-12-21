5f875442dbfd0ccc95bbaa6cab662928dd81456c
José Valim <jose.valim@plataformatec.com.br>
2018-12-21 11:03:53 +0100

    Validate reduce options in one pass

--- lib/elixir/src/elixir_expand.erl
+++ lib/elixir/src/elixir_expand.erl
@@ -272,32 +272,16 @@ expand({for, Meta, [_ | _] = Args}, E) ->
         form_error(Meta, ?key(E, file), ?MODULE, {missing_option, for, [do]})
     end,
 
-  IsReducing = lists:keymember(reduce, 1, Opts),
-  IsClauses = is_list(Expr) andalso lists:any(fun({'->', _, _}) -> true; (_) -> false end, Expr),
-
-  case IsReducing andalso (lists:keymember(into, 1, Opts) orelse lists:keymember(uniq, 1, Opts)) of
-    true ->
-      form_error(Meta, ?key(E, file), ?MODULE, for_duplicate_reduce_into_uniq);
-    false ->
-      ok
-  end,
-
-  case {IsReducing, IsClauses} of
-    {true, false} -> form_error(Meta, ?key(E, file), ?MODULE, for_with_reduce_bad_args);
-    {false, true} -> form_error(Meta, ?key(E, file), ?MODULE, for_without_reduce_with_arg);
-    {_, _} -> ok
-  end,
-
-  case lists:keyfind(uniq, 1, Opts) of
-    false -> ok;
-    {uniq, Value} when is_boolean(Value) -> ok;
-    {uniq, Value} -> form_error(Meta, ?key(E, file), ?MODULE, {for_invalid_uniq, Value})
-  end,
-
   {EOpts, EO} = expand(Opts, E),
   {ECases, EC} = lists:mapfoldl(fun expand_for/2, EO, Cases),
-  {EExpr, EE} = expand_for_do_block(Meta, Expr, EC, IsReducing),
   assert_generator_start(Meta, ECases, E),
+
+  {EExpr, EE} =
+    case validate_for_options(EOpts, false, false, false) of
+      {ok, MaybeReduce} -> expand_for_do_block(Meta, Expr, EC, MaybeReduce);
+      {error, Error} -> form_error(Meta, ?key(E, file), ?MODULE, Error)
+    end,
+
   {{for, Meta, ECases ++ [[{do, EExpr} | EOpts]]}, elixir_env:merge_and_check_unused_vars(E, EE)};
 
 %% With
@@ -715,21 +699,39 @@ generated_case_clauses([{do, Clauses}]) ->
 
 %% Comprehensions
 
-expand_for_do_block(_Meta, Expr, E, false) ->
-  expand(Expr, E);
+validate_for_options([{into, _} = Pair | Opts], _Into, Uniq, Reduce) ->
+  validate_for_options(Opts, Pair, Uniq, Reduce);
+validate_for_options([{uniq, Boolean} = Pair | Opts], Into, _Uniq, Reduce) when is_boolean(Boolean) ->
+  validate_for_options(Opts, Into, Pair, Reduce);
+validate_for_options([{uniq, Value} | _], _, _, _) ->
+  {error, {for_invalid_uniq, Value}};
+validate_for_options([{reduce, _} = Pair | Opts], Into, Uniq, _Reduce) ->
+  validate_for_options(Opts, Into, Uniq, Pair);
+validate_for_options([], Into, Uniq, {reduce, _}) when Into /= false; Uniq /= false ->
+  {error, for_conflicting_reduce_into_uniq};
+validate_for_options([], _Into, _Uniq, Reduce) ->
+  {ok, Reduce}.
 
-expand_for_do_block(Meta, Clauses, E, true) ->
+expand_for_do_block(Meta, Expr, E, false) ->
+  case Expr of
+    [{'->', _, _} | _] -> form_error(Meta, ?key(E, file), ?MODULE, for_without_reduce_bad_block);
+    _ -> expand(Expr, E)
+  end;
+expand_for_do_block(Meta, Clauses, E, {reduce, _}) ->
   Transformer = fun({_, _, [Left, _Right]} = Clause, Acc) ->
     case Left of
       [_] ->
         {EClause, EAcc} = elixir_clauses:clause(Meta, fn, fun elixir_clauses:head/2, Clause, Acc),
         {EClause, elixir_env:merge_and_check_unused_vars(Acc, EAcc)};
       _ ->
-        form_error(Meta, ?key(E, file), ?MODULE, for_with_reduce_bad_args)
+        form_error(Meta, ?key(E, file), ?MODULE, for_with_reduce_bad_block)
     end
   end,
 
-  lists:mapfoldl(Transformer, E, Clauses).
+  case Clauses of
+    [{'->', _, _} | _] -> lists:mapfoldl(Transformer, E, Clauses);
+    _ -> form_error(Meta, ?key(E, file), ?MODULE, for_with_reduce_bad_block)
+  end.
 
 %% Locals
 
@@ -1079,11 +1081,11 @@ format_error({invalid_args, Construct}) ->
   io_lib:format("invalid arguments for \"~ts\"", [Construct]);
 format_error({for_invalid_uniq, Value}) ->
   io_lib:format(":uniq option for comprehensions only accepts a boolean, got: ~ts", ['Elixir.Macro':to_string(Value)]);
-format_error(for_duplicate_reduce_into_uniq) ->
+format_error(for_conflicting_reduce_into_uniq) ->
   "cannot use :reduce alongside :into/:uniq in comprehension";
-format_error(for_with_reduce_bad_args) ->
+format_error(for_with_reduce_bad_block) ->
   "when using :reduce with comprehensions, the do block must be written using acc -> expr clauses, where each clause expects the accumulator as a single argument";
-format_error(for_without_reduce_with_arg) ->
+format_error(for_without_reduce_bad_block) ->
   "the do block was written using acc -> expr clauses but the :reduce option was not given";
 format_error(for_generator_start) ->
   "for comprehensions must start with a generator";
