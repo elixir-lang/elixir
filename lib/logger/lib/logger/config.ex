60924612f0adacd32b10fea18a24e71d9c3f4f19
José Valim <jose.valim@plataformatec.com.br>
2019-01-02 12:57:21 +0100

    Do not mix reads and writes

--- lib/logger/lib/logger/config.ex
+++ lib/logger/lib/logger/config.ex
@@ -4,6 +4,8 @@ defmodule Logger.Config do
   @behaviour :gen_event
   @name __MODULE__
   @table __MODULE__
+  @message_queue_len_pos 1
+  @counter_pos 2
 
   def configure(options) do
     :gen_event.call(Logger, @name, {:configure, options})
@@ -18,7 +20,7 @@ defmodule Logger.Config do
   end
 
   def level do
-    %{level: level} = read_translation_data!()
+    %{level: level} = read_data!(:translation_data)
     level
   end
 
@@ -34,15 +36,15 @@ defmodule Logger.Config do
   defp level_to_number(:error), do: 3
 
   def translation_data do
-    read_translation_data!()
+    read_data!(:translation_data)
   end
 
   def log_data(level) do
-    {:log_data, %{level: min_level} = config, message_queue_length} = read_log_data!()
+    %{level: min_level} = config = read_data!(:log_data)
 
     if compare_levels(level, min_level) != :lt do
       %{thresholds: {counter, sync, discard}} = config
-      counter = message_queue_length + bump_counter(counter)
+      counter = read_message_queue_len(counter) + bump_counter(counter)
 
       cond do
         counter >= discard -> {:discard, config}
@@ -79,7 +81,7 @@ defmodule Logger.Config do
 
   def init({@table, counter}) do
     :ets.lookup_element(@table, :log_data, 2) || compute_state(counter)
-    reset_counter_and_update_message_queue_length(counter)
+    reset_counter_and_update_message_queue_len(counter)
     {:ok, counter}
   end
 
@@ -92,7 +94,7 @@ defmodule Logger.Config do
   end
 
   def handle_event(_event, counter) do
-    reset_counter_and_update_message_queue_length(counter)
+    reset_counter_and_update_message_queue_len(counter)
     {:ok, counter}
   end
 
@@ -135,11 +137,14 @@ defmodule Logger.Config do
 
   # It is very important to reset the counter first and then update
   # the message queue length because that's the pessimistic approach.
-  # If the system is overloaded, then we will overestimate the overall
-  # size this is better than the inverse.
-  defp reset_counter_and_update_message_queue_length(counter) do
+  # If the system is approaching overloaded, then we will overestimate
+  # the overall size, which is better than the inverse. However, once
+  # we reach overload, we will underestimate the value, as the counter
+  # is increased by cleaned. Therefore some messages may make it back
+  # to the logger as we circle around the discard threshold value.
+  defp reset_counter_and_update_message_queue_len(counter) do
     reset_counter(counter)
-    update_message_queue_length()
+    update_message_queue_len(counter)
   end
 
   ## Counter Helpers
@@ -147,32 +152,47 @@ defmodule Logger.Config do
   # TODO: Use counters exclusively when we require Erlang/OTP 22+.
   defp new_counter() do
     if Code.ensure_loaded?(:counters) do
-      {:counters, :counters.new(1, [:atomics])}
+      {:counters, :counters.new(2, [:atomics])}
     else
       table = :ets.new(@table.Counter, [:public, write_concurrency: true])
-      :ets.insert(table, {:counter, 0})
+      :ets.insert(table, [{:counter, 0}, {:message_queue_len, 0}])
       {:ets, table}
     end
   end
 
   defp reset_counter({:ets, counter}), do: :ets.update_element(counter, :counter, {2, 0})
-  defp reset_counter({:counters, counter}), do: :counters.put(counter, 1, 0)
-
-  defp bump_counter({:ets, counter}), do: :ets.update_counter(counter, :counter, {2, 1})
-
-  defp bump_counter({:counters, counter}) do
-    :counters.add(counter, 1, 1)
-    :counters.get(counter, 1)
-  end
+  defp reset_counter({:counters, counter}), do: :counters.put(counter, @counter_pos, 0)
 
   defp delete_counter({:ets, counter}), do: :ets.delete(counter)
   defp delete_counter({:counters, _}), do: :ok
+
+  defp bump_counter({:ets, counter}),
+    do: :ets.update_counter(counter, :counter, {2, 1})
+
+  defp bump_counter({:counters, counter}) do
+    :counters.add(counter, @counter_pos, 1)
+    :counters.get(counter, @counter_pos)
+  end
+
+  defp read_message_queue_len({:ets, counter}),
+    do: :ets.lookup_element(counter, :message_queue_len, 2)
+
+  defp read_message_queue_len({:counters, counter}),
+    do: :counters.get(counter, @message_queue_len_pos)
+
+  defp update_message_queue_len({:ets, counter}),
+    do: :ets.insert(counter, Process.info(self(), :message_queue_len))
+
+  defp update_message_queue_len({:counters, counter}) do
+    {:message_queue_len, length} = Process.info(self(), :message_queue_len)
+    :counters.put(counter, @counter_pos, length)
+  end
 
   ## Data helpers
 
   defp new_data do
     entries = [
-      {:log_data, nil, 0},
+      {:log_data, nil},
       {:translation_data, nil},
       {:deleted_handlers, []}
     ]
@@ -184,16 +204,11 @@ defmodule Logger.Config do
 
   defp delete_data(@table), do: :ets.delete(@table)
 
-  defp update_message_queue_length do
-    {:message_queue_len, length} = Process.info(self(), :message_queue_len)
-    :ets.update_element(@table, :log_data, {3, length})
-  end
-
   defp update_translators(fun) do
-    translation_data = read_translation_data!()
+    translation_data = read_data!(:translation_data)
     translators = fun.(translation_data.translators)
     Application.put_env(:logger, :translators, translators)
-    update_translation_data(%{translation_data | translators: translators})
+    update_data(:translation_data, %{translation_data | translators: translators})
   end
 
   defp compute_state(counter) do
@@ -213,30 +228,14 @@ defmodule Logger.Config do
       truncate: Application.get_env(:logger, :truncate)
     }
 
-    update_log_data(log_data)
-    update_translation_data(translation_data)
+    update_data(:log_data, log_data)
+    update_data(:translation_data, translation_data)
     :ok
   end
 
-  defp read_log_data! do
+  defp read_data!(key) do
     try do
-      :ets.lookup(@table, :log_data)
-    rescue
-      ArgumentError ->
-        raise "cannot use Logger, the :logger application is not running"
-    else
-      [log_data] -> log_data
-      [] -> raise "cannot use Logger, the :logger application is not running"
-    end
-  end
-
-  defp update_log_data(log_data) do
-    :ets.update_element(@table, :log_data, {2, log_data})
-  end
-
-  defp read_translation_data! do
-    try do
-      :ets.lookup_element(@table, :translation_data, 2)
+      :ets.lookup_element(@table, key, 2)
     rescue
       ArgumentError ->
         raise "cannot use Logger, the :logger application is not running"
@@ -249,7 +248,7 @@ defmodule Logger.Config do
     end
   end
 
-  defp update_translation_data(translation_data) do
-    :ets.update_element(@table, :translation_data, {2, translation_data})
+  defp update_data(key, value) do
+    :ets.update_element(@table, key, {2, value})
   end
 end
