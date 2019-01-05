7ad213b289f2026260c6c68a8241bb64c36ac26c
Fernando Tapia Rico <fertapric@gmail.com>
2019-01-05 17:46:33 +0100

    Error on undefined variables in bitstring segments (#8598)
    Binary/bitstring matching allows to dynamically define the
    `size` of the binary in certain conditions:
    
      * if the `size` variable is defined prior to the pattern
        match:
    
            iex> size = 8
            iex> <<a::size(size), rest::binary>> = "hello"
            iex> a
            104
    
      * if the `size` variable is matched within the same
        binary/bitstring match, prior to its use:
    
            iex> <<name_size::size(8), name::binary-size(name_size), _rest::binary>> = <<5, "Frank the Walrus">>
            iex> name
            "Frank"
    
    Other cases are considered illegal patterns, for example:
    
        {name_size, <<name::binary-size(name_size), _rest::binary>>} = {5, "Frank the Walrus"}
    
    This commit raises a `CompileError: undefined variable ...` for such cases.

--- lib/elixir/lib/kernel/special_forms.ex
+++ lib/elixir/lib/kernel/special_forms.ex
@@ -246,11 +246,16 @@ defmodule Kernel.SpecialForms do
       iex> {name, species}
       {"Frank", "Walrus"}
 
-  And the variable can be defined in the match itself:
+  And the variable can be defined in the match itself (prior to its use):
 
       iex> <<name_size::size(8), name::binary-size(name_size), " the ", species::binary>> = <<5, "Frank the Walrus">>
       iex> {name, species}
       {"Frank", "Walrus"}
+
+  However, the size cannot be defined in the match outside the binary/bitstring match:
+
+      {name_size, <<name::binary-size(name_size), _rest::binary>>} = {5, <<"Frank the Walrus">>}
+      ** (CompileError): undefined variable "name_size" in bitstring segment
 
   Failing to specify the size for the non-last causes compilation to fail:
 
