8ce023f1f98938c6a4d443221d1b4f723869aaae
Fernando Tapia Rico <fertapric@gmail.com>
2019-01-20 18:46:11 +0100

    Augment doctest errors with misplaced opaque types

--- lib/ex_unit/test/ex_unit/doc_test_test.exs
+++ lib/ex_unit/test/ex_unit/doc_test_test.exs
@@ -196,6 +196,12 @@ defmodule ExUnit.DocTestTest.Invalid do
   false
   """
   def invalid_utf8, do: :ok
+
+  @doc """
+      iex> {:ok, MapSet.new([1, 2, 3])}
+      {:ok, #MapSet<[1, 2, 3]>}
+  """
+  def misplaced_opaque_type, do: :ok
 end
 |> write_beam
 
@@ -440,7 +446,7 @@ defmodule ExUnit.DocTestTest do
       doctest ExUnit.DocTestTest.Invalid
     end
 
-    doctest_line = 440
+    doctest_line = __ENV__.line - 3
 
     ExUnit.configure(seed: 0, colors: [enabled: false])
     ExUnit.Server.modules_loaded()
@@ -556,7 +562,16 @@ defmodule ExUnit.DocTestTest do
            """
 
     assert output =~ """
-            12) doctest ExUnit.DocTestTest.Invalid.b/0 (12) (ExUnit.DocTestTest.ActuallyCompiled)
+            12) doctest ExUnit.DocTestTest.Invalid.misplaced_opaque_type/0 (12) (ExUnit.DocTestTest.ActuallyCompiled)
+                test/ex_unit/doc_test_test.exs:#{doctest_line}
+                Doctest did not compile, got: (TokenMissingError) test/ex_unit/doc_test_test.exs:201: missing terminator: } (for "{" starting at line 201). If you are planning to assert on the result of an iex> expression using an opaque type (with the format #Name<...>), please make sure the opaque type is placed at the beginning of the expression; otherwise Elixir will treat it as a comment due to the leading sign #.
+                code: {:ok, #MapSet<[1, 2, 3]>}
+                stacktrace:
+                  test/ex_unit/doc_test_test.exs:201: ExUnit.DocTestTest.Invalid (module)
+           """
+
+    assert output =~ """
+            13) doctest ExUnit.DocTestTest.Invalid.b/0 (13) (ExUnit.DocTestTest.ActuallyCompiled)
                 test/ex_unit/doc_test_test.exs:#{doctest_line}
                 Doctest did not compile, got: (SyntaxError) test/ex_unit/doc_test_test.exs:165: syntax error before: '*'
                 code: 1 + * 1
