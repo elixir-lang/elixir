8594944c2e96869412a3120d005f729872ae82a8
José Valim <jose.valim@plataformatec.com.br>
2019-01-22 16:21:44 +0100

    Add version to umbrella projects too
    This will be useful when assembling releases.

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -370,6 +370,7 @@ defmodule Mix.Tasks.New do
     def project do
       [
         apps_path: "apps",
+        version: "0.1.0",
         start_permanent: Mix.env() == :prod,
         deps: deps()
       ]
