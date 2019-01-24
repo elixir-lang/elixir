ae67b56bff2b7bcabc8185fe8eb850193f01a708
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2019-01-24 13:50:15 +0000

    Make tests pass in 2020 (#8688)
    Using only 2038 to keep 32-bit UNIX systems happy.

--- lib/mix/test/mix/utils_test.exs
+++ lib/mix/test/mix/utils_test.exs
@@ -38,8 +38,8 @@ defmodule Mix.UtilsTest do
   end
 
   test "extract stale" do
-    # 2030-01-01 00:00:00
-    time = 1_893_456_000
+    # 2038-01-01 00:00:00
+    time = 2_145_916_800
     assert Mix.Utils.extract_stale([__ENV__.file], [time]) == []
 
     # 2000-01-01 00:00:00
