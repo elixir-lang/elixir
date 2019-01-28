2dd6511b87766a393a2e70296addc21f76274365
José Valim <jose.valim@plataformatec.com.br>
2019-01-28 15:14:58 +0100

    Run the formatter

--- lib/mix/test/mix/tasks/release_test.exs
+++ lib/mix/test/mix/tasks/release_test.exs
@@ -4,12 +4,12 @@ defmodule Mix.Tasks.ReleaseTest do
   use MixTest.Case
 
   test "ships a bootable release with ERTS" do
-    in_fixture "release_test", fn ->
+    in_fixture("release_test", fn ->
       Mix.Project.in_project(:release_test, ".", fn _ ->
         Mix.Task.run("release")
 
         assert System.cmd(Path.absname("_build/dev/rel/release_test/bin/start"), []) == {"", 0}
       end)
-    end
+    end)
   end
 end
