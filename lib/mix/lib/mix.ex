71de5c0dd374db421ae17c37faf0e2f5e3b15f55
José Valim <jose.valim@gmail.com>
2019-01-30 19:33:32 +0100

    Update mix.ex

--- lib/mix/lib/mix.ex
+++ lib/mix/lib/mix.ex
@@ -381,7 +381,7 @@ defmodule Mix do
   @doc """
   Raises a Mix error that is nicely formatted.
   """
-  @spec raise(binary()) :: no_return
+  @spec raise(binary) :: no_return
   def raise(message) when is_binary(message) do
     Kernel.raise(Mix.Error, mix: true, message: message)
   end
