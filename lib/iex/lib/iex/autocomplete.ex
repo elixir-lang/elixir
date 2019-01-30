7d3ded96c03293d81696220cd269db6e41485de7
José Valim <jose.valim@plataformatec.com.br>
2019-01-30 22:39:54 +0100

    Bring back autocomplete for remote nodes, closes #8743

--- lib/iex/lib/iex/autocomplete.ex
+++ lib/iex/lib/iex/autocomplete.ex
@@ -2,6 +2,19 @@ defmodule IEx.Autocomplete do
   @moduledoc false
 
   @doc """
+  Provides one helper function that is injected into connecting
+  remote nodes to properly handle autocompletion.
+  """
+  def remsh(node) do
+    fn e ->
+      case :rpc.call(node, IEx.Autocomplete, :expand, [e]) do
+        {:badrpc, _} -> {:no, '', []}
+        r -> r
+      end
+    end
+  end
+
+  @doc """
   The expansion logic.
 
   Some of the expansion has to be use the current shell
