d3a379fcf00511cfedeaae2e896e5c105635cb33
Christopher Eyre <chriseyre2000@gmail.com>
2019-02-09 14:42:16 +0000

    Added regex documentation for named character classes.

--- lib/elixir/lib/regex.ex
+++ lib/elixir/lib/regex.ex
@@ -103,6 +103,29 @@ defmodule Regex do
 
     * `list(binary)` - a list of named captures to capture
 
+  ## Posix character classes
+
+  There are several built in named character classes these are used by enclosing the name in [: :], this must be in a group [...] for example:
+
+      String.match?("123", ~r/[[:alnum:]]/)
+
+  The supported names are:
+
+  * alnum - Letters and digits
+  * alpha - Letters
+  * ascii - Character codes 0-127
+  * blank - Space or tab only
+  * cntrl - Control characters
+  * digit - Decimal digits (same as \\d)
+  * graph - Printing characters, excluding space
+  * lower - Lowercase letters
+  * print - Printing characters, including space
+  * punct - Printing characters, excluding letters, digits, and space
+  * space - Whitespace (the same as \\s from PCRE 8.34)
+  * upper - Uppercase letters
+  * word  - "Word" characters (same as \\w)
+  * xdigit - Hexadecimal digits
+
   """
 
   defstruct re_pattern: nil, source: "", opts: "", re_version: ""
