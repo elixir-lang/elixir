20f74a86ab2cf1446370d8350b92a9f7529eff60
José Valim <jose.valim@plataformatec.com.br>
2019-02-25 10:32:06 +0100

    Do not consider modules that are no longer cover compiled, closes #8825

--- lib/mix/test/fixtures/umbrella_cover/apps/bar/test/bar_tests.exs
+++ lib/mix/test/fixtures/umbrella_cover/apps/bar/test/bar_tests.exs
@@ -2,6 +2,7 @@ defmodule BarTest do
   use ExUnit.Case
 
   test "greets the world" do
-    assert Foo.hello() == :world
+    assert Bar.hello() == :world
+    assert Bar.Protocol.to_uppercase("foo") == "FOO"
   end
 end
