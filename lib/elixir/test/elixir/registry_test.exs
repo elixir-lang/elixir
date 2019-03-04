33d484231bc89f3ab218ff476cbe289394dc03ef
SpaceEEC <spaceeec@yahoo.com>
2019-03-04 02:07:01 +0100

    Ensure :name is given in Registry.start_link/1 (#8850)

--- lib/elixir/test/elixir/registry_test.exs
+++ lib/elixir/test/elixir/registry_test.exs
@@ -601,6 +601,18 @@ defmodule RegistryTest do
     assert %{id: Registry} = Registry.child_spec([])
   end
 
+  test "raises if :name is missing" do
+    assert_raise ArgumentError, ~r/expected :name option to be present/, fn ->
+      Registry.start_link(keys: :unique)
+    end
+  end
+
+  test "raises if :name is not an atom" do
+    assert_raise ArgumentError, ~r/expected :name to be an atom, got/, fn ->
+      Registry.start_link(keys: :unique, name: [])
+    end
+  end
+
   defp register_task(registry, key, value) do
     parent = self()
 
