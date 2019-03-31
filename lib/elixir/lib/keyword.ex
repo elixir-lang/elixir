23e8b877b9b7783ebd612ebaea687f7d2bfb34cb
Fernando Tapia Rico <fertapric@gmail.com>
2019-03-31 09:50:56 +0200

    Improve Keyword.pop/3 docs

--- lib/elixir/lib/keyword.ex
+++ lib/elixir/lib/keyword.ex
@@ -946,14 +946,14 @@ defmodule Keyword do
   end
 
   @doc """
-  Returns the value for `key` and removes all associated entries in the keyword list.
+  Returns the first value for `key` and removes all associated entries in the keyword list.
 
-  It returns a tuple where the first value for `key` is the first element and the
-  second element is a keyword list with all values associated with `key` are removed.
+  It returns a tuple where the first element is the first value for `key` and the
+  second element is a keyword list with all entries associated with `key` removed.
   If the `key` is not present in the keyword list, `{default, keyword_list}` is
   returned.
 
-  If you don't want to remove all the values associated with key use `pop_first/3`
+  If you don't want to remove all the entries associated with `key` use `pop_first/3`
   instead, that function will remove only the first entry.
 
   ## Examples
