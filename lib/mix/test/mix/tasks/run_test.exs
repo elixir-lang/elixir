cc72af1451c849a3a51c7a232db1bfde5a28c79b
José Valim <jose.valim@plataformatec.com.br>
2019-04-04 13:43:40 +0200

    Keep deprecated test

--- lib/mix/test/mix/tasks/run_test.exs
+++ lib/mix/test/mix/tasks/run_test.exs
@@ -11,6 +11,23 @@ defmodule Mix.Tasks.RunTest do
     Mix.Project.push(MixTest.Case.Sample)
   end
 
+  test "loads configuration", context do
+    in_tmp(context.test, fn ->
+      config = fixture_path("configs/good_config.exs")
+      expr = "IO.puts Application.get_env(:my_app, :key)"
+
+      output =
+        capture_io(:stderr, fn ->
+          assert capture_io(fn -> Mix.Task.run("run", ["--config", config, "--eval", expr]) end) ==
+                   "value\n"
+        end)
+
+      assert output =~ "deprecated"
+    end)
+  after
+    Application.delete_env(:my_app, :key)
+  end
+
   test "run requires files before evaling commands", context do
     git_repo = fixture_path("git_repo/lib/git_repo.ex")
 
