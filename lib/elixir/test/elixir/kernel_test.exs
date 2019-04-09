elixir-lang/elixir

commit 8698196921d1587fa365d3fcf463d31f4e30323d

Improvents to Kernel.in/2
1) Keep order of elements in Kernel.in/2:

The order of elements was not being kept (not doing as it was said in the docs).
Now it is guaranteed that the elements are compared in the same order they were given.

previously:

    iex> quote do 1 in [1, 2, 3] end |> Macro.expand(Map.put(__ENV__, :context, :guard)) |> Macro.to_string()
    ":erlang.orelse(:erlang.\"=:=\"(1, 2), :erlang.orelse(:erlang.\"=:=\"(1, 3), :erlang.\"=:=\"(1, 1)))"

now:

    iex>  quote do 1 in [1, 2, 3] end |> Macro.expand(Map.put(__ENV__, :context, :guard)) |> Macro.to_string()
    ":erlang.orelse(:erlang.orelse(:erlang.\"=:=\"(1, 1), :erlang.\"=:=\"(1, 2)), :erlang.\"=:=\"(1, 3))"

2) Error message improved from: invalid args for operator "in"...
to: invalid right argument for operator "in"...

3) More tests added to check for generated code

Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

lib/elixir/lib/kernel.ex
@@ -3543,6 +3543,9 @@ defmodule Kernel do
       [] when not in_module? ->
         false
 
+      [] ->
+        quote(do: _ = unquote(left))
+
       [head | tail] = list when not in_module? ->
         in_var(in_module?, left, &in_list(&1, head, tail, expand, list, in_module?))
 
@@ -3571,12 +3574,16 @@ defmodule Kernel do
         raise ArgumentError, "non-literal range in guard should be escaped with Macro.escape/2"
 
       right ->
-        raise ArgumentError, <<
-          "invalid args for operator \"in\", it expects a compile-time proper list ",
-          "or compile-time range on the right side when used in guard expressions, got: ",
-          Macro.to_string(right)::binary
-        >>
+        raise_on_invalid_args_in_2(right)
     end
+  end
+
+  defp raise_on_invalid_args_in_2(right) do
+    raise ArgumentError, <<
+      "invalid right argument for operator \"in\", it expects a compile-time proper list ",
+      "or compile-time range on the right side when used in guard expressions, got: ",
+      Macro.to_string(right)::binary
+    >>
   end
 
   defp in_var(false, ast, fun), do: fun.(ast)
@@ -3670,13 +3677,14 @@ defmodule Kernel do
   end
 
   defp in_list(left, head, tail, expand, right, in_module?) do
-    fun = fn elem, acc ->
-      quote do
-        :erlang.orelse(unquote(comp(left, elem, expand, right, in_module?)), unquote(acc))
-      end
-    end
+    [head | tail] =
+      :lists.foldl(
+        &[comp(left, &1, expand, right, in_module?) | &2],
+        [],
+        [head | tail]
+      )
 
-    :lists.foldr(fun, comp(left, head, expand, right, in_module?), tail)
+    :lists.foldl(&quote(do: :erlang.orelse(unquote(&1), unquote(&2))), head, tail)
   end
 
   defp comp(left, {:|, _, [head, tail]}, expand, right, in_module?) do
@@ -3701,11 +3709,7 @@ defmodule Kernel do
         end
 
       _ ->
-        raise ArgumentError, <<
-          "invalid args for operator \"in\", it expects a compile-time proper list ",
-          "or compile-time range on the right side when used in guard expressions, got: ",
-          Macro.to_string(right)::binary
-        >>
+        raise_on_invalid_args_in_2(right)
     end
   end
 

lib/elixir/test/elixir/kernel_test.exs
@@ -7,6 +7,12 @@ defmodule KernelTest do
 
   defp empty_list(), do: []
 
+  defp assert_eval_raise(error, msg, string) do
+    assert_raise error, msg, fn ->
+      Code.eval_string(string)
+    end
+  end
+
   test "=~/2" do
     assert "abcd" =~ ~r/c(d)/ == true
     assert "abcd" =~ ~r/e/ == false
@@ -260,6 +266,14 @@ defmodule KernelTest do
 
       assert 2 in [1 | [2, 3]]
       assert 3 in [1 | list]
+
+      some_call = & &1
+      refute :x in [1, 2 | some_call.([3, 4])]
+      assert :x in [1, 2 | some_call.([3, :x])]
+
+      assert_raise ArgumentError, fn ->
+        :x in [1, 2 | some_call.({3, 4})]
+      end
     end
 
     @at_list1 [4, 5]
@@ -405,7 +419,7 @@ defmodule KernelTest do
     end
 
     test "with a non-compile-time range in guards" do
-      message = ~r/invalid args for operator "in", .* got: :hello/
+      message = ~r/invalid right argument for operator "in", .* got: :hello/
 
       assert_eval_raise(ArgumentError, message, """
       defmodule InErrors do
@@ -415,7 +429,7 @@ defmodule KernelTest do
     end
 
     test "with a non-compile-time list cons in guards" do
-      message = ~r/invalid args for operator "in", .* got: \[1 | list\(\)\]/
+      message = ~r/invalid right argument for operator "in", .* got: \[1 | list\(\)\]/
 
       assert_eval_raise(ArgumentError, message, """
       defmodule InErrors do
@@ -426,7 +440,7 @@ defmodule KernelTest do
     end
 
     test "with a compile-time non-list in tail in guards" do
-      message = ~r/invalid args for operator "in", .* got: \[1 | 1..3\]/
+      message = ~r/invalid right argument for operator "in", .* got: \[1 | 1..3\]/
 
       assert_eval_raise(ArgumentError, message, """
       defmodule InErrors do
@@ -444,47 +458,89 @@ defmodule KernelTest do
       end
     end
 
-    test "hoists variables" do
+    test "hoists variables and keeps order" do
+      # Ranges
       result = expand_to_string(quote(do: rand() in 1..2))
       assert result =~ "var = rand()"
 
       assert result =~
                ":erlang.andalso(:erlang.is_integer(var), :erlang.andalso(:erlang.>=(var, 1), :erlang.\"=<\"(var, 2)))"
 
+      # Empty list
+      assert expand_to_string(quote(do: :x in [])) ==
+               "_ = :x"
+
+      assert expand_to_string(quote(do: :x in []), :guard) ==
+               "false"
+
+      # Lists
       result = expand_to_string(quote(do: rand() in [1, 2]))
       assert result =~ "var = rand()"
-      assert result =~ ":erlang.orelse(:erlang.\"=:=\"(var, 2), :erlang.\"=:=\"(var, 1))"
+      assert result =~ ":erlang.orelse(:erlang.\"=:=\"(var, 1), :erlang.\"=:=\"(var, 2))"
 
       result = expand_to_string(quote(do: rand() in [1 | [2]]))
       assert result =~ "var = rand()"
       assert result =~ ":erlang.orelse(:erlang.\"=:=\"(var, 1), :erlang.\"=:=\"(var, 2))"
 
+      result = expand_to_string(quote(do: rand() in [1, 2 | [3]]))
+      assert result =~ "var = rand()"
+
+      assert result =~
+               ":erlang.orelse(:erlang.\"=:=\"(var, 1), :erlang.orelse(:erlang.\"=:=\"(var, 2), :erlang.\"=:=\"(var, 3)))"
+
+      result = expand_to_string(quote(do: rand() in [1 | [2 | [3]]]))
+      assert result =~ "var = rand()"
+
+      assert result =~
+               ":erlang.orelse(:erlang.\"=:=\"(var, 1), :erlang.orelse(:erlang.\"=:=\"(var, 2), :erlang.\"=:=\"(var, 3)))"
+
       result = expand_to_string(quote(do: rand() in [1 | some_call()]))
       assert result =~ "var = rand()"
       assert result =~ "{arg0} = {some_call()}"
       assert result =~ ":erlang.orelse(:erlang.\"=:=\"(var, 1), :lists.member(var, arg0))"
+
+      result = expand_to_string(quote(do: rand() in [{1}, {2}, {3} | some_call()]))
+      assert result =~ "var = rand()"
+      assert result =~ "{arg0, arg1, arg2, arg3} = {{1}, {2}, {3}, some_call()}"
+
+      assert result =~
+               ":erlang.orelse(:erlang.\"=:=\"(var, arg1), :erlang.orelse(:erlang.\"=:=\"(var, arg2), :lists.member(var, arg3))))"
+    end
+
+    defp quote_case_in(left, right) do
+      quote do
+        case 0 do
+          _ when unquote(left) in unquote(right) -> true
+        end
+      end
     end
 
     test "is optimized" do
-      assert expand_to_string(quote(do: foo in [])) == "Enum.member?([], foo)"
+      assert expand_to_string(quote(do: foo in [])) == "_ = foo"
+
+      assert expand_to_string(quote(do: foo in [foo])) =~
+               ~r/{arg0} = {foo}\n\s+:erlang."=:="\(foo, arg0\)\n/
 
       assert expand_to_string(quote(do: foo in 0..1)) ==
                ":erlang.andalso(:erlang.is_integer(foo), :erlang.andalso(:erlang.>=(foo, 0), :erlang.\"=<\"(foo, 1)))"
 
       assert expand_to_string(quote(do: foo in -1..0)) ==
                ":erlang.andalso(:erlang.is_integer(foo), :erlang.andalso(:erlang.>=(foo, -1), :erlang.\"=<\"(foo, 0)))"
+
+      assert expand_to_string(quote(do: foo in 1..1)) ==
+               ":erlang.\"=:=\"(foo, 1)"
     end
 
-    defp expand_to_string(ast) do
+    defp expand_to_string(ast, environment_or_context \\ __ENV__)
+
+    defp expand_to_string(ast, context) when is_atom(context) do
+      expand_to_string(ast, %{__ENV__ | context: context})
+    end
+
+    defp expand_to_string(ast, environment) do
       ast
-      |> Macro.prewalk(&Macro.expand(&1, __ENV__))
+      |> Macro.prewalk(&Macro.expand(&1, environment))
       |> Macro.to_string()
-    end
-
-    defp assert_eval_raise(error, msg, string) do
-      assert_raise error, msg, fn ->
-        Code.eval_string(string)
-      end
     end
   end
 

lib/elixir/test/elixir/macro_test.exs
@@ -752,10 +752,7 @@ defmodule MacroTest do
 
     test "to_match/1" do
       quote = quote(do: x in [])
-
-      assert {{:., _, [{:__aliases__, _, [Elixir, :Enum]}, :member?]}, _, _} =
-               Macro.expand_once(quote, __ENV__)
-
+      assert {:=, [], [{:_, [], _}, {:x, [], _}]} = Macro.expand_once(quote, __ENV__)
       assert Macro.expand_once(quote, Macro.Env.to_match(__ENV__)) == false
     end
   end

lib/elixir/test/elixir/kernel/guard_test.exs
@@ -270,7 +270,7 @@ defmodule Kernel.GuardTest do
     test "fails on expressions not allowed in guards" do
       # Slightly unique errors
 
-      assert_raise ArgumentError, ~r{invalid args for operator "in"}, fn ->
+      assert_raise ArgumentError, ~r{invalid right argument for operator "in"}, fn ->
         defmodule RuntimeListUsage do
           defguard foo(bar, baz) when bar in baz
         end

commit 6e7ac8c053f3c75431e547f441f57bc7bb19f2e7

Fix bug in Kernel.in/2 when right is [] (#8946)
Bug was introduced in 8698196921d1587fa365d3fcf463d31f4e30323d

lib/elixir/lib/kernel.ex
@@ -3544,7 +3544,10 @@ defmodule Kernel do
         false
 
       [] ->
-        quote(do: _ = unquote(left))
+        quote do
+          _ = unquote(left)
+          false
+        end
 
       [head | tail] = list when not in_module? ->
         in_var(in_module?, left, &in_list(&1, head, tail, expand, list, in_module?))

lib/elixir/test/elixir/kernel_test.exs
@@ -257,12 +257,20 @@ defmodule KernelTest do
       assert 2 in 1..3
       refute 4 in [1, 2, 3]
       refute 4 in 1..3
+      refute 2 in []
+      refute false in []
+      refute true in []
     end
 
     test "with expressions on right side" do
       list = [1, 2, 3]
+      empty_list = []
       assert 2 in list
       refute 4 in list
+
+      refute 4 in empty_list
+      refute false in empty_list
+      refute true in empty_list
 
       assert 2 in [1 | [2, 3]]
       assert 3 in [1 | list]
@@ -467,8 +475,11 @@ defmodule KernelTest do
                ":erlang.andalso(:erlang.is_integer(var), :erlang.andalso(:erlang.>=(var, 1), :erlang.\"=<\"(var, 2)))"
 
       # Empty list
-      assert expand_to_string(quote(do: :x in [])) ==
-               "_ = :x"
+      assert expand_to_string(quote(do: :x in [])) =~
+               ~S"""
+                 _ = :x
+                 false
+               """
 
       assert expand_to_string(quote(do: :x in []), :guard) ==
                "false"
@@ -516,7 +527,11 @@ defmodule KernelTest do
     end
 
     test "is optimized" do
-      assert expand_to_string(quote(do: foo in [])) == "_ = foo"
+      assert expand_to_string(quote(do: foo in [])) =~
+               ~S"""
+                 _ = foo
+                 false
+               """
 
       assert expand_to_string(quote(do: foo in [foo])) =~
                ~r/{arg0} = {foo}\n\s+:erlang."=:="\(foo, arg0\)\n/

lib/elixir/test/elixir/macro_test.exs
@@ -752,7 +752,10 @@ defmodule MacroTest do
 
     test "to_match/1" do
       quote = quote(do: x in [])
-      assert {:=, [], [{:_, [], _}, {:x, [], _}]} = Macro.expand_once(quote, __ENV__)
+
+      assert {:__block__, [], [{:=, [], [{:_, [], Kernel}, {:x, [], MacroTest}]}, false]} =
+               Macro.expand_once(quote, __ENV__)
+
       assert Macro.expand_once(quote, Macro.Env.to_match(__ENV__)) == false
     end
   end