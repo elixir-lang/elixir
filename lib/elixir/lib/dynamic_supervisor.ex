7b479a2dbb8940bf0ee28cd4b3e0297119add344
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-05-27 11:47:45 +0200

    Improves module documentation of DynamicSupervisor
    Fixes #9075

--- lib/elixir/lib/dynamic_supervisor.ex
+++ lib/elixir/lib/dynamic_supervisor.ex
@@ -58,7 +58,7 @@ defmodule DynamicSupervisor do
       end
 
   See the `Supervisor` docs for a discussion of when you may want to use
-  module-based supervisors. The `@doc` annotation immediately preceding
+  module-based supervisors. A `@doc` annotation immediately preceding
   `use DynamicSupervisor` will be attached to the generated `child_spec/1`
   function.
 
