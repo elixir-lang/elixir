071025d94624247857acda16c55b8b6bedddecc2
Andrea Leopardi <an.leopardi@gmail.com>
2019-06-30 23:28:57 +0200

    Add test for @describetag outside of describe block (#9180)

--- lib/ex_unit/test/ex_unit/describe_test.exs
+++ lib/ex_unit/test/ex_unit/describe_test.exs
@@ -91,6 +91,21 @@ defmodule ExUnit.DescribeTest do
     end
   end
 
+  test "when @describetag is used outside of a describe block" do
+    message = ~s(@describetag must be set inside describe/2 blocks)
+
+    assert_raise RuntimeError, message, fn ->
+      defmodule DescribetagOutsideOfDescribeBlock do
+        use ExUnit.Case
+
+        @describetag :integration
+
+        describe "some tests" do
+        end
+      end
+    end
+  end
+
   describe "test names" do
     test "merge describe information", context do
       assert context.test == :"test test names merge describe information"
