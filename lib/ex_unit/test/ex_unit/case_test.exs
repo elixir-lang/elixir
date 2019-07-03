6f7ce49aa89154ffbe95a92e0d2ef57b641a05a5
Allen Madsen <allen.c.madsen@gmail.com>
2019-07-03 14:38:19 -0400

    Add describe and module attributes (#9181)

--- lib/ex_unit/test/ex_unit/case_test.exs
+++ lib/ex_unit/test/ex_unit/case_test.exs
@@ -6,8 +6,16 @@ defmodule ExUnit.CaseTest do
   ExUnit.Case.register_attribute(__MODULE__, :foo)
   ExUnit.Case.register_attribute(__MODULE__, :bar, accumulate: true)
   ExUnit.Case.register_attribute(__MODULE__, :baz)
+  ExUnit.Case.register_describe_attribute(__MODULE__, :describe_foo)
+  ExUnit.Case.register_describe_attribute(__MODULE__, :describe_bar, accumulate: true)
+  ExUnit.Case.register_describe_attribute(__MODULE__, :describe_baz)
+  ExUnit.Case.register_module_attribute(__MODULE__, :module_foo)
+  ExUnit.Case.register_module_attribute(__MODULE__, :module_bar, accumulate: true)
+  ExUnit.Case.register_module_attribute(__MODULE__, :module_baz)
 
   @moduletag :moduletag
+  @module_foo :hello
+  @module_bar :world
 
   test "defines __ex_unit__" do
     assert %ExUnit.TestModule{name: __MODULE__, tests: tests} = __ex_unit__()
@@ -54,5 +62,54 @@ defmodule ExUnit.CaseTest do
   test "registered attributes are set per test", context do
     assert context.registered.foo == nil
     assert context.registered.bar == []
+  end
+
+  describe "with attributes" do
+    @describe_foo :hello
+    @describe_bar :world
+
+    test "registered subscribe attributes are in context", context do
+      assert context.registered.describe_foo == :hello
+      assert context.registered.describe_bar == [:world]
+      assert context.registered.describe_baz == nil
+    end
+  end
+
+  describe "without attributes" do
+    test "registered subscribe attributes are set per subscribe", context do
+      assert context.registered.describe_foo == nil
+      assert context.registered.describe_bar == []
+    end
+  end
+
+  test "registered module attributes are in context", context do
+    assert context.registered.module_foo == :hello
+    assert context.registered.module_bar == [:world]
+    assert context.registered.module_baz == nil
+  end
+
+  test "registered module attributes stay in context", context do
+    assert context.registered.module_foo == :hello
+    assert context.registered.module_bar == [:world]
+  end
+
+  test "raises when same name is registered twice" do
+    assert_raise ArgumentError, "cannot register attribute :foo multiple times", fn ->
+      defmodule AcrossLevelDoubleRegisterTest do
+        use ExUnit.Case
+        ExUnit.Case.register_attribute(__MODULE__, :foo)
+        ExUnit.Case.register_module_attribute(__MODULE__, :foo)
+      end
+    end
+  end
+
+  test "raises when attribute is set before being registered" do
+    assert_raise RuntimeError, "you must set @foo after it has been registered", fn ->
+      defmodule SetBeforeRegisterTest do
+        use ExUnit.Case
+        @foo true
+        ExUnit.Case.register_attribute(__MODULE__, :foo, accumulate: true)
+      end
+    end
   end
 end
