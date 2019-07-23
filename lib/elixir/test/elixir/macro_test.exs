c47cc721770a8f260a216834e49b3c3a350c48d3
Hassan Shaikley <hassan.shaikley@gmail.com>
2019-07-23 02:27:28 -0700

    Format

--- lib/elixir/test/elixir/macro_test.exs
+++ lib/elixir/test/elixir/macro_test.exs
@@ -788,7 +788,7 @@ defmodule MacroTest do
       Macro.pipe(1, quote(do: unquote()), 0)
     end
 
-     assert_raise ArgumentError, ~r"cannot pipe 1 into \+1", fn ->
+    assert_raise ArgumentError, ~r"cannot pipe 1 into \+1", fn ->
       Macro.pipe(1, quote(do: + 1), 0)
     end
 
