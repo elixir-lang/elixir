29326252bd387b89b69c8b5577068fbc6b0fd20b
Benjamin Milde <benni@kobrakai.de>
2019-08-15 11:42:06 +0200

    Remove IEx.Helpers.i/1 snippet

--- lib/elixir/lib/list.ex
+++ lib/elixir/lib/list.ex
@@ -97,24 +97,8 @@ defmodule List do
       [1097, 1098, 1099]
 
   You can use the `IEx.Helpers.i/1` helper to get a condensed rundown on
-  charlists in IEx when you encounter them:
-
-      > i 'abc'
-      Term
-        'abc'
-      Data type
-        List
-      Description
-        This is a list of integers that is printed as a sequence of characters
-        delimited by single quotes because all the integers in it represent printable
-        ASCII characters. Conventionally, a list of Unicode code points is known as a
-        charlist and a list of ASCII characters is a subset of it.
-      Raw representation
-        [97, 98, 99]
-      Reference modules
-        List
-      Implemented protocols
-        Collectable, Enumerable, IEx.Info, Inspect, List.Chars, String.Chars
+  charlists in IEx when you encounter them, which shows you the type, description
+  and also the raw representation in one single summary.
 
   The rationale behind this behaviour is to better support
   Erlang libraries which may return text as charlists
