67df1a26fb5efb2c4c575a909fdfcc24104e74a1
Benjamin Milde <benni@kobrakai.de>
2019-08-15 13:04:01 +0200

    Improve documentation on charlists (#9294)

--- lib/elixir/lib/list.ex
+++ lib/elixir/lib/list.ex
@@ -88,10 +88,23 @@ defmodule List do
       iex> 'abc'
       'abc'
 
+  Even though the representation changed, the raw data does remain a list of
+  numbers, which can be handled as such:
+
+      iex> inspect('abc', charlists: :as_list)
+      "[97, 98, 99]"
+      iex> Enum.map('abc', fn num -> 1000 + num end)
+      [1097, 1098, 1099]
+
+  You can use the `IEx.Helpers.i/1` helper to get a condensed rundown on
+  charlists in IEx when you encounter them, which shows you the type, description
+  and also the raw representation in one single summary.
+
   The rationale behind this behaviour is to better support
   Erlang libraries which may return text as charlists
-  instead of Elixir strings. One example of such functions
-  is `Application.loaded_applications/0`:
+  instead of Elixir strings. In Erlang, charlists are the default
+  way of handling strings, while in Elixir it's binaries. One
+  example of such functions is `Application.loaded_applications/0`:
 
       Application.loaded_applications()
       #=>  [
