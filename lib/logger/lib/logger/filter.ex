d50fd2f8f78e8cfa70200cce5aec31e057229e15
Łukasz Niemier <lukasz@niemier.pl>
2019-09-10 17:06:11 +0200

    chore: format Logger.Filter

--- lib/logger/lib/logger/filter.ex
+++ lib/logger/lib/logger/filter.ex
@@ -5,12 +5,11 @@ defmodule Logger.Filter do
   Filter out SASL reports if `sasl_enabled?` is `false`
   """
   def sasl(sasl_enabled?) do
-    filter =
-      fn
-        %{meta: %{domain: [:otp, :sasl | _]}}, false -> :stop
-        %{meta: %{domain: [:supervisor_report | _]}}, false -> :stop
-        log, _sasl_enabled -> log
-      end
+    filter = fn
+      %{meta: %{domain: [:otp, :sasl | _]}}, false -> :stop
+      %{meta: %{domain: [:supervisor_report | _]}}, false -> :stop
+      log, _sasl_enabled -> log
+    end
 
     {filter, sasl_enabled?}
   end
@@ -20,11 +19,11 @@ defmodule Logger.Filter do
   """
   def process_disabled() do
     {fn log, _extra ->
-      if Logger.enabled?(self()) do
-        log
-      else
-        :stop
-      end
-    end, nil}
+       if Logger.enabled?(self()) do
+         log
+       else
+         :stop
+       end
+     end, nil}
   end
 end
