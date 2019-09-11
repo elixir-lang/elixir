5c5227355cf4ed00dc11987e335c5ae3384bc7a7
Łukasz Niemier <lukasz@niemier.pl>
2019-09-11 11:56:11 +0200

    fix: do not log, but just ignore when match on process filter

--- lib/logger/lib/logger/filter.ex
+++ lib/logger/lib/logger/filter.ex
@@ -2,25 +2,12 @@ defmodule Logger.Filter do
   @moduledoc false
 
   @doc """
-  Filter out SASL reports if `sasl_enabled?` is `false`
-  """
-  def sasl(sasl_enabled?) do
-    filter = fn
-      %{meta: %{domain: [:otp, :sasl | _]}}, false -> :stop
-      %{meta: %{domain: [:supervisor_report | _]}}, false -> :stop
-      log, _sasl_enabled -> log
-    end
-
-    {filter, sasl_enabled?}
-  end
-
-  @doc """
   Filter out logs if current process opted out of log reports
   """
   def process_disabled() do
-    {fn log, _extra ->
+    {fn _log, _extra ->
        if Logger.enabled?(self()) do
-         log
+         :ignore
        else
          :stop
        end
