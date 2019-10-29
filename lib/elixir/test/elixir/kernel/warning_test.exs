9aa82d92a729727202abf8fd056a064017f8814b
Wojtek Mach <wojtek@wojtekmach.pl>
2019-10-29 17:16:01 +0100

    Move test to warning_test.exs

--- lib/elixir/test/elixir/kernel/warning_test.exs
+++ lib/elixir/test/elixir/kernel/warning_test.exs
@@ -1782,6 +1782,19 @@ defmodule Kernel.WarningTest do
     purge(TestMod)
   end
 
+  test "duplicate keys" do
+    assert capture_err(fn ->
+             Code.eval_string("""
+             defmodule TestMod do
+               import Record
+               defrecord :r, [:foo, :foo, :bar]
+             end
+             """)
+           end) =~ "duplicate key :foo found in record"
+  after
+    purge(TestMod)
+  end
+
   defp purge(list) when is_list(list) do
     Enum.each(list, &purge/1)
   end
