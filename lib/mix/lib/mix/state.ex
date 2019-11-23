82b28f932b7c772aecff6e516efc435df00964ec
José Valim <jose.valim@plataformatec.com.br>
2019-11-23 10:47:06 +0100

    Further cleanup on Mix agents

--- lib/mix/lib/mix/state.ex
+++ lib/mix/lib/mix/state.ex
@@ -1,6 +1,7 @@
 defmodule Mix.State do
   @moduledoc false
   @name __MODULE__
+  @timeout 30_000
 
   use Agent
 
@@ -14,44 +15,55 @@ defmodule Mix.State do
       env: String.to_atom(System.get_env("MIX_ENV") || "dev"),
       target: String.to_atom(System.get_env("MIX_TARGET") || "host"),
       scm: [Mix.SCM.Git, Mix.SCM.Path],
-      cache: %{}
+      cache: :ets.new(@name, [:public, :set, :named_table, read_concurrency: true])
     }
   end
 
   def fetch(key) do
-    Agent.get(@name, Map, :fetch, [key])
+    Agent.get(@name, Map, :fetch, [key], @timeout)
   end
 
   def get(key, default \\ nil) do
-    Agent.get(@name, Map, :get, [key, default])
+    Agent.get(@name, Map, :get, [key, default], @timeout)
   end
 
   def put(key, value) do
-    Agent.update(@name, Map, :put, [key, value])
+    Agent.update(@name, Map, :put, [key, value], @timeout)
   end
 
   def prepend_scm(value) do
-    Agent.update(@name, fn state -> update_in(state.scm, &[value | List.delete(&1, value)]) end)
+    Agent.update(
+      @name,
+      fn state -> update_in(state.scm, &[value | List.delete(&1, value)]) end,
+      @timeout
+    )
   end
 
   def append_scm(value) do
-    Agent.update(@name, fn state -> update_in(state.scm, &(List.delete(&1, value) ++ [value])) end)
+    Agent.update(
+      @name,
+      fn state -> update_in(state.scm, &(List.delete(&1, value) ++ [value])) end,
+      @timeout
+    )
   end
 
   def read_cache(key) do
-    Agent.get(@name, & &1.cache[key])
+    case :ets.lookup(@name, key) do
+      [{^key, value}] -> value
+      [] -> nil
+    end
   end
 
   def write_cache(key, value) do
-    Agent.cast(@name, &put_in(&1.cache[key], value))
+    :ets.insert(@name, {key, value})
     value
   end
 
   def delete_cache(key) do
-    Agent.cast(@name, &(pop_in(&1.cache[key]) |> elem(1)))
+    :ets.delete(@name, key)
   end
 
   def clear_cache do
-    Agent.cast(@name, &put_in(&1.cache, %{}))
+    :ets.delete_all_objects(@name)
   end
 end
