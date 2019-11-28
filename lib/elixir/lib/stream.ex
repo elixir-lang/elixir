9938ff428dfcb18c4b4c6663ac02c6a1ecd3e302
Josh Price <josh@alembic.com.au>
2019-11-29 04:32:15 +1100

    Replace exsplus with exrop as the OTP 20 default pseudorandom algorithm (#9609)

--- lib/elixir/lib/stream.ex
+++ lib/elixir/lib/stream.ex
@@ -1332,9 +1332,9 @@ defmodule Stream do
   ## Examples
 
       # Although not necessary, let's seed the random algorithm
-      iex> :rand.seed(:exsplus, {1, 2, 3})
+      iex> :rand.seed(:exrop, {1, 2, 3})
       iex> Stream.repeatedly(&:rand.uniform/0) |> Enum.take(3)
-      [0.40502929729990744, 0.45336720247823126, 0.04094511692041057]
+      [0.7498295129076106, 0.06161655489244533, 0.7924073127680873]
 
   """
   @spec repeatedly((() -> element)) :: Enumerable.t()
