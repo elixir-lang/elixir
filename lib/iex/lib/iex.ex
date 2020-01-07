7c8e22b4dcb7aaf2c1d98844d6b8b9c32744b6c9
Hassan Schroeder <hassan@webtuitive.com>
2020-01-07 15:53:40 -0800

    Clarify which home directory is referred to by `~` in `~/.iex.exs`.

--- lib/iex/lib/iex.ex
+++ lib/iex/lib/iex.ex
@@ -272,11 +272,12 @@ defmodule IEx do
   ## The .iex.exs file
 
   When starting, IEx looks for a local `.iex.exs` file (located in the current
-  working directory), then a global one (located at `~/.iex.exs`) and loads the
-  first one it finds (if any). The code in the loaded `.iex.exs` file is
-  evaluated in the shell's context. So, for instance, any modules that are
-  loaded or variables that are bound in the `.iex.exs` file will be available in the
-  shell after it has booted.
+  working directory), then a global one (located at `~/.iex.exs` where the `~`
+  in this case represents the user that _started the application_, not necessarily
+  the user invoking IEx) ) and loads the first one it finds (if any). The code
+  in the loaded `.iex.exs` file is evaluated in the shell's context. So, for
+  instance, any modules that are loaded or variables that are bound in the
+  `.iex.exs` file will be available in the shell after it has booted.
 
   For example, take the following `.iex.exs` file:
 
