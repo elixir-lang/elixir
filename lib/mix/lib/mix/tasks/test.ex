d3cb0dbc8db12b6cd908a09b6719169a3ac0ed9c
José Valim <jose.valim@dashbit.co>
2020-03-08 14:37:39 +0100

    Update lib/mix/lib/mix/tasks/test.ex
    Co-Authored-By: Fernando Tapia Rico <fertapric@gmail.com>

--- lib/mix/lib/mix/tasks/test.ex
+++ lib/mix/lib/mix/tasks/test.ex
@@ -157,7 +157,7 @@ defmodule Mix.Tasks.Test do
     * `:export` - a file name to export results to instead of generating
       the result on the fly. See `mix test.coverage` to compile a report
       from multiple exports. This option is automatically enabled when
-      used the process partitioning feature
+      using the process partitioning feature
 
   By default, a very simple wrapper around OTP's `cover` is used as a tool,
   but it can be overridden as follows:
