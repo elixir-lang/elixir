1387035845b70a625e4036c2170ed22dcf9a4587
José Valim <jose.valim@dashbit.co>
2020-04-23 20:07:42 +0200

    Do not assume all files have started to compile, closes #9943

--- lib/elixir/lib/kernel/parallel_compiler.ex
+++ lib/elixir/lib/kernel/parallel_compiler.ex
@@ -424,10 +424,11 @@ defmodule Kernel.ParallelCompiler do
 
   # The goal of this function is to find leaves in the dependency graph,
   # i.e. to find code that depends on code that we know is not being defined.
+  # Note that not all files have been compile yet, so they may not be in waiting.
   defp without_definition(waiting, files) do
     nillify_empty(
       for {pid, _, _, _} <- files,
-          {_, ^pid, ref, on, _, _} = List.keyfind(waiting, pid, 1),
+          {_, ^pid, ref, on, _, _} <- List.wrap(List.keyfind(waiting, pid, 1)),
           not Enum.any?(waiting, fn {_, _, _, _, defining, _} -> on in defining end),
           do: {ref, :not_found}
     )
