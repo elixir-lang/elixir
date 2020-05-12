6559db64cb6bc8d5c4f66c314454ad8ed32d517a
Wojtek Mach <wojtekmach@users.noreply.github.com>
2020-05-12 11:27:02 +0200

    Update lib/mix/lib/mix/tasks/cmd.ex
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- lib/mix/lib/mix/tasks/cmd.ex
+++ lib/mix/lib/mix/tasks/cmd.ex
@@ -50,7 +50,7 @@ defmodule Mix.Tasks.Cmd do
   @impl true
   def run(args) do
     {opts, args} = OptionParser.parse_head!(args, strict: @switches)
-    apps = Enum.map(List.wrap(opts[:app] || []), &String.to_atom/1)
+    apps = Enum.map(List.wrap(opts[:app]), &String.to_atom/1)
 
     if apps == [] or Mix.Project.config()[:app] in apps do
       cmd_opts = Keyword.take(opts, [:cd])
