58483e04455b3590e2b29b325f791426cca14783
sampriddy <sam@scriptdrop.co>
2020-06-01 11:35:52 -0600

    Undo change removing assigns from Mix.Generator.copy_template docs (#10075)

--- lib/mix/lib/mix/generator.ex
+++ lib/mix/lib/mix/generator.ex
@@ -97,7 +97,8 @@ defmodule Mix.Generator do
 
   ## Examples
 
-      iex> Mix.Generator.copy_template("source/gitignore", ".gitignore")
+      iex> assigns = [project_path: "/Users/joe/newproject"]
+      iex> Mix.Generator.copy_template("source/gitignore", ".gitignore", assigns)
       * creating .gitignore
       true
 
