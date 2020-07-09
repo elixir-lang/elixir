75416bb34d822ddab6d0364a4b6174ccaf2095c7
José Valim <jose.valim@gmail.com>
2020-07-09 09:27:45 +0200

    Update lib/mix/test/mix/release_test.exs

--- lib/mix/test/mix/release_test.exs
+++ lib/mix/test/mix/release_test.exs
@@ -596,7 +596,7 @@ defmodule Mix.ReleaseTest do
       assert File.read!(Path.join(destination, "bin/erl")) =~
                ~s|ROOTDIR="$(dirname "$(dirname "$BINDIR")")"|
 
-      unless match?({:win32, _}, :os.type) do
+      unless match?({:win32, _}, :os.type()) do
         assert File.lstat!(Path.join(destination, "bin/erl")).mode |> rem(0o1000) == 0o755
       end
 
