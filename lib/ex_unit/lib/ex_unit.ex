f7c7fd0378d844a6250cd8633c00d2b31440e6b6
José Valim <jose.valim@gmail.com>
2020-07-27 19:42:51 +0200

    Update lib/ex_unit/lib/ex_unit.ex

--- lib/ex_unit/lib/ex_unit.ex
+++ lib/ex_unit/lib/ex_unit.ex
@@ -70,7 +70,7 @@ defmodule ExUnit do
 
   """
   @type state ::
-          nil | {:excluded, binary} |  {:failed, failed} | {:invalid, module} | {:skipped, binary}
+          nil | {:excluded, binary} | {:failed, failed} | {:invalid, module} | {:skipped, binary}
 
   @typedoc "The error state returned by `ExUnit.Test` and `ExUnit.TestModule`"
   @type failed :: [{Exception.kind(), reason :: term, Exception.stacktrace()}]
