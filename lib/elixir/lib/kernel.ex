67cbd59d03755000bc7aca4e0f50a8232df1c317
José Valim <jose.valim@dashbit.co>
2020-09-03 15:36:28 +0200

    Remove reference to unknown function

--- lib/elixir/lib/kernel.ex
+++ lib/elixir/lib/kernel.ex
@@ -4908,8 +4908,8 @@ defmodule Kernel do
 
   ## Examples
 
-  For example, in order to write test cases using the `ExUnit` framework
-  provided with Elixir, a developer should `use` the `ExUnit.Case` module:
+  For example, to write test cases using the `ExUnit` framework provided
+  with Elixir, a developer should `use` the `ExUnit.Case` module:
 
       defmodule AssertionTest do
         use ExUnit.Case, async: true
@@ -4919,8 +4919,11 @@ defmodule Kernel do
         end
       end
 
-  In this example, `ExUnit.Case.__using__/1` is called with the keyword list
-  `[async: true]` as its argument; `use/2` translates to:
+  In this example, Elixir will call the `__using__/1` macro in the
+  `ExUnit.Case` module with the keyword list `[async: true]` as its
+  argument.
+
+  In other words, `use/2` translates to:
 
       defmodule AssertionTest do
         require ExUnit.Case
@@ -4931,7 +4934,7 @@ defmodule Kernel do
         end
       end
 
-  `ExUnit.Case` will then define the `__using__/1` macro:
+  where `ExUnit.Case` defines the `__using__/1` macro:
 
       defmodule ExUnit.Case do
         defmacro __using__(opts) do
