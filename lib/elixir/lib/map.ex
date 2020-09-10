fdcb40f4ecdd162d4b2d801b3d9d33f46369d01d
José Valim <jose.valim@dashbit.co>
2020-09-10 12:20:15 +0200

    Update map.ex

--- lib/elixir/lib/map.ex
+++ lib/elixir/lib/map.ex
@@ -62,13 +62,15 @@ defmodule Map do
   keys on the left-hand side and their values match the ones on the left-hand
   side. This means that an empty map matches every map.
 
-      %{} = %{foo: "bar"}
-      #=> %{foo: "bar"}
-      %{a: a} = %{:a => 1, "b" => 2, [:c, :e, :e] => 3}
-      a
+      iex> %{} = %{foo: "bar"}
+      %{foo: "bar"}
+      iex> %{a: a} = %{:a => 1, "b" => 2, [:c, :e, :e] => 3}
+      iex> a
       #=> 1
+
+  But this will raise a match error:
+
       %{:c => 3} = %{:a => 1, 2 => :b}
-      ** (MatchError) no match of right hand side value: %{2 => :b, :a => 1}
 
   Variables can be used as map keys both when writing map literals as well as
   when matching:
