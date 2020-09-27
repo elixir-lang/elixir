54ac0f3c9c044219597108a562c5782c594f60a1
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2020-09-27 09:41:25 -0500

    Use timer function instead of manually calculating milliseconds (#10367)

--- lib/elixir/lib/gen_server.ex
+++ lib/elixir/lib/gen_server.ex
@@ -242,7 +242,8 @@ defmodule GenServer do
         end
 
         defp schedule_work do
-          # In 2 hours
+          # We schedule the work to happen in 2 hours (written in milliseconds).
+          # Alternatively, one might write :timer.hours(2)
           Process.send_after(self(), :work, 2 * 60 * 60 * 1000)
         end
       end
