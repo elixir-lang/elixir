8c30c06a85bc4be54884099bdeeae8cea8dbcca9
José Valim <jose.valim@gmail.com>
2020-09-27 16:40:59 +0200

    Update lib/elixir/lib/gen_server.ex

--- lib/elixir/lib/gen_server.ex
+++ lib/elixir/lib/gen_server.ex
@@ -242,7 +242,9 @@ defmodule GenServer do
         end
 
         defp schedule_work do
-          Process.send_after(self(), :work, :timer.hours(2))
+          # We schedule the work to happen in 2 hours (written in milliseconds).
+          # Alternatively, one might write :timer.hours(2)
+          Process.send_after(self(), :work, 2 * 60 * 60 * 1000)
         end
       end
 
