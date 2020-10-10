9194e305ada3aa2b36afb5475c3bfe89cf96a963
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-10-10 10:26:22 +0200

    Rename a variable in Inspect documentation

--- lib/elixir/lib/inspect.ex
+++ lib/elixir/lib/inspect.ex
@@ -26,8 +26,8 @@ defprotocol Inspect do
       defimpl Inspect, for: MapSet do
         import Inspect.Algebra
 
-        def inspect(dict, opts) do
-          concat(["#MapSet<", to_doc(MapSet.to_list(dict), opts), ">"])
+        def inspect(map_set, opts) do
+          concat(["#MapSet<", to_doc(MapSet.to_list(map_set), opts), ">"])
         end
       end
 
