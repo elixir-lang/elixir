elixir-lang/elixir

commit 2b93d964afd92cea3d1ebb8e353ab92d3db943ce

Ensure reenabling a task from inside a child propagates up, closes #10227

lib/mix/lib/mix/task.ex
@@ -355,13 +355,11 @@ defmodule Mix.Task do
     alias = Mix.Project.config()[:aliases][String.to_atom(task)]
 
     cond do
-      alias && Mix.TasksServer.run({:alias, task, proj}) ->
-        res = run_alias(List.wrap(alias), args, task, :ok)
-        Mix.TasksServer.put({:task, task, proj})
-        res
-
-      Mix.TasksServer.run({:task, task, proj}) ->
+      is_nil(alias) ->
         run_task(proj, task, args)
+
+      Mix.TasksServer.run({:alias, task, proj}) ->
+        run_alias(List.wrap(alias), args, proj, task, :ok)
 
       true ->
         :noop
@@ -369,8 +367,6 @@ defmodule Mix.Task do
   end
 
   defp run_task(proj, task, args) do
-    if Mix.debug?(), do: output_task_debug_info(task, args, proj)
-
     # 1. If the task is available, we run it.
     # 2. Otherwise we compile and load dependencies
     # 3. Finally, we compile the current project in hope it is available.
@@ -378,8 +374,6 @@ defmodule Mix.Task do
       get_task_or_run(proj, task, fn -> Mix.Task.run("deps.loadpaths") end) ||
         get_task_or_run(proj, task, fn -> Mix.Task.run("compile", []) end) ||
         get!(task)
-
-    run_requirements(module)
 
     recursive = recursive(module)
 
@@ -392,8 +386,9 @@ defmodule Mix.Task do
       not recursive && Mix.ProjectStack.recursing() ->
         Mix.ProjectStack.on_recursing_root(fn -> run(task, args) end)
 
-      true ->
-        Mix.TasksServer.put({:task, task, proj})
+      Mix.TasksServer.run({:task, task, proj}) ->
+        if Mix.debug?(), do: output_task_debug_info(task, args, proj)
+        run_requirements(module)
 
         try do
           module.run(args)
@@ -401,13 +396,16 @@ defmodule Mix.Task do
           e in OptionParser.ParseError ->
             Mix.raise("Could not invoke task #{inspect(task)}: " <> Exception.message(e))
         end
+
+      true ->
+        :noop
     end
   end
 
   defp run_requirements(module) do
     Enum.each(requirements(module), fn requirement ->
       [task | args] = OptionParser.split(requirement)
-      Mix.Task.run(task, args)
+      run(task, args)
     end)
   end
 
@@ -435,24 +433,24 @@ defmodule Mix.Task do
     end
   end
 
-  defp run_alias([h | t], alias_args, original_task, _res) when is_binary(h) do
+  defp run_alias([h | t], alias_args, proj, original_task, _res) when is_binary(h) do
     case OptionParser.split(h) do
       [^original_task | args] ->
-        res = Mix.Task.run(original_task, args ++ alias_args)
-        run_alias(t, [], original_task, res)
+        res = run_task(proj, original_task, args ++ alias_args)
+        run_alias(t, [], proj, original_task, res)
 
       [task | args] ->
-        res = Mix.Task.run(task, join_args(args, alias_args, t))
-        run_alias(t, alias_args, original_task, res)
+        res = run(task, join_args(args, alias_args, t))
+        run_alias(t, alias_args, proj, original_task, res)
     end
   end
 
-  defp run_alias([h | t], alias_args, original_task, _res) when is_function(h, 1) do
+  defp run_alias([h | t], alias_args, proj, original_task, _res) when is_function(h, 1) do
     res = h.(join_args([], alias_args, t))
-    run_alias(t, alias_args, original_task, res)
+    run_alias(t, alias_args, proj, original_task, res)
   end
 
-  defp run_alias([], _alias_task, _original_task, res) do
+  defp run_alias([], _alias_task, _proj, _original_task, res) do
     res
   end
 

lib/mix/test/mix/task_test.exs
@@ -150,11 +150,25 @@ defmodule Mix.TaskTest do
     in_fixture("umbrella_dep/deps/umbrella", fn ->
       Mix.Project.in_project(:umbrella, ".", fn _ ->
         assert [:ok, :ok] = Mix.Task.run("clean")
-        assert :noop = Mix.Task.run("clean")
+        assert [:noop, :noop] = Mix.Task.run("clean")
 
         Mix.Task.reenable("clean")
         assert [:ok, :ok] = Mix.Task.run("clean")
-        assert :noop = Mix.Task.run("clean")
+        assert [:noop, :noop] = Mix.Task.run("clean")
+      end)
+    end)
+  end
+
+  test "reenable/1 for recursive inside umbrella child" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      Mix.Project.in_project(:umbrella, ".", fn _ ->
+        assert [:ok, :ok] = Mix.Task.run("cmd", ["echo", "hello"])
+        assert [:ok, :ok] = Mix.Task.run("cmd", ["echo", "world"])
+
+        assert_received {:mix_shell, :run, ["hello" <> _]}
+        assert_received {:mix_shell, :run, ["world" <> _]}
+        assert_received {:mix_shell, :run, ["hello" <> _]}
+        assert_received {:mix_shell, :run, ["world" <> _]}
       end)
     end)
   end
@@ -178,7 +192,7 @@ defmodule Mix.TaskTest do
     in_fixture("umbrella_dep/deps/umbrella", fn ->
       Mix.Project.in_project(:umbrella, ".", fn _ ->
         assert [:ok, :ok] = Mix.Task.run("clean")
-        assert :noop = Mix.Task.run("clean")
+        assert [:noop, :noop] = Mix.Task.run("clean")
         assert [:ok, :ok] = Mix.Task.rerun("clean")
       end)
     end)
@@ -186,7 +200,6 @@ defmodule Mix.TaskTest do
 
   test "get!" do
     Mix.Project.push(MixTest.Case.Sample)
-
     assert Mix.Task.get!("hello") == Mix.Tasks.Hello
 
     assert_raise Mix.NoTaskError, "The task \"unknown\" could not be found", fn ->

commit 84feab8fb3738e462463e08f786145c89eee6669

Handle aliases with code recursion
It addresses a regression introduced in 2b93d964afd.

Closes https://github.com/nerves-project/nerves/issues/557.

lib/mix/lib/mix/task.ex
@@ -355,11 +355,11 @@ defmodule Mix.Task do
     alias = Mix.Project.config()[:aliases][String.to_atom(task)]
 
     cond do
-      is_nil(alias) ->
-        run_task(proj, task, args)
-
-      Mix.TasksServer.run({:alias, task, proj}) ->
+      alias && Mix.TasksServer.run({:alias, task, proj}) ->
         run_alias(List.wrap(alias), args, proj, task, :ok)
+
+      !Mix.TasksServer.get({:task, task, proj}) ->
+        run_task(proj, task, args)
 
       true ->
         :noop
@@ -371,8 +371,8 @@ defmodule Mix.Task do
     # 2. Otherwise we compile and load dependencies
     # 3. Finally, we compile the current project in hope it is available.
     module =
-      get_task_or_run(proj, task, fn -> Mix.Task.run("deps.loadpaths") end) ||
-        get_task_or_run(proj, task, fn -> Mix.Task.run("compile", []) end) ||
+      get_task_or_run(proj, task, fn -> run("deps.loadpaths") end) ||
+        get_task_or_run(proj, task, fn -> run("compile", []) end) ||
         get!(task)
 
     recursive = recursive(module)
@@ -450,7 +450,8 @@ defmodule Mix.Task do
     run_alias(t, alias_args, proj, original_task, res)
   end
 
-  defp run_alias([], _alias_task, _proj, _original_task, res) do
+  defp run_alias([], _alias_args, proj, original_task, res) do
+    Mix.TasksServer.put({:task, original_task, proj})
     res
   end
 

lib/mix/lib/mix/tasks_server.ex
@@ -21,6 +21,10 @@ defmodule Mix.TasksServer do
     Agent.update(@name, &Map.put(&1, tuple, true), @timeout)
   end
 
+  def get(tuple) do
+    Agent.get(@name, &Map.get(&1, tuple), @timeout)
+  end
+
   def delete_many(many) do
     Agent.update(@name, &Map.drop(&1, many), @timeout)
   end

lib/mix/test/mix/aliases_test.exs
@@ -10,11 +10,14 @@ defmodule Mix.AliasesTest do
           h: "hello",
           p: &inspect/1,
           compile: "hello",
+          cmd: &call_cmd/1,
           help: ["help", "hello"],
           "nested.h": [&Mix.shell().info(inspect(&1)), "h foo bar"]
         ]
       ]
     end
+
+    defp call_cmd(args), do: Mix.Task.run("cmd", args)
   end
 
   setup do
@@ -50,13 +53,18 @@ defmodule Mix.AliasesTest do
     assert Mix.Task.run("compile", []) == :noop
   end
 
-  test "run alias override with recursion" do
+  test "run alias override with name-recursion" do
     assert Mix.Task.rerun("help", []) == "Hello, World!"
     assert_received {:mix_shell, :info, ["mix test" <> _]}
 
     # Arguments are passed to the recursive task and not the last one.
     assert ExUnit.CaptureIO.capture_io(fn ->
-             Mix.Task.rerun("help", ["test"]) == "Hello, World!"
+             Mix.Task.rerun("help", ["test"])
            end) =~ "mix test"
+  end
+
+  test "run alias override with code-recursion" do
+    assert Mix.Task.rerun("cmd", ["echo", "hello"]) == :ok
+    assert_received {:mix_shell, :run, ["hello" <> _]}
   end
 end