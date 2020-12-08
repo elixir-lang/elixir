c42f7af02b0bc754be153b65cc053c406188e501
José Valim <jose.valim@dashbit.co>
2020-12-08 17:39:09 +0100

    Do not match on error_info on OTP 24

--- lib/elixir/test/elixir/kernel/errors_test.exs
+++ lib/elixir/test/elixir/kernel/errors_test.exs
@@ -1192,7 +1192,7 @@ defmodule Kernel.ErrorsTest do
     rescue
       ArgumentError ->
         assert [
-                 {:erlang, :apply, [1, :foo, []], []},
+                 {:erlang, :apply, [1, :foo, []], _},
                  {__MODULE__, :bad_remote_call, 1, [file: _, line: _]} | _
                ] = __STACKTRACE__
     end
