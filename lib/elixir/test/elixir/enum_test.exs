727e3a54ddd71664ae8d5925a535ea545f741421
Julian Doherty (madlep) <madlep@madlep.com>
2020-12-26 23:26:50 +1100

    add test exercising Enum.chunk_while after_fun more clearly

--- lib/elixir/test/elixir/enum_test.exs
+++ lib/elixir/test/elixir/enum_test.exs
@@ -116,6 +116,8 @@ defmodule EnumTest do
              [[0], [1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
 
     assert Enum.chunk_while([5, 7, 9, 11], [], chunk_fun, after_fun) == [[5, 7, 9]]
+
+    assert Enum.chunk_while([1, 2, 3, 5, 7], [], chunk_fun, after_fun) == [[1, 2], [3, 5, 7]]
   end
 
   test "concat/1" do
