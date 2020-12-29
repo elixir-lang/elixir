996ecaee268620b5b224f715eb9fcafd089a19c6
Tomasz Marek Sulima <eiji7@cryptolab.net>
2020-12-29 18:23:45 +0100

    Naming: change x to value

--- lib/elixir/lib/kernel.ex
+++ lib/elixir/lib/kernel.ex
@@ -2473,7 +2473,7 @@ defmodule Kernel do
   """
   @doc since: "1.12.0"
   @spec then(x, (x -> result)) :: result when result: any, x: var
-  def then(x, fun) when is_function(fun, 1), do: fun.(x)
+  def then(value, fun) when is_function(fun, 1), do: fun.(value)
 
   @doc """
   Gets a value from a nested structure.
