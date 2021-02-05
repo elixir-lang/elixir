ce27b8348c4c024fc4009d4dd05d971936fd11e8
José Valim <jose.valim@dashbit.co>
2021-02-05 07:54:52 +0100

    Do not support negative dates on the basic format

--- lib/elixir/lib/calendar/datetime.ex
+++ lib/elixir/lib/calendar/datetime.ex
@@ -892,12 +892,9 @@ defmodule DateTime do
   Converts the given datetime to
   [ISO 8601:2004](https://en.wikipedia.org/wiki/ISO_8601) format.
 
-  By default, `DateTime.to_iso8601/2` returns datetimes formatted in the "extended"
-  format, for human readability. It also supports the "basic" format through passing the `:basic` option.
-
-  Only supports converting datetimes which are in the ISO calendar,
-  attempting to convert datetimes from other calendars will raise.
-  You can also optionally specify an offset for the formatted string.
+  By default, `DateTime.to_iso8601/2` returns naive datetimes formatted
+  in the "extended" format, for human readability. It also supports
+  the "basic" format through passing the `:basic` option.
 
   WARNING: the ISO 8601 datetime format does not contain the time zone nor
   its abbreviation, which means information is lost when converting to such
