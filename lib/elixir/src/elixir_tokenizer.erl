b15a5ee95fb495f8e35b1f00c9c83d173eb16765
Eksperimental <eksperimental@autistici.org>
2021-02-17 06:08:53 -0500

    Fix warning message mentioning quoted atoms may end with ! or ? (#10724)

--- lib/elixir/src/elixir_tokenizer.erl
+++ lib/elixir/src/elixir_tokenizer.erl
@@ -438,7 +438,7 @@ tokenize([$:, H | T] = Original, Line, Column, Scope, Tokens) when ?is_quote(H) 
         true ->
           WarnMsg = io_lib:format(
             "found quoted atom \"~ts\" but the quotes are not required. "
-            "Atoms made exclusively of ASCII letters, numbers, and underscores "
+            "Atoms made exclusively of ASCII letters, numbers, underscores, and optionally ending with ! or ? "
             "do not require quotes",
             [hd(Parts)]
           ),
