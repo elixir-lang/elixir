544466a4d1feec5ab9b0c894cafff506a1505ff7
Eksperimental <eksperimental@autistici.org>
2021-03-29 15:35:32 -0500

    Fix t:Range.t/2 (#10833)
    We need to be explicit and define the step field, otherwise IEx and ExDoc will spec it as "term()"
    
        (1)> t Range.t/2
        @type t(first, last) :: %Range{first: first, last: last, step: term()

--- lib/elixir/lib/range.ex
+++ lib/elixir/lib/range.ex
@@ -96,7 +96,7 @@ defmodule Range do
   @type last :: integer
   @type step :: pos_integer | neg_integer
   @type t :: %__MODULE__{first: first, last: last, step: step}
-  @type t(first, last) :: %__MODULE__{first: first, last: last}
+  @type t(first, last) :: %__MODULE__{first: first, last: last, step: step}
 
   @doc """
   Creates a new range.
