1e092ace7604b66e38f5078fc81bf0186783a760
Wojtek Mach <wojtek@wojtekmach.pl>
2021-04-04 00:05:03 +0200

    Use docs canonical version in all built-in apps
    Before:
    
    https://hexdocs.pm/elixir/1.12.0-rc.0/changelog.html contains links like:
    
      - [`Mix.install/2`](https://hexdocs.pm/mix/Mix.html#install/2)
    
    which currently 404.
    
    After:
    
    https://hexdocs.pm/elixir/1.12.0-rc.0/changelog.html would contain:
    
      - [`Mix.install/2`](https://hexdocs.pm/mix/1.12/Mix.html#install/2)
    
    See demo: http://wojtekmach.pl/docs/elixir/1.12/elixir/changelog.html

---
+++ lib/mix/docs.exs
@@ -0,0 +1,12 @@
+canonical = System.fetch_env!("CANONICAL")
+
+[
+  deps: [
+    eex: "https://hexdocs.pm/eex/#{canonical}",
+    elixir: "https://hexdocs.pm/elixir/#{canonical}",
+    ex_unit: "https://hexdocs.pm/ex_unit/#{canonical}",
+    iex: "https://hexdocs.pm/iex/#{canonical}",
+    logger: "https://hexdocs.pm/logger/#{canonical}",
+    mix: "https://hexdocs.pm/mix/#{canonical}"
+  ]
+]
