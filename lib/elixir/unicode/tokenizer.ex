c19437015dfd44639564bb5219ce1bb6e72a5bbb
José Valim <jose.valim@gmail.com>
2021-04-03 15:19:57 +0200

    Update lib/elixir/unicode/tokenizer.ex

--- lib/elixir/unicode/tokenizer.ex
+++ lib/elixir/unicode/tokenizer.ex
@@ -130,7 +130,7 @@ defmodule String.Tokenizer do
   defp unicode_start?(_), do: false
 
   unless {13312, 19903} in range do
-    raise "CHECK: CJK Ideograph not in range"
+    raise "CHECK: CJK Ideograph not in range. Make sure all properties are listed."
   end
 
   for {first, last} <- rangify.(unicode_continue) do
