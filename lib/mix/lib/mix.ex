acc5a5a170ff0e9a19a3b6737f600a0140b9d09a
Wojtek Mach <wojtekmach@users.noreply.github.com>
2021-04-04 20:18:56 +0200

    Mix.install/2: Allow calling with the same dependencies and don't generate .app file (#10875)

--- lib/mix/lib/mix.ex
+++ lib/mix/lib/mix.ex
@@ -480,8 +480,8 @@ defmodule Mix do
   will avoid unnecessary downloads and compilations. The location of the cache
   directory can be controlled using the `MIX_INSTALL_DIR` environment variable.
 
-  This function can only be called outside of a Mix project and only once in a
-  given VM.
+  This function can only be called outside of a Mix project and only with the
+  same dependencies in the given VM.
 
   **Note:** this feature is currently experimental and it may change
   in future releases.
@@ -516,8 +516,23 @@ defmodule Mix do
       Mix.raise("Mix.install/2 cannot be used inside a Mix project")
     end
 
-    if Mix.State.get(:install_called?) do
-      Mix.raise("Mix.install/2 can only be called once")
+    deps =
+      Enum.map(deps, fn
+        dep when is_atom(dep) -> {dep, ">= 0.0.0"}
+        dep -> dep
+      end)
+
+    force? = !!opts[:force]
+
+    case Mix.State.get(:installed) do
+      nil ->
+        :ok
+
+      ^deps when not force? ->
+        :ok
+
+      _ ->
+        Mix.raise("Mix.install/2 can only be called with the same dependencies in the given VM")
     end
 
     installs_root =
@@ -532,15 +547,9 @@ defmodule Mix do
       Mix.shell().info("using #{dir}")
     end
 
-    if opts[:force] do
+    if force? do
       File.rm_rf!(dir)
     end
-
-    deps =
-      Enum.map(deps, fn
-        dep when is_atom(dep) -> {dep, ">= 0.0.0"}
-        dep -> dep
-      end)
 
     config = [
       version: "0.1.0",
@@ -552,6 +561,7 @@ defmodule Mix do
       app: :mix_install,
       erlc_paths: ["src"],
       elixirc_paths: ["lib"],
+      compilers: [],
       consolidate_protocols: Keyword.get(opts, :consolidate_protocols, true)
     ]
 
@@ -574,7 +584,7 @@ defmodule Mix do
     end
 
     Mix.ProjectStack.pop()
-    Mix.State.put(:install_called?, true)
+    Mix.State.put(:installed, deps)
     :ok
   end
 end
