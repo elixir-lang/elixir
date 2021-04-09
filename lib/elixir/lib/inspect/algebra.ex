6d86a27e30c6924d620fa37d9dfdcabcc192c2e6
José Valim <jose.valim@dashbit.co>
2021-04-09 12:47:52 +0200

    Update lib/elixir/lib/inspect/algebra.ex

--- lib/elixir/lib/inspect/algebra.ex
+++ lib/elixir/lib/inspect/algebra.ex
@@ -133,8 +133,13 @@ defmodule Inspect.Opts do
   in order for entities to comply with rules and legislations
   on data security and data privacy.
 
-  It is extremely discouraged for libraries to set their own
-  function as this must be controlled by applications.
+  It is **extremely discouraged** for libraries to set their own
+  function as this must be controlled by applications. Libraries
+  should instead define their own structs with custom inspect
+  implementations. If a library must change the default inspect
+  function, then it is best to define to ask users of your library
+  to explicitly call `default_inspect_fun/1` with your function of
+  choice.
 
   The default is `Inspect.inspect/2`.
 
