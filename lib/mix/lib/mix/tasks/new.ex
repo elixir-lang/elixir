d413ad126ba53dcaf4c540838ac55206a298f9cd
Eksperimental <eksperimental@autistici.org>
2021-04-19 15:32:05 +0000

    Simplify link in README file with 'mix new' task (#10925)

--- lib/mix/lib/mix/tasks/new.ex
+++ lib/mix/lib/mix/tasks/new.ex
@@ -248,7 +248,7 @@ defmodule Mix.Tasks.New do
 
   Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
   and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-  be found at [https://hexdocs.pm/<%= @app %>](https://hexdocs.pm/<%= @app %>).
+  be found at <https://hexdocs.pm/<%= @app %>>.
   <% end %>
   """)
 
