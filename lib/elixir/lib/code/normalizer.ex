a4ec8126f845b967185263d022aad52b7280167a
Dorgan <dorgandash@gmail.com>
2021-05-23 22:56:31 +0000

    Fix normalizing issues

--- lib/elixir/lib/code/normalizer.ex
+++ lib/elixir/lib/code/normalizer.ex
@@ -1,6 +1,94 @@
 defmodule Code.Normalizer do
   @moduledoc false
 
+  # Operators that are logical cannot be mixed without parens
+  @required_parens_forms [:||, :|||, :or, :&&, :&&&, :and, :{}]
+
+  @locals_without_parens [
+    # Special forms
+    alias: 1,
+    alias: 2,
+    case: 2,
+    cond: 1,
+    for: :*,
+    import: 1,
+    import: 2,
+    quote: 1,
+    quote: 2,
+    receive: 1,
+    require: 1,
+    require: 2,
+    try: 1,
+    with: :*,
+
+    # Kernel
+    def: 1,
+    def: 2,
+    defp: 1,
+    defp: 2,
+    defguard: 1,
+    defguardp: 1,
+    defmacro: 1,
+    defmacro: 2,
+    defmacrop: 1,
+    defmacrop: 2,
+    defmodule: 2,
+    defdelegate: 2,
+    defexception: 1,
+    defoverridable: 1,
+    defstruct: 1,
+    destructure: 2,
+    raise: 1,
+    raise: 2,
+    reraise: 2,
+    reraise: 3,
+    if: 2,
+    unless: 2,
+    use: 1,
+    use: 2,
+
+    # Stdlib,
+    defrecord: 2,
+    defrecord: 3,
+    defrecordp: 2,
+    defrecordp: 3,
+
+    # Testing
+    assert: 1,
+    assert: 2,
+    assert_in_delta: 3,
+    assert_in_delta: 4,
+    assert_raise: 2,
+    assert_raise: 3,
+    assert_receive: 1,
+    assert_receive: 2,
+    assert_receive: 3,
+    assert_received: 1,
+    assert_received: 2,
+    doctest: 1,
+    doctest: 2,
+    refute: 1,
+    refute: 2,
+    refute_in_delta: 3,
+    refute_in_delta: 4,
+    refute_receive: 1,
+    refute_receive: 2,
+    refute_receive: 3,
+    refute_received: 1,
+    refute_received: 2,
+    setup: 1,
+    setup: 2,
+    setup_all: 1,
+    setup_all: 2,
+    test: 1,
+    test: 2,
+
+    # Mix config
+    config: 2,
+    config: 3,
+    import_config: 1
+  ]
+
   defguard is_literal(x)
            when is_integer(x) or
                   is_float(x) or
@@ -270,6 +358,8 @@ defmodule Code.Normalizer do
   end
 
   defp normalize_call({form, meta, args}, state) do
+    meta = patch_meta_line(meta, state.parent_meta)
+
     # Only normalize the form if it's a qualified call
     form =
       if is_atom(form) do
@@ -279,9 +369,8 @@ defmodule Code.Normalizer do
       end
 
     meta =
-      if meta == [] do
-        line = state.parent_meta[:line]
-        [line: line, closing: [line: line]]
+      if is_nil(meta[:no_parens]) and not local_without_parens?(form, args) do
+        [closing: [line: meta[:line]]] ++ meta
       else
         meta
       end
@@ -299,13 +388,13 @@ defmodule Code.Normalizer do
 
         normalize_kw_blocks(form, meta, args, state)
 
-      true ->
+      form not in @required_parens_forms ->
         args = normalize_args(args, %{state | parent_meta: state.parent_meta})
 
-        {last_arg, leading_args} = List.pop_at(args, -1)
+        {last_arg, leading_args} = List.pop_at(args, -1, [])
 
         last_args =
-          case List.last(args) do
+          case last_arg do
             {:__block__, _, [[{{:__block__, key_meta, _}, _}] | _] = last_args} ->
               if key_meta[:format] == :keyword do
                 last_args
@@ -313,7 +402,7 @@ defmodule Code.Normalizer do
                 [last_arg]
               end
 
-            nil ->
+            [] ->
               []
 
             _ ->
@@ -321,6 +410,10 @@ defmodule Code.Normalizer do
           end
 
         {form, meta, leading_args ++ last_args}
+
+      true ->
+        args = normalize_args(args, %{state | parent_meta: state.parent_meta})
+        {form, meta, args}
     end
   end
 
@@ -487,5 +580,14 @@ defmodule Code.Normalizer do
 
   defp interpolated?(_) do
     false
+  end
+
+  defp local_without_parens?(fun, args) do
+    length = length(args)
+
+    length > 0 and
+      Enum.any?(@locals_without_parens, fn {key, val} ->
+        key == fun and (val == :* or val == length)
+      end)
   end
 end
