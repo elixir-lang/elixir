e814d78d5a2bd5a34d715301092fcba91c9e712c
José Valim <jose.valim@dashbit.co>
2021-06-06 09:21:30 +0200

    Add ParallelChecker to stacktrace pruning

--- lib/elixir/lib/kernel/cli.ex
+++ lib/elixir/lib/kernel/cli.ex
@@ -195,7 +195,8 @@ defmodule Kernel.CLI do
 
   @elixir_internals [:elixir, :elixir_aliases, :elixir_expand, :elixir_compiler, :elixir_module] ++
                       [:elixir_clauses, :elixir_lexical, :elixir_def, :elixir_map] ++
-                      [:elixir_erl, :elixir_erl_clauses, :elixir_erl_pass, Kernel.ErrorHandler]
+                      [:elixir_erl, :elixir_erl_clauses, :elixir_erl_pass] ++
+                      [Kernel.ErrorHandler, Module.ParallelChecker]
 
   defp prune_stacktrace([{mod, _, _, _} | t]) when mod in @elixir_internals do
     prune_stacktrace(t)
