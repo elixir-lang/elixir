7c435eaa5639bd569116695579b24eba543d13c4
José Valim <jose.valim@dashbit.co>
2021-07-13 11:12:01 +0200

    Fix regex

--- lib/elixir/test/elixir/kernel/expansion_test.exs
+++ lib/elixir/test/elixir/kernel/expansion_test.exs
@@ -2688,7 +2688,8 @@ defmodule Kernel.ExpansionTest do
         expand(code)
       end
 
-      message = ~r["a -1..a+1" looks like a function call but there is a variable named "a"]
+      message =
+        ~r["a -1\.\.\(a \+ 1\)" looks like a function call but there is a variable named "a"]
 
       assert_raise CompileError, message, fn ->
         code =
