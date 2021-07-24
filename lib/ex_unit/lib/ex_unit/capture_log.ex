59aadec272849e2c3b779a1d3f8f444495a81f7b
José Valim <jose.valim@gmail.com>
2021-07-24 19:21:44 +0200

    Update capture_log.ex

--- lib/ex_unit/lib/ex_unit/capture_log.ex
+++ lib/ex_unit/lib/ex_unit/capture_log.ex
@@ -11,10 +11,12 @@ defmodule ExUnit.CaptureLog do
         require Logger
 
         test "example" do
-          {result, log} = with_log(fn ->
-                   Logger.error("log msg")
-                   2 + 2
-                 end)
+          {result, log} =
+            with_log(fn ->
+              Logger.error("log msg")
+              2 + 2
+            end)
+
           assert result == 4
           assert log =~ "log msg"
         end
