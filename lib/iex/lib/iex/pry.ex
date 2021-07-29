113a0d9871f2c2a2cbf5fb498393c3f373142425
José Valim <jose.valim@gmail.com>
2021-07-29 15:53:48 +0200

    Update lib/iex/lib/iex/pry.ex

--- lib/iex/lib/iex/pry.ex
+++ lib/iex/lib/iex/pry.ex
@@ -82,7 +82,7 @@ defmodule IEx.Pry do
 
   def pry(binding, opts) when is_list(opts) do
     vars = for {k, _} when is_atom(k) <- binding, do: {k, nil}
-    pry(binding, %{:elixir.env_for_eval(opts) | current_vars: vars})
+    pry(binding, opts |> :elixir.env_for_eval() |> :elixir_env.with_vars(vars))
   end
 
   @elixir_internals [:elixir, :erl_eval, IEx.Evaluator, IEx.Pry]
