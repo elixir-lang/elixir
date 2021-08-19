ccd347311907582d760d40817e5681a442b84e9e
Wojtek Mach <wojtekmach@users.noreply.github.com>
2021-08-19 16:17:24 +0200

    Make Inspect.Opts.new/1 stricter (#11192)

--- lib/elixir/lib/io.ex
+++ lib/elixir/lib/io.ex
@@ -416,7 +416,8 @@ defmodule IO do
   """
   @spec inspect(device, item, keyword) :: item when item: var
   def inspect(device, item, opts) when is_list(opts) do
-    label = if label = opts[:label], do: [to_chardata(label), ": "], else: []
+    {label, opts} = Keyword.pop(opts, :label)
+    label = if label, do: [to_chardata(label), ": "], else: []
     opts = Inspect.Opts.new(opts)
     doc = Inspect.Algebra.group(Inspect.Algebra.to_doc(item, opts))
     chardata = Inspect.Algebra.format(doc, opts.width)
