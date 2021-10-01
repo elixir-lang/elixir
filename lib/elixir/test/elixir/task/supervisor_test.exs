440fa8e997c6e2294150e7b62cf9fe78e1dba7b4
José Valim <jose.valim@dashbit.co>
2021-10-01 20:49:02 +0200

    Add task ignore

--- lib/elixir/test/elixir/task/supervisor_test.exs
+++ lib/elixir/test/elixir/task/supervisor_test.exs
@@ -316,6 +316,24 @@ defmodule Task.SupervisorTest do
   end
 
   describe "await/1" do
+    if System.otp_release() >= "24" do
+      test "demonitors and unalias on timeout", config do
+        task = Task.Supervisor.async(config[:supervisor], fn ->
+          assert_receive :go
+          :done
+        end)
+
+        assert catch_exit(Task.await(task, 0)) == {:timeout, {Task, :await, [task, 0]}}
+        new_ref = Process.monitor(task.pid)
+        old_ref = task.ref
+
+        send(task.pid, :go)
+        assert_receive {:DOWN, ^new_ref, _, _, _}
+        refute_received {^old_ref, :done}
+        refute_received {:DOWN, ^old_ref, _, _, _}
+      end
+    end
+
     test "exits on task throw", config do
       Process.flag(:trap_exit, true)
       task = Task.Supervisor.async(config[:supervisor], fn -> throw(:unknown) end)
