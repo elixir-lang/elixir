d5dff32304cb59a023d95f5189f64a1c7aeccef9
José Valim <jose.valim@gmail.com>
2021-10-25 22:58:12 +0200

    Update elixir_errors.erl

--- lib/elixir/src/elixir_errors.erl
+++ lib/elixir/src/elixir_errors.erl
@@ -89,7 +89,7 @@ snippet(InputString, Location, StartLine, StartColumn) ->
       Lines = string:split(InputString, "\n", all),
       Snippet = elixir_utils:characters_to_binary(lists:nth(Line - StartLine + 1, Lines)),
       Offset = if Line == StartLine -> Column - StartColumn; true -> Column - 1 end,
-      #{content => Snippet, offset => Offset};
+      #{content => unicode:characters_to_binary(Snippet), offset => Offset};
 
     false ->
       nil
