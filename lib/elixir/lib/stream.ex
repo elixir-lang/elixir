458ef37253116358ee9cec8d8328ab0d225cd009
MPaul <moshepaul@PAPA-A000274.local>
2021-11-25 11:43:03 -0500

    took out duplicate verbiage in documentation for Stream.transform/3

--- lib/elixir/lib/stream.ex
+++ lib/elixir/lib/stream.ex
@@ -800,8 +800,8 @@ defmodule Stream do
   @doc """
   Transforms an existing stream.
 
-  It expects an accumulator and a function that receives each stream element
-  and an accumulator. It must return a tuple, where the first element is a new
+  It expects an accumulator and a function that receives each stream element.
+  It must return a tuple, where the first element is a new
   stream (often a list) or the atom `:halt`, and the second element is the
   accumulator to be used by the next element, if any, in both cases.
 
