41110f5a6fd13e626bbe97edfe5823d1816593bf
José Valim <jose.valim@gmail.com>
2022-01-07 20:34:34 +0100

    Apply suggestions from code review

--- lib/elixir/src/elixir_tokenizer.erl
+++ lib/elixir/src/elixir_tokenizer.erl
@@ -1480,10 +1480,7 @@ invalid_do_error(Prefix) ->
   "or the equivalent construct:\n\n"
   "    if(some_condition?, do: :this, else: :that)\n\n"
   "where \"some_condition?\" is the first argument and the second argument is a keyword list.\n\n"
-  "** Note:\n\n"
-  "You may see this error if you forget a trailing comma before the \"do\" in a \"do\" expression.\n"
-  "This is common when using a `with` statement or a `for` comprehension spanning multiple lines.\n"
-  "If this is the case, please verify it is contructed correctly."}.
+  "You may see this error if you forget a trailing comma before the \"do\" in a \"do\" block"}.
 
 invalid_do_with_fn_error(Prefix) ->
   {Prefix, ". Anonymous functions are written as:\n\n"
