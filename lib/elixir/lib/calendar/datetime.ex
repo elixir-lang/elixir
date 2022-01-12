036edc3a724728550d1c607ddf7636ef390677a1
Francis Chabouis <fchabouis@gmail.com>
2022-01-12 14:31:31 +0100

    update DateTime doc
    explain how to use `Enum.min/3` and `Enum.max/3`
    with datetimes

--- lib/elixir/lib/calendar/datetime.ex
+++ lib/elixir/lib/calendar/datetime.ex
@@ -13,7 +13,10 @@ defmodule DateTime do
 
   Remember, comparisons in Elixir using `==/2`, `>/2`, `</2` and friends
   are structural and based on the DateTime struct fields. For proper
-  comparison between datetimes, use the `compare/2` function.
+  comparison between datetimes, use the `compare/2` function. The
+  existence of the `compare/2` function in this module also allows
+  using `Enum.min/3` and `Enum.max/3` functions to get the minimum and
+  maximum datetime of an `Enum`.
 
   Developers should avoid creating the `DateTime` struct directly
   and instead rely on the functions provided by this module as
