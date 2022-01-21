a1ada5d7d826d856a2e270fea713d46809e2820f
José Valim <jose.valim@gmail.com>
2022-01-21 09:28:41 +0100

    Update lib/elixir/unicode/unicode.ex

--- lib/elixir/unicode/unicode.ex
+++ lib/elixir/unicode/unicode.ex
@@ -7,11 +7,8 @@
 # 3. Replace SpecialCasing.txt by copying original and removing conditional mappings
 # 4. Replace IdentifierType.txt by copying original (from /Public/security)
 # 5. Replace confusables.txt by copying original (from /Public/security)
-# 6. Replace ScriptExtensions.txt by copying original
-# 7. Replace Scripts.txt by copying original
-# 8. Replace PropertyAliases.txt by copying original
-# 9. Update String.Unicode.version/0 and on String module docs (version and link)
-# 10. make unicode
+# 6. Update String.Unicode.version/0 and on String module docs (version and link)
+# 7. make unicode
 
 data_path = Path.join(__DIR__, "UnicodeData.txt")
 
