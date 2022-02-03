e6a244a8dd4253e262d4d108939eb4075d927221
Jorge Bejar <jmbejar@gmail.com>
2022-02-03 09:00:13 -0300

    Add the ability to limit tasks in given apps in umbrella applications (#11593)

--- lib/mix/lib/mix/tasks/do.ex
+++ lib/mix/lib/mix/tasks/do.ex
@@ -15,20 +15,63 @@ defmodule Mix.Tasks.Do do
 
       mix do compile --list, deps
 
-  Note however that the majority of Mix tasks are only
-  executed once per invocation. So for example, the following
-  command will only compile once:
+  You can limit where the tasks are run, in the context of
+  umbrella projects, by passing the app names using --app:
+
+      mix do --app app1 --app app2 compile --list, deps
+
+  Note that the majority of Mix tasks are only executed once
+  per invocation. So for example, the following command will
+  only compile once:
 
       mix do compile, some_other_command, compile
 
   When `compile` is executed again, Mix will notice the task
   has already ran, and skip it.
+
+  ## Command line options
+
+    * `--app` - limit running the tasks to the given app. This option may
+      be given multiple times and must come before any of the tasks.
   """
 
   @impl true
   def run(args) do
     Mix.Task.reenable("do")
-    Enum.each(gather_commands(args), fn [task | args] -> Mix.Task.run(task, args) end)
+
+    {apps, args} = extract_apps_from_args(args)
+    show_forgotten_apps_warning(apps)
+
+    Enum.each(gather_commands(args), fn [task | args] ->
+      if apps == [] do
+        Mix.Task.run(task, args)
+      else
+        Mix.Task.run_in_apps(task, apps, args)
+      end
+    end)
+  end
+
+  defp show_forgotten_apps_warning([]), do: nil
+
+  defp show_forgotten_apps_warning(apps) do
+    if Mix.Project.umbrella?() do
+      forgotten_apps = apps -- Enum.map(Mix.Dep.Umbrella.cached(), & &1.app)
+
+      for app <- forgotten_apps do
+        Mix.shell().info([:yellow, "warning: could not find application #{inspect(app)}"])
+      end
+    end
+  end
+
+  defp extract_apps_from_args(args) do
+    {opts, args} = OptionParser.parse_head!(args, switches: [app: :keep])
+
+    apps =
+      opts
+      |> Keyword.get_values(:app)
+      |> Enum.map(&String.to_atom/1)
+
+    {apps, args}
   end
 
   @doc false
--- lib/mix/test/mix/tasks/do_test.exs
+++ lib/mix/test/mix/tasks/do_test.exs
@@ -24,4 +24,115 @@ defmodule Mix.Tasks.DoTest do
 
     assert gather_commands(["test", ",", "help"]) == [["test"], ["help"]]
   end
+
+  test "runs given tasks for a single app specified by app flag" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      Mix.Project.in_project(:umbrella, ".", fn _ ->
+        Mix.Tasks.Do.run(["--app", "bar", "compile", "--list,", "cmd", "echo", "hello"])
+
+        nl = os_newline()
+        assert_received {:mix_shell, :info, ["==> bar"]}
+        assert_received {:mix_shell, :run, ["hello" <> ^nl]}
+        refute_received {:mix_shell, :info, ["==> foo"]}
+        refute_received {:mix_shell, :run, ["hello" <> ^nl]}
+      end)
+    end)
+  end
+
+  test "runs given tasks for each app specified by app flag" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      Mix.Project.in_project(:umbrella, ".", fn _ ->
+        Mix.Tasks.Do.run([
+          "--app",
+          "bar",
+          "--app",
+          "foo",
+          "compile",
+          "--list,",
+          "cmd",
+          "echo",
+          "hello"
+        ])
+
+        nl = os_newline()
+        assert_received {:mix_shell, :info, ["==> bar"]}
+        assert_received {:mix_shell, :run, ["hello" <> ^nl]}
+        assert_received {:mix_shell, :info, ["==> foo"]}
+        assert_received {:mix_shell, :run, ["hello" <> ^nl]}
+      end)
+    end)
+  end
+
+  test "runs non-recursive tasks at project root level" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      Mix.Project.in_project(:umbrella, ".", fn _ ->
+        Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "compile", "--list,", "help"])
+
+        assert_received {:mix_shell, :info, ["mix help" <> _]}
+        assert_received {:mix_shell, :info, ["mix compile.app" <> _]}
+      end)
+    end)
+  end
+
+  test "raises when -app is given but the project is not an umbrella" do
+    assert_raise Mix.Error,
+                 "Could not run \"cmd\" with the --app option because this is not an umbrella project",
+                 fn ->
+                   Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "cmd", "echo", "hello"])
+                 end
+  end
+
+  test "runs given aliases for each app specified by app flag" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      aliases = [
+        e: ["cmd echo hello"],
+        p: fn val -> Mix.shell().info(inspect(val)) end
+      ]
+
+      Mix.Project.in_project(:umbrella, ".", [aliases: aliases], fn _ ->
+        Mix.Tasks.Do.run(["compile"])
+        Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "e,", "p", "Foo"])
+
+        nl = os_newline()
+        assert_received {:mix_shell, :info, ["==> bar"]}
+        assert_received {:mix_shell, :run, ["hello" <> ^nl]}
+        assert_received {:mix_shell, :info, ["[\"Foo\"]"]}
+
+        assert_received {:mix_shell, :info, ["==> foo"]}
+        assert_received {:mix_shell, :run, ["hello" <> ^nl]}
+        assert_received {:mix_shell, :info, ["[\"Foo\"]"]}
+      end)
+    end)
+  end
+
+  test "runs non-recursive aliases at project root level" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      aliases = [
+        h: "help"
+      ]
+
+      Mix.Project.in_project(:umbrella, ".", [aliases: aliases], fn _ ->
+        Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "h"])
+
+        assert_received {:mix_shell, :info, ["mix help" <> _]}
+      end)
+    end)
+  end
+
+  test "raise with aliases when -app is given but the project is not an umbrella" do
+    in_fixture("umbrella_dep/deps/umbrella", fn ->
+      aliases = [
+        e: ["cmd echo hello"],
+        p: fn val -> Mix.shell().info(inspect(val)) end
+      ]
+
+      Mix.Project.in_project(:foo, "apps/foo", [aliases: aliases], fn _ ->
+        assert_raise Mix.Error,
+                     "Could not run \"e\" with the --app option because this is not an umbrella project",
+                     fn ->
+                       Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "e,", "p", "Foo"])
+                     end
+      end)
+    end)
+  end
 end
