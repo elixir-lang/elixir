f809d03370f35cd46c3ef86ffd624be0e417f04c
Michał Łępicki <michallepicki@users.noreply.github.com>
2022-02-17 10:05:40 +0100

    Update Mix.DepTest for Erlang/OTP 25 (#11636)
    :digraph_utils.topsort/1 can return a different (but also valid)
    topological ordering
    
    The test failure on Erlang/OTP 25.0-rc1 was:
    
        1) test deps_paths (Mix.DepTest)
           test/mix/dep_test.exs:491
           Assertion with == failed
           code:  assert Enum.map(Mix.Dep.load_on_environment([]), & &1.app) == [:git_repo, :abc_repo, :deps_repo]
           left:  [:abc_repo, :git_repo, :deps_repo]
           right: [:git_repo, :abc_repo, :deps_repo]
           stacktrace:
             test/mix/dep_test.exs:499: anonymous fn/0 in Mix.DepTest."test deps_paths"/1
             (elixir 1.14.0-dev) lib/file.ex:1555: File.cd!/2
             test/test_helper.exs:127: MixTest.Case.in_fixture/3
             test/mix/dep_test.exs:31: Mix.DepTest.with_deps/2
             test/mix/dep_test.exs:497: (test)

--- lib/mix/test/mix/dep_test.exs
+++ lib/mix/test/mix/dep_test.exs
@@ -496,8 +496,11 @@ defmodule Mix.DepTest do
 
     with_deps(deps, fn ->
       in_fixture("deps_status", fn ->
-        assert Enum.map(Mix.Dep.load_on_environment([]), & &1.app) ==
-                 [:git_repo, :abc_repo, :deps_repo]
+        # Both orders below are valid after topological sort
+        assert Enum.map(Mix.Dep.load_on_environment([]), & &1.app) in [
+                 [:git_repo, :abc_repo, :deps_repo],
+                 [:abc_repo, :git_repo, :deps_repo]
+               ]
 
         assert Map.keys(Mix.Project.deps_paths()) == [:abc_repo, :deps_repo, :git_repo]
 
