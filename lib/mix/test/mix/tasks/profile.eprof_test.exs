6ed32b435af2987920329aabbe7d9ec060c07e8e
Tobias Pfeiffer <pragtob@gmail.com>
2022-02-20 22:34:08 +0100

    Profiler return the return value of the function (#11657)
    As discussed in: https://groups.google.com/g/elixir-lang-core/c/f0gP0It-yuU

--- lib/mix/test/mix/tasks/profile.eprof_test.exs
+++ lib/mix/test/mix/tasks/profile.eprof_test.exs
@@ -112,4 +112,12 @@ defmodule Mix.Tasks.Profile.EprofTest do
              end) =~ "Warmup..."
     end)
   end
+
+  describe ".profile/2" do
+    test "returns the return value of the function call" do
+      capture_io(fn ->
+        assert 42 == Eprof.profile(fn -> 42 end)
+      end)
+    end
+  end
 end
