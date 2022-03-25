1811ebe33201d00e7bd5e0ccead39f508bb63801
José Valim <jose.valim@dashbit.co>
2022-03-25 09:32:24 +0100

    Fixes for multi args before + in mix do

--- lib/mix/test/mix/tasks/do_test.exs
+++ lib/mix/test/mix/tasks/do_test.exs
@@ -3,21 +3,30 @@ Code.require_file("../../test_helper.exs", __DIR__)
 defmodule Mix.Tasks.DoTest do
   use MixTest.Case
 
+  import Mix.Tasks.Do, only: [gather_commands: 1]
+
   test "runs given tasks", context do
     in_tmp(context.test, fn ->
-      Mix.Tasks.Do.run(["compile", "--list,", "help"])
+      Mix.Tasks.Do.run(["compile", "--list", "+", "help"])
       assert_received {:mix_shell, :info, ["mix help" <> _]}
       assert_received {:mix_shell, :info, ["mix compile.app" <> _]}
     end)
   end
 
   test "gather_command returns a list of commands" do
-    import Mix.Tasks.Do, only: [gather_commands: 1]
-    assert gather_commands(["compile", "--list,", "help"]) == [["compile", "--list"], ["help"]]
-    assert gather_commands(["help,", "compile", "--list"]) == [["help"], ["compile", "--list"]]
+    assert gather_commands(["help", "+", "compile"]) ==
+             [["help"], ["compile"]]
 
     assert gather_commands(["help", "+", "compile", "--list"]) ==
              [["help"], ["compile", "--list"]]
+
+    assert gather_commands(["help", "--list", "+", "compile", "--list"]) ==
+             [["help", "--list"], ["compile", "--list"]]
+  end
+
+  test "gather_command supports deprecated comma commands" do
+    assert gather_commands(["compile", "--list,", "help"]) == [["compile", "--list"], ["help"]]
+    assert gather_commands(["help,", "compile", "--list"]) == [["help"], ["compile", "--list"]]
 
     assert gather_commands(["compile,", "run", "-e", "IO.puts :hello"]) ==
              [["compile"], ["run", "-e", "IO.puts :hello"]]
@@ -31,7 +40,7 @@ defmodule Mix.Tasks.DoTest do
   test "runs given tasks for a single app specified by app flag" do
     in_fixture("umbrella_dep/deps/umbrella", fn ->
       Mix.Project.in_project(:umbrella, ".", fn _ ->
-        Mix.Tasks.Do.run(["--app", "bar", "compile", "--list,", "cmd", "echo", "hello"])
+        Mix.Tasks.Do.run(~w(--app bar compile --list + cmd echo hello))
 
         nl = os_newline()
         assert_received {:mix_shell, :info, ["==> bar"]}
@@ -45,17 +54,7 @@ defmodule Mix.Tasks.DoTest do
   test "runs given tasks for each app specified by app flag" do
     in_fixture("umbrella_dep/deps/umbrella", fn ->
       Mix.Project.in_project(:umbrella, ".", fn _ ->
-        Mix.Tasks.Do.run([
-          "--app",
-          "bar",
-          "--app",
-          "foo",
-          "compile",
-          "--list,",
-          "cmd",
-          "echo",
-          "hello"
-        ])
+        Mix.Tasks.Do.run(~w(--app bar --app foo compile --list + cmd echo hello))
 
         nl = os_newline()
         assert_received {:mix_shell, :info, ["==> bar"]}
@@ -69,7 +68,7 @@ defmodule Mix.Tasks.DoTest do
   test "runs non-recursive tasks at project root level" do
     in_fixture("umbrella_dep/deps/umbrella", fn ->
       Mix.Project.in_project(:umbrella, ".", fn _ ->
-        Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "compile", "--list,", "help"])
+        Mix.Tasks.Do.run(~w(--app bar --app foo compile --list + help))
 
         assert_received {:mix_shell, :info, ["mix help" <> _]}
         assert_received {:mix_shell, :info, ["mix compile.app" <> _]}
@@ -81,7 +80,7 @@ defmodule Mix.Tasks.DoTest do
     assert_raise Mix.Error,
                  "Could not run \"cmd\" with the --app option because this is not an umbrella project",
                  fn ->
-                   Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "cmd", "echo", "hello"])
+                   Mix.Tasks.Do.run(~w(--app bar --app foo cmd echo hello))
                  end
   end
 
@@ -94,7 +93,7 @@ defmodule Mix.Tasks.DoTest do
 
       Mix.Project.in_project(:umbrella, ".", [aliases: aliases], fn _ ->
         Mix.Tasks.Do.run(["compile"])
-        Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "e,", "p", "Foo"])
+        Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "e", "+", "p", "Foo"])
 
         nl = os_newline()
         assert_received {:mix_shell, :info, ["==> bar"]}
@@ -133,7 +132,7 @@ defmodule Mix.Tasks.DoTest do
         assert_raise Mix.Error,
                      "Could not run \"e\" with the --app option because this is not an umbrella project",
                      fn ->
-                       Mix.Tasks.Do.run(["--app", "bar", "--app", "foo", "e,", "p", "Foo"])
+                       Mix.Tasks.Do.run(~w(--app bar --app foo e + p Foo))
                      end
       end)
     end)
