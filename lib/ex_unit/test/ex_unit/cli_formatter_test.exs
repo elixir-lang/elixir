1f10f3a7bd161e76bd2e0d823f2f09bc58a97e0a
Joel C <joel@vorce.se>
2022-04-11 15:43:03 +0200

    Add ExUnit.CLIFormatter alias

--- lib/ex_unit/test/ex_unit/cli_formatter_test.exs
+++ lib/ex_unit/test/ex_unit/cli_formatter_test.exs
@@ -1,22 +1,24 @@
 defmodule ExUnit.CLIFormatterTest do
   use ExUnit.Case, async: true
 
+  alias ExUnit.CLIFormatter
+
   describe "test_timings" do
     test "ignored when slowest is unused" do
-      {:ok, config} = ExUnit.CLIFormatter.init(slowest: 0, include: [], exclude: [], colors: [])
+      {:ok, config} = CLIFormatter.init(slowest: 0, include: [], exclude: [], colors: [])
       message = {:test_finished, %ExUnit.Test{state: nil, tags: %{test_type: :test}}}
 
-      {:noreply, new_config} = ExUnit.CLIFormatter.handle_cast(message, config)
+      {:noreply, new_config} = CLIFormatter.handle_cast(message, config)
 
       assert new_config.test_timings == []
     end
 
     test "recorded when slowest is used" do
-      {:ok, config} = ExUnit.CLIFormatter.init(slowest: 1, include: [], exclude: [], colors: [])
+      {:ok, config} = CLIFormatter.init(slowest: 1, include: [], exclude: [], colors: [])
       test = %ExUnit.Test{state: nil, tags: %{test_type: :test}}
       message = {:test_finished, test}
 
-      {:noreply, new_config} = ExUnit.CLIFormatter.handle_cast(message, config)
+      {:noreply, new_config} = CLIFormatter.handle_cast(message, config)
 
       assert new_config.test_timings == [test]
     end
