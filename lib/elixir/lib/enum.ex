1b9d2b5f053320edd39445093ed6209962393343
Eksperimental <eksperimental@autistici.org>
2022-05-20 05:37:37 +0000

    Use zero-arity instead of 0-arity and so on (#11848)

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -3829,7 +3829,7 @@ defmodule Enum do
   Zips corresponding elements from two enumerables into a list, transforming them with
   the `zip_fun` function as it goes.
 
-  The corresponding elements from each collection are passed to the provided 2-arity `zip_fun`
+  The corresponding elements from each collection are passed to the provided two-arity `zip_fun`
   function in turn. Returns a list that contains the result of calling `zip_fun` for each pair of
   elements.
 
@@ -3880,7 +3880,7 @@ defmodule Enum do
   into list, transforming them with the `zip_fun` function as it goes.
 
   The first element from each of the enums in `enumerables` will be put
-  into a list which is then passed to the 1-arity `zip_fun` function.
+  into a list which is then passed to the one-arity `zip_fun` function.
   Then, the second elements from each of the enums are put into a list
   and passed to `zip_fun`, and so on until any one of the enums in
   `enumerables` runs out of elements.
