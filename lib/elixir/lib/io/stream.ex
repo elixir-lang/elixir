a7794032b251fa80667363f8ec93fffe161cb610
initialCCC <seasharp.ex@gmail.com>
2022-06-22 01:50:56 +0100

    add IO.Stream fields typespec

--- lib/elixir/lib/io/stream.ex
+++ lib/elixir/lib/io/stream.ex
@@ -26,7 +26,11 @@ defmodule IO.Stream do
 
   defstruct device: nil, raw: true, line_or_bytes: :line
 
-  @type t :: %__MODULE__{}
+  @type t :: %__MODULE__{
+    device: IO.device(),
+    raw: boolean(),
+    line_or_bytes: :line | non_neg_integer()
+  }
 
   @doc false
   def __build__(device, raw, line_or_bytes) do
