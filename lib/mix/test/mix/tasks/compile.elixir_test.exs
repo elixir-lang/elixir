cadd15cb41fd4c1948c078cd12737efae8055276
José Valim <jose.valim@dashbit.co>
2022-07-05 22:49:44 +0200

    Include column information on error diagnostics when possible, closes #11964

--- lib/mix/test/mix/tasks/compile.elixir_test.exs
+++ lib/mix/test/mix/tasks/compile.elixir_test.exs
@@ -1372,7 +1372,7 @@ defmodule Mix.Tasks.Compile.ElixirTest do
         assert %Diagnostic{
                  file: ^file,
                  severity: :error,
-                 position: 2,
+                 position: {2, 20},
                  message: "** (SyntaxError) lib/a.ex:2:" <> _,
                  compiler_name: "Elixir"
                } = diagnostic
