63bdd82ae4aea16aef06ff68e7647219c8faed5c
Wojtek Mach <wojtek@wojtekmach.pl>
2022-07-22 20:25:01 +0200

    Up

--- lib/mix/test/mix/tasks/test_test.exs
+++ lib/mix/test/mix/tasks/test_test.exs
@@ -546,8 +546,6 @@ defmodule Mix.Tasks.TestTest do
   end
 
   describe "--exit-status" do
-    @describetag :unix
-
     test "returns custom exit status" do
       in_fixture("test_failed", fn ->
         {output, exit_status} = mix_code(["test", "--exit-status", "5"])
