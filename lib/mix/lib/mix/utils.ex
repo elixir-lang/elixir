df82aa0c5c28e3c30ae5d6bade2827622ab4a860
José Valim <jose.valim@dashbit.co>
2022-07-26 12:56:17 +0200

    Update lib/mix/lib/mix/utils.ex

--- lib/mix/lib/mix/utils.ex
+++ lib/mix/lib/mix/utils.ex
@@ -520,9 +520,7 @@ defmodule Mix.Utils do
       {:error, _} ->
         files =
           File.cp_r!(source, target, fn orig, dest ->
-            {orig_mtime, orig_size} = last_modified_and_size(orig)
-            {dest_mtime, dest_size} = last_modified_and_size(dest)
-            orig_mtime > dest_mtime or orig_size != dest_size
+            File.stat!(orig).mtime > File.stat!(dest).mtime
           end)
 
         {:ok, files}
