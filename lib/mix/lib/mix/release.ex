ba875f295242203f082f4912a262c0b61d9626b2
José Valim <jose.valim@gmail.com>
2022-07-27 08:33:06 +0200

    Update lib/mix/lib/mix/release.ex

--- lib/mix/lib/mix/release.ex
+++ lib/mix/lib/mix/release.ex
@@ -773,7 +773,7 @@ defmodule Mix.Release do
 
     release.erts_source
     |> Path.join("bin")
-    |> File.cp_r!(destination)
+    |> File.cp_r!(destination, on_conflict: fn _, _ -> false end)
 
     _ = File.rm(Path.join(destination, "erl"))
     _ = File.rm(Path.join(destination, "erl.ini"))
