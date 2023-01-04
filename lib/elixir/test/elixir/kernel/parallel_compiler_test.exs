f8540ab193c900cd68031035138b6130b8d10439
Greg Mefford <greg@gregmefford.com>
2023-01-04 17:31:21 -0500

    Add details about waiting information in `--profile time` compiler output (#12310)

--- lib/elixir/test/elixir/kernel/parallel_compiler_test.exs
+++ lib/elixir/test/elixir/kernel/parallel_compiler_test.exs
@@ -44,7 +44,7 @@ defmodule Kernel.ParallelCompilerTest do
         end)
 
       assert profile =~
-               ~r"\[profile\] [\s\d]{6}ms compiling \+      0ms waiting for .*tmp/profile_time/bar.ex"
+               ~r"\[profile\] [\s\d]{6}ms compiling \+      0ms waiting while compiling .*tmp/profile_time/bar.ex"
 
       assert profile =~ ~r"\[profile\] Finished compilation cycle of 1 modules in \d+ms"
       assert profile =~ ~r"\[profile\] Finished group pass check of 1 modules in \d+ms"
