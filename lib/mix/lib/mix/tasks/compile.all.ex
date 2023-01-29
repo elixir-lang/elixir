9f927fdb040373b6c3175787e72c092c729fedeb
José Valim <jose.valim@dashbit.co>
2023-01-29 13:58:01 +0100

    Embed application tracer into app loader

--- lib/mix/lib/mix/tasks/compile.all.ex
+++ lib/mix/lib/mix/tasks/compile.all.ex
@@ -14,6 +14,13 @@ defmodule Mix.Tasks.Compile.All do
     Mix.Project.get!()
     config = Mix.Project.config()
 
+    # Compute the app cache if it is stale and we are
+    # not compiling from a dependency.
+    app_cache =
+      unless "--from-mix-deps-compile" in args do
+        Mix.AppLoader.stale_cache(config)
+      end
+
     # Make sure Mix.Dep is cached to avoid loading dependencies
     # during compilation. This is also important because we prune
     # the load paths before compiling, which means any SCM coming
@@ -21,7 +28,14 @@ defmodule Mix.Tasks.Compile.All do
     deps = Mix.Dep.cached()
     apps = project_apps(config)
     validate_compile_env? = "--no-validate-compile-env" not in args
-    loaded_paths = Mix.AppLoader.load_apps(apps, deps, config, validate_compile_env?)
+
+    {loaded_paths, loaded_modules} =
+      Mix.AppLoader.load_apps(apps, deps, config, validate_compile_env?, {[], []}, fn
+        {app, path}, {paths, mods} ->
+          paths = if path, do: [path | paths], else: paths
+          mods = if app_cache, do: [{app, Application.spec(app, :modules)} | mods], else: mods
+          {paths, mods}
+      end)
 
     # We compute the diff as that will be more efficient
     # than re-adding common paths several times
@@ -43,6 +57,10 @@ defmodule Mix.Tasks.Compile.All do
           |> compile(args, :noop, [])
         end)
       end
+
+    if app_cache do
+      Mix.AppLoader.write_cache(app_cache, Map.new(loaded_modules))
+    end
 
     compile_path = to_charlist(Mix.Project.compile_path())
     _ = Code.prepend_path(compile_path)
