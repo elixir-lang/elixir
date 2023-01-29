c4a342c0945f98c3764da1c772c949bfe2a645f6
José Valim <jose.valim@dashbit.co>
2023-01-29 18:23:06 +0100

    Improve version mismatch error message

--- lib/mix/test/mix/local_test.exs
+++ lib/mix/test/mix/local_test.exs
@@ -100,7 +100,7 @@ defmodule Mix.LocalTest do
       assert {"1.0.0", "1.2.3", "DEF"} =
                Mix.Local.find_matching_versions_from_signed_csv!("name", "1.2.3", "csv")
 
-      assert_raise Mix.Error, "Could not find a matching version of name", fn ->
+      assert_raise Mix.Error, "Could not find a version of name matching: 1.3.0", fn ->
         Mix.Local.find_matching_versions_from_signed_csv!("name", "1.3.0", "csv")
       end
     end)
