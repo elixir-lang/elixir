dae4035282cd01b762453f2ddd903c777d87aef7
José Valim <jose.valim@dashbit.co>
2023-02-02 21:45:24 +0100

    Move path loading and IEx booting to Elixir (#12375)
    This gives us more control to cache paths
    and over boot compared to relying on scripts.
    
    This also makes it easier for someone to provide
    alternatives to IEx.

--- lib/elixir/src/elixir_config.erl
+++ lib/elixir/src/elixir_config.erl
@@ -1,10 +1,12 @@
 -module(elixir_config).
 -compile({no_auto_import, [get/1]}).
--export([new/1, warn/2, serial/1]).
+-export([new/1, warn/2, serial/1, booted/0, wait_until_booted/0]).
 -export([static/1, is_bootstrap/0, identifier_tokenizer/0]).
 -export([delete/1, put/2, get/1, get/2, update/2, get_and_put/2]).
 -export([start_link/0, init/1, handle_call/3, handle_cast/2]).
 -behaviour(gen_server).
+
+%% Persistent term
 
 static(Map) when is_map(Map) ->
   persistent_term:put(?MODULE, maps:merge(persistent_term:get(?MODULE, #{}), Map)).
@@ -12,6 +14,8 @@ is_bootstrap() ->
   maps:get(bootstrap, persistent_term:get(?MODULE, #{}), false).
 identifier_tokenizer() ->
   maps:get(identifier_tokenizer, persistent_term:get(?MODULE, #{}), 'Elixir.String.Tokenizer').
+
+%% Key-value store (concurrent reads, serial writes)
 
 get(Key) ->
   [{_, Value}] = ets:lookup(?MODULE, Key),
@@ -26,16 +30,31 @@ get(Key, Default) ->
   end.
 
 put(Key, Value) ->
-  gen_server:call(?MODULE, {put, Key, Value}).
+  gen_server:call(?MODULE, {put, Key, Value}, infinity).
 
 get_and_put(Key, Value) ->
-  gen_server:call(?MODULE, {get_and_put, Key, Value}).
+  gen_server:call(?MODULE, {get_and_put, Key, Value}, infinity).
 
 update(Key, Fun) ->
-  gen_server:call(?MODULE, {update, Key, Fun}).
+  gen_server:call(?MODULE, {update, Key, Fun}, infinity).
+
+new(Opts) ->
+  Tab = ets:new(?MODULE, [named_table, public, {read_concurrency, true}]),
+  true = ets:insert_new(?MODULE, Opts),
+  Tab.
+
+delete(?MODULE) ->
+  ets:delete(?MODULE).
+
+%% MISC
+
+booted() ->
+  gen_server:call(?MODULE, booted, infinity).
+wait_until_booted() ->
+  gen_server:call(?MODULE, wait_until_booted, infinity).
 
 serial(Fun) ->
-  gen_server:call(?MODULE, {serial, Fun}).
+  gen_server:call(?MODULE, {serial, Fun}, infinity).
 
 %% Used to guarantee warnings are emitted only once per caller.
 warn(Key, [{Mod, Fun, ArgsOrArity, _} | _]) ->
@@ -48,37 +67,35 @@ warn(_, _) ->
 to_arity(Args) when is_list(Args) -> length(Args);
 to_arity(Arity) -> Arity.
 
-%% ets life-cycle api
-
-new(Opts) ->
-  Tab = ets:new(?MODULE, [named_table, public, {read_concurrency, true}]),
-  true = ets:insert_new(?MODULE, Opts),
-  Tab.
-
-delete(?MODULE) ->
-  ets:delete(?MODULE).
-
 %% gen_server api
 
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, ?MODULE, []).
 
-init(Tab) ->
-  {ok, Tab}.
+init(?MODULE) ->
+  {ok, []}.
 
-handle_call({serial, Fun}, _From, Tab) ->
-  {reply, Fun(), Tab};
-handle_call({put, Key, Value}, _From, Tab) ->
-  ets:insert(Tab, {Key, Value}),
-  {reply, ok, Tab};
-handle_call({update, Key, Fun}, _From, Tab) ->
+handle_call(booted, _From, Booted) when is_list(Booted) ->
+  [gen_server:reply(Caller, ok) || Caller <- Booted],
+  {reply, ok, done};
+handle_call(wait_until_booted, From, Booted) ->
+  if
+    is_list(Booted) -> {noreply, [From | Booted]};
+    Booted =:= done -> {reply, ok, Booted}
+  end;
+handle_call({serial, Fun}, _From, Booted) ->
+  {reply, Fun(), Booted};
+handle_call({put, Key, Value}, _From, Booted) ->
+  ets:insert(?MODULE, {Key, Value}),
+  {reply, ok, Booted};
+handle_call({update, Key, Fun}, _From, Booted) ->
   Value = Fun(get(Key)),
-  ets:insert(Tab, {Key, Value}),
-  {reply, Value, Tab};
-handle_call({get_and_put, Key, Value}, _From, Tab) ->
+  ets:insert(?MODULE, {Key, Value}),
+  {reply, Value, Booted};
+handle_call({get_and_put, Key, Value}, _From, Booted) ->
   OldValue = get(Key),
-  ets:insert(Tab, {Key, Value}),
-  {reply, OldValue, Tab}.
+  ets:insert(?MODULE, {Key, Value}),
+  {reply, OldValue, Booted}.
 
 handle_cast(Cast, Tab) ->
   {stop, {bad_cast, Cast}, Tab}.
