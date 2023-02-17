9531a5f6aaaedb15014dd55c0a7e50d4d68e8b8a
José Valim <jose.valim@gmail.com>
2023-02-17 23:30:07 +0100

    Apply suggestions from code review

--- lib/elixir/lib/system.ex
+++ lib/elixir/lib/system.ex
@@ -46,8 +46,8 @@ defmodule System do
       mode](https://www.erlang.org/doc/apps/erts/time_correction.html#Time_Warp_Modes);
 
     * `monotonic_time/0` - a monotonically increasing time provided
-      by the Erlang VM. **This is not strictly monotonically increasing. Multiple
-      sequential calls of the function may return the same value.**
+      by the Erlang VM. This is not strictly monotonically increasing. Multiple
+      sequential calls of the function may return the same value.
 
   The time functions in this module work in the `:native` unit
   (unless specified otherwise), which is operating system dependent. Most of
@@ -1187,8 +1187,8 @@ defmodule System do
   Returns the current monotonic time in the `:native` time unit.
 
   This time is monotonically increasing and starts in an unspecified
-  point in time. **This is not strictly monotonically increasing. Multiple
-  sequential calls of the function may return the same value.**
+  point in time. This is not strictly monotonically increasing. Multiple
+  sequential calls of the function may return the same value.
 
   Inlined by the compiler.
   """
