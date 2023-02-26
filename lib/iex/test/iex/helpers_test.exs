3cc4ddfa85f20985f0bbea55226a528ff91dbf18
José Valim <jose.valim@dashbit.co>
2023-02-26 22:45:08 +0100

    Show allocators as a table

--- lib/iex/test/iex/helpers_test.exs
+++ lib/iex/test/iex/helpers_test.exs
@@ -318,8 +318,11 @@ defmodule IEx.HelpersTest do
     test "shows VM information" do
       assert "\n## System and architecture" <> _ = capture_io(fn -> runtime_info() end)
 
-      assert "\n## Loaded OTP Applications" <> _ =
-               capture_io(fn -> runtime_info([:applications, :allocators]) end)
+      assert "\n## Loaded OTP applications" <> _ =
+               capture_io(fn -> runtime_info([:applications]) end)
+
+      assert "\n## Memory allocators" <> _ =
+               capture_io(fn -> runtime_info([:allocators]) end)
     end
   end
 
