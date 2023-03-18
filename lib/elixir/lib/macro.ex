adb3f6a1a5fbc084fc424e7a4b75bd7fc8d83c3b
sabiwara <sabiwara@gmail.com>
2023-03-18 11:57:23 +0900

    Add more info for :with expressions in dbg/1

--- lib/elixir/lib/macro.ex
+++ lib/elixir/lib/macro.ex
@@ -2674,6 +2674,48 @@ defmodule Macro do
     end
   end
 
+  defp dbg_ast_to_debuggable({:with, meta, args} = ast, _env) do
+    {opts, clauses} = List.pop_at(args, -1)
+
+    # Ref variable used as a key to store the acc in the process dict.
+    # We use the process dict because we want to keep the acc even if
+    # a clause does not match.
+    acc_ref_var = unique_var(:acc_ref, __MODULE__)
+
+    modified_clauses =
+      Enum.flat_map(clauses, fn
+        # We only detail assignments and pattern-matching clauses that
+        # can be helpful to understand how the result is constructed.
+        {symbol, _meta, [left, right]} when symbol in [:<-, :=] ->
+          quote do
+            [
+              value = unquote(right),
+              Process.put(unquote(acc_ref_var), [
+                {unquote(escape(right)), value} | Process.get(unquote(acc_ref_var))
+              ]),
+              unquote(symbol)(unquote(left), value)
+            ]
+          end
+
+        # Other expressions like side effects are omitted.
+        expr ->
+          [expr]
+      end)
+
+    modified_with_ast = {:with, meta, modified_clauses ++ [opts]}
+
+    quote do
+      unquote(acc_ref_var) = make_ref()
+      Process.put(unquote(acc_ref_var), [])
+
+      value = unquote(modified_with_ast)
+
+      acc = Process.get(unquote(acc_ref_var))
+      Process.delete(unquote(acc_ref_var))
+      {:with, unquote(escape(ast)), Enum.reverse(acc), value}
+    end
+  end
+
   # Any other AST.
   defp dbg_ast_to_debuggable(ast, _env) do
     quote do: {:value, unquote(escape(ast)), unquote(ast)}
@@ -2821,6 +2863,25 @@ defmodule Macro do
       dbg_format_ast_with_value(condition_ast, condition_result, options),
       ?\n,
       dbg_maybe_underline("If expression", options),
+      ":\n",
+      dbg_format_ast_with_value(ast, result, options)
+    ]
+
+    {formatted, result}
+  end
+
+  defp dbg_format_ast_to_debug({:with, ast, clauses, result}, options) do
+    formatted_clauses =
+      Enum.map(clauses, fn {clause_ast, clause_result} ->
+        dbg_format_ast_with_value(clause_ast, clause_result, options)
+      end)
+
+    formatted = [
+      dbg_maybe_underline("With clauses", options),
+      ":\n",
+      formatted_clauses,
+      ?\n,
+      dbg_maybe_underline("With expression", options),
       ":\n",
       dbg_format_ast_with_value(ast, result, options)
     ]
