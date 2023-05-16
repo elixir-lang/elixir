5eac9eb896699f943eee5d08e3fbd8a9798b3a3d
Andrea Leopardi <an.leopardi@gmail.com>
2023-05-16 17:17:36 +0200

    Improve specs for profiling-relates functions

--- lib/mix/lib/mix/tasks/profile.eprof.ex
+++ lib/mix/lib/mix/tasks/profile.eprof.ex
@@ -183,7 +183,7 @@ defmodule Mix.Tasks.Profile.Eprof do
     * `:sort` - sort the results by `:time` or `:calls` (default: `:time`)
 
   """
-  @spec profile((-> any()), keyword()) :: any()
+  @spec profile((-> result), keyword()) :: result when result: any()
   def profile(fun, opts \\ []) when is_function(fun, 0) do
     Mix.ensure_application!(:tools)
     {return_value, results} = profile_and_analyse(fun, opts)
