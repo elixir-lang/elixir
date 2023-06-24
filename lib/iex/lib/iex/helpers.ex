4d008142d76ba4592ba8d35154f977528bcc8f94
José Valim <jose.valim@dashbit.co>
2023-06-24 20:18:07 +0200

    Return errors on mix compile instead of raising

--- lib/iex/lib/iex/helpers.ex
+++ lib/iex/lib/iex/helpers.ex
@@ -99,7 +99,7 @@ defmodule IEx.Helpers do
       reenable_tasks(config)
 
       force? = Keyword.get(options, :force, false)
-      args = ["--purge-consolidation-path-if-stale", consolidation]
+      args = ["--purge-consolidation-path-if-stale", "--return-errors", consolidation]
       args = if force?, do: ["--force" | args], else: args
 
       {result, _} = Mix.Task.run("compile", args)
