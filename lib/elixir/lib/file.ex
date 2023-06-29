f51e32b654e1443d9e5731605ac697da254dc3b6
Vinícius Müller <vinigm.nho@gmail.com>
2023-06-29 11:40:11 -0300

    Improve File.ls docs

--- lib/elixir/lib/file.ex
+++ lib/elixir/lib/file.ex
@@ -1630,6 +1630,10 @@ defmodule File do
   @doc """
   Returns the list of files in the given directory.
 
+  Since directories are considered files by the file system, they will also be
+  inlcuded. Also note that hidden files are not ignored and the results are not
+  sorted.
+
   Returns `{:ok, files}` in case of success,
   `{:error, reason}` otherwise.
   """
