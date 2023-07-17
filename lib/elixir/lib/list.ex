1481f65ea76d5952d265ed73f92e8b0bf36bc025
B. Burt <beeburrt@outlook.com>
2023-07-17 12:18:06 -0700

    Delete extra word in documentation - list.ex

--- lib/elixir/lib/list.ex
+++ lib/elixir/lib/list.ex
@@ -57,8 +57,8 @@ defmodule List do
       iex> list ++ [4] # slow
       [1, 2, 3, 4]
 
-  Most of the functions in this module work in linear time. This means that,
-  that the time it takes to perform an operation grows at the same rate as the
+  Most of the functions in this module work in linear time. This means that
+  the time it takes to perform an operation grows at the same rate as the
   length of the list. For example `length/1` and `last/1` will run in linear
   time because they need to iterate through every element of the list, but
   `first/1` will run in constant time because it only needs the first element.
