37a0775ca1667ba9e3cba74154197e65d43aec92
Artem Solomatin <artem00298@gmail.com>
2023-10-05 09:08:12 +0300

    Apply PR suggestions

--- lib/elixir/lib/io.ex
+++ lib/elixir/lib/io.ex
@@ -567,7 +567,7 @@ defmodule IO do
 
   """
   @doc since: "1.12.0"
-  @spec stream() :: Enumerable.t()
+  @spec stream() :: Enumerable.t(String.t())
   def stream, do: stream(:stdio, :line)
 
   @doc """
@@ -621,7 +621,7 @@ defmodule IO do
 
   """
   @doc since: "1.12.0"
-  @spec binstream() :: Enumerable.t()
+  @spec binstream() :: Enumerable.t(binary)
   def binstream, do: binstream(:stdio, :line)
 
   @doc """
