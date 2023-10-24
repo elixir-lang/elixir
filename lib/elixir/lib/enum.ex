0a7a41cc2c064b69b1698ce8b6146d165e399a2b
Gilbert Bishop-White <gilbert@bishopwhite.uk>
2023-10-24 19:34:51 +0100

    Add example of inserting into Map that already has key (#13013)

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -1493,6 +1493,9 @@ defmodule Enum do
       iex> Enum.into([a: 1, a: 2], %{})
       %{a: 2}
 
+      iex> Enum.into([a: 2], %{a: 1, b: 3})
+      %{a: 2, b: 3}
+
   """
   @spec into(Enumerable.t(), Collectable.t()) :: Collectable.t()
   def into(enumerable, collectable)
