a5ac8b2b2ca5093aebfa68db857e464297d2f72c
Damir Vandic <info@dvic.io>
2023-11-21 09:26:04 +0100

    Add additional docs to Kernel.if/2 (#13120)

--- lib/elixir/lib/kernel.ex
+++ lib/elixir/lib/kernel.ex
@@ -3777,7 +3777,9 @@ defmodule Kernel do
   Provides an `if/2` macro.
 
   This macro expects the first argument to be a condition and the second
-  argument to be a keyword list.
+  argument to be a keyword list. Similar to `case/2`, any assignment in
+  the condition will be available on both clauses, as well as after the
+  `if` expression.
 
   ## One-liner examples
 
