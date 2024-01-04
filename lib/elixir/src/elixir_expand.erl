fa088a43588c391baf86798ec7b29cc1ea7d978b
José Valim <jose.valim@dashbit.co>
2024-01-04 13:01:44 +0100

    Do not hardcode newlines

--- lib/elixir/src/elixir_expand.erl
+++ lib/elixir/src/elixir_expand.erl
@@ -1206,9 +1206,9 @@ format_error(unhandled_cons_op) ->
   "misplaced operator |/2\n\n"
   "The | operator is typically used between brackets as the cons operator:\n\n"
   "    [head | tail]\n\n"
-  "where head is a sequence of elements separated by commas and the tail\n"
+  "where head is a sequence of elements separated by commas and the tail "
   "is the remaining of a list.\n\n"
-  "It is also used to update maps and structs, via the %{map | key: value} notation,\n"
+  "It is also used to update maps and structs, via the %{map | key: value} notation, "
   "and in typespecs, such as @type and @spec, to express the union of two types";
 format_error(unhandled_type_op) ->
   "misplaced operator ::/2\n\n"
