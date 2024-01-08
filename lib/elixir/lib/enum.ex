5f90e499520633903b24bd277ffd16764e345d67
Brad Hanks <hanks.brad@gmail.com>
2024-01-08 16:35:45 -0700

    revert to Erlang exception

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -1526,20 +1526,14 @@ defmodule Enum do
 
   defp into_map(%{} = enumerable), do: enumerable
 
-  defp into_map(enumerable) when is_list(enumerable),
-    do: :maps.from_list(valid_kw_list!(enumerable))
+  defp into_map(enumerable) when is_list(enumerable), do: :maps.from_list(enumerable)
 
-  defp into_map(enumerable) do
-    enumerable
-    |> Enum.to_list()
-    |> valid_kw_list!
-    |> :maps.from_list()
-  end
+  defp into_map(enumerable), do: enumerable |> Enum.to_list() |> :maps.from_list()
 
   defp into_map(%{} = enumerable, collectable), do: Map.merge(collectable, enumerable)
 
   defp into_map(enumerable, collectable) when is_list(enumerable),
-    do: Map.merge(collectable, :maps.from_list(valid_kw_list!(enumerable)))
+    do: Map.merge(collectable, :maps.from_list(enumerable))
 
   defp into_map(enumerable, collectable),
     do: Enum.reduce(enumerable, collectable, fn {key, val}, acc -> Map.put(acc, key, val) end)
@@ -1568,19 +1562,6 @@ defmodule Enum do
       {:cont, fun.(acc, {:cont, x})}
     end)
     |> elem(1)
-  end
-
-  defp valid_kw_list!(enumerable) do
-    case enumerable do
-      [] ->
-        enumerable
-
-      [h | _] when is_tuple(h) and tuple_size(h) == 2 ->
-        enumerable
-
-      [h | _] ->
-        raise ArgumentError, "collecting into a map requires {key, value} tuples, got: #{h}"
-    end
   end
 
   @doc """
