537ffd2fc8987a9547826da84631114c278fcb3d
Michael Goodyear <m@gdyr.co>
2024-08-01 00:35:27 +1000

    Use System.cmd rather than Mix.shell().cmd() for Rebar3 to avoid escaping
    Closes #13750.

--- lib/mix/test/mix/rebar_test.exs
+++ lib/mix/test/mix/rebar_test.exs
@@ -240,7 +240,7 @@ defmodule Mix.RebarTest do
       in_tmp("rebar3 env with spaces", fn ->
         File.cp!(Mix.Rebar.local_rebar_path(:rebar3), "rebar3")
         System.put_env("MIX_REBAR3", Path.absname("rebar3"))
-        assert Mix.Rebar.rebar_cmd(:rebar3) =~ " "
+        assert Mix.Rebar.rebar_path(:rebar3) =~ " "
 
         Mix.Project.push(RebarAsDep)
         Mix.Tasks.Deps.Get.run([])
