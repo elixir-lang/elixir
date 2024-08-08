91d24e9333e3191dac7282a7c7b73fbe022b12a2
Enzo <enzo.baldisserri@aworldforus.com>
2024-08-08 13:26:27 +0200

    xref: do not limit compile-connected to runtime dependencies

--- lib/mix/lib/mix/tasks/xref.ex
+++ lib/mix/lib/mix/tasks/xref.ex
@@ -61,8 +61,8 @@ defmodule Mix.Tasks.Xref do
   because compile time dependencies are transitive.
 
   Having compile time dependencies is a common feature in Elixir projects.
-  However, the modules you depend on at compile-time must avoid runtime
-  dependencies within the same project. You can understand all of the
+  However, the modules you depend on at compile-time must avoid dependencies
+  to modules within the same project. You can understand all of the
   dependencies of a given file by running:
 
       $ mix xref trace lib/livebook_web.ex
@@ -75,7 +75,7 @@ defmodule Mix.Tasks.Xref do
   Elixir tracks three types of dependencies between modules: compile,
   exports, and runtime. If a module has a compile time dependency on
   another module, the caller module has to be recompiled whenever the
-  callee changes (or any runtime dependency of the callee changes).
+  callee changes (or any dependency of the callee changes).
   Let's see an example:
 
       # lib/a.ex
