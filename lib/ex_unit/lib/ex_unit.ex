b35b0ef43572d9f9230283f0abec9b5aa46d8b6e
Dave Lucia <davelucianyc@gmail.com>
2024-11-09 05:01:48 -0500

    Simplify the explanation of a basic test case (#13982)
    Simplifies the language used to describe a "basic setup for ExUnit"
    
    In particular, I removed the term "test case" which was an alias for a
    "test module". "test case" can easily be confused with a `test` block.

--- lib/ex_unit/lib/ex_unit.ex
+++ lib/ex_unit/lib/ex_unit.ex
@@ -11,11 +11,11 @@ defmodule ExUnit do
       # 1) Start ExUnit.
       ExUnit.start()
 
-      # 2) Create a new test module (test case) and use "ExUnit.Case".
+      # 2) Create a new test module and use "ExUnit.Case".
       defmodule AssertionTest do
-        # 3) Note that we pass "async: true", this runs the test case
-        #    concurrently with other test cases. The individual tests
-        #    within each test case are still run serially.
+        # 3) Note that we pass "async: true", this runs the tests in the
+        #    test module concurrently with other test modules. The 
+        #    individual tests within each test module are still run serially.
         use ExUnit.Case, async: true
 
         # 4) Use the "test" macro instead of "def" for clarity.
