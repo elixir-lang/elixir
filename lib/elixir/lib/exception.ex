1fed20357c38b9efae49e79d5be4bdb1f6eb3bc5
Dave Lucia <davelucianyc@gmail.com>
2024-11-20 18:11:10 -0500

    fix formatter

--- lib/elixir/lib/exception.ex
+++ lib/elixir/lib/exception.ex
@@ -1997,8 +1997,10 @@ defmodule Protocol.UndefinedError do
 
   @impl true
   def message(%{protocol: protocol, value: value, description: description}) do
-    "protocol #{inspect(protocol)} not implemented for type " <> value_type(value) <>
-      maybe_description(description) <> maybe_available(protocol) <>
+    "protocol #{inspect(protocol)} not implemented for type " <>
+      value_type(value) <>
+      maybe_description(description) <>
+      maybe_available(protocol) <>
       "\n\nGot value: #{inspect(value, pretty: true)}"
   end
 
