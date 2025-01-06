13baeb53dc34cefa58538ae3ad22093351e0f187
Roeland van Batenburg <roelandvanbatenburg@gmail.com>
2025-01-06 23:03:57 +1300

    Mention alternatives to DateTime.compare/2 for comparison (#14147)

--- lib/elixir/lib/calendar/datetime.ex
+++ lib/elixir/lib/calendar/datetime.ex
@@ -13,8 +13,8 @@ defmodule DateTime do
 
   Remember, comparisons in Elixir using `==/2`, `>/2`, `</2` and friends
   are structural and based on the DateTime struct fields. For proper
-  comparison between datetimes, use the `compare/2` function. The
-  existence of the `compare/2` function in this module also allows
+  comparison between datetimes, use the `compare/2`, `after?/2` and `before?/2` functions.
+  The existence of the `compare/2` function in this module also allows
   using `Enum.min/2` and `Enum.max/2` functions to get the minimum and
   maximum datetime of an `Enum`. For example:
 
