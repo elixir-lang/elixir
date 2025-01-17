1644fe4894b9dcc6e4a16ee682470b34162bdd62
Jean Klingler <sabiwara@proton.me>
2025-01-17 20:47:20 +0900

    Add more info to comment
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- lib/elixir/test/elixir/fixtures/consolidation/sample.ex
+++ lib/elixir/test/elixir/fixtures/consolidation/sample.ex
@@ -5,6 +5,8 @@ defprotocol Protocol.ConsolidationTest.Sample do
   @spec ok(t) :: boolean
   def ok(term)
 
-  # Not a protocol function
+  # Not a protocol function. While this is not "officially" supported,
+  # it does happen in practice, so we need to make sure we preserve
+  # its signature.
   Kernel.def(regular_fun(term), do: term + 1)
 end
