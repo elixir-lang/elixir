fb7b34550eb4496fc5526567e32305b4b7db3824
José Valim <jose.valim@dashbit.co>
2025-02-04 22:38:24 +0100

    Remove deprecation warning from Dialyzer suite

--- lib/elixir/test/elixir/kernel/dialyzer_test.exs
+++ lib/elixir/test/elixir/kernel/dialyzer_test.exs
@@ -116,11 +116,6 @@ defmodule Kernel.DialyzerTest do
     assert_dialyze_no_warnings!(context)
   end
 
-  test "no warnings on struct update", context do
-    copy_beam!(context, Dialyzer.StructUpdate)
-    assert_dialyze_no_warnings!(context)
-  end
-
   @tag warnings: [:specdiffs]
   test "no warnings on protocol calls with opaque types", context do
     alias Dialyzer.ProtocolOpaque
