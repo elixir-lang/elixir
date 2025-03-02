f040f1c76d07d1d5894b709c3827af955db477dd
sabiwara <sabiwara@gmail.com>
2025-03-02 09:38:52 +0900

    Add test for cycling over empty enumerable

--- lib/elixir/test/elixir/stream_test.exs
+++ lib/elixir/test/elixir/stream_test.exs
@@ -245,6 +245,10 @@ defmodule StreamTest do
       Stream.cycle(%{}) |> Enum.to_list()
     end
 
+    assert_raise ArgumentError, "cannot cycle over an empty enumerable", fn ->
+      Stream.cycle(..) |> Enum.to_list()
+    end
+
     assert Stream.cycle([1, 2, 3]) |> Stream.take(5) |> Enum.to_list() == [1, 2, 3, 1, 2]
     assert Enum.take(stream, 5) == [1, 2, 3, 1, 2]
   end
