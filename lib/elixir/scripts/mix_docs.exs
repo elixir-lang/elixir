eda7b5e250d22e6aeb4df056ef4898cc5aee07e5
Jean Klingler <sabiwara@gmail.com>
2025-03-28 15:41:58 +0900

    Fix usage of mermaid in ExUnit &co (#14376)

--- lib/elixir/scripts/mix_docs.exs
+++ lib/elixir/scripts/mix_docs.exs
@@ -13,5 +13,41 @@ canonical = System.fetch_env!("CANONICAL")
     logger: "https://hexdocs.pm/logger/#{canonical}",
     mix: "https://hexdocs.pm/mix/#{canonical}"
   ],
-  formatters: ["html", "epub"]
+  formatters: ["html", "epub"],
+  before_closing_body_tag: fn
+    :html ->
+      """
+      <script defer src="https://cdn.jsdelivr.net/npm/mermaid@10.2.3/dist/mermaid.min.js"></script>
+      <script>
+        let initialized = false;
+
+        window.addEventListener("exdoc:loaded", () => {
+          if (!initialized) {
+            mermaid.initialize({
+              startOnLoad: false,
+              theme: document.body.className.includes("dark") ? "dark" : "default"
+            });
+            initialized = true;
+          }
+
+          let id = 0;
+          for (const codeEl of document.querySelectorAll("pre code.mermaid")) {
+            const preEl = codeEl.parentElement;
+            const graphDefinition = codeEl.textContent;
+            const graphEl = document.createElement("div");
+            const graphId = "mermaid-graph-" + id++;
+            mermaid.render(graphId, graphDefinition).then(({svg, bindFunctions}) => {
+              graphEl.innerHTML = svg;
+              bindFunctions?.(graphEl);
+              preEl.insertAdjacentElement("afterend", graphEl);
+              preEl.remove();
+            });
+          }
+        });
+      </script>
+      """
+
+    _ ->
+      ""
+  end
 ]
