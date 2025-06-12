f395d7694a54207ea1bcc42981d02bc91daa3958
Tobias Pfeiffer <pragtob@gmail.com>
2025-06-12 10:49:43 +0200

    Non executable example as it emits error
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- lib/elixir/lib/map.ex
+++ lib/elixir/lib/map.ex
@@ -316,9 +316,11 @@ defmodule Map do
 
       iex> Map.fetch!(%{a: 1}, :a)
       1
-      iex> Map.fetch!(%{a: 1}, :b)
-      ** (KeyError) key :b not found in:
-      ...
+
+  When the key is missing, an exception is raised:
+      
+      Map.fetch!(%{a: 1}, :b)
+      ** (KeyError) key :b not found in: %{a: 1}
 
   """
   @spec fetch!(map, key) :: value
