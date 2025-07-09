fff2cf0d81299f8c9a6e3ebfac5f658639873ba3
Eksperimental <eksperimental@autistici.org>
2025-07-09 13:09:07 -0500

    Standardize "Examples" heading section levels in docs (#14638)
    * Convert "Examples" 3rd level headings to 2nd level when not under a 2nd level
    
    * Convert 1st level "Examples" heading to 2nd level

--- lib/elixir/lib/calendar/time.ex
+++ lib/elixir/lib/calendar/time.ex
@@ -225,7 +225,7 @@ defmodule Time do
   @doc """
   Converts the given `time` to a string.
 
-  ### Examples
+  ## Examples
 
       iex> Time.to_string(~T[23:00:00])
       "23:00:00"
@@ -334,7 +334,7 @@ defmodule Time do
   format, for human readability. It also supports the "basic" format through
   passing the `:basic` option.
 
-  ### Examples
+  ## Examples
 
       iex> Time.to_iso8601(~T[23:00:13])
       "23:00:13"
