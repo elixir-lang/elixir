66c3f1182b64aae0029b70016148681a421f5377
Eksperimental <eksperimental@autistici.org>
2025-08-28 07:03:56 -0500

    Minor optimizations for `Range.shift/2`

--- lib/elixir/lib/range.ex
+++ lib/elixir/lib/range.ex
@@ -290,9 +290,14 @@ defmodule Range do
   """
   @doc since: "1.14.0"
   @spec shift(t, integer) :: t
-  def shift(first..last//step, steps_to_shift)
-      when is_integer(steps_to_shift) do
-    new(first + steps_to_shift * step, last + steps_to_shift * step, step)
+  def shift(range, steps_to_shift)
+
+  def shift(range, 0) when is_struct(range, Range), do: range
+
+  def shift(first..last//step, steps_to_shift) when is_integer(steps_to_shift) do
+    shift = steps_to_shift * step
+
+    new(first + shift, last + shift, step)
   end
 
   @doc """
