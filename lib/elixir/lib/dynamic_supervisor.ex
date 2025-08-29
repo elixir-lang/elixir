f0afef21662915c0bf2d02e4515f646446556db0
Eksperimental <eksperimental@autistici.org>
2025-08-29 08:12:04 -0500

    Simplify return type of `DynamicSupervisor.child_spec/1`

--- lib/elixir/lib/dynamic_supervisor.ex
+++ lib/elixir/lib/dynamic_supervisor.ex
@@ -206,12 +206,7 @@ defmodule DynamicSupervisor do
   See `Supervisor` for more information about child specifications.
   """
   @doc since: "1.6.1"
-  @spec child_spec(options) :: %{
-          id: atom() | term(),
-          start: {DynamicSupervisor, :start_link, args :: [options]},
-          type: :supervisor
-        }
-        when options: [init_option() | GenServer.option()]
+  @spec child_spec([init_option() | GenServer.option()]) :: Supervisor.child_spec()
   def child_spec(options) when is_list(options) do
     id =
       case Keyword.get(options, :name, DynamicSupervisor) do
