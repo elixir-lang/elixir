205a6960345264a6f5870d622b068ad247cd5a20
Eksperimental <eksperimental@autistici.org>
2025-10-08 11:59:00 -0500

    Update copyright year for Ericsson to 2025

--- lib/elixir/src/elixir_json.erl
+++ lib/elixir/src/elixir_json.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2024-2024. All Rights Reserved.
+%% Copyright Ericsson AB 2024-2025. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
