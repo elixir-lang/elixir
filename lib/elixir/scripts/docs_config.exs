6239e20ffe6b39b35f15907cb434d37c07b84be6
Eksperimental <eksperimental@autistici.org>
2025-11-22 10:52:57 -0500

    Do not crash when building docs from sources with no Git repo
    Closes https://github.com/elixir-lang/elixir/issues/14943

--- lib/elixir/scripts/docs_config.exs
+++ lib/elixir/scripts/docs_config.exs
@@ -3,41 +3,63 @@
 
 # Generate docs_config.js for version chooser in ExDoc
 [app] = System.argv()
-
-{text_tags, 0} = System.cmd("git", ["tag"])
 skipped = Version.parse!("1.0.3")
+root_dir = Path.expand("../../../", __DIR__)
+
+git_repo? =
+  root_dir
+  |> Path.join(".git")
+  |> File.dir?()
 
 versions =
-  for(
-    "v" <> rest <- String.split(text_tags),
-    not String.ends_with?(rest, "-latest"),
-    version = Version.parse!(rest),
-    Version.compare(version, skipped) == :gt,
-    do: version
-  )
-  |> Enum.sort({:desc, Version})
+  if git_repo? do
+    {text_tags, 0} = System.cmd("git", ["tag"])
+
+    for(
+      "v" <> rest <- String.split(text_tags),
+      not String.ends_with?(rest, "-latest"),
+      version = Version.parse!(rest),
+      Version.compare(version, skipped) == :gt,
+      do: version
+    )
+    |> Enum.sort({:desc, Version})
+  else
+    []
+  end
 
 latest =
-  versions
-  |> Stream.filter(&(&1.pre == []))
-  |> Enum.fetch!(0)
-  |> Version.to_string()
+  if git_repo? do
+    versions
+    |> Stream.filter(&(&1.pre == []))
+    |> Enum.fetch!(0)
+    |> Version.to_string()
+  else
+    nil
+  end
 
 version_nodes =
-  for version <- versions do
-    version_string = Version.to_string(version)
-    map = %{version: "v#{version_string}", url: "https://hexdocs.pm/#{app}/#{version_string}"}
+  if git_repo? do
+    for version <- versions do
+      version_string = Version.to_string(version)
+      map = %{version: "v#{version_string}", url: "https://hexdocs.pm/#{app}/#{version_string}"}
 
-    if version_string == latest do
-      Map.put(map, :latest, true)
-    else
-      map
+      if version_string == latest do
+        Map.put(map, :latest, true)
+      else
+        map
+      end
     end
+  else
+    []
   end
 
 search_nodes =
-  for app <- ~w(eex elixir ex_unit iex logger mix)s do
-    %{name: app, version: latest}
+  if git_repo? do
+    for app <- ~w(eex elixir ex_unit iex logger mix)s do
+      %{name: app, version: latest}
+    end
+  else
+    []
   end
 
 File.mkdir_p!("doc/#{app}")
