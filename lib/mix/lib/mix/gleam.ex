2f0efe4fca4c6279497f9c5e57e5d7236601c3be
Rodrigo Álvarez <papipo@gmail.com>
2025-11-28 09:02:05 +0100

    Update copyright comments
    Co-authored-by: Eksperimental <eksperimental@autistici.org>

--- lib/mix/lib/mix/gleam.ex
+++ lib/mix/lib/mix/gleam.ex
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2021 The Elixir Team
-# SPDX-FileCopyrightText: 2012 Plataformatec
 
 defmodule Mix.Gleam do
   # Version that introduced `gleam export package-information` command
