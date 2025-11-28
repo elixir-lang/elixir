f15d947289976cb17bf7dbaa4d57dbb6380efd15
Jonatan Männchen <jonatan@maennchen.ch>
2025-11-28 10:16:20 +0100

    Fix Plataformatec Copyright

--- lib/elixir/test/elixir/calendar_test.exs
+++ lib/elixir/test/elixir/calendar_test.exs
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2021 The Elixir Team
+# SPDX-FileCopyrightText: 2012 Plataformatec
 
 Code.require_file("test_helper.exs", __DIR__)
 
