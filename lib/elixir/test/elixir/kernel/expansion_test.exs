24e83c98e4faed3c2c99a24cc73ab8d676f6cb72
Gonzalo <456459+grzuy@users.noreply.github.com>
2026-03-12 14:34:38 -0300

    fix: fixes missing space typo in compiler error message

--- lib/elixir/test/elixir/kernel/expansion_test.exs
+++ lib/elixir/test/elixir/kernel/expansion_test.exs
@@ -810,7 +810,7 @@ defmodule Kernel.ExpansionTest do
 
     test "in guards with macros" do
       message =
-        ~r"you must require the moduleInteger before invoking macro Integer.is_even/1 inside a guard"
+        ~r"you must require the module Integer before invoking macro Integer.is_even/1 inside a guard"
 
       assert_compile_error(message, fn ->
         expand(quote(do: fn arg when Integer.is_even(arg) -> arg end))
