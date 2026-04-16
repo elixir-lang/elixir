elixir-lang/elixir

commit 146fb4e3fffa61815babfd5c2b18d4fd16960691

Make has_unquote/1 quote-aware (#12836)

lib/elixir/src/elixir_quote.erl
@@ -30,17 +30,35 @@ fun_to_quoted(Function) ->
 
 %% has_unquotes
 
-has_unquotes({unquote, _, [_]}) -> true;
-has_unquotes({unquote_splicing, _, [_]}) -> true;
-has_unquotes({{'.', _, [_, unquote]}, _, [_]}) -> true;
-has_unquotes({Var, _, Ctx}) when is_atom(Var), is_atom(Ctx) -> false;
-has_unquotes({Name, _, Args}) when is_list(Args) ->
-  has_unquotes(Name) orelse lists:any(fun has_unquotes/1, Args);
-has_unquotes({Left, Right}) ->
-  has_unquotes(Left) orelse has_unquotes(Right);
-has_unquotes(List) when is_list(List) ->
-  lists:any(fun has_unquotes/1, List);
-has_unquotes(_Other) -> false.
+has_unquotes(Ast) -> has_unquotes(Ast, 0).
+
+has_unquotes({quote, _, [Child]}, QuoteLevel) ->
+  has_unquotes(Child, QuoteLevel + 1);
+has_unquotes({quote, _, [QuoteOpts, Child]}, QuoteLevel) ->
+  case disables_unquote(QuoteOpts) of
+    true -> false;
+    _ -> has_unquotes(Child, QuoteLevel + 1)
+  end;
+has_unquotes({Unquote, _, [Child]}, QuoteLevel)
+  when Unquote == unquote; Unquote == unquote_splicing ->
+  case QuoteLevel of
+    0 -> true;
+    _ ->  has_unquotes(Child, QuoteLevel - 1)
+end;
+has_unquotes({{'.', _, [_, unquote]}, _, [_]}, _) -> true;
+has_unquotes({Var, _, Ctx}, _) when is_atom(Var), is_atom(Ctx) -> false;
+has_unquotes({Name, _, Args}, QuoteLevel) when is_list(Args) ->
+  has_unquotes(Name) orelse lists:any(fun(Child) -> has_unquotes(Child, QuoteLevel) end, Args);
+has_unquotes({Left, Right}, QuoteLevel) ->
+  has_unquotes(Left, QuoteLevel) orelse has_unquotes(Right, QuoteLevel);
+has_unquotes(List, QuoteLevel) when is_list(List) ->
+  lists:any(fun(Child) -> has_unquotes(Child, QuoteLevel) end, List);
+has_unquotes(_Other, _) -> false.
+
+disables_unquote([{unquote, false} | _]) -> true;
+disables_unquote([{bind_quoted, _} | _]) -> true;
+disables_unquote([_H | T]) -> disables_unquote(T);
+disables_unquote(_) -> false.
 
 %% Apply the line from site call on quoted contents.
 %% Receives a Key to look for the default line as argument.

lib/elixir/test/elixir/kernel/quote_test.exs
@@ -653,3 +653,124 @@ defmodule Kernel.QuoteTest.ImportsHygieneTest do
     assert meta[:imports] == [{0, BinaryUtils}]
   end
 end
+
+defmodule Kernel.QuoteTest.HasUnquoteTest do
+  use ExUnit.Case, async: true
+
+  test "expression without unquote returns false" do
+    ast =
+      quote unquote: false do
+        opts = [x: 5]
+        x = Keyword.fetch!(opts, :x)
+        x + 1
+      end
+
+    refute :elixir_quote.has_unquotes(ast)
+  end
+
+  test "expression with unquote returns true" do
+    ast =
+      quote unquote: false do
+        [x: unquote(x)]
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        unquote(module).fun(x)
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        module.unquote(fun)(x)
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        module.fun(unquote(x))
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        module.fun(unquote_splicing(args))
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+  end
+
+  test "expression with unquote within quote returns false" do
+    ast =
+      quote unquote: false do
+        quote do
+          x + unquote(y)
+        end
+      end
+
+    refute :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        quote do
+          foo = bar(unquote_splicing(args))
+        end
+      end
+
+    refute :elixir_quote.has_unquotes(ast)
+  end
+
+  test "expression with unquote within unquote within quote returns true" do
+    ast =
+      quote unquote: false do
+        quote do
+          x + unquote(unquote(y))
+        end
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        quote do
+          foo = bar(unquote_splicing(unquote(args)))
+        end
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        quote do
+          foo = bar(unquote(unquote_splicing(args)))
+        end
+      end
+
+    assert :elixir_quote.has_unquotes(ast)
+  end
+
+  test "expression within quote disabling unquotes always returns false" do
+    ast =
+      quote unquote: false do
+        quote unquote: false do
+          x + unquote(unquote(y))
+        end
+      end
+
+    refute :elixir_quote.has_unquotes(ast)
+
+    ast =
+      quote unquote: false do
+        quote bind_quoted: [x: x] do
+          x + unquote(unquote(y))
+        end
+      end
+
+    refute :elixir_quote.has_unquotes(ast)
+  end
+end

commit 9a9135e2cdff3741024a344a738c56ad5d33ce68

Do not reset quote level on calls in `has_unquotes/2`
The bug was introduced in 146fb4e3fffa61815babfd5c2b18d4fd16960691

lib/elixir/src/elixir_quote.erl
@@ -55,7 +55,7 @@ end;
 has_unquotes({{'.', _, [_, unquote]}, _, [_]}, _) -> true;
 has_unquotes({Var, _, Ctx}, _) when is_atom(Var), is_atom(Ctx) -> false;
 has_unquotes({Name, _, Args}, QuoteLevel) when is_list(Args) ->
-  has_unquotes(Name) orelse lists:any(fun(Child) -> has_unquotes(Child, QuoteLevel) end, Args);
+  has_unquotes(Name, QuoteLevel) orelse lists:any(fun(Child) -> has_unquotes(Child, QuoteLevel) end, Args);
 has_unquotes({Left, Right}, QuoteLevel) ->
   has_unquotes(Left, QuoteLevel) orelse has_unquotes(Right, QuoteLevel);
 has_unquotes(List, QuoteLevel) when is_list(List) ->