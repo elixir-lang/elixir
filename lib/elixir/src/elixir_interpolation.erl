6cde8680abbf03be4a42cb795d9878f0d35bd1a4
Daniel Kukula <904179+dkuku@users.noreply.github.com>
2026-08-18 17:36:38 +0200

    Remove intermediate list in elixir_interpolation hex_to_int (#15767)

--- lib/elixir/src/elixir_interpolation.erl
+++ lib/elixir/src/elixir_interpolation.erl
@@ -219,7 +219,7 @@ unescape_chars(<<>>, _Map, Acc) -> Acc.
 % Unescape Helpers
 
 unescape_hex(<<A, B, Rest/binary>>, Map, Acc) when ?is_hex(A), ?is_hex(B) ->
-  Bytes = list_to_integer([A, B], 16),
+  Bytes = (hex_to_int(A) bsl 4) bor hex_to_int(B),
   unescape_chars(Rest, Map, <<Acc/binary, Bytes>>);
 
 unescape_hex(<<_/binary>>, _Map, _Acc) ->
@@ -259,6 +259,10 @@ append_codepoint(Rest, Map, List, Acc, Base) ->
     error:badarg ->
       throw({error, "invalid or reserved Unicode code point \\u{" ++ List ++ "}", "\\u"})
   end.
+
+hex_to_int(H) when ?is_digit(H) -> H - $0;
+hex_to_int(H) when H >= $A, H =< $F -> H - $A + 10;
+hex_to_int(H) when H >= $a, H =< $f -> H - $a + 10.
 
 unescape_map(newline) -> true;
 unescape_map(unicode) -> true;
